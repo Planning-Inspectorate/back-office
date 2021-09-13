1d9275b1e094c859de8f3643ed174aa3152f09dd
David Shepherd <david@x-designs.co.uk>
2021-09-13 17:04:25 +0100

    feat(as-2855): move test mocks out of the src directory and add additional mocks

---
+++ packages/web-app/test/utils/mocks.js
@@ -0,0 +1,45 @@
+jest.mock('../../src/lib/logger');
+
+const logger = require('../../src/lib/logger');
+
+const mockGet = jest.fn();
+const mockPost = jest.fn();
+const mockPut = jest.fn();
+const mockPatch = jest.fn();
+const mockUse = jest.fn();
+const mockReq = {
+  log: logger,
+  params: {},
+  body: {},
+  session: {},
+};
+const mockRes = () => {
+  const res = {};
+  res.redirect = jest.fn().mockReturnValue(res);
+  res.render = jest.fn().mockReturnValue(res);
+  res.status = jest.fn().mockReturnValue(res);
+  res.send = jest.fn().mockReturnValue(res);
+  return res;
+};
+const mockNext = jest.fn();
+
+jest.doMock('express', () => ({
+  Router: () => ({
+    get: mockGet,
+    post: mockPost,
+    patch: mockPatch,
+    put: mockPut,
+    use: mockUse,
+  }),
+}));
+
+module.exports = {
+  mockGet,
+  mockPost,
+  mockPut,
+  mockPatch,
+  mockUse,
+  mockReq,
+  mockRes,
+  mockNext,
+};
