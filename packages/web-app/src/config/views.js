de532c99678998e70219bf20d5a6f9a2809180c3
swdpcomputing <swdpcomputing@outlook.com>
2021-09-29 09:24:54 +0100

    feat(web-app): create questionnaire list page

--- packages/web-app/src/config/views.js
+++ packages/web-app/src/config/views.js
@@ -1,5 +1,6 @@
 const views = {
   appealsList: 'appeals-list',
+  questionnairesList: 'questionnaires-list',
   home: '',
   reviewAppealSubmission: 'review-appeal-submission',
   validAppealDetails: 'valid-appeal-details',
--- packages/web-app/src/routes/index.js
+++ packages/web-app/src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const appealsList = require('./appeals-list');
+const questionnairesList = require('./questionnaires-list');
 const reviewAppealSubmission = require('./review-appeal-submission');
 const validAppealDetails = require('./valid-appeal-details');
 const home = require('./home');
@@ -8,6 +9,7 @@ const documentsServiceProxyRouter = require('./document-service-proxy');
 const router = express.Router();
 
 router.use('/', appealsList);
+router.use('/', questionnairesList);
 router.use('/', home);
 router.use('/', reviewAppealSubmission);
 router.use('/', validAppealDetails);
