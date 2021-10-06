420c6985e3b5a870f0ace963ed6257013b14ff58
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-10-06 19:07:33 +0100

    feat(as-3496): add initial check and confirm page

--- packages/web-app/src/routes/index.js
+++ packages/web-app/src/routes/index.js
@@ -4,6 +4,7 @@ const reviewAppealSubmission = require('./review-appeal-submission');
 const validAppealDetails = require('./valid-appeal-details');
 const invalidAppealDetails = require('./invalid-appeal-details');
 const missingOrWrongAppealDetails = require('./missing-or-wrong');
+const checkAndConfirm = require('./check-and-confirm');
 const home = require('./home');
 const documentsServiceProxy = require('./document-service-proxy');
 const appealAlreadyReviewed = require('./appeal-already-reviewed');
@@ -26,5 +27,10 @@ router.use(`/${views.invalidAppealDetails}`, handleAppealAlreadyReviewed, invali
 router.use(`/${views.missingOrWrong}`, handleAppealAlreadyReviewed, missingOrWrongAppealDetails);
 router.use(`/${views.document}`, documentsServiceProxy);
 router.use(`/${views.appealAlreadyReviewed}`, appealAlreadyReviewed);
+router.use('/', reviewAppealSubmission);
+router.use('/', validAppealDetails);
+router.use('/', invalidAppealDetails);
+router.use('/', missingOrWrongAppealDetails);
+router.use('/', checkAndConfirm);
 
 module.exports = router;
