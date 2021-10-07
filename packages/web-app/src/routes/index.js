a595f2cdb299eb5fb33b5572df529d6ebc3330cb
David Shepherd <david@x-designs.co.uk>
2021-10-07 09:49:03 +0100

    feat(as-3185): update naming of /document route vars for consistency

--- packages/web-app/src/routes/index.js
+++ packages/web-app/src/routes/index.js
@@ -5,7 +5,7 @@ const validAppealDetails = require('./valid-appeal-details');
 const invalidAppealDetails = require('./invalid-appeal-details');
 const missingOrWrongAppealDetails = require('./missing-or-wrong');
 const home = require('./home');
-const documentsServiceProxyRouter = require('./document-service-proxy');
+const documentsServiceProxy = require('./document-service-proxy');
 const appealAlreadyReviewed = require('./appeal-already-reviewed');
 const views = require('../config/views');
 const handleAppealAlreadyReviewed = require('../lib/handle-appeal-already-reviewed');
@@ -24,7 +24,7 @@ router.use(
 router.use(`/${views.validAppealDetails}`, handleAppealAlreadyReviewed, validAppealDetails);
 router.use(`/${views.invalidAppealDetails}`, handleAppealAlreadyReviewed, invalidAppealDetails);
 router.use(`/${views.missingOrWrong}`, handleAppealAlreadyReviewed, missingOrWrongAppealDetails);
-router.use('/document', documentsServiceProxyRouter);
+router.use(`/${views.document}`, documentsServiceProxy);
 router.use(`/${views.appealAlreadyReviewed}`, appealAlreadyReviewed);
 
 module.exports = router;
