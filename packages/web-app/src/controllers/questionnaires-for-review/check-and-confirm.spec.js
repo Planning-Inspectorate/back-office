1e0e1d0e2f30d8ff76f05fccb023564ef0f63107
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-10-13 15:43:31 +0100

    feat(as-3496): add extra tests + middleware + review comments

--- packages/web-app/src/controllers/questionnaires-for-review/check-and-confirm.spec.js
+++ packages/web-app/src/controllers/questionnaires-for-review/check-and-confirm.spec.js
@@ -24,12 +24,15 @@ describe('controllers/check-and-confirm', () => {
         param: {
           appealId,
         },
+        session: {
+          outcome: 'COMPLETE',
+        },
       };
       const res = {
         render: jest.fn(),
       };
 
-      await getCheckAndConfirmController.getCheckAndConfirm(req, res);
+      getCheckAndConfirmController.getCheckAndConfirm(req, res);
       expect(res.render).toBeCalledTimes(1);
       expect(res.render).toBeCalledWith(views.checkAndConfirm, {
         backLink: `/questionnaires-for-review/${appealId}`,
@@ -37,6 +40,50 @@ describe('controllers/check-and-confirm', () => {
         sections,
         pageTitle: 'Check and confirm',
         reviewOutcome: 'COMPLETE',
+      });
+    });
+
+    it('should render view with incomplete status', () => {
+      const appealId = '123';
+
+      const sections = [
+        { title: 'Review outcome', value: 'Complete' },
+        { title: 'Appeal reference', value: 'APP' },
+        { title: 'Appeal site', value: 'Site' },
+        { title: 'Local planning department', value: 'Newark and Sherwood District Council' },
+        {
+          title: 'Missing or incorrect documents',
+          value: [],
+        },
+      ];
+
+      const breadcrumbs = [
+        { title: 'Case officer dashboard' },
+        { title: 'Questionnaires for review' },
+        { title: 'Dummy Appeal Reference' },
+        { title: 'Check and confirm' },
+      ];
+
+      const req = {
+        param: {
+          appealId,
+        },
+        session: {
+          outcome: 'INCOMPLETE',
+        },
+      };
+      const res = {
+        render: jest.fn(),
+      };
+
+      getCheckAndConfirmController.getCheckAndConfirm(req, res);
+      expect(res.render).toBeCalledTimes(1);
+      expect(res.render).toBeCalledWith(views.checkAndConfirm, {
+        backLink: `/questionnaires-for-review/${appealId}`,
+        breadcrumbs,
+        sections,
+        pageTitle: 'Check and confirm',
+        reviewOutcome: 'INCOMPLETE',
       });
     });
   });
