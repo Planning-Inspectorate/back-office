4d0ac7e2e78e12929118116cdc6d52240d308b88
Karuna Kanumuri <karuna.kanumuri@foundry4.com>
2021-10-19 08:53:29 +0100

    test: add tests for Description of development
    add tests for Description of development

---
+++ e2e-tests/cypress/support/PageObjects/vo-validappeal-description-of-development-po.js
@@ -0,0 +1,7 @@
+export const descriptionOfDevelopmentPage = () => cy.visit('http://localhost:9004/valid-appeal-details');
+
+export const errorMessageDesDevelopment = () => cy.findByText('Enter a description of development');
+
+export const enterDescriptionOfDevelopmentTxt = () => cy.get('[id=valid-appeal-details]').type('This is a Test Description and the appeal is considered to be Valid');
+
+export const checkAndConfirmPageValid = () => cy.url().should('include', 'http://localhost:9004/check-and-confirm');
