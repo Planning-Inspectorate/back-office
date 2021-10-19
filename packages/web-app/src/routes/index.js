45d7ee175f18c2ead25fc196ad6d32774373d7a9
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-10-19 14:00:39 +0100

    refactor: add prefix route to questionnaires for review route

--- packages/web-app/src/routes/index.js
+++ packages/web-app/src/routes/index.js
@@ -34,7 +34,7 @@ router.use(`/${views.document}`, documentsServiceProxy);
 router.use(`/${views.appealAlreadyReviewed}`, appealAlreadyReviewed);
 router.use(`/${views.checkAndConfirm}`, checkAndConfirmDetails);
 router.use(
-  `/${views.questionnairesForReview}/${views.checkAndConfirm}`,
+  `/planning-inspectorate/appeals/${views.questionnairesForReview}/${views.checkAndConfirm}`,
   questionnaireCheckAndConfirm
 );
 
