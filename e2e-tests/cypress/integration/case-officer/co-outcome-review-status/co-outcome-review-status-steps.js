7244fa6769b0f98d4904533a3b9b6fb9ff65c53c
shilpabasu22 <shilpabasu22@gmail.com>
2021-10-20 15:45:48 +0100

    test: added feature file and step definition skeleton

---
+++ e2e-tests/cypress/integration/case-officer/co-outcome-review-status/co-outcome-review-status-steps.js
@@ -0,0 +1,32 @@
+Given('a Case Officer selects continue on the review questionnaire page',()=>{
+
+});
+
+Given('a case officer wants to amend the outcome of a review',()=>{
+
+});
+
+Given('a Case Officer is on the {string} page',(page)=>{
+
+});
+
+Given('the outcome of the review is complete',()=>{});
+When('a Case Officer selects the {string} link in the breadcrumb',(breadcrumbLink)=>{});
+
+When('the questionnaire review is {string}',(questionnaireReviewStatus)=>{
+
+});
+
+When('case officer select Go back to review questionnaire',()=>{});
+
+When('a Case Officer amends the outcome to {string}',(questionnaireReviewStatus)=>{});
+When('case officer navigates to the {string} page',(page)=>{});
+Then('the Review Questionnaire screen is displayed',()=>{});
+Then('case officer is able to view and amend the previously entered data',()=>{});
+
+Then('the {string} page is displayed showing the questionnaire as {string}',(page,questionnaireReviewStatus)=>{
+
+});
+
+Then('the {string} page will be displayed',(page)=>{});
+Then('the review outcome will be displayed as {string}',(questionnaireReviewStatus)=>{})
