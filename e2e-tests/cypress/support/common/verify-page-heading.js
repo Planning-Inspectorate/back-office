d59832bd9b16e9818a3e316538a516af305d11e5
shilpabasu22 <shilpabasu22@gmail.com>
2021-11-03 11:52:52 +0000

    test: new tests added for AS-3158 - Questionnaire already reviewed

--- e2e-tests/cypress/support/common/verify-page-heading.js
+++ e2e-tests/cypress/support/common/verify-page-heading.js
@@ -1,6 +1,6 @@
 import{pageHeading as pageHeadingObject} from "../PageObjects/common-po";
 export const verifyPageHeading = (pageHeading) => {
-    //cy.checkPageA11y();
+    cy.checkPageA11y();
     pageHeadingObject()
         .invoke( 'text' )
         .then( (text) => {
