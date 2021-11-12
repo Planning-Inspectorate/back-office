2641a2a066b4d271070f984e26c1a77da84d757c
Demzy <79277486+demzzydev@users.noreply.github.com>
2021-11-12 12:23:24 +0000

    refactor(as-3690): removed unneeded functions

--- packages/api/src/lib/db-wrapper.js
+++ packages/api/src/lib/db-wrapper.js
@@ -1,12 +1,6 @@
 const Sequelize = require('sequelize');
-const { QueryTypes } = require('sequelize');
-const { v4: uuid4 } = require('uuid');
 const ApiError = require('./api-error');
 const config = require('../../database/config/config');
-const DbError = require('./db-error');
-
-const db = require('../models');
-const { mapQuestionnaireOutcome } = require('./mapper');
 
 const createRecord = (procedure, data) => {
   try {
@@ -42,64 +36,7 @@ const findOneAppeal = async (appealId) => {
   }
 };
 
-const findQuestionnaireOutcome = async (appealId) => {
-  try {
-    const query = `SELECT AppealID, EventDateTime, LookUpQuestionnaireOutcome.Outcome
-    FROM backofficedev.dbo.HASAppeal
-    LEFT JOIN backofficedev.dbo.LookUpQuestionnaireOutcome
-    ON HASAppeal.LPAQuestionnaireReviewOutcomeID = LookUpQuestionnaireOutcome.ID
-    WHERE AppealID = ?
-    ORDER BY EventDateTime DESC`;
-
-    const appeals = await db.sequelize.query(query, {
-      type: QueryTypes.SELECT,
-      replacements: [appealId],
-      plain: false,
-    });
-
-    const [appeal] = appeals;
-
-    return {
-      id: appeal?.AppealID,
-      outcome: appeal?.Outcome,
-      datetime: appeal?.EventDateTime,
-    };
-  } catch (err) {
-    throw new DbError('Failed to find a specific questionnaire outcome');
-  }
-};
-
-const setQuestionnaireOutcome = async (appealId, outcome) => {
-  const query = `SELECT *
-    FROM backofficedev.dbo.HASAppeal
-    WHERE AppealID = ?
-    ORDER BY EventDateTime DESC`;
-
-  const appeals = await db.sequelize.query(query, {
-    type: QueryTypes.SELECT,
-    replacements: [appealId],
-    plain: false,
-  });
-
-  const [appeal] = appeals;
-
-  const insertQuery = `INSERT INTO backofficedev.dbo.HASAppeal (ID, AppealID, MinisterialTargetDate) 
-  VALUES ($ID, $AppealID, $MinisterialTargetDate)`;
-
-  const [_, metadata] = await db.sequelize.query(insertQuery, {
-    type: QueryTypes.INSERT,
-    bind: {
-      ...appeal,
-      ID: uuid4().toString(),
-      LPAQuestionnaireReviewOutcomeID: mapQuestionnaireOutcome(outcome),
-      MinisterialTargetDate: new Date(),
-    },
-  });
-
-  return metadata;
-};
-
-const findAllQuestionnaires = async () => {
+const findAllQuestionnaires = async (db) => {
   try {
     const query = 'SELECT * FROM QuestionnaireData';
     const result = await db.query(query);
@@ -128,6 +65,4 @@ module.exports = {
   findOneAppeal,
   findAllQuestionnaires,
   findOneQuestionnaire,
-  findQuestionnaireOutcome,
-  setQuestionnaireOutcome,
 };
