e68dd848745840916910fb39e25bfb611a81e767
Ray Mitchell <ray@Rays-Air-2.broadband>
2021-11-22 10:40:29 +0000

    feat(as-3680): amend search appeal issue

--- packages/api/src/controllers/appeal-search.js
+++ packages/api/src/controllers/appeal-search.js
@@ -7,7 +7,6 @@ const appealSearch = async (req, res) => {
   try {
     const { searchString } = req.params;
 
-    console.log(searchString);
     if (!searchString) {
       throw new ApiError('No search string given');
     }
@@ -15,8 +14,8 @@ const appealSearch = async (req, res) => {
     const appeal = await searchAppeal(searchString);
     res.status(200).send(appeal);
   } catch (err) {
-    logger.error({ err }, 'Failed to get appeal');
-    res.status(500).send(`Failed to get appeal - ${err.message}`);
+    logger.error({ err }, 'Failed to get appeals');
+    res.status(500).send(`Failed to get appeals - ${err.message}`);
   }
 };
 
