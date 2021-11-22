f7292bfaa604926fbc0a45c86d97d984d8b89a28
Ray Mitchell <ray@Rays-Air-2.broadband>
2021-11-22 15:08:33 +0000

    fix(as-3680): jest tests api

---
+++ packages/api/src/controllers/appeal-search.spec.js
@@ -0,0 +1,105 @@
+const { mockReq, mockRes } = require('../../test/utils/mocks');
+const mockDbRecord = require('../../test/data/has-appeal-submission-db-record');
+const mockDocumentsMetadata = require('../../test/data/documents-metadata');
+
+jest.mock('../lib/db-wrapper', () => ({
+  findOneAppeal: jest
+    .fn()
+    .mockImplementationOnce(() => mockDbRecord)
+    .mockImplementationOnce(() => {
+      throw new Error('Internal Server Error');
+    }),
+  findAllAppeals: jest
+    .fn()
+    .mockImplementationOnce(() => [mockDbRecord])
+    .mockImplementationOnce(() => {
+      throw new Error('Internal Server Error');
+    }),
+  createRecord: jest
+    .fn()
+    .mockImplementationOnce(() => mockDbRecord)
+    .mockImplementationOnce(() => {
+      throw new Error('Internal Server Error');
+    }),
+  createHasAppealRecord: jest
+    .fn()
+    .mockImplementationOnce(() => mockDbRecord)
+    .mockImplementationOnce(() => {
+      throw new Error('Internal Server Error');
+    }),
+}));
+
+jest.mock('../lib/documents-api-wrapper', () => ({
+  getDocumentsMetadata: jest.fn().mockReturnValue(mockDocumentsMetadata),
+}));
+
+const { getOneAppeal, getAllAppeals, postAppeal } = require('./appeal');
+
+describe('controllers/appeal', () => {
+  let req;
+  let res;
+
+  beforeEach(() => {
+    req = mockReq();
+    res = mockRes();
+  });
+
+  describe('getAllAppeals', () => {
+    it('should return the correct response when data can be fetched', async () => {
+      await getAllAppeals(req, res);
+
+      expect(res.status).toBeCalledWith(200);
+      expect(res.send).toBeCalledWith([mockDbRecord]);
+    });
+
+    it('should return the correct response when an error occurs', async () => {
+      await getAllAppeals(req, res);
+
+      expect(res.status).toBeCalledWith(500);
+      expect(res.send).toBeCalledWith('Failed to get appeals');
+    });
+  });
+
+  describe('getOneAppeal', () => {
+    it('should return the correct response when given an appeal id and data can be fetched', async () => {
+      req.params.appealId = 'da8f8051-bc7f-403c-8431-e9788563c07b';
+
+      await getOneAppeal(req, res);
+
+      expect(res.status).toBeCalledWith(200);
+      expect(res.send).toBeCalledWith(mockDbRecord);
+    });
+
+    it('should return the correct response when not given an appeal id', async () => {
+      await getOneAppeal(req, res);
+
+      expect(res.status).toBeCalledWith(500);
+      expect(res.send).toBeCalledWith('Failed to get appeal - No AppealId given');
+    });
+
+    it('should return the correct response when an error occurs', async () => {
+      req.params.appealId = 'da8f8051-bc7f-403c-8431-e9788563c07b';
+
+      await getOneAppeal(req, res);
+
+      expect(res.status).toBeCalledWith(500);
+      expect(res.send).toBeCalledWith('Failed to get appeal - Internal Server Error');
+    });
+  });
+
+  describe('postAppeal', () => {
+    it('should return the correct response when data can be inserted', async () => {
+      await postAppeal(req, res);
+
+      expect(res.status).toBeCalledWith(200);
+      expect(res.send).toBeCalledWith(mockDbRecord);
+    });
+
+    it('should return the correct response when an error occurs', async () => {
+      await postAppeal(req, res);
+
+      expect(res.status).toBeCalledWith(500);
+      expect(res.send).toBeCalledWith('Failed to insert appeal');
+    });
+  });
+});
