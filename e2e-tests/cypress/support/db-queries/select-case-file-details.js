b8a171012b1b39ebe2b873b3c31a6128f0abfe99
karunak21 <72738391+karunak21@users.noreply.github.com>
2021-12-02 17:44:25 +0000

    test: create and update existing tests (#120)
    update existing tests

--- e2e-tests/cypress/support/db-queries/select-case-file-details.js
+++ e2e-tests/cypress/support/db-queries/select-case-file-details.js
@@ -1,5 +1,5 @@
-export const selectCaseFileDetailsFromDb = ()=>{
-    cy.sqlServer("select * from dbo.HASAppealSubmission where appealid = 'bf213cc7-f5f1-4f25-9d1a-d442ea11e485'").then( (res)=>{
-        cy.wrap(res).as('caseFileReferenceCST');
+export const selectCaseFileDetailsFromDb = (appealID) => {
+    cy.sqlServer("select * from dbo.HASAppealSubmission where appealid = appealID").then( (res)=>{
+        cy.wrap(res[0]).as('caseFileReferenceCST');
     });
 }
