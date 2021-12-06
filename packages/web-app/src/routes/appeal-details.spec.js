7c39e7ecc61f22337198b398f04e919fa1e4dc9e
Birol <softnutuk@gmail.com>
2021-12-06 08:41:31 +0000

    feat(update site address): page implementation

--- packages/web-app/src/routes/appeal-details.spec.js
+++ packages/web-app/src/routes/appeal-details.spec.js
@@ -4,20 +4,22 @@ const getCaseData = require('../lib/get-case-data');
 const appellantRouter = require('./appeal-details/appellant');
 const caseOfficerRouter = require('./appeal-details/case-officer');
 const applicationDecisionDateRouter = require('./appeal-details/application-decision-date');
+const siteAddressRouter = require('./appeal-details/site-address');
 
 describe('routes/appeal-details', () => {
   it('should define the correct route', () => {
     // eslint-disable-next-line global-require
     require('./appeal-details');
 
-    expect(mockUse).toBeCalledTimes(4);
+    expect(mockUse).toBeCalledTimes(5);
+    expect(mockUse).toBeCalledWith('/:appealId', getCaseData, getAppealDetails);
+    expect(mockUse).toBeCalledWith('/:appealId/appellant', getCaseData, appellantRouter);
     expect(mockUse).toBeCalledWith(
       '/:appealId/application-decision-date',
       getCaseData,
       applicationDecisionDateRouter
     );
-    expect(mockUse).toBeCalledWith('/:appealId/appellant', getCaseData, appellantRouter);
+    expect(mockUse).toBeCalledWith('/:appealId/site-address', getCaseData, siteAddressRouter);
     expect(mockUse).toBeCalledWith('/:appealId/case-officer', getCaseData, caseOfficerRouter);
-    expect(mockUse).toBeCalledWith('/:appealId', getCaseData, getAppealDetails);
   });
 });
