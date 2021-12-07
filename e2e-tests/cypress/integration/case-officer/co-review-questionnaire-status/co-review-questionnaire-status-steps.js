c8d48e41fe444d2ba296089b0d1c4b32ccbc4a01
shilpabasu22 <shilpabasu22@gmail.com>
2021-12-07 19:43:19 +0000

    Fix/as 4000 data not removed (#134)
    * fix(as-4030): fix the issue - Remove redundant data
    
    * feat(as-4000): re-adding overwritten cypress tests
    
    * test: updated tests and removed console log
    
    Co-authored-by: nssdesikan <nssdesikan@rediffmail.com>

--- e2e-tests/cypress/integration/case-officer/co-review-questionnaire-status/co-review-questionnaire-status-steps.js
+++ e2e-tests/cypress/integration/case-officer/co-review-questionnaire-status/co-review-questionnaire-status-steps.js
@@ -1,12 +1,16 @@
 import {goToCaseOfficerPage} from "../../../support/case-officer/go-to-page";
 import {Given, Then } from "cypress-cucumber-preprocessor/steps";
 import {
+    getAppealReceivedDate,
+    getAppealSiteAddress,
     getAwaitingStatus,
     getOverdueStatus,
     getReceivedStatus
 } from "../../../support/PageObjects/co-questionnaire-list-po";
 import {selectCaseReferenceFromDb} from "../../../support/db-queries/select-case-reference-from-db";
 import {getAppealsLink} from "../../../support/PageObjects/co-review-questionnaire-po";
+import {dateFormat} from "../../../support/common/date-format";
+import {getAppealSite} from "../../../support/PageObjects/co-check-and-confirm-po";
 
 Given('the Case Officer is on the Questionnaires for review page',()=>{
 goToCaseOfficerPage();
@@ -15,6 +19,10 @@ goToCaseOfficerPage();
 Then('the page will show the questionnaires with the status {string}',(status)=>{
     selectCaseReferenceFromDb(status);
     cy.get('@caseReference').then((caseReference)=>{
-        getAppealsLink(caseReference).click();
+        let appealSiteAddress = caseReference[25]+", "+caseReference[26]+", "+caseReference[27]+", "+caseReference[28]+", "+caseReference[29];
+        let questionnaireReceivedDate = dateFormat(caseReference[2],'DD MMMM YYYY');
+        getAppealSiteAddress(appealSiteAddress).should('exist');
+        getAppealReceivedDate(questionnaireReceivedDate).should('exist');
+        getAppealsLink(caseReference[23]).click();
     });
 });
