abc068b537e619baabfbf021a5a75fcdacbc5cd5
shilpabasu22 <shilpabasu22@gmail.com>
2021-12-07 16:01:18 +0000

    test: added db query

---
+++ e2e-tests/cypress/support/db-queries/select-lpa-questionnaire-outcome-for-case-officer-from-db.js
@@ -0,0 +1,5 @@
+export const selectLpaQuestionnaireOutcomeForCaseOfficerFromDb =(appealId) =>{
+    cy.sqlServer(`select LPAQuestionnaireReviewOutcomeID from [HASAppeal] where AppealID='{appealId}' and LatestEvent=1'`).then((res)=>{
+        cy.wrap(res).as('lpaQuestionnaireOutcome');
+    });
+}
