4a4ae6dc90f4a48e9825c5e8ed413962bcf91406
shilpabasu22 <shilpabasu22@gmail.com>
2021-12-09 09:28:32 +0000

    test: added tests for db review

--- e2e-tests/cypress/support/case-officer/review-section-missing-information.js
+++ e2e-tests/cypress/support/case-officer/review-section-missing-information.js
@@ -50,43 +50,55 @@ export const reviewSectionMissingInformation = (missing_info, documentSection) =
 
         }
     }else {
-        if (documentSection === 'Plans used to reach decision')
+        if (documentSection === 'Plans used to reach decision') {
             getPlansUsedToReachDecisionText()
                 .should('be.visible')
                 .type(missing_info);
-        else if (documentSection === 'Statutory development plan policies')
+            getPlansUsedToReachDecisionText().should('have.length.at.most',1000);
+        }
+        else if (documentSection === 'Statutory development plan policies'){
             getStatutoryDevelopmentPlanText()
                 .should('be.visible')
                 .type(missing_info);
-        else if (documentSection === 'Other relevant policies')
+            getStatutoryDevelopmentPlanText().should('have.length.at.most',1000);
+        }
+        else if (documentSection === 'Other relevant policies') {
             getOtherRelevantPoliciesText()
                 .should('be.visible')
                 .type(missing_info);
-        else if (documentSection === 'Supplementary planning documents')
+            getOtherRelevantPoliciesText().should('have.length.at.most',1000);
+        }
+        else if (documentSection === 'Supplementary planning documents'){
             getSupplementaryPlanningText()
                 .should('be.visible')
                 .type(missing_info);
-        else if (documentSection === 'Conservation area map and guidance')
+            getSupplementaryPlanningText().should('have.length.at.most',1000);
+        }
+        else if (documentSection === 'Conservation area map and guidance'){
             getConservationAreaMapText()
                 .should('be.visible')
                 .type(missing_info);
+            getConservationAreaMapText().should('have.length.at.most',1000);
+        }
         else if (documentSection === 'Listing description') if(cy.findByText('Would the development affect the setting of a listed building?').siblings('ul').contains('No')) {
         }else{
             getListingDescriptionText()
                 .should('be.visible')
                 .type(missing_info);
+            getListingDescriptionText().should('have.length.at.most',1000);
         }
         else if (documentSection === 'Application notification') {
                getApplicationNotificationListOfAddressesCheckbox().click();
                 getApplicationNotificationCopyOfLetterCheckbox().click();
-        } else if (documentSection === 'Representations')
+        } else if (documentSection === 'Representations'){
             getRepresentationsText()
                 .should('be.visible')
                 .type(missing_info);
+            getRepresentationsText().should('have.length.at.most',1000);
+        }
         else if (documentSection === 'Appeal notification') {
                 getAppealNotificationListOfAddressesCheckbox().click();
                 getAppealNotificationCopyOfLetterCheckbox().click();
-
         }
     }
 }
