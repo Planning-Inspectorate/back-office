058513882b9d700a4d451d739ad4c859b5cd818d
radum <radu.micu@gmail.com>
2022-03-01 16:48:46 +0000

    Use the config module to load env vars in server

--- apps/web/src/server/server.js
+++ apps/web/src/server/server.js
@@ -4,10 +4,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const https = require('node:https');
 const kleur = require('kleur');
-const { loadEnvironment } = require('./lib/load-environment');
-
-// Load .env files based on the rules defined in the docs
-loadEnvironment(process.env.NODE_ENV);
+const { config } = require('./config/config');
 
 // Express app
 const { app } = require('./app/express');
@@ -19,18 +16,18 @@ const { app } = require('./app/express');
 app.set('trust proxy', true);
 
 // Express http/s ports
-app.set('http-port', process.env.HTTP_PORT || 8080);
-app.set('https-port', process.env.HTTPS_PORT || 8443);
+app.set('http-port', config.HTTP_PORT || 8080);
+app.set('https-port', config.HTTPS_PORT || 8443);
 
 const listener = app.listen(app.get('http-port'), () => {
 	// eslint-disable-next-line no-console
 	console.log('%s Server is running at http://localhost:%d in %s mode', kleur.green('✓'), listener.address().port, app.get('env'));
 });
 
-if (process.env.HTTPS_ENABLED === 'true') {
+if (config.HTTPS_ENABLED === 'true') {
 	https.createServer({
-		cert: fs.readFileSync(path.resolve(__dirname, process.env.SSL_CERT_FILE)),
-		key: fs.readFileSync(path.resolve(__dirname, process.env.SSL_KEY_FILE))
+		cert: fs.readFileSync(path.resolve(__dirname, config.SSL_CERT_FILE)),
+		key: fs.readFileSync(path.resolve(__dirname, config.SSL_KEY_FILE))
 	}, app).listen(app.get('https-port'), () => {
 		// eslint-disable-next-line no-console
 		console.log('%s Server is running at https://localhost:%d in %s mode', kleur.green('✓'), app.get('https-port'), app.get('env'));
