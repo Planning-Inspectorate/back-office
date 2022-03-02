d149688be3b799213f23aeaf8ee79b82e8ae2629
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-03-02 17:53:02 +0000

    BOCM-5 adds compression, logging(morgan), and security(helmet) middleware

--- apps/api/src/server/app.js
+++ apps/api/src/server/app.js
@@ -2,8 +2,15 @@
 
 const express = require("express");
 const homeRoutes = require("./app/home/home.routes");
+const compression = require("compression");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
+
+app.use(compression());
+app.use(morgan('combined'))
+app.use(helmet());
 
 app.use('/', homeRoutes);
 
