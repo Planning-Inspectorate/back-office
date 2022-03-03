5429d8fe837658eed01c90e2a8374b7182deff52
radum <radu.micu@gmail.com>
2022-03-03 13:03:30 +0000

    BOCM-5 - Update linting config

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -3,10 +3,8 @@
 const merge = require('lodash/merge');
 const eslintConfig = require('eslint-config');
 const eslintConfigNode = require('eslint-config/node');
+const eslintConfigJest = require('eslint-config/jest');
 
-module.exports = merge(eslintConfig, eslintConfigNode, {
-	root: true,
-	env: {
-		jest: true
-	}
+module.exports = merge(eslintConfig, eslintConfigNode, eslintConfigJest, {
+	root: true
 });
