e2d644caf4e11f69bba733067ecd7135a7745a74
radum <radu.micu@gmail.com>
2022-03-03 16:44:53 +0000

    Update turbo config

--- apps/web/src/client/app.js
+++ apps/web/src/client/app.js
@@ -2,16 +2,3 @@
 //* This will be updated based ona router -> loader flow.
 
 import './pages/default';
-
-const x = 1;
-
-console.log(x);
-
-const y = (args) => {
-	return {
-		...args,
-		b: 2
-	}
-}
-
-y();
