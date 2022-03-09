b88c4a8776a4b5f91d171e38f1eddd6ad8d5cae1
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-03-09 12:58:06 +0000

    BOCM-31 basic /api-docs page loading

--- apps/api/src/server/app.js
+++ apps/api/src/server/app.js
@@ -7,11 +7,41 @@ import { validationRoutes } from './app/validation/validation.routes.js';
 
 const app = express();
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+const swaggerOptions = {
+	definition: {
+		swagger: "2.0",
+		info: { 
+			title: "PINS Back-office project ",
+			description: "PINS Back-office project API Information",
+    		version: "2.0"
+			},
+		servers: ["http://localhost:3000/"],
+		paths: {
+			"/validation": {
+				get: {
+					summary: "List of appeals to be validated",
+					parameters: {
+						
+					}
+				}
+
+		}
+	},
+		},
+	//apis: [".routes/*.js"]
+	apis: ["app.js"]
+}
+const specs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
 app.use(compression());
 app.use(morgan('combined'));
 app.use(helmet());
 
 app.use('/', homeRoutes);
+
 app.use('/validation', validationRoutes);
 
 export {
