8b2fa25f9ded50f3842e56f06525adfd5f3f0efc
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-03-10 10:39:48 +0000

    BOCM-39 corrections on validate-appeal.test

--- apps/api/src/server/app/state_machine/validate-appeal.test.js
+++ apps/api/src/server/app/state_machine/validate-appeal.test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import machine from './household-appeal.machine.js';
 
-test("should have 'submitted' as initial state", t => {
+test('should have \'submitted\' as initial state', (t) => {
 	const initial_state = machine.initialState;
 	t.is(initial_state.value, 'submitted');
 });
@@ -13,10 +13,10 @@ function applyAction(t, initial_state, action, expected_state, has_changed) {
 }
 
 applyAction.title = (providedTitle = '', initial_state, action, expected_state, has_changed) =>
-	`${providedTitle}: from state [${initial_state}] action [${action}] produces state 
-	[${expected_state}] ${has_changed ? '' : ' without'} having transitioned`
+	`${providedTitle}: from state [${initial_state}] action [${action}] produces state
+	[${expected_state}] ${has_changed ? '' : ' without'} having transitioned`;
 
-for (const param of [
+for (const parameter of [
 	['submitted', 'INVALID', 'invalid', true],
 	['submitted', 'VALID', 'with_case_officer', true],
 	['submitted', 'INFO_MISSING', 'awaiting_validation_info', true],
@@ -30,5 +30,5 @@ for (const param of [
 	['with_case_officer', 'INFO_MISSING', 'with_case_officer', false],
 	['with_case_officer', 'VALID', 'with_case_officer', false],
 ]) {
-	test(applyAction, ...param)
+	test(applyAction, ...parameter);
 }
