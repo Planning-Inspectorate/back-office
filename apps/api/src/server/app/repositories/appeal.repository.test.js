9ac6384c2b2a594b6ea2dce021d4f486b6c235ae
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-03-10 17:56:29 +0000

    [BOCM-38] adds appeals repository with tests

---
+++ apps/api/src/server/app/repositories/appeal.repository.test.js
@@ -0,0 +1,18 @@
+import test from 'ava';
+import Appeal from '../../../db/models/appeal.js';
+import appealRepository from './appeal.repository.js';
+
+test('get all appeals', async (t) => {
+    const appeals = await appealRepository.getAll();
+    t.is(appeals.length, 0);
+})
+
+test('gets appeal after database seeded', async(t) => {
+    await Appeal.create({reference: "ABC"});
+    const appeals = await appealRepository.getAll();
+    t.is(appeals.length, 1);
+})
+
+test.beforeEach('cleanup', t => {
+    Appeal.truncate();
+})
