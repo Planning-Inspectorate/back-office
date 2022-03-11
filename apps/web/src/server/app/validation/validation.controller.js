59b54b9c9d0062e3064757c1d5d9e305c173ca94
hamidhazrati <hamid.hazrati@hmcts.net>
2022-03-11 16:17:36 +0000

    Bocm-12 PR tidy

--- apps/web/src/server/app/validation/validation.controller.js
+++ apps/web/src/server/app/validation/validation.controller.js
@@ -1,20 +1,6 @@
 import { findAllNewAppeals } from './validation.service.js';
 
 /**
- * Given an item object transform it into - govuk/components/table/macro.njk" import govukTable format
- *
- * @param {*} item to do
- * @returns {any} to do
- */
-function makeGovTableRow(item){
-	return [
-		{ html: '<a href="#">'  + item.AppealReference + '</a>' },
-		{ text: item.Received },
-		{ text: item.AppealSite }
-	];
-}
-
-/**
  * Main Route Entrey Point
  *
  * @param {*} request to do
@@ -40,22 +26,6 @@ async function getValidationDashboard(request, response) {
 			newAppeals.push(row);
 		}
 	} );
-
-	const appealsList = {
-		incompleteAppeals: incompleteAppeals,
-		newAppeals: newAppeals
-	};
-
-	const incompleteAppeals = [];
-	const newAppeals = [];
-
-	for (const item in appealsListData ) {
-		if ( appealsListData[item].AppealStatus === 'incomplete'){
-			incompleteAppeals.push(makeGovTableRow(appealsListData[item]));
-		} else if ( appealsListData[item].AppealStatus === 'new'){
-			newAppeals.push(makeGovTableRow(appealsListData[item]));
-		}
-	}
 
 	const appealsList = {
 		incompleteAppeals: incompleteAppeals,
