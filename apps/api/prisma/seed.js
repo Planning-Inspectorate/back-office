0bc8080ecae24e6cd6e04ae2b4a906479cf7db61
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-03-15 12:17:26 +0000

    BOCM-53 - formatted

--- apps/api/prisma/seed.js
+++ apps/api/prisma/seed.js
@@ -1,38 +1,38 @@
 import Prisma from '@prisma/client';
+
 const { PrismaClient } = Prisma;
 
 const prisma = new PrismaClient();
 
 const appealsData = [
-    {
-        reference: 'SOME REFERENCE',
-        address: {
-            create:
-            {
-                addressLine1: 'first line',
-                city: 'city',
-                postcode: 'A11 0ZZ'
-            },
-        },
-    }
-]
+	{
+		reference: 'SOME REFERENCE',
+		address: {
+			create: {
+				addressLine1: 'first line',
+				city: 'city',
+				postcode: 'A11 0ZZ'
+			}
+		}
+	}
+];
 
 async function main() {
-    console.log(`Start seeding ...`)
-    for (const a of appealsData) {
-        const appeal = await prisma.appeal.create({
-            data: a
-        })
-        console.log(`Created appeal with id: ${appeal.id}`)
-    }
-    console.log(`Seeding finished.`)
+	console.log(`Start seeding ...`);
+	for (const a of appealsData) {
+		const appeal = await prisma.appeal.create({
+			data: a
+		});
+		console.log(`Created appeal with id: ${appeal.id}`);
+	}
+	console.log(`Seeding finished.`);
 }
 
 main()
-    .catch((e) => {
-        console.error(e)
-        process.exit(1)
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
