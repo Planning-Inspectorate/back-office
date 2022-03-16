57fd5ad99b7c22a1bcd3925c033a43e8b353bfb8
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-03-16 11:39:56 +0000

    BOCM-80 moves updateValidation controller tests to own file

--- apps/api/src/server/app/validation/validation.test.js
+++ apps/api/src/server/app/validation/validation.test.js
@@ -4,23 +4,6 @@ import { app } from '../../app.js';
 
 const request = supertest(app);
 
-test('should modify the selected field of the appeal', async (t) => {
-	const resp = await request.patch('/validation/' + 1)
-		.send({
-			AppellantName: 'Leah Thornton'
-		});
-	t.is(resp.status, 200);
-	// once we have a database, we will check if the register with id I equals to the new value
-});
-
-test('should send error when the sent ApellantName is a number', async (t) => {
-	const resp = await request.patch('/validation/' + 1)
-		.send({
-			AppellantName: '123456789'
-		});
-	t.is(resp.status, 400);
-	// once we have a database, we will check if the register with id I equals to the new value
-});
 
 test('should submit validation decision', async (t) => {
 	const resp = await request.post('/validation/' + 1)
