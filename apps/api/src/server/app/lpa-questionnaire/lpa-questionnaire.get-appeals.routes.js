bec1684ad7f699a5ef88e3fe994f50a085a46acd
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-03-21 16:20:04 +0000

    BOCM-63 adding text for LPA questionnaire complete

--- apps/api/src/server/app/lpa-questionnaire/lpa-questionnaire.get-appeals.routes.js
+++ apps/api/src/server/app/lpa-questionnaire/lpa-questionnaire.get-appeals.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getAppeals, getAppealsDetail } from './lpa-questionnaire.get-appeals.controller.js';
+import asyncHandler from '../middleware/async-handler.js';
+import { getAppeals, getAppealsDetail, confirmingLPAQuestionnaire } from './lpa-questionnaire.get-appeals.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAppeals);
 router.get('/id:', getAppealsDetail);
+router.post('/id:/confirm', asyncHandler(confirmingLPAQuestionnaire));
 
 
 export {
