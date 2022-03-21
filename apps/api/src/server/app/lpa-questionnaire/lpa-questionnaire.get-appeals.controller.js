8ee419910dbf09e185bc957365127a52bf9af21c
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-03-21 09:55:28 +0000

    BOCM-60 changed to array of appeals, plus correct date format in appeals

--- apps/api/src/server/app/lpa-questionnaire/lpa-questionnaire.get-appeals.controller.js
+++ apps/api/src/server/app/lpa-questionnaire/lpa-questionnaire.get-appeals.controller.js
@@ -1,9 +1,17 @@
-const appealWithQuestionnnaire = { AppealId : 1,
+const appealWithQuestionnnaire = [{
+	AppealId : 1,
 	AppealReference: 'APP/Q9999/D/21/1345264',
-	QuestionnaireDueDate:'01-Jun-2022',
+	QuestionnaireDueDate:'01 Jun 2022',
 	AppealSite:'96 The Avenue, Maidstone, Kent, MD21 5XY',
 	QuestionnaireStatus: 'received'
-};
+},
+{
+	AppealId : 2,
+	AppealReference: 'APP/Q9999/D/21/5463281',
+	QuestionnaireDueDate: ' 05 Jun 2022',
+	AppealSite:'55 Butcher Street, Thurnscoe, S63 0RB' ,
+	QuestionnaireStatus: 'incomplete'
+}];
 
 const getAppeals = function (request, response) {
 	response.send(appealWithQuestionnnaire);
--- apps/api/src/server/app/lpa-questionnaire/lpa-questionnaire.get-appeals.test.js
+++ apps/api/src/server/app/lpa-questionnaire/lpa-questionnaire.get-appeals.test.js
@@ -5,12 +5,21 @@ const request = supertest(app);
 
 test('gets the appeals information with received questionnaires', async (t) => {
 	const resp = await request.get('/appeals');
-	const appealExample = { AppealId : 1,
+	const appealExample = [{
+		AppealId : 1,
 		AppealReference: 'APP/Q9999/D/21/1345264',
-		QuestionnaireDueDate:'01-Jun-2022',
+		QuestionnaireDueDate:'01 Jun 2022',
 		AppealSite:'96 The Avenue, Maidstone, Kent, MD21 5XY',
 		QuestionnaireStatus: 'received'
-	};
+	},
+	{
+		AppealId : 2,
+		AppealReference: 'APP/Q9999/D/21/5463281',
+		QuestionnaireDueDate: ' 05 Jun 2022',
+		AppealSite:'55 Butcher Street, Thurnscoe, S63 0RB' ,
+		QuestionnaireStatus: 'incomplete'
+	}];
+
 	t.is(resp.status, 200);
 	t.deepEqual(resp.body, appealExample);
 });
