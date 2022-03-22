b3499485d762212c469029eb838ecf7865a0a255
localadmin <localadmin@DLT-JHILL2.local.amazerealise.com>
2022-03-22 17:55:30 +0000

    BOCM-32: PR amends - fixed issue where missing or wrong docs reasons rendering was broken on check and confirm page if only one reason selected

--- apps/web/src/server/lib/helpers.js
+++ apps/web/src/server/lib/helpers.js
@@ -24,11 +24,33 @@ export const checkboxDataToCheckValuesObject = (checkboxData) => {
 	}
 };
 
-export const makeValidator_StringMatchesOrArrayContainsMatch = (valueToMatch) => (value) => {
+/**
+ * Factory which creates a function to be passed to an express-validator custom validator
+ * The factory function takes a string to match against
+ * The returned validator function takes either a string or an array of strings as input
+ * If value is an array, it returns true if the array contains the string to match against
+ * Else if value is a string, it returns true if it matches the string to match against
+ * Else it returns false
+ * @param {string} stringToMatch - the value to match against
+ * @returns {bool} - true if the string matches or if the array contains a match, else false
+ */
+export const makeValidator_StringMatchesOrArrayContainsMatch = (stringToMatch) => (value) => {
 	if (Array.isArray(value)) {
-		return value.includes(valueToMatch);
+		return value.includes(stringToMatch);
 	} else if (typeof value === 'string') {
-		return value === valueToMatch;
+		return value === stringToMatch;
 	}
 	return false;
+};
+
+/**
+ * Returns the supplied value wrapped in an array, if it is a string. Otherwise returns the value unmodified.
+ * Used to ensure checkbox values are always an array of ID strings, even when only one checkbox is checked,
+ * in which case the raw value would just be a string.
+ * @param {array<string> | string | any} value - the value to normalize
+ * @returns {array<string> | any} - an array containing the string, or the unmodified value if it is not a string
+ */
+export const arrayifyIfString = (value) => {
+	if (typeof value === 'string') return [value];
+	return value;
 };
