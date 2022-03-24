3fc8aff99621dd01bcd5e95cb5694a4f76acecf9
Mayya Bondarevskaya <89029990+mayya-bond-kc@users.noreply.github.com>
2022-03-24 10:15:17 +0000

    BOCM-193 - adds more new appeal cases in new and missing details state (#191)

--- apps/api/prisma/seed.js
+++ apps/api/prisma/seed.js
@@ -30,6 +30,87 @@ const appealsData = [
 		}
 	},
 	{
+		reference: 'APP/Q9999/D/21/5463281',
+		appellantName: 'Haley Eland',
+		localPlanningDepartment: 'Barnsley Metropolitan Borough Council',
+		planningApplicationReference: '32569/APP/2021/2102',
+		address: {
+			create: {
+				addressLine1: '55 Butcher Street',
+				city: 'Thurnscoe',
+				postcode: 'S63 0RB'
+			}
+		}
+	},
+	{
+		reference: 'APP/Q9999/D/21/1203521',
+		appellantName: 'Roger Simmons',
+		localPlanningDepartment: 'Worthing Borough Council',
+		planningApplicationReference: '25468/APP/2021/1185',
+		address: {
+			create: {
+				addressLine1: '8 The Chase',
+				city: 'Findon',
+				postcode: 'BN14 0TT'
+			}
+		}
+	},
+	{
+		reference: 'APP/Q9999/D/21/1154923',
+		appellantName: 'Sophie Skinner',
+		localPlanningDepartment: 'Dorset Council',
+		planningApplicationReference: '19457/APP/2021/5421',
+		address: {
+			create: {
+				addressLine1: '96 The Avenue',
+				addressLine2: 'Maidstone',
+				city: 'Kent',
+				postcode: 'MD21 5XY'
+			}
+		}
+	},
+	{
+		reference: 'APP/Q9999/D/21/1087562',
+		appellantName: 'Ryan Marshall',
+		localPlanningDepartment: 'Basingstoke and Deane Borough Council',
+		planningApplicationReference: '10016/APP/2021/960',
+		address: {
+			create: {
+				addressLine1: '44 Rivervale',
+				city: 'Bridport',
+				postcode: 'DT6 5RN'
+			}
+		}
+	},
+	{
+		reference: 'APP/Q9999/D/21/1365524',
+		appellantName: 'Fiona Burgess',
+		localPlanningDepartment: 'Wiltshire Council',
+		planningApplicationReference: '9423/APP/2021/1223',
+		address: {
+			create: {
+				addressLine1: '92 Huntsmoor Road',
+				city: 'Tadley',
+				postcode: 'RG26 4BX'
+			}
+		}
+	},
+	{
+		reference: 'APP/Q9999/D/21/1224115',
+		appellantName: 'Kevin Fowler',
+		localPlanningDepartment: 'Waveney District Council',
+		planningApplicationReference: '18543/APP/2021/6627',
+		status: 'awaiting_validation_info',
+		address: {
+			create: {
+				addressLine1: '1 Grove Cottage',
+				addressLine2: 'Shotesham Road',
+				city: 'Woodton',
+				postcode: 'NR35 2ND'
+			}
+		}
+	},
+	{
 		reference: 'APP/Q9999/D/21/1345264',
 		appellantName: 'Lee Thornton',
 		localPlanningDepartment: 'Maidstone Borough Council',
