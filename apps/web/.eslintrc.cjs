0237339fd7a77eca05f309e559784a04f4362c5c
localadmin <localadmin@DLT-JHILL2.local.amazerealise.com>
2022-03-24 10:58:07 +0000

    updated eslintrc to avoid need for lodash

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,6 +1,5 @@
-const merge = require('lodash/merge');
 const eslintConfig = require('eslint-config');
 
-module.exports = merge(eslintConfig, {
-	root: true
-});
+eslintConfig.root = true;
+
+module.exports = eslintConfig;
