80ba38c0d04c0f5c5b71ac43b1d37d0cf1406723
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-04-04 11:26:56 +0100

    BOCM-63 review questionnaire test added

---
+++ apps/api/src/server/app/repositories/review-questionnaire.repository.test.js
@@ -0,0 +1,47 @@
+// eslint-disable-next-line import/no-unresolved
+import test from 'ava';
+import newReviewRepository from './review-questionnaire.repository.js';
+import sinon from 'sinon';
+import DatabaseFactory from './database.js';
+
+const addReview = sinon.stub();
+
+const newReview = {
+	appealId: 1,
+	complete: false,
+	applicationPlansToReachDecisionMissingOrIncorrect: true,
+	applicationPlansToReachDecisionMissingOrIncorrectDescription: 'Some description'
+};
+
+addReview.returns(newReview);
+
+class MockDatabaseClass {
+	constructor(_parameters) {
+		this.pool = {
+			reviewQuestionnaire: {
+				create: addReview
+			}
+		};
+	}
+}
+
+test.before('sets up Database connection mock', () => {
+	sinon.stub(DatabaseFactory, 'getInstance').callsFake((arguments_) => new MockDatabaseClass(arguments_));
+});
+
+test('adds new Validation decision', async(t) => {
+	const review = await newReviewRepository.addReview(
+		1,
+		false,
+		{ applicationPlansToReachDecisionMissingOrIncorrect: true },
+		{ applicationPlansToReachDecisionMissingOrIncorrectDescription: 'Some description' });
+	t.deepEqual(review, newReview);
+	sinon.assert.calledWith(addReview, {
+		data: {
+			appealId: 1,
+			complete: false,
+			applicationPlansToReachDecisionMissingOrIncorrect: true,
+			applicationPlansToReachDecisionMissingOrIncorrectDescription: 'Some description'
+		}
+	});
+});
