f0ea107ff753108ad5d801c6cc809495a21eb2a0
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-04-05 14:35:21 +0100

    BOCM-223 - adds test if LPA provider is unable to return data

--- apps/api/src/server/app/validation/validation.get-lpa-list.test.js
+++ apps/api/src/server/app/validation/validation.get-lpa-list.test.js
@@ -9,7 +9,7 @@ import { app } from '../../app.js';
 const request = supertest(app);
 const getStub = sinon.stub();
 
-const otherFake = {
+const fakeGet = {
 	json: function() {
 		return {
 			features: [
@@ -28,11 +28,22 @@ const otherFake = {
 	}
 };
 
-test('gets all LPAs from external API', async(t) => {
-	sinon.stub(got, 'get').callsFake(getStub);
-	getStub.returns(otherFake);
+test.serial('gets all LPAs from external API', async(t) => {
+	const gotStub = sinon.stub(got, 'get').callsFake(getStub);
+	getStub.returns(fakeGet);
 	const resp = await request.get('/validation/lpa-list');
-	console.log(resp.body);
 	t.is(resp.status, 200);
 	t.deepEqual(resp.body, ['first LPA', 'second LPA']);
+	got.get.restore();
+});
+
+test.serial('returns 500 if unable to get list of LPAs', async(t) => {
+	const gotStub = sinon.stub(got, 'get').callsFake(getStub);
+	getStub.throws(new Error('Unable to get data'));
+	const resp = await request.get('/validation/lpa-list');
+	t.is(resp.status, 500);
+	t.deepEqual(resp.body, {
+		error: 'Unable to get data',
+	});
+	got.get.restore();
 });
