3d831a428256adac7e48dab08780ac3a051bc7fb
localadmin <localadmin@DLT-JHILL2.local.amazerealise.com>
2022-04-05 14:59:21 +0100

    BOCM-52: PR amends - JSDoc comments for LPA service methods

--- apps/web/src/server/app/lpa/lpa.service.js
+++ apps/web/src/server/app/lpa/lpa.service.js
@@ -1,6 +1,10 @@
 import request from './../../lib/request.js';
 
-// TODO: JSDoc comment
+/**
+ * Call the API to get all incoming and incomplete questionnaires
+ *
+ * @returns {object} - questionnaires list data object containing two arrays, one for incoming questionnaires and one for incomplete questionnaires
+ */
 export async function findAllIncomingIncompleteQuestionnaires() {
 	const data = await request('case-officer');
 
@@ -21,7 +25,12 @@ export async function findAllIncomingIncompleteQuestionnaires() {
 	return questionnairesListData;
 }
 
-// TODO: JSDoc comment
+/**
+ * Call the API to get all incoming and incomplete questionnaires
+ *
+ * @param {string} id - numerical appeal id of the desired questionnaire
+ * @returns {object} - questionnaires list data object containing two arrays, one for incoming questionnaires and one for incomplete questionnaires
+ */
 export async function findQuestionnaireById(id) {
 	const data = await request(`case-officer/${id}`);
 
