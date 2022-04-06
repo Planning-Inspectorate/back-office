83ae790cbf36b0e7a9f31b656d63cad1c05df121
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-04-06 09:54:34 +0100

    BOCM-63 adding strict equals

--- apps/api/src/server/app/case-officer/case-officer-review.js
+++ apps/api/src/server/app/case-officer/case-officer-review.js
@@ -3,45 +3,47 @@
 import CaseOfficerError from './case-officer-error.js';
 import _ from 'lodash';
 
+// the questionnaire review is not completed until all the points in the review are completed as well.
+// This validation establishes the status of an appeal as complete or incomplete according to this criteria
 const reviewComplete = function (body) {
 	return ((
-		body.reason.applicationPlanningOfficersReportMissingOrIncorrect == true ||
-		body.reason.applicationPlansToReachDecisionMissingOrIncorrect  == true ||
-		body.reason.policiesStatutoryDevelopmentPlanPoliciesMissingOrIncorrect  == true ||
-		body.reason.policiesOtherRelevantPoliciesMissingOrIncorrect  == true ||
-		body.reason.policiesSupplementaryPlanningDocumentsMissingOrIncorrect   == true ||
-		body.reason.siteConservationAreaMapAndGuidanceMissingOrIncorrect  == true ||
-		body.reason.siteListedBuildingDescriptionMissingOrIncorrect  == true ||
-		body.reason.thirdPartyApplicationNotificationMissingOrIncorrect  == true ||
-		body.reason.thirdPartyApplicationPublicityMissingOrIncorrect  == true ||
-		body.reason.thirdPartyRepresentationsMissingOrIncorrect  == true ||
-		body.reason.thirdPartyAppealNotificationMissingOrIncorrect  == true
+		body.reason.applicationPlanningOfficersReportMissingOrIncorrect === true ||
+		body.reason.applicationPlansToReachDecisionMissingOrIncorrect  === true ||
+		body.reason.policiesStatutoryDevelopmentPlanPoliciesMissingOrIncorrect  === true ||
+		body.reason.policiesOtherRelevantPoliciesMissingOrIncorrect  === true ||
+		body.reason.policiesSupplementaryPlanningDocumentsMissingOrIncorrect   === true ||
+		body.reason.siteConservationAreaMapAndGuidanceMissingOrIncorrect  === true ||
+		body.reason.siteListedBuildingDescriptionMissingOrIncorrect  === true ||
+		body.reason.thirdPartyApplicationNotificationMissingOrIncorrect  === true ||
+		body.reason.thirdPartyApplicationPublicityMissingOrIncorrect  === true ||
+		body.reason.thirdPartyRepresentationsMissingOrIncorrect  === true ||
+		body.reason.thirdPartyAppealNotificationMissingOrIncorrect  === true
 	)?
 		false : true );
 };
 
 const invalidWithoutReasons = function (body) {
 	return ((
-		(body.reason.applicationPlansToReachDecisionMissingOrIncorrect  == true &&
-		body.reason.applicationPlansToReachDecisionMissingOrIncorrectDescription == undefined) ||
-		(body.reason.policiesStatutoryDevelopmentPlanPoliciesMissingOrIncorrect == true &&
-		body.reason.policiesStatutoryDevelopmentPlanPoliciesMissingOrIncorrectDescription == undefined) ||
-		(body.reason.policiesOtherRelevanPoliciesMissingOrIncorrect == true &&
-		body.reason.policiesOtherRelevanPoliciesMissingOrIncorrectDescription == undefined) ||
-		(body.reason.policiesSupplementaryPlanningDocumentsMissingOrIncorrect == true &&
-		body.reason.policiesSupplementaryPlanningDocumentsMissingOrIncorrectDescription == undefined) ||
-		(body.reason.siteConservationAreaMapAndGuidanceMissingOrIncorrect == true &&
+		(body.reason.applicationPlansToReachDecisionMissingOrIncorrect  === true &&
+		body.reason.applicationPlansToReachDecisionMissingOrIncorrectDescription === undefined) ||
+		(body.reason.policiesStatutoryDevelopmentPlanPoliciesMissingOrIncorrect === true &&
+		body.reason.policiesStatutoryDevelopmentPlanPoliciesMissingOrIncorrectDescription === undefined) ||
+		(body.reason.policiesOtherRelevanPoliciesMissingOrIncorrect === true &&
+		body.reason.policiesOtherRelevanPoliciesMissingOrIncorrectDescription === undefined) ||
+		(body.reason.policiesSupplementaryPlanningDocumentsMissingOrIncorrect === true &&
+		body.reason.policiesSupplementaryPlanningDocumentsMissingOrIncorrectDescription === undefined) ||
+		(body.reason.siteConservationAreaMapAndGuidanceMissingOrIncorrect === true &&
 		body.reason.siteConservationAreaMapAndGuidanceMissingOrIncorrectDescription == undefined) ||
-		(body.reason.siteListedBuildingDescriptionMissingOrIncorrect == true &&
-		body.reason.siteListedBuildingDescriptionMissingOrIncorrectDescription == undefined) ||
-		(body.reason.thirdPartyApplicationNotificationMissingOrIncorrect == true &&
-			(body.reason.thirdPartyApplicationNotificationMissingOrIncorrectListOfAddresses == false &&
-			body.reason.thirdPartyApplicationNotificationMissingOrIncorrectCopyOfLetterOrSiteNotice == false)) ||
-		(body.reason.thirdPartyRepresentationsMissingOrIncorrect == true &&
-		body.reason.thirdPartyRepresentationsMissingOrIncorrectDescription == undefined) ||
-		(body.reason.thirdPartyAppealNotificationMissingOrIncorrect == true &&
-			(body.reason.thirdPartyAppealNotificationMissingOrIncorrectListOfAddresses == false &&
-			body.reason.thirdPartyAppealNotificationMissingOrIncorrectCopyOfLetterOrSiteNotice == false))
+		(body.reason.siteListedBuildingDescriptionMissingOrIncorrect === true &&
+		body.reason.siteListedBuildingDescriptionMissingOrIncorrectDescription === undefined) ||
+		(body.reason.thirdPartyApplicationNotificationMissingOrIncorrect === true &&
+			(body.reason.thirdPartyApplicationNotificationMissingOrIncorrectListOfAddresses === false &&
+			body.reason.thirdPartyApplicationNotificationMissingOrIncorrectCopyOfLetterOrSiteNotice === false)) ||
+		(body.reason.thirdPartyRepresentationsMissingOrIncorrect === true &&
+		body.reason.thirdPartyRepresentationsMissingOrIncorrectDescription === undefined) ||
+		(body.reason.thirdPartyAppealNotificationMissingOrIncorrect === true &&
+			(body.reason.thirdPartyAppealNotificationMissingOrIncorrectListOfAddresses === false &&
+			body.reason.thirdPartyAppealNotificationMissingOrIncorrectCopyOfLetterOrSiteNotice === false))
 	)?
 		true : false);
 };
