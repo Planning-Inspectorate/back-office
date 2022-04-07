b79598dfb663006a55533ac402b5b4417df1b12a
Radu Micu <radu.micu@gmail.com>
2022-04-07 06:22:34 +0100

    Update review-questionnaire.repository.js

--- apps/api/src/server/app/repositories/review-questionnaire.repository.js
+++ apps/api/src/server/app/repositories/review-questionnaire.repository.js
@@ -2,8 +2,11 @@ import DatabaseFactory from './database.js';
 
 const newReviewRepository = (function() {
 	/**
-		* @returns {object} connection to database
-		*/
+	 * Retun the connection pool of database connections
+	 * TODO: This should be a utility function part of the main db lib.
+	 *
+	 * @returns {object} connection to database
+	 */
 	function getPool () {
 		return DatabaseFactory.getInstance().pool;
 	}
