128dfd8331c9caac3870feaceca2038b05d94ed8
Ben Ripley <ben.ripley@fimatix.com>
2022-04-10 23:39:06 +0100

    refactor express configuration for web

---
+++ apps/web/src/server/config/session.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+import session from 'express-session';
+
+// TODO: Regeneration of Session After Login
+// TODO: Set Expiration
+
+export default session({
+	secret: 'PINSBackOffice',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+    secure: process.env.NODE_ENV === 'production'
+  }  
+});
