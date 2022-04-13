4366cf84f6c199e106f67e8f1533d4c3abf41d44
Ben Ripley <ben.ripley@fimatix.com>
2022-04-13 17:33:40 +0100

    add transition state error handler

--- apps/api/src/server/app/state-machine/household-appeal.machine.js
+++ apps/api/src/server/app/state-machine/household-appeal.machine.js
@@ -3,31 +3,59 @@ import { validationStates, validationActions } from './validation-states.js';
 import { lpaQuestionnaireStates, lpaQuestionnaireActions } from './lpa-questionnaire-states.js';
 import { inspectorStates, inspectorActions } from './inspector-states.js';
 
-const createHouseholpAppealMachine = function(context) {
-	return createMachine({
-		id: 'household_appeal',
-		context: context,
-		initial: 'received_appeal',
-		states: {
-			...validationStates,
-			...lpaQuestionnaireStates,
-			...inspectorStates
+/**
+ * @typedef {object} TransitionStateErrorConfig
+ * @property {string} type - The state machine action being performed.
+ * @property {object} context - The context on which the state machine error occurred.
+ */
+
+export class TransitionStateError extends Error {
+	/**
+	 * @param {string} message
+	 * @param {TransitionStateErrorConfig} config
+	 */
+	constructor(message, { action, appeal }) {
+		super(message);
+		this.type = action;
+		this.context = appeal;
+	}
+}
+
+const createHouseholpAppealMachine = function (context) {
+	return createMachine(
+		{
+			id: 'household_appeal',
+			context: context,
+			initial: 'received_appeal',
+			states: {
+				...validationStates,
+				...lpaQuestionnaireStates,
+				...inspectorStates
+			}
 		},
-	}, {
-		actions: {
-			...validationActions,
-			...lpaQuestionnaireActions,
-			...inspectorActions
+		{
+			actions: {
+				...validationActions,
+				...lpaQuestionnaireActions,
+				...inspectorActions
+			}
 		}
-	});
+	);
 };
 
-const transitionState = function(context, status, machineAction) {
+const transitionState = function (context, status, machineAction) {
 	const service = interpret(createHouseholpAppealMachine(context));
 	service.start(status);
 	service.send({ type: machineAction });
 	const nextState = service.state;
 	service.stop();
+
+	if (nextState.value === status) {
+		throw new TransitionStateError(`Could not transition '${status}' using '${machineAction}'.`, {
+			type: machineAction,
+			context
+		});
+	}
 	return nextState;
 };
 
