f5deed19cf736433bec3a3760a15b0615c00bb3f
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-04-19 21:32:21 +0100

    BOCM-358 - moves tests to own folder

--- apps/api/src/server/app/utils/__tests__/days-between-dates.test.js
+++ apps/api/src/server/app/utils/__tests__/days-between-dates.test.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
 import test from 'ava';
-import daysBetweenDates from './days-between-dates.js';
+import daysBetweenDates from '../days-between-dates.js';
 
 test('returns 1 between date at end of day and next day at start of day', (t) => {
 	const days = daysBetweenDates(new Date(2022, 3, 6, 22), new Date(2022, 3, 7, 2));
