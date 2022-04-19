65ab40b0051028aaf3d4c0ff46e23ee664d1af47
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-04-19 14:46:43 +0100

    BOCM-277 - merges all migrations into single migration

--- apps/api/prisma/migrations/20220419134619_adds_details_to_appeal_details_from_appellant_table/migration.sql
+++ apps/api/prisma/migrations/20220419134619_adds_details_to_appeal_details_from_appellant_table/migration.sql
@@ -2,6 +2,7 @@
   Warnings:
 
   - You are about to drop the column `doesInspectorNeedToEnterSideDescription` on the `LPAQuestionnaire` table. All the data in the column will be lost.
+  - You are about to drop the column `healthAndSafetyIsueesDescription` on the `LPAQuestionnaire` table. All the data in the column will be lost.
   - You are about to drop the column `sideVisibleFromPublicLandDescription` on the `LPAQuestionnaire` table. All the data in the column will be lost.
 
 */
@@ -10,9 +11,20 @@ BEGIN TRY
 BEGIN TRAN;
 
 -- AlterTable
+ALTER TABLE [dbo].[AppealDetailsFromAppellant] ADD [appellantOwnsWholeSite] BIT,
+[appellantOwnsWholeSiteDescription] NVARCHAR(1000),
+[healthAndSafetyIssues] BIT,
+[healthAndSafetyIssuesDescription] NVARCHAR(1000),
+[siteVisibleFromPublicLandDescription] NVARCHAR(1000);
+
+-- AlterTable
 ALTER TABLE [dbo].[LPAQuestionnaire] DROP COLUMN [doesInspectorNeedToEnterSideDescription],
+[healthAndSafetyIsueesDescription],
 [sideVisibleFromPublicLandDescription];
 ALTER TABLE [dbo].[LPAQuestionnaire] ADD [doesInspectorNeedToEnterSiteDescription] NVARCHAR(1000),
+[emergingDevelopmentPlanOrNeighbourhoodPlan] BIT,
+[emergingDevelopmentPlanOrNeighbourhoodPlanDescription] NVARCHAR(1000),
+[healthAndSafetyIssuesDescription] NVARCHAR(1000),
 [siteVisibleFromPublicLandDescription] NVARCHAR(1000);
 
 COMMIT TRAN;
