e0306c34271b1b042d4020ff5bd35a2714318044
Ben Ripley <ben.ripley@fimatix.com>
2022-05-05 15:17:54 +0100

    add integration tests for auth

---
+++ apps/web/testing/factory/account-info.js
@@ -0,0 +1,31 @@
+import { faker } from '@faker-js/faker';
+import { camelCase, snakeCase } from 'lodash-es';
+
+/** @typedef {import('@azure/msal-node').AccountInfo} AccountInfo */
+/** @typedef {Omit<AccountInfo, 'idTokenClaims'> & { groups?: string[] }} AccountInfoOptions */
+
+/**
+ * @param {Partial<AccountInfoOptions>} [options={}]
+ * @returns {AccountInfo}
+ */
+export function createAccountInfo({
+	homeAccountId = faker.datatype.uuid(),
+	name = `${faker.name.firstName()} ${faker.name.lastName()}`,
+	environment = 'login.planninginspectorate.gov.uk',
+	tenantId = 'PlanningInspectorate',
+	username = snakeCase(name),
+	localAccountId = camelCase(name),
+	groups = ['case_officer', 'inspector', 'validation_officer']
+} = {}) {
+	return {
+		homeAccountId,
+		environment,
+		tenantId,
+		username,
+		localAccountId,
+		name,
+		idTokenClaims: {
+			groups
+		}
+	};
+}
