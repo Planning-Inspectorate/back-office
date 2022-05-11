342dfae3ad91c22c70b5abd651dd3299f3f1517b
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-05-11 18:19:56 +0100

    BOCM-486 - linting

--- apps/document-storage/src/server.js
+++ apps/document-storage/src/server.js
@@ -1,6 +1,6 @@
 import app from './server/app.js';
-import config from './server/config/config.js'
+import config from './server/config/config.js';
 
 app.listen(config.PORT, () => {
-    console.log(`Server is live at http://localhost:${config.PORT}`);
+	console.log(`Server is live at http://localhost:${config.PORT}`);
 });
