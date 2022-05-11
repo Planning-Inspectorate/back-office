dc444987ca307ef940fa976f84cf402016d89ac4
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-05-11 10:44:55 +0100

    BOCM-486 - moves middleware to own files

--- apps/document-storage/src/server/app/middleware/async-handler.js
+++ apps/document-storage/src/server/app/middleware/async-handler.js
@@ -1,9 +1,9 @@
 /**
  * @template T
- * @param {import('express').RequestHandler<T>} requestHandler request handler
- * @returns {import('express').RequestHandler<T>} request handler
+ * @param {import('express').RequestHandler<T>} requestHandler
+ * @returns {import('express').RequestHandler<T>}
  */
 export const asyncHandler = (requestHandler) => (request, response, next) =>
-	Promise.resolve(requestHandler(request, response, next))
-	// eslint-disable-next-line promise/no-callback-in-promise
-		.catch(next);
+ Promise.resolve(requestHandler(request, response, next))
+     // eslint-disable-next-line promise/no-callback-in-promise
+     .catch(next);
