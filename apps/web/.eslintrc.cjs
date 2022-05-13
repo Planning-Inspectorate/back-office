e69ff86234543a75c031ec37e36a16ab9b198d6c
Ben Ripley <ben.ripley@fimatix.com>
2022-05-13 15:07:45 +0100

    add joi environment schema checking

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -7,6 +7,14 @@ module.exports = {
 				// https://github.com/weiran-zsd/eslint-plugin-node/blob/cb4f099ae928d627abfc635966622cf4266a7b1c/docs/rules/no-unpublished-import.md
 				'n/no-unpublished-import': 'off'
 			}
+		},
+		{
+			files: ['environment/schema.js'],
+			rules: {
+				// allow `then` within objects just for this file – joi's api is built around it
+				// https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v42.0.0/docs/rules/no-thenable.md
+				'unicorn/no-thenable': 'off'
+			}
 		}
 	]
 };
--- apps/web/environment/config.js
+++ apps/web/environment/config.js
@@ -1,77 +1,68 @@
 import { loadEnvironment } from '@pins/platform';
-
-/** @typedef {'development' | 'test' | 'production'} EnvironmentType */
+import schema from './schema.js';
 
 /**
  * @typedef {object} EnvironmentConfig
- * @property {EnvironmentType} NODE_ENV
- * @property {string} API_HOST
- * @property {boolean} APP_RELEASE
- * @property {boolean} HTTPS_ENABLED
- * @property {number} HTTP_PORT
- * @property {number} HTTPS_PORT
- * @property {string} SSL_CERT_FILE
- * @property {string} SSL_KEY_FILE
- * @property {boolean} bundleAnalyzer
- * @property {boolean} isProd
- * @property {boolean} isRelease
- * @property {boolean} isTest
+ * @property {string} apiUrl
  * @property {boolean} authDisabled
- * @property {string[]} authSimulatedGroups
- * @property {object} auth
- * @property {string} auth.redirectUri
- * @property {string} auth.inspectorGroupID
- * @property {string} auth.caseOfficerGroupID
- * @property {string} auth.validationOfficerGroupID
- * @property {object} auth.sso
- * @property {string} auth.sso.clientId
- * @property {string} auth.sso.clientSecret
- * @property {string} auth.sso.cloudInstanceId
- * @property {string} auth.sso.tenantId
+ * @property {string} authRedirectTo
+ * @property {boolean} bundleAnalyzer
+ * @property {'development' | 'test' | 'production'} env
+ * @property {boolean} isProduction
+ * @property {boolean} isDevelopment
+ * @property {boolean} isTest
+ * @property {object} msal
+ * @property {string} msal.clientId
+ * @property {string} msal.clientSecret
+ * @property {string} msal.cloudInstanceId
+ * @property {string} msal.tenantId
+ * @property {boolean=} release
+ * @property {string} serverProtocol
+ * @property {number} serverPort
+ * @property {string} sslCertificateFile
+ * @property {string} sslCertificateKeyFile
+ * @property {object} referencedata
+ * @property {object} referencedata.groups
+ * @property {string} referencedata.groups.inspectorGroupId
+ * @property {string} referencedata.groups.caseOfficerGroupId
+ * @property {string} referencedata.groups.validationOfficerGroupId
  */
-
-// All env variables used by the app should be defined in this file.
-
-// To define new env:
-// 1. Add env variable to `.env.development` and you can override locally in .env.local file;
-// 2. Provide validation rules for your env in envsSchema; // TODO: Enforce this.
-// 3. Make it visible outside of this module in export section;
-// 4. Access your env variable only via config file.
-// Do not use process.env object outside of this file.
 
 const environment = loadEnvironment(process.env.NODE_ENV);
 
-/** @type {EnvironmentConfig} */
-export default {
-	NODE_ENV: /** @type {EnvironmentType} */ (environment.NODE_ENV),
-	APP_RELEASE: environment.APP_RELEASE === 'true',
-	HTTPS_ENABLED: environment.HTTPS_ENABLED === 'true',
-	HTTP_PORT: Number(environment.HTTP_PORT) || 8080,
-	HTTPS_PORT: Number(environment.HTTPS_PORT) || 8443,
-	SSL_CERT_FILE: environment.SSL_CERT_FILE,
-	SSL_KEY_FILE: environment.SSL_KEY_FILE,
-
-	bundleAnalyzer: environment.BUNDLE_ANALYZER === 'true',
-	isProd: environment.NODE_ENV === 'production',
-	isRelease: environment.APP_RELEASE === 'true',
-	isTest: environment.NODE_ENV === 'test',
-	authDisabled: environment.AUTH_DISABLED === 'true',
-	authSimulatedGroups: (environment.AUTH_SIMULATED_GROUPS || '')
-		.split(',')
-		.map((group) => group.trim()),
-
-	API_HOST: environment.API_HOST || '',
-
-	auth: {
-		sso: {
-			clientId: environment.AUTH_CLIENT_ID || '',
-			cloudInstanceId: environment.AUTH_CLOUD_INSTANCE_ID || '',
-			tenantId: environment.AUTH_TENANT_ID || '',
-			clientSecret: environment.AUTH_CLIENT_SECRET || ''
-		},
-		redirectUri: environment.AUTH_REDIRECT_URI || '/auth/redirect',
-		inspectorGroupID: environment.AUTH_INSPECTOR_GROUP_ID || 'inspector',
-		caseOfficerGroupID: environment.AUTH_CASEOFFICER_GROUP_ID || 'case_officer',
-		validationOfficerGroupID: environment.AUTH_VALIDATIONOFFICER_GROUP_ID || 'validation_officer'
+const { value, error } = schema.validate({
+	apiUrl: environment.API_HOST,
+	authDisabled: environment.AUTH_DISABLED,
+	authRedirectTo: environment.AUTH_REDIRECT_URI || '/auth/redirect',
+	bundleAnalyzer: environment.BUNDLE_ANALYZER,
+	env: environment.NODE_ENV,
+	msal: {
+		clientId: environment.AUTH_CLIENT_ID,
+		cloudInstanceId: environment.AUTH_CLOUD_INSTANCE_ID,
+		clientSecret: environment.AUTH_CLIENT_SECRET,
+		tenantId: environment.AUTH_TENANT_ID,
+	},	
+	release: environment.RELEASE,
+	serverPort: environment.HTTPS_ENABLED === 'true' ? environment.HTTP_PORT : environment.HTTPS_PORT,
+	serverProtocol: environment.HTTPS_ENABLED === 'true' ? 'https' : 'http',
+	sslCertificateFile: environment.SSL_CERT_FILE,
+	sslCertificateKeyFile: environment.SSL_KEY_FILE,
+	referencedata: {
+		groups: {
+			inspectorGroupID: environment.AUTH_INSPECTOR_GROUP_ID,
+			caseOfficerGroupID: environment.AUTH_CASEOFFICER_GROUP_ID,
+			validationOfficerGroupID: environment.AUTH_VALIDATIONOFFICER_GROUP_ID
+		}
 	}
+});
+
+if (error) {
+	throw error;
+}
+
+export default {
+	...value,
+	isProduction: value.env === 'production',
+	isDevelopment: value.env === 'development',
+	isTest: value.env === 'test'
 };
