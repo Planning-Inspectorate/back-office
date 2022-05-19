beb281ddebe0009d56a6f6e47a6b9dee7d19bcbf
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-05-19 16:38:44 +0100

    BOAS-15 - fixes test environment vars

--- apps/api/src/server/app/middleware/version-routes.js
+++ apps/api/src/server/app/middleware/version-routes.js
@@ -2,24 +2,24 @@
  *
  * @param {import('express').Request} request
  * @returns {string}
- */ 
+ */
 function getVersion(request) {
 	return request.headers['accept-version']?.toString() || '1';
 }
 
 /**
- * 
- * @param {object} versionToControllerPairs 
- * @returns {function}
+ *
+ * @param {Object<string, Function>} versionToControllerPairs
+ * @returns {Function}
  */
 export default function versionRoutes(versionToControllerPairs) {
-    /**
-     * @type {import('express').RequestHandler}
-     */
-	return function(request, response, next) {
-		const that = this;
+	/**
+	 * @type {import('express').RequestHandler}
+	 */
+	return function (request, response, next) {
 		const version = getVersion(request);
-		versionToControllerPairs[version].call(that, request, response, next);
-        return
-	}
+
+		versionToControllerPairs[version](request, response, next);
+		
+	};
 }
