f381d167463b5611e7b31cb16b2c2c1298887402
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-06-06 10:36:01 +0100

    fix: BOAS-11 Express usage warning WIP

--- apps/api/src/server/app/inspector/inspector.controller.js
+++ apps/api/src/server/app/inspector/inspector.controller.js
@@ -111,7 +111,7 @@ export const issueDecision = async ({ body, file, params }, response) => {
 	await inspector.issueDecision({
 		appealId: params.appealId,
 		outcome: body.outcome,
-		decisionLetter: /** @type {Express.Multer.File} */ (file)
+		decisionLetter: (file)
 	});
 
 	const updatedAppeal = await appealRepository.getById(params.appealId, {
