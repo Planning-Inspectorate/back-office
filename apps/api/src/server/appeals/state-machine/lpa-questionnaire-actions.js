e6739f76764271e6ffe778bd4b9f108fc65f25c5
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-06-08 11:26:57 +0100

    feat(api): updates domain of all appeals endpoints to be prefixed with 'appeals'

---
+++ apps/api/src/server/appeals/state-machine/lpa-questionnaire-actions.js
@@ -0,0 +1,13 @@
+import logger from '../../lib/logger.js';
+import lpaQuestionnaireActionsService from './lpa-questionnaire-actions.service.js';
+
+const lpaQuestionnaireActions = {
+	async sendLPAQuestionnaire(context) {
+		await lpaQuestionnaireActionsService.sendLpaQuestionnaire(context.appealId);
+	},
+	nudgeLPAQuestionnaire: () => {
+		logger.info('Sending an email to nudge LPA regarding questionnaire');
+	}
+};
+
+export default lpaQuestionnaireActions;
