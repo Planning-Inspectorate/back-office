db60032b719b73f01406c4d7af1163ed08416ad0
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-06-15 15:38:03 +0100

    docs(api/applications): adds documentation for applications endpoint

--- apps/api/src/server/applications/case-officer/case-officer.routes.js
+++ apps/api/src/server/applications/case-officer/case-officer.routes.js
@@ -5,14 +5,17 @@ import { getApplications } from './case-officer.controller.js';
 const router = new express.Router();
 
 router.get(
-    '/',
-    /*
+	'/',
+	/*
+        #swagger.tags = ['Applications']
+        #swagger.path = '/applications/case-officer'
         #swagger.description = 'Gets all applications associated with case officer'
-        #swagger.desponses[200] = {
-            description: 'List of applications assigned to case officer'
+        #swagger.responses[200] = {
+            description: 'List of applications assigned to case officer',
+            schema: { $ref: '#/definitions/ApplicationsForCaseOfficer' }
         }
     */
-    asyncHandler(getApplications)
-)
+	asyncHandler(getApplications)
+);
 
-export { router as caseOfficerRoutes }
+export { router as caseOfficerRoutes };
