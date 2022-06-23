a8b4f9061f7b742c1265def2f16d676f830d21d7
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-06-23 11:20:58 +0100

    feat(api/applications): adding mapping to Repository, test WIP (BOAS-92)

--- apps/api/src/server/applications/case-search/case-search.controller.js
+++ apps/api/src/server/applications/case-search/case-search.controller.js
@@ -1,7 +1,21 @@
 import * as applicationRepository from '../../repositories/application.repository.js';
+import { mapApplicationWithSeachCriteria} from '../../utils/mapping/map-application-with-sector-and-subsector.js';
 
+/**
+ * @typedef {{name: string, displayNameEn: string, displayNameCy: string, abbreviation: string}} SectorResponse
+ * @typedef {{id: number, modifiedDate: number, reference: string, sector: SectorResponse, subSector: SectorResponse}} ApplicationWithSectorResponse
+ */
+
+/**
+ *
+ * @param {import('@pins/api').Schema.Application[]} applications
+ * @returns {ApplicationWithSectorResponse[]}
+ */
+ const getBySearchCriteria = (applications) => {
+	return applications.map((application) => mapApplicationWithSeachCriteria(application));
+};
 export const getApplications = async (_request, response) => {
-	const applications = await applicationRepository.getByCaseID(caseId);
+	const applications = await applicationRepository.getByCaseID(searchCriteria);
 
 	return response.send(mapApplicationsByCriteria(applications));
 };
