412bfe56804290635ae391e1b774e63e59d164bd
Mayya Bondarevskaya <89029990+mayya-bond-kc@users.noreply.github.com>
2022-06-28 10:58:39 +0100

    Feature/boas 137 endpoint to get sectors and sub sectors (#413)
    feat(api/applications): finish endpoint to get sectors (BOAS-137)
    
    * feat(api/applications): wip sector endpoint (BOAS-137)
    
    * feat(api/applications): finish endpoint to get sectors (BOAS-137)

--- apps/api/src/server/applications/applications.routes.js
+++ apps/api/src/server/applications/applications.routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { caseAdminOfficerRoutes } from './case-admin-officer/case-admin-officer.routes.js';
 import { caseOfficerRoutes } from './case-officer/case-officer.routes.js';
 import { inspectorRoutes } from './inspector/inspector.routes.js';
+import { sectorRoutes } from './sector/sector.routes.js';
 
 const router = new express.Router();
 
@@ -10,5 +11,7 @@ router.use('/case-officer', caseOfficerRoutes);
 router.use('/case-admin-officer', caseAdminOfficerRoutes);
 
 router.use('/inspector', inspectorRoutes);
+
+router.use('/sector', sectorRoutes);
 
 export { router as applicationsRoutes };
