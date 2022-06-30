f3f9a7c2baacb52e37b7b64840c30772903e76bf
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-06-30 17:20:23 +0100

    feat(web/applications): set validator for name and description to be less than 500chars (boas-120)

--- apps/web/src/server/applications/create/applications-create.validators.js
+++ apps/web/src/server/applications/create/applications-create.validators.js
@@ -2,11 +2,21 @@ import { createValidator } from '@pins/express';
 import { body } from 'express-validator';
 
 export const validateApplicationsCreateName = createValidator(
-	body('applicationName').trim().isLength({ min: 1 }).withMessage('Enter a name')
+	body('applicationName')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Enter the name')
+		.isLength({ max: 500 })
+		.withMessage('Search term must be 500 characters or fewer')
 );
 
 export const validateApplicationsCreateDescription = createValidator(
-	body('applicationDescription').trim().isLength({ min: 1 }).withMessage('Enter a description')
+	body('applicationDescription')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Enter the description')
+		.isLength({ max: 500 })
+		.withMessage('Search term must be 500 characters or fewer')
 );
 
 export const validateApplicationsCreateSector = createValidator(
