7d385d176128d7d2a12fc0deec118e041e46c9b4
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-07-01 10:06:35 +0100

    feat(api/applications): swagger final correction (BOAS-92)

--- apps/api/src/server/applications/search/case-search.controller.js
+++ apps/api/src/server/applications/search/case-search.controller.js
@@ -3,7 +3,7 @@ import * as applicationRepository from '../../repositories/application.repositor
 import { mapApplicationWithSearchCriteria } from '../../utils/mapping/map-application-with-search-criteria.js';
 
 /**
- * @typedef {{id: number, reference: string, title: string, description: string, state: string, modifiedDate: number}} ApplicationWithSearchCriteriaResponse
+ * @typedef {{id: number, reference: string, modifiedDate: number, title: string, description: string, status: string}} ApplicationWithSearchCriteriaResponse
  */
 
 /**
