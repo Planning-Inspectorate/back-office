a0d69db7956611990bd5f7c5355a54bf80e1bf96
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-07-08 12:10:00 +0100

    chore(api): adds jsdocs

--- apps/api/src/server/utils/get-appeal-status-created-at.js
+++ apps/api/src/server/utils/get-appeal-status-created-at.js
@@ -1,5 +1,11 @@
 import { filter } from 'lodash-es';
 
+/**
+ *
+ * @param {{createdAt: any}[]} appealStatuses
+ * @param {string} status
+ * @returns {any}
+ */
 export const getAppealStatusCreatedAt = (appealStatuses, status) => {
 	return filter(appealStatuses, { status })[0].createdAt;
 };
