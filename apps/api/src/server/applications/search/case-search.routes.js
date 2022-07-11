562eb840bdd2ef86213444a7e6736d9d8e307df6
David White <50454208+DavidJWhiteDev@users.noreply.github.com>
2022-07-11 16:46:32 +0100

    feat(api/applications): swagger fix, testing for null result WIP (BOAS-92)

--- apps/api/src/server/applications/search/case-search.routes.js
+++ apps/api/src/server/applications/search/case-search.routes.js
@@ -11,6 +11,12 @@ router.post(
         #swagger.tags = ['Applications']
         #swagger.path = '/applications/search'
         #swagger.description = 'Gets all applications as per search criteria'
+		#swagger.parameters['body'] = {
+			in: 'body',
+			description: 'case query parameters',
+			schema: { $ref: '#/definitions/ApplicationsForSearchCriteriaRequestBody' },
+			required: true
+		}
         #swagger.responses[200] = {
             description: 'List of applications as per search criteria',
             schema: { $ref: '#/definitions/ApplicationsForSearchCriteria' }
