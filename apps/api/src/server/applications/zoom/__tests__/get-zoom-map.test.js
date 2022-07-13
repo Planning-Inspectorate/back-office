b93939de6be3b819930dbdcd6d419e811cf32b17
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-07-13 15:53:32 +0100

    feat(api/applications): zoom levels working and tested (BOAS-181)

---
+++ apps/api/src/server/applications/zoom/__tests__/get-zoom-map.test.js
@@ -0,0 +1,34 @@
+import test from 'ava';
+import sinon from 'sinon';
+import supertest from 'supertest';
+import { app } from '../../../app.js';
+import { databaseConnector } from '../../../utils/database-connector.js';
+
+const request = supertest(app);
+
+const mapZoomLevels = {
+	id: 1,
+	name: 'test',
+	displayNameEn: 'test name en',
+	displayNameCy: 'test name cy'
+};
+
+test.before('set up mocking', () => {
+	sinon.stub(databaseConnector, 'zoomLevel').get(() => {
+		return { findMany: sinon.stub().returns([mapZoomLevels]) };
+	});
+});
+
+test('gets all map zoom levels', async (t) => {
+	const resp = await request.get('/applications/zoom-level');
+
+	t.is(resp.status, 200);
+	t.deepEqual(resp.body, [
+		{
+			id: mapZoomLevels.id,
+			name: mapZoomLevels.name,
+			displayNameEn: mapZoomLevels.displayNameEn,
+			displayNameCy: mapZoomLevels.displayNameCy
+		}
+	]);
+});
