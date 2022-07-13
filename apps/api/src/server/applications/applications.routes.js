969d245ea1eb3fae99af99c76361be259d6a3151
Maria Valero Gonzalez <maria.valero-gonzalez@kinandcarta.com>
2022-07-13 15:53:32 +0100

    feat(api/applications): zoom levels working and tested (BOAS-181)

--- apps/api/src/server/applications/applications.routes.js
+++ apps/api/src/server/applications/applications.routes.js
@@ -4,6 +4,7 @@ import { caseOfficerRoutes } from './case-officer/case-officer.routes.js';
 import { inspectorRoutes } from './inspector/inspector.routes.js';
 import { regionRoutes } from './region/region.routes.js';
 import { sectorRoutes } from './sector/sector.routes.js';
+import { zoomLevelRoutes } from './zoom/zoom.routes.js';
 
 const router = new express.Router();
 
@@ -16,5 +17,7 @@ router.use('/inspector', inspectorRoutes);
 router.use('/region', regionRoutes);
 
 router.use('/sector', sectorRoutes);
+
+router.use('/zoom-level', zoomLevelRoutes);
 
 export { router as applicationsRoutes };
---
+++ apps/api/src/server/applications/zoom/zoom.routes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { asyncHandler } from '../../middleware/async-handler.js';
+import { getZoomLevels } from './zoom.controller.js';
+
+const router = new express.Router();
+
+router.get(
+	'/',
+	/*
+        #swagger.tags = ['Applications']
+        #swagger.path = '/applications/zoom-level'
+        #swagger.description = 'Gets all map zoom levels available'
+        #swagger.responses[200] = {
+            description: 'List of map zoom level',
+            schema: { $ref: '#/definitions/MapZoomLevelForApplications' }
+        }
+    */
+	asyncHandler(getZoomLevels)
+);
+
+export { router as zoomLevelRoutes };
