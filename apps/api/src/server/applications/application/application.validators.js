e0127d82d02937d17664831358197874ad9256f8
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-07-14 08:15:52 +0100

    feat(api/applications): creates endpoint to create applications (BOAS-129)

---
+++ apps/api/src/server/applications/application/application.validators.js
@@ -0,0 +1,72 @@
+import { composeMiddleware } from '@pins/express';
+import { validateFutureDate, validatePastDate } from '@pins/platform';
+import { body } from 'express-validator';
+import { validationErrorHandler } from '../../middleware/error-handler.js';
+import * as subSectorRepository from '../../repositories/sub-sector.repository.js';
+
+/**
+ *
+ * @param {string} value
+ */
+const validateExistingSubsector = async (value) => {
+	const subSector = await subSectorRepository.getByName(value);
+
+	if (subSector === null) {
+		throw new Error('Unknown Sub-Sector');
+	}
+};
+
+/**
+ *
+ * @param {number} value
+ * @returns {Date}
+ */
+const timestampToDate = (value) => {
+	return new Date(value);
+};
+
+export const validateCreateApplication = composeMiddleware(
+	body('geographicalInformation.gridReference.easting')
+		.isLength({ min: 6, max: 6 })
+		.toInt()
+		.withMessage('Easting must be integer with 6 digits')
+		.optional({ nullable: true }),
+	body('geographicalInformation.gridReference.northing')
+		.isLength({ min: 6, max: 6 })
+		.toInt()
+		.withMessage('Northing must be integer with 6 digits')
+		.optional({ nullable: true }),
+	// TODO: add validation once we have map zoom levels in a table
+	// body('geographicalInformation.mapZoomLevel').withMessage('Must be a valid map zoom level').optional({nullable: true}),
+	body('applicant.email')
+		.isEmail()
+		.withMessage('Email must be a valid email')
+		.optional({ nullable: true }),
+	body('applicant.phoneNumber')
+		.isMobilePhone('en-GB')
+		.withMessage('Phone Number must be a valid UK number')
+		.optional({ nullable: true }),
+	body('applicant.address.postcode')
+		.isPostalCode('GB')
+		.withMessage('Postcode must be a valid UK postcode')
+		.optional({ nullable: true }),
+	body('applicant.website')
+		.isURL()
+		.withMessage('Website must be a valid website')
+		.optional({ nullable: true }),
+	body('keyDates.firstNotifiedDate')
+		.customSanitizer(timestampToDate)
+		.custom(validatePastDate)
+		.withMessage('First notified date must be in the past')
+		.optional({ nullable: true }),
+	body('keyDates.submissionDate')
+		.customSanitizer(timestampToDate)
+		.custom(validateFutureDate)
+		.withMessage('Submission date must be in the future')
+		.optional({ nullable: true }),
+	body('subSectorName')
+		.custom(validateExistingSubsector)
+		.withMessage('Must be existing sub-sector')
+		.optional({ nullable: true }),
+	validationErrorHandler
+);
