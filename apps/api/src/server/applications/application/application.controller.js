2e736b19a346338735ccc86f02bbc04561d9f37c
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-07-18 10:54:52 +0100

    feat(api/applications): allows list of regions to be assigned to case (BOAS-129)

--- apps/api/src/server/applications/application/application.controller.js
+++ apps/api/src/server/applications/application/application.controller.js
@@ -1,22 +1,26 @@
-import { isEmpty, pick } from 'lodash-es';
+import { filter, isEmpty, pick } from 'lodash-es';
 import * as caseRepository from '../../repositories/case.repository.js';
 import { mapKeysUsingObject } from '../../utils/mapping/map-keys-using-object.js';
+import { mapValuesUsingObject } from '../../utils/mapping/map-values-using-object.js';
 
 /**
  * @param {import('@pins/applications').CreateUpdateApplication} applicationDetails
  * @returns {{
- *  caseDetails?: import('@pins/api').Schema.Case,
- * 	gridReference?: import('@pins/api').Schema.GridReference,
- *  application?: import('@pins/api').Schema.ApplicationDetails,
- *  applicant?: import('@pins/api').Schema.ServiceCustomer,
- *  applicantAddress?: import('@pins/api').Schema.Address}}
+ *  caseDetails?: { title?: string | undefined, description?: string | undefined },
+ * 	gridReference?: { easting?: number | undefined, northing?: number | undefined },
+ *  application?: { locationDescription?: string | undefined, firstNotifiedAt?: Date | undefined, submissionAt?: Date | undefined },
+ *  subSectorName?: string | undefined,
+ *  applicant?: { organisationName?: string | undefined, firstName?: string | undefined, middleName?: string | undefined, lastName?: string | undefined, email?: string | undefined, website?: string | undefined, phoneNumber?: string | undefined},
+ *  mapZoomLevelName?: string | undefined,
+ *  regionNames?: string[],
+ *  applicantAddress?: { addressLine1?: string | undefined, addressLine2?: string | undefined, town?: string | undefined, county?: string | undefined, postcode?: string | undefined}}}
  */
 const mapCreateApplicationRequestToRepository = (applicationDetails) => {
 	const formattedCaseDetails = pick(applicationDetails, ['title', 'description']);
 
-	const formattedGridReferenceDetails = pick(
-		applicationDetails.geographicalInformation?.gridReference,
-		['easting', 'northing']
+	const formattedGridReferenceDetails = mapValuesUsingObject(
+		pick(applicationDetails.geographicalInformation?.gridReference, ['easting', 'northing']),
+		{ easting: Number, northing: Number }
 	);
 
 	const formattedApplicationDetails = pick(
@@ -38,7 +42,6 @@ const mapCreateApplicationRequestToRepository = (applicationDetails) => {
 		'lastName',
 		'email',
 		'website',
-		'email',
 		'phoneNumber'
 	]);
 
@@ -50,6 +53,10 @@ const mapCreateApplicationRequestToRepository = (applicationDetails) => {
 		'postcode'
 	]);
 
+	const regionNames = applicationDetails.geographicalInformation?.regionNames;
+
+	const mapZoomLevelName = applicationDetails?.geographicalInformation?.mapZoomLevelName;
+
 	return {
 		...(!isEmpty(formattedCaseDetails) && { caseDetails: formattedCaseDetails }),
 		...(!isEmpty(formattedGridReferenceDetails) && {
@@ -57,9 +64,8 @@ const mapCreateApplicationRequestToRepository = (applicationDetails) => {
 		}),
 		...(!isEmpty(formattedApplicationDetails) && { application: formattedApplicationDetails }),
 		...(applicationDetails.subSectorName && { subSectorName: applicationDetails.subSectorName }),
-		...(applicationDetails?.geographicalInformation?.mapZoomLevelName && {
-			mapZoomLevelName: applicationDetails?.geographicalInformation?.mapZoomLevelName
-		}),
+		...(mapZoomLevelName && { mapZoomLevelName }),
+		...(typeof regionNames !== 'undefined' && { regionNames }),
 		...(!isEmpty(formattedApplicantDetails) && { applicant: formattedApplicantDetails }),
 		...(!isEmpty(formattedApplicantAddressDetails) && {
 			applicantAddress: formattedApplicantAddressDetails
@@ -84,11 +90,15 @@ export const createApplication = async (request, response) => {
 export const updateApplication = async (request, response) => {
 	const mappedApplicationDetails = mapCreateApplicationRequestToRepository(request.body);
 
-	const application = await caseRepository.updateApplication({
+	const updateResults = await caseRepository.updateApplication({
 		caseId: request.params.id,
 		applicantId: request.body.applicant?.id,
 		...mappedApplicationDetails
 	});
 
-	response.send({ id: application.id });
+	const updatedCase = filter(updateResults, (result) => {
+		return typeof result.id !== 'undefined';
+	})[0];
+
+	response.send({ id: updatedCase.id });
 };
