facc45cb6a51212dcb22d13dc09914178f89faa2
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-07-20 11:49:13 +0100

    feat(web/applications): add session values for applicantid and sector (boas-167)

---
+++ apps/web/src/server/applications/create/case/applications-create-case-session.service.js
@@ -0,0 +1,34 @@
+/**
+ * @typedef {import('express-session').Session & { caseSectorName?: string }} SessionWithCaseSectorName
+ */
+
+/**
+ * Save in the session the list of information types to be provided in the create-new-applicant form.
+ *
+ * @param {SessionWithCaseSectorName} session
+ * @param {string} selectedCaseSectorName
+ * @returns {void}
+ */
+export const setSessionCaseSectorName = (session, selectedCaseSectorName) => {
+	session.caseSectorName = selectedCaseSectorName;
+};
+
+/**
+ * Retrieve the applicant information types from the session.
+ *
+ * @param {SessionWithCaseSectorName} session
+ * @returns {string|undefined}
+ */
+export const getSessionCaseSectorName = (session) => {
+	return session.caseSectorName;
+};
+
+/**
+ * Clear the case sector from the session.
+ *
+ * @param {SessionWithCaseSectorName} session
+ * @returns {void}
+ */
+export const destroySessionCaseSectorName = (session) => {
+	delete session.caseSectorName;
+};
