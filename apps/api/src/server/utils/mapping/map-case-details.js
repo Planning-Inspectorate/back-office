d1b080e2f4182f49242f49d2af45cb73d2dc453c
Xfarya <farya243@gmail.com>
2022-07-22 10:24:33 +0100

    feat(api/applications): basic formatting of enpoint output, needs refactor (BOAS-164)

--- apps/api/src/server/utils/mapping/map-case-details.js
+++ apps/api/src/server/utils/mapping/map-case-details.js
@@ -1,7 +1,8 @@
 // @ts-nocheck
-import { isEmpty, pick } from 'lodash-es';
-import { mapKeysUsingObject } from '../../utils/mapping/map-keys-using-object.js';
-import { mapValuesUsingObject } from '../../utils/mapping/map-values-using-object.js';
+// import { regions } from 'apps/api/prisma/seed-samples.js';
+// import { pick } from 'lodash-es';
+// import { mapKeysUsingObject } from '../../utils/mapping/map-keys-using-object.js';
+// import { mapValuesUsingObject } from '../../utils/mapping/map-values-using-object.js';
 
 /**
  * @param {import('@pins/applications').ApplicationDetails} applicationDetails
@@ -9,7 +10,6 @@ import { mapValuesUsingObject } from '../../utils/mapping/map-values-using-objec
  *  caseDetails?: { title?: string | undefined, description?: string | undefined, caseEmail?: string | undefined },
  * 	gridReference?: { easting?: number | undefined, northing?: number | undefined },
  *  regions: { displayNameEn?: string | undefined, displayNameCy?: string | undefined }
- *  geographicalInformation?: { locationDescription?: string | undefined },
  *  keyDates?: { firstNotifiedDate?: number | undefined, submissionDate?: number | undefined }
  *  subSector?: { name: string | undefined, abbreviation: string | undefined, displayNameEn: string | undefined, displayNameCy: string | undefined },
  *  sector?: { name: string | undefined, abbreviation: string | undefined, displayNameEn: string | undefined, displayNameCy: string | undefined },
@@ -19,26 +19,73 @@ import { mapValuesUsingObject } from '../../utils/mapping/map-values-using-objec
  *  address?: { addressLine1?: string | undefined, addressLine2?: string | undefined, town?: string | undefined, county?: string | undefined, postcode?: string | undefined}}}
  */
 export const mapCaseDetails = (applicationDetails) => {
+	// const caseDetails = pick(applicationDetails, ['id', 'reference', 'status', 'title', 'description', 'caseEmail']);
 
-    const caseDetails = pick(applicationDetails, ['id', 'reference', 'status', 'title', 'description', 'caseEmail']);
+	// const sector = pick(applicationDetails.sector, ['name', 'abbreviation', 'displayNameEn', 'displayNameCy' ]);
 
-    const sector = pick(applicationDetails.sector, ['name', 'abbreviation', 'displayNameEn', 'displayNameCy' ]);
+	// const subSector = pick(applicationDetails.subSector, ['name', 'abbreviation', 'displayNameEn', 'displayNameCy' ]);
 
-    const subSector = pick(applicationDetails.subSector, ['name', 'abbreviation', 'displayNameEn', 'displayNameCy' ]);
-    
-    const geographicalInformation = pick(applicationDetails.regions, ['displayNameEn', 'displayNameCy']);
+	// const geographicalInformation = pick(applicationDetails.regions, ['displayNameEn', 'displayNameCy']);
 
-    const mapZoomLevel = pick(applicationDetails.mapZoomLevel, ['displayNameEn', 'displayNameCy', 'locationDescription' ]);
+	// const mapZoomLevel = pick(applicationDetails.mapZoomLevel, ['displayNameEn', 'displayNameCy', 'locationDescription' ]);
 
-    const gridReference = pick(applicationDetails.gridReference, ['easting', 'northing']);
-    
-    const keyDates = pick(applicationDetails.keyDates, ['firstNotifiedDate', 'submissionDate']);
+	// const gridReference = pick(applicationDetails.gridReference, ['easting', 'northing']);
 
-    const applicant = pick(applicationDetails.application, ['firstName', 'middleName', 'lastName', 'email', 'address', 'website', 'phoneNumber']);
+	// const keyDates = pick(applicationDetails.keyDates, ['firstNotifiedDate', 'submissionDate']);
 
-    const address = pick(applicationDetails.address, ['addressLine1', 'addressLine2', 'county', 'town', 'postcode'])
+	// const applicant = pick(applicationDetails.application, ['firstName', 'middleName', 'lastName', 'email', 'address', 'website', 'phoneNumber']);
 
-    return { caseDetails, sector, subSector, geographicalInformation, mapZoomLevel, gridReference, applicant, address, keyDates }
+	// const address = pick(applicationDetails.address, ['addressLine1', 'addressLine2', 'county', 'town', 'postcode'])
 
+	return {
+		id: applicationDetails.id,
+		reference: applicationDetails.reference,
+		status: applicationDetails.status,
+		title: applicationDetails.title,
+		description: applicationDetails.description,
+		caseEmail: applicationDetails.ApplicationDetails.caseEmail,
+		sector: {
+			name: applicationDetails.ApplicationDetails.subSector.sector.name,
+			abbreviation: applicationDetails.ApplicationDetails.subSector.sector.abbreviation,
+			displayNameEn: applicationDetails.ApplicationDetails.subSector.sector.displayNameEn,
+			displayNameCy: applicationDetails.ApplicationDetails.subSector.sector.displayNameCy
+		},
+		subSector: {
+			name: applicationDetails.ApplicationDetails.subSector.name,
+			abbreviation: applicationDetails.ApplicationDetails.subSector.abbreviation,
+			displayNameEn: applicationDetails.ApplicationDetails.subSector.displayNameEn,
+			displayNameCy: applicationDetails.ApplicationDetails.subSector.displayNameCy
+		},
+		geographicalInformation: {
+			mapZoomLevel: {
+				displayNameEn: applicationDetails.ApplicationDetails.zoomLevel.displayNameEn,
+				displayNameCy: applicationDetails.ApplicationDetails.zoomLevel.displayNameCy
+			},
+			// locationDescription: geographicalInformation.locationDescription,
+			// gridReference: { easting: geographicalInformation.gridReference, northing: geographicalInformation.gridReference },
+			regions: {
+				displayNameEn: applicationDetails.ApplicationDetails.regions[0].region.displayNameEn,
+				displayNameCy: applicationDetails.ApplicationDetails.regions[0].region.displayNameCy
+			},
+			applicant: {
+				firstName: applicationDetails.serviceCustomer[0].firstName,
+				middleName: applicationDetails.serviceCustomer[0].middleName,
+				lastName: applicationDetails.serviceCustomer[0].lastName,
+				email: applicationDetails.serviceCustomer[0].email,
+				address: {
+					addressLine1: applicationDetails.serviceCustomer[0].address.addressLine1,
+					addressLine2: applicationDetails.serviceCustomer[0].address.addressLine2,
+					county: applicationDetails.serviceCustomer[0].address.county,
+					town: applicationDetails.serviceCustomer[0].address.town,
+					postcode: applicationDetails.serviceCustomer[0].address.postcode
+				},
+				website: applicationDetails.serviceCustomer[0].website,
+				phoneNumber: applicationDetails.serviceCustomer[0].phoneNumber
+			}
+		},
+		keyDates: {
+			firstNotifiedDate: applicationDetails.ApplicationDetails.firstNotifiedAt,
+			submissionDate: applicationDetails.ApplicationDetails.submissionAt
+		}
+	};
 };
-
