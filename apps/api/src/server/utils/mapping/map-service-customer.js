b2d20749809d6d4985c043325528d45c23c2f019
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-07-22 11:45:44 +0100

    feat(api/applications): refactor of get details endpoint (BOAS-164)

---
+++ apps/api/src/server/utils/mapping/map-service-customer.js
@@ -0,0 +1,25 @@
+import { pick } from 'lodash-es';
+import formatAddressLowerCase from '../address-formatter-lowercase.js';
+
+/**
+ *
+ * @param {import('@pins/api').Schema.ServiceCustomer} serviceCustomer
+ * @returns {object}
+ */
+export const mapServiceCustomer = (serviceCustomer) => {
+	const applicantInfo = pick(serviceCustomer, [
+		'firstName',
+		'middleName',
+		'lastName',
+		'email',
+		'address',
+		'website',
+		'phoneNumber'
+	]);
+	const applicantAddress = formatAddressLowerCase(serviceCustomer.address);
+
+	return {
+		...applicantInfo,
+		address: applicantAddress
+	};
+};
