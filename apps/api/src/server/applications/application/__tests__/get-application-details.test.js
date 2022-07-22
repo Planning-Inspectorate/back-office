e77da483f2421626bbe073782af75a16ebee8d6e
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-07-22 11:45:44 +0100

    feat(api/applications): refactor of get details endpoint (BOAS-164)

--- apps/api/src/server/applications/application/__tests__/get-application-details.test.js
+++ apps/api/src/server/applications/application/__tests__/get-application-details.test.js
@@ -9,7 +9,7 @@ const request = supertest(app);
 
 const application = applicationFactoryForTests({
 	id: 1,
-	createdAt: new Date(),
+	status: 'draft',
 	modifiedAt: new Date()
 });
 
@@ -29,14 +29,15 @@ test('gets all data for a case when everything is available', async (t) => {
 		id: 1,
 		title: 'EN010003 - NI Case 3 Name',
 		description: 'EN010003 - NI Case 3 Name Description',
-		geographicalInformation: {
-			applicant: {
+		status: 'draft',
+		applicant: [
+			{
 				address: {
 					addressLine1: 'Addr Line 1',
 					addressLine2: 'Addr Line 2',
 					town: 'Town',
 					county: 'County',
-					postcode: 'Postcode'
+					postCode: 'Postcode'
 				},
 				email: 'service.customer@email.com',
 				firstName: 'Service Customer First Name',
@@ -44,17 +45,40 @@ test('gets all data for a case when everything is available', async (t) => {
 				middleName: 'Service Customer Middle Name',
 				phoneNumber: '01234567890',
 				website: 'Service Customer Website'
+			}
+		],
+		geographicalInformation: {
+			gridReference: {
+				easting: 123_456,
+				northing: 987_654
 			},
 			mapZoomLevel: {
 				displayNameCy: 'Zoom Level Name Cy',
-				displayNameEn: 'Zoom Level Name En'
+				displayNameEn: 'Zoom Level Name En',
+				displayOrder: 100,
+				id: 1,
+				name: 'zoom-level'
 			},
-			regions: {
-				displayNameCy: 'Region Name Cy',
-				displayNameEn: 'Region Name En'
-			}
+			regions: [
+				{
+					id: 1,
+					name: 'region1',
+					displayNameCy: 'Region Name 1 Cy',
+					displayNameEn: 'Region Name 1 En'
+				},
+				{
+					id: 2,
+					name: 'region2',
+					displayNameCy: 'Region Name 2 Cy',
+					displayNameEn: 'Region Name 2 En'
+				}
+			],
+			locationDescription: 'Some Location'
 		},
-		keyDates: {},
+		keyDates: {
+			firstNotifiedDate: 1_658_486_313,
+			submissionDate: 1_658_486_313
+		},
 		reference: application.reference,
 		sector: {
 			abbreviation: 'BB',
