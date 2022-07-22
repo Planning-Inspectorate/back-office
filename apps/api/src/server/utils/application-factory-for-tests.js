1713f7ce62f3017457bc3095711cd41b1da19cb9
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-07-22 11:45:44 +0100

    feat(api/applications): refactor of get details endpoint (BOAS-164)

--- apps/api/src/server/utils/application-factory-for-tests.js
+++ apps/api/src/server/utils/application-factory-for-tests.js
@@ -29,6 +29,7 @@ export const applicationFactoryForTests = ({ id, status = 'open', modifiedAt = n
 				email: 'service.customer@email.com',
 				website: 'Service Customer Website',
 				phoneNumber: '01234567890',
+				organisationName: 'Organisation',
 				address: {
 					addressLine1: 'Addr Line 1',
 					addressLine2: 'Addr Line 2',
@@ -41,15 +42,31 @@ export const applicationFactoryForTests = ({ id, status = 'open', modifiedAt = n
 		ApplicationDetails: {
 			id,
 			caseId: id,
+			firstNotifiedAt: new Date(1_658_486_313_000),
+			submissionAt: new Date(1_658_486_313_000),
+			locationDescription: 'Some Location',
 			regions: [
 				{
 					region: {
-						displayNameEn: 'Region Name En',
-						displayNameCy: 'Region Name Cy'
+						id: 1,
+						name: 'region1',
+						displayNameEn: 'Region Name 1 En',
+						displayNameCy: 'Region Name 1 Cy'
+					}
+				},
+				{
+					region: {
+						id: 2,
+						name: 'region2',
+						displayNameEn: 'Region Name 2 En',
+						displayNameCy: 'Region Name 2 Cy'
 					}
 				}
 			],
 			zoomLevel: {
+				id: 1,
+				displayOrder: 100,
+				name: 'zoom-level',
 				displayNameEn: 'Zoom Level Name En',
 				displayNameCy: 'Zoom Level Name Cy'
 			},
@@ -67,6 +84,11 @@ export const applicationFactoryForTests = ({ id, status = 'open', modifiedAt = n
 					displayNameEn: 'Sector Name En',
 					displayNameCy: 'Sector Name Cy'
 				}
+			},
+			gridReference: {
+				id: 1,
+				easting: 123_456,
+				northing: 987_654
 			}
 		},
 		CaseStatus: [
