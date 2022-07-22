3a8d4549d124e1390a61a52d4321da444ad7e521
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-07-22 10:24:02 +0100

    refactor(web/applications): change fe properties name to match be (boas-167)

---
+++ apps/web/src/server/lib/body-formatter.js
@@ -0,0 +1,52 @@
+/**
+ * Create the application with name and description
+ *
+ * @param {Record<*, string>} body
+ * @returns {object}
+ */
+export const bodyToValues = (body) => {
+	/** @type {Record<*, string>} */ const values = {};
+	const bodyKeys = Object.keys(body);
+
+	for (const key of bodyKeys) {
+		values[key] = body[key];
+	}
+
+	return values;
+};
+
+/**
+ * Create the application with name and description
+ *
+ * @param {Record<*, string>} body
+ * @returns {object}
+ */
+export const bodyToPayload = (body) => {
+	/** @type {Record<*, *>} */ let payload = {};
+
+	const fieldsKeys = Object.keys(body);
+
+	for (const fieldKey of fieldsKeys) {
+		const keys = fieldKey.split('.');
+		const fieldDepth = keys.length;
+
+		switch (fieldDepth) {
+			case 2:
+				payload = { ...payload };
+				payload[keys[0]] = { ...payload[keys[0]] };
+				payload[keys[0]][keys[1]] = body[fieldKey];
+				break;
+			case 3:
+				payload = { ...payload };
+				payload[keys[0]] = { ...payload[keys[0]] };
+				payload[keys[0]][keys[1]] = { ...payload[keys[0]][keys[1]] };
+				payload[keys[0]][keys[1]][keys[2]] = body[fieldKey];
+				break;
+			default:
+				payload = { ...payload };
+				break;
+		}
+	}
+
+	return payload;
+};
