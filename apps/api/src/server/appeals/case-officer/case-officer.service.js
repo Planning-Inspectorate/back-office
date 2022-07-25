df3f24ba83aa0385dcb661adba89884cb724eeb6
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-07-25 21:12:35 +0100

    feat(api/applications): adds saving new state to database (BOAS-165)

--- apps/api/src/server/appeals/case-officer/case-officer.service.js
+++ apps/api/src/server/appeals/case-officer/case-officer.service.js
@@ -6,10 +6,21 @@ import { transitionState } from '../../utils/transition-state.js';
 
 /** @typedef {import('@pins/api').Schema.Appeal} Appeal */
 
+/**
+ *
+ * @param {object} reviewReason
+ * @returns {any}
+ */
 const reviewComplete = (reviewReason) => {
 	return Object.keys(reviewReason).every((index) => !reviewReason[index]);
 };
 
+/**
+ *
+ * @param {object} reviewReason
+ * @param {number} appealId
+ * @returns {Promise<void>}
+ */
 export const confirmLPAQuestionnaireService = async (reviewReason, appealId) => {
 	const reviewResult = reviewComplete(reviewReason);
 	const appeal = await appealRepository.getById(appealId);
@@ -18,13 +29,13 @@ export const confirmLPAQuestionnaireService = async (reviewReason, appealId) => 
 
 	const appealStatemachineStatus = reviewResult ? 'COMPLETE' : 'INCOMPLETE';
 	const appealStatus = buildAppealCompundStatus(appeal.appealStatus);
-	const nextState = transitionState({
+	const nextStatus = transitionState({
 		caseType: appeal.appealType.type,
 		context: { appealId: appeal.id },
 		status: appealStatus,
 		machineAction: appealStatemachineStatus
 	});
-	const newState = breakUpCompoundStatus(nextState.value, appeal.id);
+	const newState = breakUpCompoundStatus(nextStatus.value, appeal.id);
 
 	await appealRepository.updateStatusById(appeal.id, newState, appeal.appealStatus);
 };
