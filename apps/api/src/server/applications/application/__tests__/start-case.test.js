66727ad5dbec3112fade141c22b48dba4fcb9db1
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-07-25 21:12:35 +0100

    feat(api/applications): adds saving new state to database (BOAS-165)

--- apps/api/src/server/applications/application/__tests__/start-case.test.js
+++ apps/api/src/server/applications/application/__tests__/start-case.test.js
@@ -1,3 +1,4 @@
+import Prisma from '@prisma/client';
 import test from 'ava';
 import sinon from 'sinon';
 import supertest from 'supertest';
@@ -8,16 +9,20 @@ const request = supertest(app);
 
 const findUniqueStub = sinon.stub();
 
-findUniqueStub.withArgs({ where: { id: 1 } }).returns({ id: 1 });
+findUniqueStub.withArgs({ where: { id: 1 } }).returns({ id: 1, CaseStatus: [{ status: 'draft' }] });
 findUniqueStub.withArgs({ where: { id: 2 } }).returns(null);
+
+const updateStub = sinon.stub();
 
 test.before('set up mocks', () => {
 	sinon.stub(databaseConnector, 'case').get(() => {
-		return { findUnique: findUniqueStub };
+		return { findUnique: findUniqueStub, update: updateStub };
 	});
+
+	sinon.stub(Prisma.PrismaClient.prototype, '$transaction');
 });
 
-test('starts application if all needed information is present', async (t) => {
+test.only('starts application if all needed information is present', async (t) => {
 	const response = await request.post('/applications/1/start');
 
 	t.is(response.status, 200);
@@ -38,3 +43,7 @@ test('throws an error if the application id is not recognised', async (t) => {
 		}
 	});
 });
+
+test.todo('throws an error if the application does not have all the required information to start');
+
+test.todo('throws an error if the application is not in draft state');
