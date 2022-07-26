6039d1cabe1fe9d36845922922b228f51eef8b86
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-07-26 13:01:15 +0100

    feat(web/applications): continue integration with patch/post api (boas-167)

--- apps/web/testing/applications/fixtures/applications.js
+++ apps/web/testing/applications/fixtures/applications.js
@@ -1,14 +1,23 @@
-import { createApplication } from '../factory/application.js';
-import { fixtureSectors } from './options-item.js';
+import {createApplication} from '../factory/application.js';
+import {fixtureSectors} from './options-item.js';
 
 export const fixtureApplications = [
-	createApplication({
-		id: 1,
-		modifiedDate: `${new Date(2022, 0, 1).getTime() / 1000}`,
-		reference: 'APPLICATION/01',
-		sector: fixtureSectors[0],
-		subSector: fixtureSectors[1]
-	}),
+	{
+		...createApplication({
+			id: 1,
+			modifiedDate: `${new Date(2022, 0, 1).getTime() / 1000}`,
+			reference: 'APPLICATION/01',
+			sector: fixtureSectors[0],
+			subSector: fixtureSectors[1]
+		}),
+		geographicalInformation: {
+			locationDescription: 'London',
+			gridReference: {
+				easting: '123456',
+				northing: '987654'
+			}
+		}
+	},
 	createApplication({
 		id: 2,
 		modifiedDate: `${new Date(2022, 0, 31).getTime() / 1000}`,
@@ -22,5 +31,18 @@ export const fixtureApplications = [
 		reference: 'APPLICATION/03',
 		sector: fixtureSectors[2],
 		subSector: fixtureSectors[3]
-	})
+	}),
+	{
+		id: 4,
+		reference: 'APPLICATION/04',
+		title: `Application with no sector`,
+		status: 'draft'
+	},
+	{
+		id: 5,
+		reference: 'APPLICATION/05',
+		title: `Application with no subsector`,
+		sector: fixtureSectors[0],
+		status: 'draft'
+	},
 ];
