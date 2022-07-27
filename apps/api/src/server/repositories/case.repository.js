6ecbc0c7fb1fd8b285cdc16ae1a6515463476d7f
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-07-27 17:45:40 +0100

    chore(web/applications): clean code from temporary backend changes (167)

--- apps/api/src/server/repositories/case.repository.js
+++ apps/api/src/server/repositories/case.repository.js
@@ -33,16 +33,14 @@ export const getAll = () => {
 						include: {
 							sector: true
 						}
-					},
-					zoomLevel: true
+					}
 				}
 			},
 			CaseStatus: {
 				where: {
 					valid: true
 				}
-			},
-			gridReference: true,
+			}
 		}
 	});
 };
