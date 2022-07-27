17485b7cadf197bc71e728809b31dc1c34fd7104
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-07-27 19:16:35 +0100

    test(api): fixes tests which test error handling (BOAS-165)

--- apps/document-storage/src/server/app/__tests__/get-all-documents.test.js
+++ apps/document-storage/src/server/app/__tests__/get-all-documents.test.js
@@ -10,12 +10,15 @@ const blobServiceClientFromConnectionString = {
 	getContainerClient: sinon.stub().returns({
 		listBlobFlatSegment: sinon.stub().returns({
 			segment: {
-				blobItems: [{
-					metadata: { fileType: 'test' },
-					name: '036075328008901675-simple.pdf',
-				}, {
-					name: '35481621312046846-simple.pdf',
-				}]
+				blobItems: [
+					{
+						metadata: { fileType: 'test' },
+						name: '036075328008901675-simple.pdf'
+					},
+					{
+						name: '35481621312046846-simple.pdf'
+					}
+				]
 			}
 		})
 	})
@@ -29,54 +32,65 @@ test.afterEach.always(() => {
 	}
 });
 
-test.serial('gets all files associated with appeal id', async(t) => {
-	sinon.stub(BlobServiceClient, 'fromConnectionString').returns(blobServiceClientFromConnectionString);
+test.serial('gets all files associated with appeal id', async (t) => {
+	sinon
+		.stub(BlobServiceClient, 'fromConnectionString')
+		.returns(blobServiceClientFromConnectionString);
 
 	const resp = await request.get('/').query({ type: 'appeal', id: 1 });
 
 	t.is(resp.status, 200);
-	t.deepEqual(resp.body, [{
-		name: '036075328008901675-simple.pdf',
-		metadata: { fileType: 'test' }
-	}, {
-		name: '35481621312046846-simple.pdf'
-	}]);
+	t.deepEqual(resp.body, [
+		{
+			name: '036075328008901675-simple.pdf',
+			metadata: { fileType: 'test' }
+		},
+		{
+			name: '35481621312046846-simple.pdf'
+		}
+	]);
 });
 
-test.serial('returns error if error thrown', async(t) => {
+test.serial('returns error if error thrown', async (t) => {
 	sinon.stub(BlobServiceClient, 'fromConnectionString').throws();
 
 	const resp = await request.get('/').query({ type: 'appeal', id: 1 });
 
 	t.is(resp.status, 500);
-	t.deepEqual(resp.body, { error: 'Oops! Something went wrong' });
+	t.deepEqual(resp.body, { errors: 'Oops! Something went wrong' });
 });
 
-test.serial('throws error if no type or id provided', async(t) => {
+test.serial('throws error if no type or id provided', async (t) => {
 	const resp = await request.get('/');
 
 	t.is(resp.status, 400);
-	t.deepEqual(resp.body, { errors: { 
-		type: 'Select a valid type',
-		id: 'Provide appeal/application id'
-	} }); 
+	t.deepEqual(resp.body, {
+		errors: {
+			type: 'Select a valid type',
+			id: 'Provide appeal/application id'
+		}
+	});
 });
 
-test.serial('throws error if unfamiliar type provided', async(t) => {
+test.serial('throws error if unfamiliar type provided', async (t) => {
 	const resp = await request.get('/').query({ type: 'test' });
 
 	t.is(resp.status, 400);
-	t.deepEqual(resp.body, { errors: { 
-		type: 'Select a valid type',
-		id: 'Provide appeal/application id'
-	} }); 
+	t.deepEqual(resp.body, {
+		errors: {
+			type: 'Select a valid type',
+			id: 'Provide appeal/application id'
+		}
+	});
 });
 
-test.serial('throws error if non numeric id provided', async(t) => {
+test.serial('throws error if non numeric id provided', async (t) => {
 	const resp = await request.get('/').query({ type: 'application', id: 'test' });
 
 	t.is(resp.status, 400);
-	t.deepEqual(resp.body, { errors: { 
-		id: 'Provide appeal/application id'
-	} }); 
+	t.deepEqual(resp.body, {
+		errors: {
+			id: 'Provide appeal/application id'
+		}
+	});
 });
