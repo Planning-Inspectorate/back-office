ad008eecc1b1a9fe38eb03b6b5cbbd97cadf5085
Francesco Brenci <francesco.brenci@kinandcarta.com>
2022-07-29 11:12:27 +0100

    fix(web/applications): fixed modified date format in dashboards (BOAS-237)

--- apps/web/testing/applications/fixtures/applications.js
+++ apps/web/testing/applications/fixtures/applications.js
@@ -4,21 +4,21 @@ import { fixtureSectors } from './options-item.js';
 export const fixtureApplications = [
 	createApplication({
 		id: 1,
-		modifiedDate: new Date(2022, 0, 1).toISOString(),
+		modifiedDate: `${new Date(2022, 0, 1).getTime() / 1000}`,
 		reference: 'APPLICATION/01',
 		sector: fixtureSectors[0],
 		subSector: fixtureSectors[1]
 	}),
 	createApplication({
 		id: 2,
-		modifiedDate: new Date(2022, 0, 31).toISOString(),
+		modifiedDate: `${new Date(2022, 0, 31).getTime() / 1000}`,
 		reference: 'APPLICATION/02',
 		sector: fixtureSectors[0],
 		subSector: fixtureSectors[1]
 	}),
 	createApplication({
 		id: 3,
-		modifiedDate: new Date(2022, 0, 1).toISOString(),
+		modifiedDate: `${new Date(2022, 0, 1).getTime() / 1000}`,
 		reference: 'APPLICATION/03',
 		sector: fixtureSectors[2],
 		subSector: fixtureSectors[3]
