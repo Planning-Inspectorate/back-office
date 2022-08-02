7fec70ae672ea623a4f9219cc9da156917a8faea
David White <50454208+DavidJWhiteDev@users.noreply.github.com>
2022-08-02 14:28:46 +0100

    refactor(web/applications): group post and get tests 4 website, email (BOAS-154)

--- apps/web/src/server/applications/create/applicant/__tests__/applications-create-applicant.test.js
+++ apps/web/src/server/applications/create/applicant/__tests__/applications-create-applicant.test.js
@@ -77,96 +77,96 @@ describe('applications create applicant', () => {
 		});
 	});
 
-	describe('GET /applicant-website', () => {
+	describe('Applicant website', () => {
 		const baseUrl = '/applications-service/create-new-case/123/applicant-website';
 
-		it('should render the page if there is data in the session', async () => {
-			await request
-				.post('/applications-service/create-new-case/123/applicant-information-types')
-				.send({
-					selectedApplicantInfoTypes: ['applicant-website']
-				});
+		describe('GET /applicant-website', () => {
+			it('should render the page if there is data in the session', async () => {
+				await request
+					.post('/applications-service/create-new-case/123/applicant-information-types')
+					.send({
+						selectedApplicantInfoTypes: ['applicant-website']
+					});
 
-			const response = await request.get(baseUrl);
-			const element = parseHtml(response.text);
-
-			expect(element.innerHTML).toMatchSnapshot();
-			expect(element.innerHTML).toContain('Save and continue');
-		});
-
-		it('should show not render the page if there is no session data', async () => {
-			const response = await request.get(baseUrl);
-
-			expect(response?.headers?.location).toMatch(
-				'/applications-service/create-new-case/123/key-dates'
-			);
-		});
-	});
-
-	describe('POST /application-website', () => {
-		const baseUrl = '/applications-service/create-new-case/123/applicant-website';
-
-		beforeEach(async () => {
-			await request.get('/applications-service/case-officer');
-		});
-
-		describe('Web-side validation:', () => {
-			it('should show validation errors if an invalid URL is input', async () => {
-				const response = await request.post(baseUrl).send({
-					'applicant.website': 'bad_url'
-				});
+				const response = await request.get(baseUrl);
 				const element = parseHtml(response.text);
 
 				expect(element.innerHTML).toMatchSnapshot();
-				expect(element.innerHTML).toContain('error-summary-title');
-				expect(element.innerHTML).toContain('applicant.website-error');
+				expect(element.innerHTML).toContain('Save and continue');
+			});
+
+			it('should show not render the page if there is no session data', async () => {
+				const response = await request.get(baseUrl);
+
+				expect(response?.headers?.location).toMatch(
+					'/applications-service/create-new-case/123/key-dates'
+				);
+			});
+		});
+
+		describe('POST /application-website', () => {
+			beforeEach(async () => {
+				await request.get('/applications-service/case-officer');
+			});
+
+			describe('Web-side validation:', () => {
+				it('should show validation errors if an invalid URL is input', async () => {
+					const response = await request.post(baseUrl).send({
+						'applicant.website': 'bad_url'
+					});
+					const element = parseHtml(response.text);
+
+					expect(element.innerHTML).toMatchSnapshot();
+					expect(element.innerHTML).toContain('error-summary-title');
+					expect(element.innerHTML).toContain('applicant.website-error');
+				});
 			});
 		});
 	});
 
-	describe('GET /applicant-email', () => {
+	describe('Applicant email', () => {
 		const baseUrl = '/applications-service/create-new-case/123/applicant-email';
 
-		it('should render the page if there is data in the session', async () => {
-			await request
-				.post('/applications-service/create-new-case/123/applicant-information-types')
-				.send({
-					selectedApplicantInfoTypes: ['applicant-email']
-				});
+		describe('GET /applicant-email', () => {
+			it('should render the page if there is data in the session', async () => {
+				await request
+					.post('/applications-service/create-new-case/123/applicant-information-types')
+					.send({
+						selectedApplicantInfoTypes: ['applicant-email']
+					});
 
-			const response = await request.get(baseUrl);
-			const element = parseHtml(response.text);
-
-			expect(element.innerHTML).toMatchSnapshot();
-			expect(element.innerHTML).toContain('Save and continue');
-		});
-
-		it('should show not render the page if there is no session data', async () => {
-			const response = await request.get(baseUrl);
-
-			expect(response?.headers?.location).toMatch(
-				'/applications-service/create-new-case/123/key-dates'
-			);
-		});
-	});
-
-	describe('POST /application-email', () => {
-		const baseUrl = '/applications-service/create-new-case/123/applicant-email';
-
-		beforeEach(async () => {
-			await request.get('/applications-service/case-officer');
-		});
-
-		describe('Web-side validation:', () => {
-			it('should show validation errors if an invalid email is input', async () => {
-				const response = await request.post(baseUrl).send({
-					'applicant.email': 'bad_email_address'
-				});
+				const response = await request.get(baseUrl);
 				const element = parseHtml(response.text);
 
 				expect(element.innerHTML).toMatchSnapshot();
-				expect(element.innerHTML).toContain('error-summary-title');
-				expect(element.innerHTML).toContain('applicant.email-error');
+				expect(element.innerHTML).toContain('Save and continue');
+			});
+
+			it('should show not render the page if there is no session data', async () => {
+				const response = await request.get(baseUrl);
+
+				expect(response?.headers?.location).toMatch(
+					'/applications-service/create-new-case/123/key-dates'
+				);
+			});
+		});
+
+		describe('POST /application-email', () => {
+			beforeEach(async () => {
+				await request.get('/applications-service/case-officer');
+			});
+
+			describe('Web-side validation:', () => {
+				it('should show validation errors if an invalid email is input', async () => {
+					const response = await request.post(baseUrl).send({
+						'applicant.email': 'bad_email_address'
+					});
+					const element = parseHtml(response.text);
+
+					expect(element.innerHTML).toMatchSnapshot();
+					expect(element.innerHTML).toContain('error-summary-title');
+					expect(element.innerHTML).toContain('applicant.email-error');
+				});
 			});
 		});
 	});
