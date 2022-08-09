7de18eba90e020691c2344b1519273c93ef6bed0
Xfarya <farya243@gmail.com>
2022-08-09 14:26:26 +0100

    docs(api/applications): added response to swagger (BOAS-164)

--- apps/api/src/server/utils/address-formatter-lowercase.js
+++ apps/api/src/server/utils/address-formatter-lowercase.js
@@ -5,11 +5,11 @@
  */
 const formatAddressLowerCase = (address) => {
 	return {
-		...(address.addressLine1 && { addressLine1: address.addressLine1 }),
-		...(address.addressLine2 && { addressLine2: address.addressLine2 }),
-		...(address.town && { town: address.town }),
-		...(address.county && { county: address.county }),
-		postCode: address.postcode
+		...(address?.addressLine1 && { addressLine1: address.addressLine1 }),
+		...(address?.addressLine2 && { addressLine2: address.addressLine2 }),
+		...(address?.town && { town: address.town }),
+		...(address?.county && { county: address.county }),
+		postCode: address?.postcode
 	};
 };
 
