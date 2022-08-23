01893593acd14b1a253e5bb56c0f0dbf2818b103
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-08-23 14:18:29 +0100

    feat(web): saves applicant org name and address (BOAS-244)

--- apps/web/src/server/applications/create/applicant/applications-create-applicant.validators.js
+++ apps/web/src/server/applications/create/applicant/applications-create-applicant.validators.js
@@ -41,15 +41,7 @@ export const validateApplicationsCreateApplicantTelephoneNumber = createValidato
 );
 
 export const validateApplicationsCreateApplicantPostCode = createValidator(
-	body('postcode')
-		.custom((value) =>
-			value
-				.replace(/\s*/g, '')
-				.match(
-					/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z]\d{1,2})|(([A-Za-z][A-HJ-Ya-hj-y]\d{1,2})|(([A-Za-z]\d[A-Za-z])|([A-Za-z][A-HJ-Ya-hj-y]\d[A-Za-z]?))))\s?\d[A-Za-z]{2})$/
-				)
-		)
-		.withMessage('Enter a valid postcode'),
+	body('postcode').isPostalCode('GB').withMessage('Enter a valid postcode'),
 	body('apiReference').trim().not().equals('-1')
 .withMessage('Choose an address from the list')
 );
