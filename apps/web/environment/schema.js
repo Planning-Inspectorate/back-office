b9e1ca907b2eaa9ca68fbf9b9919472e6b963a93
Francesco Brenci <francesco.brenci@kinandcarta.com>
2022-08-24 15:22:42 +0100

    feat(web): added local environment (BOAS-275)

--- apps/web/environment/schema.js
+++ apps/web/environment/schema.js
@@ -2,37 +2,32 @@ import joi from 'joi';
 
 const logLevel = ['fatal', 'error', 'warn', 'info', 'debug', 'trace', 'silent'];
 
-export default joi
-	.object({
-		apiUrl: joi.string().uri(),
-		authDisabled: joi.boolean().optional(),
-		env: joi.string().valid('development', 'production', 'test'),
-		isRelease: joi.boolean().optional(),
-		logLevelFile: joi.string().valid(...logLevel),
-		logLevelStdOut: joi.string().valid(...logLevel),
-		msal: joi
-			.object({
-				clientId: joi.string(),
-				clientSecret: joi.string(),
-				tenantId: joi.string()
-			}),
-		serverProtocol: joi.string().valid('http', 'https'),
-		serverPort: joi.number(),
-		sslCertificateFile: joi.string(),
-		sslCertificateKeyFile: joi.string(),
-		referenceData: joi
-			.object({
-				appeals: joi
-					.object({
-						caseOfficerGroupId: joi.string(),
-						inspectorGroupId: joi.string(),
-						validationOfficerGroupId: joi.string()
-					}),
-				applications: joi
-					.object({
-						caseAdminOfficerGroupId: joi.string(),
-						caseOfficerGroupId: joi.string(),
-						inspectorGroupId: joi.string()
-					})
-			})
-	});
+export default joi.object({
+	apiUrl: joi.string().uri(),
+	authDisabled: joi.boolean().optional(),
+	env: joi.string().valid('development', 'production', 'test', 'local'),
+	isRelease: joi.boolean().optional(),
+	logLevelFile: joi.string().valid(...logLevel),
+	logLevelStdOut: joi.string().valid(...logLevel),
+	msal: joi.object({
+		clientId: joi.string(),
+		clientSecret: joi.string(),
+		tenantId: joi.string()
+	}),
+	serverProtocol: joi.string().valid('http', 'https'),
+	serverPort: joi.number(),
+	sslCertificateFile: joi.string(),
+	sslCertificateKeyFile: joi.string(),
+	referenceData: joi.object({
+		appeals: joi.object({
+			caseOfficerGroupId: joi.string(),
+			inspectorGroupId: joi.string(),
+			validationOfficerGroupId: joi.string()
+		}),
+		applications: joi.object({
+			caseAdminOfficerGroupId: joi.string(),
+			caseOfficerGroupId: joi.string(),
+			inspectorGroupId: joi.string()
+		})
+	})
+});
