2c1126960821f145eac90044b4e68dede30b5694
David White <50454208+DavidJWhiteDev@users.noreply.github.com>
2022-08-30 14:57:09 +0100

    feat(web/applications): check your answers page - field list (WIP) (BOAS-157)

--- apps/web/src/server/applications/create/check-your-answers/applications-create-check-your-answers.types.d.ts
+++ apps/web/src/server/applications/create/check-your-answers/applications-create-check-your-answers.types.d.ts
@@ -5,6 +5,29 @@ export type ApplicationsCreateConfirmationProps = {
 
 export type ApplicationsCreateCheckYourAnswersProps = {
 	values: {
+		'case.title': string;
+		'case.description': string;
+		'case.sector'?: string;
+		'case.subSector'?: string;
+		'case.location'?: string;
+		'case.easting'?: string;
+		'case.northing'?: string;
+		'case.regions'?: string;
+		'case.zoomLevel'?: string;
+		'case.teamEmail'?: string;
+
+		'applicant.organisationName'?: string;
+		'applicant.firstName'?: string;
+		'applicant.middleName'?: string;
+		'applicant.lastName'?: string;
+		'applicant.address.addressLine1'?: string;
+		'applicant.address.addressLine2'?: string;
+		'applicant.address.town'?: string;
+		'applicant.address.postcode'?: string;
+		'applicant.website'?: string;
+		'applicant.email'?: string;
+		'applicant.phoneNumber'?: string;
+
 		'keyDates.submissionDatePublished': string;
 		'keyDates.submissionDateInternal': string;
 	};
