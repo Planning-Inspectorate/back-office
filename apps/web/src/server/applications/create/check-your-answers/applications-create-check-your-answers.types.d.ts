9bc623daa2000cced1638e1de4dda44aa4315f91
David White <50454208+DavidJWhiteDev@users.noreply.github.com>
2022-08-30 12:01:24 +0100

    feat(web/applications): create case check your answers page (WIP) (BOAS-157)

--- apps/web/src/server/applications/create/check-your-answers/applications-create-check-your-answers.types.d.ts
+++ apps/web/src/server/applications/create/check-your-answers/applications-create-check-your-answers.types.d.ts
@@ -1,3 +1,19 @@
+import { ValidationErrors } from '@pins/express';
 export type ApplicationsCreateConfirmationProps = {
 	values: { reference: string };
+}
+
+export type ApplicationsCreateCheckYourAnswersProps = {
+	values: {
+		'keyDates.submissionDatePublished': string;
+		'keyDates.submissionDateInternal': string;
+	};
+	errors?: ValidationErrors;
+};
+
+export type ApplicationsCreateCheckYourAnswersBody = {
+	submissionDatePublished: string;
+	submissionInternalDay: string;
+	submissionInternalMonth: string;
+	submissionInternalYear: string;
 };
