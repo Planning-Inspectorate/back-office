122724da97f0c781b7abd3695574fec93cd7f2a4
David White <50454208+DavidJWhiteDev@users.noreply.github.com>
2022-09-14 10:16:20 +0100

    refactor(web/applications): applicant data - filtered get data calls (BOAS-354)

--- apps/web/src/server/applications/create/applicant/applications-create-applicant.service.js
+++ apps/web/src/server/applications/create/applicant/applications-create-applicant.service.js
@@ -83,3 +83,22 @@ export async function getNthApplicant(applicationId, applicantNumber) {
 
 	return applicants && applicants.length > 0 ? applicants[applicantNumber] : null;
 }
+
+/**
+ * Returns the applicant matching id of a draft application
+ *
+ * @param {number} applicationId
+ * @param {number} applicantId
+ * @param {null | string} query
+ * @returns {Promise<Applicant|null>}
+ */
+export async function getApplicantById(applicationId, applicantId, query = null) {
+	const { applicants } = await getApplicationDraft(applicationId, query);
+
+	let applicant;
+
+	if (applicants && applicants.length > 0) {
+		applicant = applicants.find((apps) => apps.id === applicantId);
+	}
+	return applicant || null;
+}
