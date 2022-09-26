83f5836a3b82c0539de3ed6d1c11c6beb02760b7
David White <50454208+DavidJWhiteDev@users.noreply.github.com>
2022-09-26 15:27:44 +0100

    fix(web/applications): case summary - layout and overview page WIP (BOAS-312)

---
+++ apps/web/src/server/applications/summary/applications-summary.router.js
@@ -0,0 +1,15 @@
+import { Router as createRouter } from 'express';
+import * as locals from '../applications.locals.js';
+import * as controller from './applications-summary.controller.js';
+
+const applicationSummaryRouter = createRouter();
+
+applicationSummaryRouter.param('applicationId', locals.loadApplication);
+
+applicationSummaryRouter.route('/:applicationId').get(controller.viewApplicationSummary);
+
+applicationSummaryRouter
+	.route('/:applicationId/project-information')
+	.get(controller.viewApplicationProjectInformation);
+
+export default applicationSummaryRouter;
