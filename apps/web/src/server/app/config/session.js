0405d2cca3e3a9f18964232d4535e2846e4eddf6
Francesco Brenci <francesco.brenci@kinandcarta.com>
2022-10-10 12:28:33 +0100

    chore(web/applications): stored env var in config (BOAS-396)

--- apps/web/src/server/app/config/session.js
+++ apps/web/src/server/app/config/session.js
@@ -1,16 +1,11 @@
 import config from '@pins/web/environment/config.js';
 import session from 'express-session';
-import { env } from 'node:process';
 
 export const store = new session.MemoryStore();
 
-/**
- * @param {string} sessionSecret
- */
-const sessionSecret = env.SESSION_SECRET || '';
-
 export default session({
-	secret: sessionSecret,
+	// @ts-ignore – sessionSecret can no be undefined
+	secret: config.sessionSecret,
 	resave: false,
 	saveUninitialized: false,
 	store,
