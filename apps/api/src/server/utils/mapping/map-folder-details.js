8699342fe4f8a47f35ccf6cb50eacdee2bef6fc3
Xfarya <farya243@gmail.com>
2022-10-19 12:45:43 +0100

    feat(api/applications): mapped necessary details (BOAS-458)

---
+++ apps/api/src/server/utils/mapping/map-folder-details.js
@@ -0,0 +1,12 @@
+import { pick } from 'lodash-es';
+
+/**
+ *
+ * @param { any } folderDetails
+ * @returns {{id: number, displayNameEn: string, displayOrder: number}}
+ */
+export const mapFolderDetails = (folderDetails) => {
+	return folderDetails.map((/** @type {any} */ folder) =>
+		pick(folder, ['id', 'displayNameEn', 'displayOrder'])
+	);
+};
