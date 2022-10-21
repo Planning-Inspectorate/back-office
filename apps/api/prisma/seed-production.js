c13cf328d40858f47cf20bfa1aea7b51cf016e2c
David White <50454208+DavidJWhiteDev@users.noreply.github.com>
2022-10-21 13:31:44 +0100

    feat(api/applications): create seed db prod pipeline (BOAS-395)

---
+++ apps/api/prisma/seed-production.js
@@ -0,0 +1,45 @@
+import config from '../src/server/config/config.js';
+import { databaseConnector } from '../src/server/utils/database-connector.js';
+import logger from '../src/server/utils/logger.js';
+import { regions, sectors, subSectors, zoomLevels } from './seed-samples.js';
+
+// create reference data only, no test data, as delivering to production DB
+const productionMain = async () => {
+	try {
+		for (const sector of sectors) {
+			await databaseConnector.sector.upsert({
+				create: sector,
+				where: { name: sector.name },
+				update: {}
+			});
+		}
+		for (const { subSector, sectorName } of subSectors) {
+			await databaseConnector.subSector.upsert({
+				create: { ...subSector, sector: { connect: { name: sectorName } } },
+				update: {},
+				where: { name: subSector.name }
+			});
+		}
+		for (const region of regions) {
+			await databaseConnector.region.upsert({
+				create: region,
+				where: { name: region.name },
+				update: {}
+			});
+		}
+		for (const zoomLevel of zoomLevels) {
+			await databaseConnector.zoomLevel.upsert({
+				create: zoomLevel,
+				where: { name: zoomLevel.name },
+				update: {}
+			});
+		}
+	} catch (error) {
+		logger.error(error);
+		throw error;
+	} finally {
+		await databaseConnector.$disconnect();
+	}
+};
+
+await (config.NODE_ENV === 'production' ? productionMain() : null);
