99d0d756a2cdcf2af6f24715dc333b0a0370ca96
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-11-02 11:26:35 +0000

    feat(web/applications): create mime types filter

--- apps/web/src/client/components/file-uploader/file-uploader.module.js
+++ apps/web/src/client/components/file-uploader/file-uploader.module.js
@@ -1,10 +1,12 @@
 import registerUIEvents from './_ui.js';
+import registerValidation from './_validation.js';
 
 const allUploadForms = document.querySelectorAll('.pins-file-upload');
 
 const initFileUploaderModule = () => {
 	if (allUploadForms.length === 0) return;
 	registerUIEvents(allUploadForms[0]);
+	registerValidation(allUploadForms[0]);
 };
 
 export default initFileUploaderModule;
