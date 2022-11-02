ae6cd2c396b3762f1fee73f8043f3ac95f8901f8
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-11-02 11:29:48 +0000

    refactor(web/applications): move uploadform check to init function

--- apps/web/src/client/components/file-uploader/file-uploader.module.js
+++ apps/web/src/client/components/file-uploader/file-uploader.module.js
@@ -3,6 +3,7 @@ import registerUIEvents from './_ui.js';
 const allUploadForms = document.querySelectorAll('.pins-file-upload');
 
 const initFileUploaderModule = () => {
+	if (allUploadForms.length === 0) return;
 	registerUIEvents(allUploadForms[0]);
 };
 
