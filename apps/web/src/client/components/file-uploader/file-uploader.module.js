b3647941ea35d6d4f7d032c0fb6c06c73faac9fb
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-11-02 16:34:55 +0000

    feat(web/applications): handle submit flow

--- apps/web/src/client/components/file-uploader/file-uploader.module.js
+++ apps/web/src/client/components/file-uploader/file-uploader.module.js
@@ -1,12 +1,25 @@
-import registerUIEvents from './_ui.js';
-import registerValidation from './_validation.js';
+import registerEvents from './_registrations.js';
 
 const allUploadForms = document.querySelectorAll('.pins-file-upload');
 
 const initFileUploaderModule = () => {
 	if (allUploadForms.length === 0) return;
-	registerUIEvents(allUploadForms[0]);
-	registerValidation(allUploadForms[0]);
+
+	/**
+		@param {Event} clickEvent
+	 */
+	const onSubmit = async (clickEvent) => {
+		clickEvent.preventDefault();
+		try {
+			// await preValidation();
+			// await preBlobStorage();
+			// await blobStorage()
+		} catch (error) {
+			return error;
+		}
+	};
+
+	registerEvents(allUploadForms[0], onSubmit, () => {});
 };
 
 export default initFileUploaderModule;
