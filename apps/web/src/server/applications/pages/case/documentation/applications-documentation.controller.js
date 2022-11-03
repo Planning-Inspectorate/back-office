cd39041545157c0020062eda9812d8915229477b
David White <50454208+DavidJWhiteDev@users.noreply.github.com>
2022-11-03 11:37:33 +0000

    feat(web/applications): sub folder documents pages (BOAS-508)

--- apps/web/src/server/applications/pages/case/documentation/applications-documentation.controller.js
+++ apps/web/src/server/applications/pages/case/documentation/applications-documentation.controller.js
@@ -1,5 +1,9 @@
 import { sortBy } from 'lodash-es';
-import { getCaseDocumentationCategories } from '../../../lib/services/case.service.js';
+import {
+	getCaseDocumentationCategories,
+	getCaseDocumentationFilesInFolder,
+	getCaseDocumentationFolderTree
+} from '../../../lib/services/case.service.js';
 
 /** @typedef {import('../../../applications.types').DocumentationCategory} DocumentationCategory */
 
@@ -11,8 +15,44 @@ import { getCaseDocumentationCategories } from '../../../lib/services/case.servi
 export async function viewApplicationsCaseDocumentationCategories(request, response) {
 	const { caseId } = response.locals;
 	const documentationCategories = await getCaseDocumentationCategories(caseId);
-
 	const properties = { documentationCategories: sortBy(documentationCategories, ['displayOrder']) };
 
 	response.render(`applications/case/project-documentation`, properties);
+}
+
+/**
+ * View a folder, showing files in the folder, and listing subfolders
+ *
+ * @param {*} request
+ * @param {*} response
+ */
+export async function viewApplicationsCaseDocumentationFolder(request, response) {
+	const { caseId } = response.locals;
+	const { folderId } = request.params;
+	const topLevelDocumentationCategories = await getCaseDocumentationCategories(caseId);
+	const currentFolderProperties = {
+		topDocumentationCategories: sortBy(topLevelDocumentationCategories, ['displayOrder'])
+	};
+	const currentFileCategory = currentFolderProperties.topDocumentationCategories.find(
+		(cat) => cat.id === Number.parseInt(folderId, 10)
+	);
+
+	// TODO: get all the sub folders in this folder
+
+	/** @type {import('../../../applications.types').DocumentationPageProps }  */
+	const properties = {
+		caseId,
+		folderId,
+		folderName: currentFileCategory?.displayNameEn,
+		folderTree: null,
+		documentationFiles: null
+	};
+
+	// get the folderTree for breadcrumbing
+	properties.folderTree = getCaseDocumentationFolderTree(caseId, currentFileCategory);
+
+	// and get all the files in this folder
+	properties.documentationFiles = getCaseDocumentationFilesInFolder(caseId, currentFileCategory);
+
+	response.render(`applications/case-documentation/project-documentation-folder`, properties);
 }
