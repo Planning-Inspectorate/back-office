1d6333799372847ac265b276239d2d97283afc03
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-11-07 09:18:26 +0000

    feat(web/applications): set new error trigger on choose file action

--- apps/web/src/server/applications/pages/case/documentation/applications-documentation.router.js
+++ apps/web/src/server/applications/pages/case/documentation/applications-documentation.router.js
@@ -11,7 +11,12 @@ applicationsDocumentationRouter
 	.get(controller.viewApplicationsCaseDocumentationCategories);
 
 applicationsDocumentationRouter
-	.route('/:categoryId/:folders/upload')
+	.route('/:folderId/:folders/upload')
 	.get(controller.viewApplicationsCaseDocumentationUpload);
+
+// TODO: this should be moved to a generic route valid for both appeals and applications
+applicationsDocumentationRouter
+	.route('/:folderId/upload')
+	.post(controller.postApplicationsCaseDocumentationUpload);
 
 export default applicationsDocumentationRouter;
