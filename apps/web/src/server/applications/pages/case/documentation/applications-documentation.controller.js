0206d75e4910c9a4959ab086f1793cd4ea1bc8f3
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-11-08 12:31:50 +0000

    feat(web/applications): create interaction with the api endpoint for documents upload

--- apps/web/src/server/applications/pages/case/documentation/applications-documentation.controller.js
+++ apps/web/src/server/applications/pages/case/documentation/applications-documentation.controller.js
@@ -2,6 +2,7 @@ import { sortBy } from 'lodash-es';
 import { getCaseDocumentationCategories } from '../../../lib/services/case.service.js';
 
 /** @typedef {import('../../../applications.types').DocumentationCategory} DocumentationCategory */
+/** @typedef {import('./applications-documentation.types').CaseDocumentationUploadProps} CaseDocumentationUploadProps */
 
 /**
  * View the documentation for a single case
@@ -17,11 +18,10 @@ export async function viewApplicationsCaseDocumentationCategories(request, respo
 	response.render(`applications/case/project-documentation`, properties);
 }
 
-// TODO: define propTypes
 /**
  * View the documentation upload page
  *
- * @type {import('@pins/express').RenderHandler<{currentFolder: string, nextPageUrl: string, folderId: string, caseId: string}, {}>}
+ * @type {import('@pins/express').RenderHandler<CaseDocumentationUploadProps, {}>}
  */
 export async function viewApplicationsCaseDocumentationUpload(request, response) {
 	const { folderId, caseId } = request.params;
@@ -31,22 +31,5 @@ export async function viewApplicationsCaseDocumentationUpload(request, response)
 	const currentFolder = 'Subfolder Placeholder';
 	const properties = { currentFolder, folderId, caseId, nextPageUrl };
 
-	response.render(`applications/documentation/upload`, properties);
-}
-
-// TODO: fix the properties types
-// TODO: this should be moved to a generic component valid for both appeals and applications
-/**
- * Get info for the blob storage upload
- *
- * @param {*} request
- * @param {*} response
- * @returns {Promise<Array<*>>}
- */
-export async function postApplicationsCaseDocumentationUpload(request, response) {
-	// TODO: call the API and get the necessary information for the blob storage upload
-	// TODO: set the proper type for the api response/controller return
-	const APIResponse = request.body;
-
-	return response.send(APIResponse);
+	response.render(`applications/case-documentation/upload`, properties);
 }
