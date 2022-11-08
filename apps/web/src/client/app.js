ed175c8ecd0f9179744546396c73de51da933fd2
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-11-08 12:31:50 +0000

    feat(web/applications): create interaction with the api endpoint for documents upload

--- apps/web/src/client/app.js
+++ apps/web/src/client/app.js
@@ -1,8 +1,7 @@
 // Import default components for all pages
 //* This will be updated based on a router -> loader flow.
 
-// eslint-disable-next-line no-unused-vars
-import regeneratorRuntime from 'regenerator-runtime';
+import 'regenerator-runtime/runtime';
 import './pages/default';
 import { initAll as initGovUKScripts } from 'govuk-frontend';
 import initFileUploaderModule from './components/file-uploader/file-uploader.module.js';
