9eb0d19d7105948b834e16714a1f78402a18215a
Francesco Brenci <francesco.brenci@kinandcarta.com>
2022-11-09 17:21:23 +0000

    feat(web/applications): applied new header globally (BOAS-559)

--- apps/web/src/server/applications/applications.filters.js
+++ apps/web/src/server/applications/applications.filters.js
@@ -1,5 +1,3 @@
-import nunjucks from '../app/config/nunjucks.js';
-
 /** @typedef {import('./applications.types').DomainType} DomainType */
 /** @typedef {import('nunjucks').Environment} Environment */
 
@@ -9,26 +7,5 @@ import nunjucks from '../app/config/nunjucks.js';
  * @type {import('express').RequestHandler<{}, *, *, *, {}>}
  */
 export const registerFilters = (req, response, next) => {
-	setDisplayValueFilter();
-
 	next();
 };
-
-/**
- * Register the displayValue filter for the domainType
- *
- * @returns {Environment}
- */
-const setDisplayValueFilter = () =>
-	nunjucks.addFilter('displayValue', (/** @type {DomainType} */ key) => {
-		switch (key) {
-			case 'case-admin-officer':
-				return 'Case admin officer';
-			case 'case-officer':
-				return 'Case officer';
-			case 'inspector':
-				return 'Inspector';
-			default:
-				return '';
-		}
-	});
