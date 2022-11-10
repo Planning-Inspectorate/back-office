14f4c3cacbb40ca98fd8118dd509ec69e89df1ac
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-11-10 14:22:00 +0000

    feat(api): saves space where document lives in blob store (BOAS-505)

---
+++ apps/api/prisma/migrations/20221110140732_adds_blob_storage_container_and_path_to_document/migration.sql
@@ -0,0 +1,20 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[Document] ADD [blobStorageContainer] NVARCHAR(1000),
+[blocStoragePath] NVARCHAR(1000);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
