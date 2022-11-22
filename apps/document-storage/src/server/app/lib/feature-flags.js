8ced3f3da892ba8b675f3148d76e12378b0a0ce3
Francesco Brenci <francesco.brenci@kinandcarta.com>
2022-11-22 17:17:33 +0000

    fix(web/applications): added context to logs (BOAS-521)

--- apps/document-storage/src/server/app/lib/feature-flags.js
+++ apps/document-storage/src/server/app/lib/feature-flags.js
@@ -8,18 +8,20 @@ import pino from './logger.js';
  */
 
 export const isFeatureActive = (featureFlagName) => {
-	pino.info(`flag name: ${featureFlagName}`);
+	pino.info(`[STORAGE] flag name: ${featureFlagName}`);
 
 	if (
 		!featureFlagName ||
 		!Object.prototype.hasOwnProperty.call(config.featureFlags, featureFlagName)
 	) {
-		pino.info(`a flag name must be supplied: ${featureFlagName} does not exist`);
+		pino.info(`[STORAGE] a flag name must be supplied: ${featureFlagName} does not exist`);
 		// we assume the flag doesn't exists so the feature is available
 		return true;
 	}
 
-	pino.info(`is flag ${featureFlagName} enabled: ${config.featureFlags[featureFlagName]}`);
+	pino.info(
+		`[STORAGE] is flag ${featureFlagName} enabled: ${config.featureFlags[featureFlagName]}`
+	);
 
 	return config.featureFlags[featureFlagName];
 };
