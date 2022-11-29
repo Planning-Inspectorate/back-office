195702e9f4aaf9ba89fb24af42a0c131b3ffc0cb
Francesco Brenci <francesco.brenci@kinandcarta.com>
2022-11-29 12:51:22 +0000

    fix(web/applications): updated libraries test (BOAS-598)

--- apps/web/src/server/lib/__tests__/libraries.test.js
+++ apps/web/src/server/lib/__tests__/libraries.test.js
@@ -1,7 +1,49 @@
+import { addressToString } from '../address-formatter.js';
 import asyncRoute from '../async-route.js';
 import { bodyToPayload } from '../body-formatter.js';
+import { dateIsValid, isDateInstance } from '../dates.js';
+import { nameToString } from '../person-name-formatter.js';
 
 describe('Libraries', () => {
+	describe('addressFormatter', () => {
+		it('should converts a multi part address to a single string', () => {
+			const address = {
+				postCode: 'postcode',
+				addressLine1: 'address 1',
+				addressLine2: 'address 2',
+				town: 'town'
+			};
+
+			const adressFormatted = addressToString(address);
+
+			expect(typeof adressFormatted).toBe('string');
+		});
+	});
+
+	describe('async-route helper', () => {
+		it('should throw error if route throws error', () => {
+			const error = new Error('some error');
+
+			// @ts-ignore
+			const route = async (req, res) => {
+				throw error;
+			};
+
+			expect(asyncRoute(route)).rejects.toThrowError(error);
+		});
+
+		it('should throw error if route returns rejected promise', () => {
+			const error = new Error('some error');
+
+			// @ts-ignore
+			const route = async (req, res) => {
+				await Promise.reject(error);
+			};
+
+			expect(asyncRoute(route)).rejects.toThrowError(error);
+		});
+	});
+
 	describe('BodyToPayload', () => {
 		it('should format correct the body', () => {
 			const body = {
@@ -37,27 +79,68 @@ describe('Libraries', () => {
 		});
 	});
 
-	describe('async-route helper', () => {
-		it('should throw error if route throws error', () => {
-			const error = new Error('some error');
+	describe('dates', () => {
+		describe('dateIsValid', () => {
+			it('should determine if a date is valid', () => {
+				const year = 1989;
+				const month = 3;
+				const day = 25;
 
-			// @ts-ignore
-			const route = async (req, res) => {
-				throw error;
-			};
+				const validDate = dateIsValid(year, month, day);
 
-			expect(asyncRoute(route)).rejects.toThrowError(error);
+				expect(typeof validDate).toEqual('boolean');
+			});
+
+			it('should return true if a date is valid', () => {
+				const year = 1989;
+				const month = 3;
+				const day = 25;
+
+				const validDate = dateIsValid(year, month, day);
+
+				expect(validDate).toEqual(true);
+			});
+
+			it('should return false if a date is invalid', () => {
+				const year = 190;
+				const month = 33;
+				const day = 255;
+
+				const validDate = dateIsValid(year, month, day);
+
+				expect(validDate).toEqual(false);
+			});
 		});
 
-		it('should throw error if route returns rejected promise', () => {
-			const error = new Error('some error');
+		describe('isDateInstance', () => {
+			it('should determine if a date is an instance of Date', () => {
+				const year = 1989;
+				const month = 3;
+				const day = 25;
+				const date = new Date(year, month - 1, day);
 
-			// @ts-ignore
-			const route = async (req, res) => {
-				await Promise.reject(error);
+				const instanceOfDate = isDateInstance(date);
+
+				expect(typeof instanceOfDate).toEqual('boolean');
+			});
+		});
+	});
+
+	describe('nameToString', () => {
+		it('should converts a multi part person name to a single string', () => {
+			const applicant = {
+				firstName: 'firstName',
+				lastName: 'lastName',
+				id: 1,
+				organisationName: 'organisationName',
+				email: 'email',
+				website: 'website',
+				phoneNumber: 'phoneNumber'
 			};
 
-			expect(asyncRoute(route)).rejects.toThrowError(error);
+			const formattedApplicant = nameToString(applicant);
+
+			expect(typeof formattedApplicant).toEqual('string');
 		});
 	});
 });
