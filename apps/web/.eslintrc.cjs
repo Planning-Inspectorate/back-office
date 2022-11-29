f8aa259155af562c3ad455a1617bf022df3f46c4
Francesco Brenci <francesco.brenci@kinandcarta.com>
2022-11-29 16:32:30 +0000

    fix(web/applications): pr feedback (BOAS-598)

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -7,8 +7,7 @@ module.exports = {
 				// allow devdependencies
 				// https://github.com/weiran-zsd/eslint-plugin-node/blob/cb4f099ae928d627abfc635966622cf4266a7b1c/docs/rules/no-unpublished-import.md
 				'n/no-unpublished-import': 'off',
-				'jest/valid-expect': 'off',
-				'no-unused-vars': 'off'
+				'jest/valid-expect': 'off'
 
 			}
 		},
