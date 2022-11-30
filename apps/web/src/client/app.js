d559fa97d9f54953ae431fc3b2480989c9f0f6a6
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-11-30 17:51:27 +0000

    feat(web/applications): add client-side functionalities (boas-594)

--- apps/web/src/client/app.js
+++ apps/web/src/client/app.js
@@ -4,10 +4,12 @@
 import './pages/default';
 import { initAll as initGovUKScripts } from 'govuk-frontend';
 import initFileUploaderModule from './components/file-uploader/file-uploader.module.js';
+import initFilesListModule from './components/files-list/files-list.module.js';
 
 const initAll = () => {
 	initGovUKScripts();
 	initFileUploaderModule();
+	initFilesListModule();
 };
 
 initAll();
