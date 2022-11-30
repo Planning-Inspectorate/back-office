9b26b27fb405fe2829908431ef21fda9e210536f
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-11-30 17:51:27 +0000

    feat(web/applications): add client-side functionalities (boas-594)

---
+++ apps/web/src/client/components/files-list/files-list.module.js
@@ -0,0 +1,54 @@
+/** @type {NodeListOf<HTMLInputElement>} */
+const fileCheckBoxes = document.querySelectorAll('input[name="selectedFilesIds"]');
+/** @type {HTMLElement | null} */
+const selectAllCheckBox = document.querySelector('input[name="selectAll"]');
+/** @type {HTMLElement | null} */
+const pageSizeSelect = document.querySelector('select[name="pageSize"]');
+
+const initFilesListModule = () => {
+	if (!selectAllCheckBox || fileCheckBoxes.length === 0 || !pageSizeSelect) return;
+
+	selectAllCheckBox.addEventListener('click', toggleSelectAll);
+	pageSizeSelect.addEventListener('change', changePageSize);
+	for (const checkbox of fileCheckBoxes) {
+		checkbox.addEventListener('change', updateSelectedFilesCounter);
+	}
+};
+
+/**
+ * Toggle the bulk selection of files
+ *
+ * @param {*} clickEvent
+ */
+const toggleSelectAll = (clickEvent) => {
+	for (const checkbox of fileCheckBoxes) {
+		checkbox.checked = clickEvent.target.checked;
+	}
+	updateSelectedFilesCounter();
+};
+
+/**
+ * Refresh the page with the new pageSize value
+ *
+ * @param {*} changeEvent
+ */
+const changePageSize = (changeEvent) => {
+	document.location = `.?number=1&size=${changeEvent.target.value}`;
+};
+
+/**
+ * Update the counter of the selected pages
+ *
+ */
+const updateSelectedFilesCounter = () => {
+	/** @type {HTMLElement | null} */
+	const selectedFilesNumber = document.querySelector('#selectedFilesNumber');
+
+	if (selectedFilesNumber) {
+		const checkedFiles = document.querySelectorAll('input[name="selectedFilesIds"]:checked');
+
+		selectedFilesNumber.textContent = `${(checkedFiles || []).length}`;
+	}
+};
+
+export default initFilesListModule;
