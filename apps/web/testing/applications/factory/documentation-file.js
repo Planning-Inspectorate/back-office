64183ca774b6a91fcb9a576741343dfb3112fbd8
Ludovico <ludovico.tamigio@kinandcarta.com>
2022-12-01 14:00:29 +0000

    test(web/applications): add server side tests (boas-594)

--- apps/web/testing/applications/factory/documentation-file.js
+++ apps/web/testing/applications/factory/documentation-file.js
@@ -1,5 +1,9 @@
 import { fake } from '@pins/platform';
-import { createRandomDescription } from './util.js';
+import {
+	createRandomDescription,
+	createUniqueRandomBooleanFromSeed,
+	createUniqueRandomNumberFromSeed
+} from './util.js';
 
 /** @typedef {import('../../../src/server/applications/applications.types').DocumentationFile} DocumentationFile */
 
@@ -10,21 +14,24 @@ import { createRandomDescription } from './util.js';
  */
 export function createDocumentationFile({
 	id = fake.createUniqueId(),
-	date = Math.floor(Math.random() * 10_000_000_000),
-	size = Math.floor(Math.random() * 10_000_000),
-	isChecked = fake.randomBoolean(),
-	isRedacted = fake.randomBoolean()
+	date = createUniqueRandomNumberFromSeed(10_000_000, 10_000_000_000, id),
+	size = createUniqueRandomNumberFromSeed(100, 10_000_000, id),
+	isChecked = createUniqueRandomBooleanFromSeed(id),
+	isRedacted = createUniqueRandomBooleanFromSeed(id)
 } = {}) {
 	const fileName = `${id + 1} ${createRandomDescription({
-		wordsNumber: Math.floor(Math.random() * 5) + 2,
-		startOffset: Math.floor(Math.random() * 30)
+		wordsNumber: createUniqueRandomNumberFromSeed(2, 5, id),
+		startOffset: createUniqueRandomNumberFromSeed(0, 30, id)
 	})}`;
 	const url = `url/to/file/${id}`;
 	const from = `${createRandomDescription({
 		wordsNumber: 1,
-		startOffset: Math.floor(Math.random() * 30)
-	})} ${createRandomDescription({ wordsNumber: 1, startOffset: Math.floor(Math.random() * 30) })}`;
-	const type = ['DOC', 'PDF', 'JPG', 'MP3'][Math.floor(Math.random() * 4)];
+		startOffset: createUniqueRandomNumberFromSeed(0, 30, id)
+	})} ${createRandomDescription({
+		wordsNumber: 1,
+		startOffset: createUniqueRandomNumberFromSeed(5, 20, id)
+	})}`;
+	const type = ['DOC', 'PDF', 'JPG', 'MP3'][createUniqueRandomNumberFromSeed(0, 4, id)];
 
 	return {
 		id,
