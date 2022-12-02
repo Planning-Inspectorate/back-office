797c7c5265341387ead0a8c24c085a56f3d09194
Xfarya <farya243@gmail.com>
2022-12-02 12:30:25 +0000

    feat(api/applications): rebased branch added previous changes (BOAS-537)

--- apps/api/src/server/repositories/document.repository.js
+++ apps/api/src/server/repositories/document.repository.js
@@ -27,3 +27,25 @@ export const update = (documentGuid, documentDetails) => {
 		data: documentDetails
 	});
 };
+
+/**
+ *
+ * @param {string} documentGUID
+ * @returns {import('@prisma/client').PrismaPromise<import('@pins/api').Schema.Document | null>}
+ */
+export const getByDocumentGUID = (documentGUID) => {
+	return databaseConnector.document.findUnique({
+		where: { guid: documentGUID }
+	});
+};
+
+/**
+ * @param {{guid: string, status: import('xstate').StateValue }} documentStatusUpdate
+ * @returns {import('@prisma/client').PrismaPromise<import('@pins/api').Schema.Document>}
+ */
+export const updateDocumentStatus = ({ guid, status }) => {
+	return databaseConnector.document.update({
+		where: { guid },
+		data: { status }
+	});
+};
