64920b13cddc68e1766fbd1c68189f7fdfcf3556
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-12-06 09:49:53 +0000

    test(document-check-function): adds tests for doc check function (BOAS-536)

--- apps/document-check-function/check-new-document/__tests__/check-new-document.test.js
+++ apps/document-check-function/check-new-document/__tests__/check-new-document.test.js
@@ -1,5 +1,43 @@
 import test from 'ava';
+import NodeClam from 'clamscan';
+import got from 'got';
+import ReadableStream from 'node:stream';
+import sinon from 'sinon';
+import { checkMyBlob } from '../index.js';
 
-test('basic test', async (t) => {
+const fileStream = new ReadableStream();
+
+const patchStub = sinon.stub().returns({ json: sinon.stub() });
+
+test.before('set up mocks', () => {
+	sinon.stub(got, 'patch').callsFake(patchStub);
+});
+
+test.serial("sends 'passed' machine action to back office if passed AV check", async (t) => {
+	sinon
+		.stub(NodeClam.prototype, 'init')
+		.returns({ scanStream: sinon.stub().returns({ isInfected: false }) });
+	await checkMyBlob({ bindingData: { uri: 'applications/1/123-345' } }, fileStream);
 	t.is(true, true);
+
+	sinon.assert.calledWith(
+		patchStub,
+		'http://localhost:3000/applications/1/documents/123-345/status',
+		{ json: { machineAction: 'passed' } }
+	);
+	NodeClam.prototype.init.restore();
+});
+
+test.serial("sends 'failed' machine action to back office if failed AV check", async (t) => {
+	sinon
+		.stub(NodeClam.prototype, 'init')
+		.returns({ scanStream: sinon.stub().returns({ isInfected: true }) });
+	await checkMyBlob({ bindingData: { uri: 'applications/1/123-345' } }, fileStream);
+	t.is(true, true);
+
+	sinon.assert.calledWith(
+		patchStub,
+		'http://localhost:3000/applications/1/documents/123-345/status',
+		{ json: { machineAction: 'failed' } }
+	);
 });
