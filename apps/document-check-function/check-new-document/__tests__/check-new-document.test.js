bb2d3754a8b274c09e20a10d5d906cc1d645c051
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-12-06 17:37:18 +0000

    feat(document-check-function): throws error if unexpected doc path (BOAS-536)

--- apps/document-check-function/check-new-document/__tests__/check-new-document.test.js
+++ apps/document-check-function/check-new-document/__tests__/check-new-document.test.js
@@ -13,18 +13,24 @@ test.before('set up mocks', () => {
 	sinon.stub(got, 'patch').callsFake(patchStub);
 });
 
+test.serial('throws error if blob path doesnt have 4 parts', async (t) => {
+	const error = await t.throwsAsync(async () => {
+		await checkMyBlob({ bindingData: { uri: '/applications/1/test.pdf' } }, fileStream);
+	});
+
+	t.is(error?.message, 'Unexpected blob URI /applications/1/test.pdf');
+});
+
 test.serial("sends 'passed' machine action to back office if passed AV check", async (t) => {
 	sinon
 		.stub(NodeClam.prototype, 'init')
 		.returns({ scanStream: sinon.stub().returns({ isInfected: false }) });
-	await checkMyBlob({ bindingData: { uri: 'applications/1/123-345' } }, fileStream);
+	await checkMyBlob({ bindingData: { uri: '/applications/1/123-345/test.pdf' } }, fileStream);
 	t.is(true, true);
 
-	sinon.assert.calledWith(
-		patchStub,
-		'http://localhost:3000/applications/1/documents/123-345/status',
-		{ json: { machineAction: 'check_pass' } }
-	);
+	sinon.assert.calledWith(patchStub, 'test-api-host:3000/applications/1/documents/123-345/status', {
+		json: { machineAction: 'check_pass' }
+	});
 	NodeClam.prototype.init.restore();
 });
 
@@ -32,12 +38,10 @@ test.serial("sends 'failed' machine action to back office if failed AV check", a
 	sinon
 		.stub(NodeClam.prototype, 'init')
 		.returns({ scanStream: sinon.stub().returns({ isInfected: true }) });
-	await checkMyBlob({ bindingData: { uri: 'applications/1/123-345' } }, fileStream);
+	await checkMyBlob({ bindingData: { uri: '/applications/1/123-345/test.pdf' } }, fileStream);
 	t.is(true, true);
 
-	sinon.assert.calledWith(
-		patchStub,
-		'http://localhost:3000/applications/1/documents/123-345/status',
-		{ json: { machineAction: 'check_fail' } }
-	);
+	sinon.assert.calledWith(patchStub, 'test-api-host:3000/applications/1/documents/123-345/status', {
+		json: { machineAction: 'check_fail' }
+	});
 });
