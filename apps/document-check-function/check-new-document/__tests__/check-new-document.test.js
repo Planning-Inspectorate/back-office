c43ddb18f907ba4950174e68eb68cc3f7b25e1b6
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-12-20 15:09:18 +0000

    feat(document-check-function): allow any http error to occur when calling BO api (BOAS-536)

--- apps/document-check-function/check-new-document/__tests__/check-new-document.test.js
+++ apps/document-check-function/check-new-document/__tests__/check-new-document.test.js
@@ -6,7 +6,58 @@ import { clamAvClient } from '../clam-av-client.js';
 import { checkMyBlob } from '../index.js';
 
 const fileStream = new Readable();
-const backOfficePatchStub = sinon.stub().returns({ json: sinon.stub() });
+
+// ------------------------
+// Back Office API Stubbing
+// ------------------------
+
+const backOfficePatchStub = sinon.stub();
+
+backOfficePatchStub
+	.withArgs('test-api-host:3000/applications/1/documents/123-345/status', {
+		json: {
+			machineAction: 'check_pass'
+		}
+	})
+	.returns({ json: sinon.stub() });
+backOfficePatchStub
+	.withArgs('test-api-host:3000/applications/1/documents/123-345/status', {
+		json: {
+			machineAction: 'check_fail'
+		}
+	})
+	.returns({ json: sinon.stub() });
+backOfficePatchStub
+	.withArgs('test-api-host:3000/applications/1/documents/123-345-678/status', {
+		json: {
+			machineAction: 'check_fail'
+		}
+	})
+	.returns({ json: sinon.stub() });
+
+const backOfficeErrorResponse = {
+	body: '{"errors":{"application":"Could not transition \'not_user_checked\' using \'check_success\'."}}',
+	statusCode: 409
+};
+
+// @ts-ignore
+const backOfficeHttpError = new HTTPError(backOfficeErrorResponse);
+
+// @ts-ignore
+backOfficeHttpError.response = backOfficeErrorResponse;
+
+backOfficePatchStub
+	.withArgs('test-api-host:3000/applications/1/documents/123-345-901/status', {
+		json: {
+			machineAction: 'check_fail'
+		}
+	})
+	.throwsException(backOfficeHttpError);
+
+// -----------------------------
+// Document Storage API Stubbing
+// -----------------------------
+
 const documentStorageDeleteStub = sinon.stub();
 
 documentStorageDeleteStub
@@ -14,22 +65,32 @@ documentStorageDeleteStub
 		json: { documentPath: '/applications/1/123-345/test.pdf' }
 	})
 	.returns({ json: sinon.stub() });
+documentStorageDeleteStub
+	.withArgs('http://localhost:3001/document', {
+		json: { documentPath: '/applications/1/123-345/test-that-has-been-marked-as-failed.pdf' }
+	})
+	.returns({ json: sinon.stub() });
+documentStorageDeleteStub
+	.withArgs('http://localhost:3001/document', {
+		json: { documentPath: '/applications/1/123-345-901/test-that-has-been-marked-as-failed.pdf' }
+	})
+	.returns({ json: sinon.stub() });
 
-const errorResponse = {
+const documentStorageErrorResponse = {
 	body: '{"errors":{"documentPath":"Document does not exist in Blob Storage"}}',
 	statusCode: 404
 };
 // @ts-ignore
-const httpError = new HTTPError(errorResponse);
+const documentStorageHttpError = new HTTPError(documentStorageErrorResponse);
 
 // @ts-ignore
-httpError.response = errorResponse;
+documentStorageHttpError.response = documentStorageErrorResponse;
 
 documentStorageDeleteStub
 	.withArgs('http://localhost:3001/document', {
-		json: { documentPath: '/applications/1/123-345/test-that-has-been-deleted.pdf' }
+		json: { documentPath: '/applications/1/123-345-678/test-that-has-been-deleted.pdf' }
 	})
-	.throwsException(httpError);
+	.throwsException(documentStorageHttpError);
 
 const errorStub = sinon.stub();
 const infoStub = sinon.stub();
@@ -50,37 +111,96 @@ test.serial('throws error if blob path doesnt have 4 parts', async (t) => {
 	t.is(error?.message, 'Unexpected blob URI /applications/1/test.pdf');
 });
 
-test.serial("sends 'passed' machine action to back office if passed AV check", async (t) => {
-	sinon.stub(clamAvClient, 'scanStream').returns({ isInfected: false });
-	await checkMyBlob(
-		{ bindingData: { uri: '/applications/1/123-345/test.pdf' }, error: errorStub },
-		fileStream
-	);
-	t.is(true, true);
-
-	sinon.assert.calledWith(
-		backOfficePatchStub,
-		'test-api-host:3000/applications/1/documents/123-345/status',
-		{
-			json: { machineAction: 'check_pass' }
-		}
-	);
-	clamAvClient.scanStream.restore();
-});
-
 test.serial(
-	"sends 'failed' machine action to back office even if already deleted from blob storage",
+	"if document passes AV checks, sends 'passed' machine action to back office if passed AV check",
 	async (t) => {
-		sinon.stub(clamAvClient, 'scanStream').returns({ isInfected: true });
+		sinon.stub(clamAvClient, 'scanStream').returns({ isInfected: false });
 		await checkMyBlob(
 			{
-				bindingData: { uri: '/applications/1/123-345/test-that-has-been-deleted.pdf' },
+				bindingData: { uri: '/applications/1/123-345/test.pdf' },
 				error: errorStub,
 				info: infoStub
 			},
 			fileStream
 		);
 		t.is(true, true);
+
+		sinon.assert.calledWith(
+			backOfficePatchStub,
+			'test-api-host:3000/applications/1/documents/123-345/status',
+			{
+				json: { machineAction: 'check_pass' }
+			}
+		);
+		clamAvClient.scanStream.restore();
+	}
+);
+
+test.serial(
+	"if document fails AV checks, sends 'failed' machine action to back office even if already deleted from blob storage",
+	async (t) => {
+		sinon.stub(clamAvClient, 'scanStream').returns({ isInfected: true });
+		await checkMyBlob(
+			{
+				bindingData: { uri: '/applications/1/123-345-678/test-that-has-been-deleted.pdf' },
+				error: errorStub,
+				info: infoStub
+			},
+			fileStream
+		);
+		t.is(true, true);
+
+		// Checks that we send a request to mark document as failed AV checks
+		sinon.assert.calledWith(
+			backOfficePatchStub,
+			'test-api-host:3000/applications/1/documents/123-345-678/status',
+			{
+				json: { machineAction: 'check_fail' }
+			}
+		);
+
+		clamAvClient.scanStream.restore();
+	}
+);
+
+test.serial(
+	'if document fails AV checks, deletes document from blob storage even if already marked as failed AV check in back office DB',
+	async (t) => {
+		sinon.stub(clamAvClient, 'scanStream').returns({ isInfected: true });
+
+		await checkMyBlob(
+			{
+				bindingData: { uri: '/applications/1/123-345-901/test-that-has-been-marked-as-failed.pdf' },
+				error: errorStub,
+				info: infoStub
+			},
+			fileStream
+		);
+		t.is(true, true);
+
+		// Checks that we send request to delete document
+		sinon.assert.calledWith(documentStorageDeleteStub, 'http://localhost:3001/document', {
+			json: { documentPath: '/applications/1/123-345-901/test-that-has-been-marked-as-failed.pdf' }
+		});
+
+		clamAvClient.scanStream.restore();
+	}
+);
+
+test.serial(
+	"if document fails AV checks, sends 'failed' machine action to back office and deletes from blob storage",
+	async (t) => {
+		sinon.stub(clamAvClient, 'scanStream').returns({ isInfected: true });
+		await checkMyBlob(
+			{
+				bindingData: { uri: '/applications/1/123-345/test.pdf' },
+				error: errorStub,
+				info: infoStub
+			},
+			fileStream
+		);
+		t.is(true, true);
+
 		sinon.assert.calledWith(
 			backOfficePatchStub,
 			'test-api-host:3000/applications/1/documents/123-345/status',
@@ -88,26 +208,10 @@ test.serial(
 				json: { machineAction: 'check_fail' }
 			}
 		);
+		sinon.assert.calledWith(documentStorageDeleteStub, 'http://localhost:3001/document', {
+			json: { documentPath: '/applications/1/123-345/test.pdf' }
+		});
+
 		clamAvClient.scanStream.restore();
 	}
 );
-
-test.serial("sends 'failed' machine action to back office if failed AV check", async (t) => {
-	sinon.stub(clamAvClient, 'scanStream').returns({ isInfected: true });
-	await checkMyBlob(
-		{ bindingData: { uri: '/applications/1/123-345/test.pdf' }, error: errorStub, info: infoStub },
-		fileStream
-	);
-	t.is(true, true);
-
-	sinon.assert.calledWith(
-		backOfficePatchStub,
-		'test-api-host:3000/applications/1/documents/123-345/status',
-		{
-			json: { machineAction: 'check_fail' }
-		}
-	);
-	sinon.assert.calledWith(documentStorageDeleteStub, 'http://localhost:3001/document', {
-		json: { documentPath: '/applications/1/123-345/test.pdf' }
-	});
-});
--- apps/document-check-function/check-new-document/back-office-api-client.js
+++ apps/document-check-function/check-new-document/back-office-api-client.js
@@ -1,12 +1,13 @@
-import got from 'got';
+import got, { HTTPError } from 'got';
 import config from './config.js';
 
 /**
- * @param {string} documentGuid
+ *
  * @param {string} caseId
+ * @param {string} documentGuid
  * @param {string} machineAction
  */
-export const sendDocumentStateAction = async (documentGuid, caseId, machineAction) => {
+const sendRequestToBackOffice = async (caseId, documentGuid, machineAction) => {
 	await got
 		.patch(`${config.API_HOST}/applications/${caseId}/documents/${documentGuid}/status`, {
 			json: {
@@ -14,4 +15,39 @@ export const sendDocumentStateAction = async (documentGuid, caseId, machineActio
 			}
 		})
 		.json();
+};
+
+/**
+ *
+ * @param {Error} error
+ * @param {string} machineAction
+ * @returns {boolean}
+ */
+const errorIsDueToDocumentAlreadyMakedWithNewStatus = (error, machineAction) => {
+	return (
+		error instanceof HTTPError &&
+		error.code === 'ERR_NON_2XX_3XX_RESPONSE' &&
+		error.response.statusCode === 409 &&
+		machineAction !== null
+		// JSON.parse(error.response.body)?.errors?.application.match(/(\w+)=(.*?)(?=(?: \w+=)|$)/g)
+		// `Could not transition 'not_user_checked' using '${machineAction}'.`
+	);
+};
+
+/**
+ * @param {string} documentGuid
+ * @param {string} caseId
+ * @param {string} machineAction
+ * @param {import('./index.js').Context} context
+ */
+export const sendDocumentStateAction = async (documentGuid, caseId, machineAction, context) => {
+	try {
+		await sendRequestToBackOffice(caseId, documentGuid, machineAction);
+	} catch (error) {
+		if (errorIsDueToDocumentAlreadyMakedWithNewStatus(error, machineAction)) {
+			context.info('Document status already updated based on AV checks');
+		} else {
+			throw error;
+		}
+	}
 };
