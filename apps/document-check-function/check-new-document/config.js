4637ac80b5a24229dd67acef3f2fea6bfae8d49d
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-12-20 11:06:24 +0000

    feat(document-check-function): proceeds with marking doc as failed even if deleted (BOAS-536)

--- apps/document-check-function/check-new-document/config.js
+++ apps/document-check-function/check-new-document/config.js
@@ -5,7 +5,10 @@ const schema = joi.object({
 	NODE_ENV: joi.string().valid('development', 'production', 'test'),
 	API_HOST: joi.string(),
 	CLAM_AV_HOST: joi.string(),
-	CLAM_AV_PORT: joi.number()
+	CLAM_AV_PORT: joi.number(),
+	log: joi.object({
+		levelStdOut: joi.string()
+	})
 });
 
 const environment = loadEnvironment(process.env.NODE_ENV);
@@ -14,7 +17,10 @@ const { value, error } = schema.validate({
 	NODE_ENV: environment.NODE_ENV,
 	API_HOST: environment.API_HOST,
 	CLAM_AV_HOST: environment.CLAM_AV_HOST,
-	CLAM_AV_PORT: environment.CLAM_AV_PORT
+	CLAM_AV_PORT: environment.CLAM_AV_PORT,
+	log: {
+		levelStdOut: environment.LOG_LEVEL_STDOUT || 'debug'
+	}
 });
 
 if (error) {
