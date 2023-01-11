a8aba1b3d411f8d2904594f0cf76a11306e555fe
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-01-11 16:54:37 +0000

    feat(document-check-function): uses es6 for index (BOAS-536)

--- apps/document-check-function/check-new-document/index.js
+++ apps/document-check-function/check-new-document/index.js
@@ -4,7 +4,7 @@ import { checkMyBlob } from './check-my-blob.js';
  * @param {import('./check-my-blob').Context} context
  * @param {import('node:stream').Readable} myBlob
  */
-module.exports = async function (context, myBlob) {
+export const index = async (context, myBlob) => {
 	context.log(
 		'JavaScript blob trigger function processed blob \n Blob:',
 		context.bindingData.blobTrigger,
