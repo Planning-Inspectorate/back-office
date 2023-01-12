e22652b79ea764be4ff13a0e3782e7bd631ff6d6
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-01-12 12:08:50 +0000

    feat(web/applications): use AD credentials to generate sas token (boas-696)

--- apps/web/src/server/lib/sas-token.js
+++ apps/web/src/server/lib/sas-token.js
@@ -1,29 +1,68 @@
 import {
-	AccountSASPermissions,
-	AccountSASResourceTypes,
-	AccountSASServices,
-	generateAccountSASQueryParameters,
-	SASProtocol,
-	StorageSharedKeyCredential
+	BlobServiceClient,
+	ContainerSASPermissions,
+	generateBlobSASQueryParameters,
+	SASProtocol
 } from '@azure/storage-blob';
 import config from '../../../environment/config.js';
 
-const createSasToken = async () => {
-	const sharedKeyCredential = new StorageSharedKeyCredential(
-		config.blobStorageAccountName,
-		config.blobStorageAccountKey
-	);
+/** @typedef {import('@azure/core-auth').AccessToken} AccessToken */
+/** @typedef {import('@azure/storage-blob').ServiceGetUserDelegationKeyResponse} DelegationKey */
 
-	const sasOptions = {
-		services: AccountSASServices.parse('btqf').toString(),
-		resourceTypes: AccountSASResourceTypes.parse('sco').toString(),
-		permissions: AccountSASPermissions.parse('r'),
-		protocol: SASProtocol.Https,
-		startsOn: new Date(Date.now() - 60 * 1000),
-		expiresOn: new Date(Date.now())
+const sasTokenStartTime = () => new Date(Date.now() - 60 * 1000);
+const sasTokenExpirationTime = () => new Date(Date.now());
+
+const { blobStorageUrl, blobStorageAccountName } = config;
+
+/**
+ * Get the Azure user delegation key
+ *
+ * @param {AccessToken} accessToken
+ * @returns {Promise<DelegationKey>}
+ */
+const createDelegationKey = async (accessToken) => {
+	const credentials = {
+		getToken: () => Promise.resolve(accessToken)
 	};
 
-	const sasToken = generateAccountSASQueryParameters(sasOptions, sharedKeyCredential).toString();
+	const blobServiceClient = new BlobServiceClient(blobStorageUrl, credentials);
+
+	return blobServiceClient.getUserDelegationKey(sasTokenStartTime(), sasTokenExpirationTime());
+};
+
+/**
+ * Generate a user delegation SAS token
+ *
+ * @param {AccessToken} accessToken
+ * @param {string} containerName
+ * @param {string} blobName
+ * @returns {Promise<string>}
+ */
+const createSasToken = async (accessToken, containerName, blobName) => {
+	// Use the AD access token to get a "delegation key"
+	const userDelegationKey = await createDelegationKey(accessToken);
+
+	const sasOptions = {
+		// specific containerName
+		containerName,
+		// specific blobName
+		blobName,
+		// read only
+		permissions: ContainerSASPermissions.parse('r'),
+		protocol: SASProtocol.Https,
+		// token starts a few seconds in the past
+		startsOn: sasTokenStartTime(),
+		// expires immediately but due to server latency could take up to 40s
+		expiresOn: sasTokenExpirationTime()
+	};
+
+	// generate a "user delegation SAS token",
+	// i.e. a SAS token based on user credentials rather than azure account
+	const sasToken = generateBlobSASQueryParameters(
+		sasOptions,
+		userDelegationKey,
+		blobStorageAccountName
+	).toString();
 
 	return `?${sasToken}`;
 };
