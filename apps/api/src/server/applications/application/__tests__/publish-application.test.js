9c94437cf0651c19e108027ce6b9e7031542c259
personal <personal@personals-MacBook-Pro.local>
2023-01-16 14:25:53 +0000

    feat(api/applications): created api and test to publish a case

--- apps/api/src/server/applications/application/__tests__/publish-application.test.js
+++ apps/api/src/server/applications/application/__tests__/publish-application.test.js
@@ -3,7 +3,7 @@ import sinon from 'sinon';
 import supertest from 'supertest';
 import { app } from '../../../app.js';
 import { databaseConnector } from '../../../utils/database-connector.js';
-import { mapDateStringToUnixTimestamp } from '../../../utils/mapping/map-date-string-to-unix-timestamp.js';
+import logger from '../../../utils/logger.js';
 
 const request = supertest(app);
 
@@ -14,32 +14,44 @@ const findUniqueSubSectorStub = sinon.stub();
 findUniqueSubSectorStub.withArgs({ where: { name: 'some_sub_sector' } }).returns({});
 findUniqueSubSectorStub.withArgs({ where: { name: 'some unknown subsector' } }).returns(null);
 
+const mockDate = new Date(1_649_319_144_000);
+
 const findUniqueStub = sinon.stub();
 
 const mockPublishedAt = new Date('2023-01-15T23:14:04.193Z');
 
 findUniqueStub.withArgs({ where: { id: 1 } }).returns({ id: 1, publishedAt: mockPublishedAt });
 
-test.before('set up mocks', () => {
+const loggerInfo = sinon.stub(logger, 'info');
+
+test.before('set up timer  mock before all tests', () => {
+	sinon.useFakeTimers({ now: 1_649_319_144_000 });
+});
+
+test.beforeEach('set up db mocks before each tests', () => {
 	sinon.stub(databaseConnector, 'case').get(() => {
 		return { update: updateStub, findUnique: findUniqueStub };
 	});
 
 	updateStub.resolves({
-		publishedAt: new Date(1_649_319_144_000)
+		publishedAt: mockDate
 	});
-
-	sinon.useFakeTimers({ now: 1_649_319_144_000 });
 });
 
-test.only('publish an application', async (t) => {
+test('publish an application and return published Date as a timestamp', async (t) => {
 	const caseId = 1;
 
 	const response = await request.patch(`/applications/${caseId}/publish`);
 
 	t.is(response.status, 200);
 
-	const publishedDate = mapDateStringToUnixTimestamp(new Date(1_649_319_144_000).toISOString());
+	const publishedDate = 1_649_319_144;
+
+	sinon.assert.callCount(loggerInfo, 3);
+
+	sinon.assert.calledWithExactly(loggerInfo, `attempting to publish a case with id ${caseId}`);
+	sinon.assert.calledWithExactly(loggerInfo, `case was published at ${mockDate}`);
+	sinon.assert.calledWithExactly(loggerInfo, `successfully published case with id ${caseId}`);
 
 	t.deepEqual(response.body, {
 		publishedDate
@@ -48,12 +60,12 @@ test.only('publish an application', async (t) => {
 	sinon.assert.calledWith(updateStub, {
 		where: { id: caseId },
 		data: {
-			publishedAt: new Date(1_649_319_144_000)
+			publishedAt: mockDate
 		}
 	});
 });
 
-test('throw error if an invalid caseId is provided', async (t) => {
+test('returns 404 error if a caseId does not exist', async (t) => {
 	const caseId = 134;
 
 	const response = await request.patch(`/applications/${caseId}/publish`);
