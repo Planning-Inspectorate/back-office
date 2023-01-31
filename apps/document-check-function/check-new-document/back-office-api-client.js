f685e07e9e4b06f552de77fd2315c7e883e37010
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-01-31 18:56:42 +0000

    fix(document-check-function): uses jest for testing and gets doc store api host (BOAS-723)

--- apps/document-check-function/check-new-document/back-office-api-client.js
+++ apps/document-check-function/check-new-document/back-office-api-client.js
@@ -7,13 +7,11 @@ import config from './config.js';
  * @param {string} machineAction
  */
 const sendRequestToBackOffice = async (documentGuid, machineAction) => {
-	await got
-		.patch(`https://${config.API_HOST}/applications/documents/${documentGuid}/status`, {
-			json: {
-				machineAction
-			}
-		})
-		.json();
+	await got.patch(`https://${config.API_HOST}/applications/documents/${documentGuid}/status`, {
+		json: {
+			machineAction
+		}
+	});
 };
 
 /**
