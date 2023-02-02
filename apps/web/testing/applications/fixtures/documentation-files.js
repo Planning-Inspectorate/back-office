6e05d944b293635caf9186db1e30a3bda81e04dd
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-02-02 12:10:18 +0000

    test(web/applications): add tests for document properties page (boas-732)

--- apps/web/testing/applications/fixtures/documentation-files.js
+++ apps/web/testing/applications/fixtures/documentation-files.js
@@ -5,26 +5,25 @@ import { createDocumentationFile } from '../factory/documentation-file.js';
  * @typedef {import('../../../src/server/applications/applications.types').PaginatedResponse<DocumentationFile>} PaginatedDocumentationFiles
  */
 
+/** @type {DocumentationFile[]} */
+export const fixtureDocumentationFiles = [...Array.from({ length: 123 }).keys()].map((index) =>
+	createDocumentationFile({ guid: `${index}` })
+);
+
 /**
  *
  * @param {number} page
  * @param {number} pageDefaultSize
  * @returns {PaginatedDocumentationFiles}
  */
-export const fixtureDocumentationFiles = (page, pageDefaultSize) => {
-	const allItems = [...Array.from({ length: 123 }).keys()].map((index) =>
-		createDocumentationFile({ guid: `${index}` })
-	);
-
-	return {
-		page,
-		pageSize: pageDefaultSize,
-		pageDefaultSize,
-		pageCount: Math.ceil(123 / pageDefaultSize),
-		itemCount: 123,
-		items: allItems.slice(
-			(page - 1) * pageDefaultSize,
-			pageDefaultSize + (page - 1) * pageDefaultSize
-		)
-	};
-};
+export const fixturePaginatedDocumentationFiles = (page, pageDefaultSize) => ({
+	page,
+	pageSize: pageDefaultSize,
+	pageDefaultSize,
+	pageCount: Math.ceil(123 / pageDefaultSize),
+	itemCount: 123,
+	items: fixtureDocumentationFiles.slice(
+		(page - 1) * pageDefaultSize,
+		pageDefaultSize + (page - 1) * pageDefaultSize
+	)
+});
