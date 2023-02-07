f8c95af3f0a39c4935b37b6b160085b1164b6bfd
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-02-07 16:48:47 +0000

    test(api): attempts to clean up leaked memory (BOAS-598)

--- apps/api/.eslintrc.cjs
+++ apps/api/.eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
 	overrides: [
 		{
-			files: ['**/__tests__/*','**/swagger.js'],
+			files: ['**/__tests__/*','**/swagger.js','setup-tests.js'],
 			rules: {
 				// allow devdependencies
 				// https://github.com/weiran-zsd/eslint-plugin-node/blob/cb4f099ae928d627abfc635966622cf4266a7b1c/docs/rules/no-unpublished-import.md
