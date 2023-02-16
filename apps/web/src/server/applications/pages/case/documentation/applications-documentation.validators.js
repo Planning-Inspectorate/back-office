bdca92a6b60b0a52f2c5263d91698c233c965bc3
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-02-16 13:25:15 +0000

    fix(web/applications): change beahaviour when deleting readytopublish document (boas-769)

--- apps/web/src/server/applications/pages/case/documentation/applications-documentation.validators.js
+++ apps/web/src/server/applications/pages/case/documentation/applications-documentation.validators.js
@@ -9,8 +9,6 @@ export const validateApplicationsDocumentations = createValidator(
 
 export const validateApplicationsDocumentationsDeleteStatus = createValidator(
 	body('status')
-		.custom((value) => value !== 'ready_to_publish')
-		.withMessage('This document is in the publishing queue ready to be published.')
 		.custom((value) => value !== 'published')
 		.withMessage('This document is published.')
 );
