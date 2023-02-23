479e485d876ac7c1d94331e6f43bb41d23091895
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-02-23 14:07:44 +0000

    refactor(document-check-function): removes unnecessary duplication of env loading

--- apps/document-check-function/check-new-document/config.js
+++ apps/document-check-function/check-new-document/config.js
@@ -1,34 +1,5 @@
-// import { loadEnvironment } from '@pins/platform';
-import dotenv from 'dotenv';
+import { loadEnvironment } from '@pins/platform';
 import joi from 'joi';
-import path from 'node:path';
-
-/**
- * Evaluates all .env files, and returns a merged configuration of locally
- * defined variables.
- *
- * @param {string} [environment='development']
- * @returns {Record<string, string | undefined>}
- */
-export function loadEnvironment(environment = 'development') {
-	/** @type {Record<string, string | undefined>} */
-	const config = {};
-	const sourceFiles = ['.env', `.env.${environment}`];
-
-	// Ensure tests can run in a deterministic fashion regardless of changes made
-	// to local environment (such as during development)
-	if (environment !== 'test') {
-		sourceFiles.push('.env.local', `.env.${environment}.local`);
-	}
-
-	for (const pathToFile of sourceFiles) {
-		Object.assign(config, dotenv.config({ path: path.resolve(pathToFile) }).parsed);
-	}
-
-	return Object.fromEntries(
-		Object.entries({ ...config, ...process.env }).filter(([, value]) => Boolean(value))
-	);
-}
 
 const schema = joi.object({
 	NODE_ENV: joi.string().valid('development', 'production', 'test'),
