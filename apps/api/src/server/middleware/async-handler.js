7d3bd9ae62e4ac43c771dfbea9cdd57f3d3c42de
Xfarya <farya243@gmail.com>
2023-02-24 17:08:32 +0000

    feat(api/applications): some little doc updates (BOAS-17)

--- apps/api/src/server/middleware/async-handler.js
+++ apps/api/src/server/middleware/async-handler.js
@@ -1,7 +1,7 @@
 /**
  * @template T
  * @param {import('express').RequestHandler<T>} requestHandler
- * @returns {import('express').RequestHandler<any, any, any, any, any>}
+ * @returns {import('express').RequestHandler<T>}
  */
 export const asyncHandler = (requestHandler) => (request, response, next) =>
 	Promise.resolve(requestHandler(request, response, next)).catch(next);
