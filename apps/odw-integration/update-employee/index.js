ef7eedef4b2426d2b4dde37d0fdb91295885e5dd
Christopher Daly <christopher.daly@kinandcarta.com>
2023-02-27 10:23:31 +0000

    fix(api): remove pii from comment

--- apps/odw-integration/update-employee/index.js
+++ apps/odw-integration/update-employee/index.js
@@ -2,5 +2,5 @@
  * @type {import('@azure/functions').AzureFunction}
  */
 export const index = async (context, employee) => {
-	context.log('Received employee update', employee);
+	context.log('Received employee update', employee?.EmployeeId);
 };
