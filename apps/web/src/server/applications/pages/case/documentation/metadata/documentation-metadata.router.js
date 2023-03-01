1ab1ee78cb8bde1d99d371d2dfafefbe09df35d4
Lunix <lunix@lunix-swift.Home>
2023-03-01 14:07:27 +0000

    feat(web/applications): metadata form pages (boas-740)

---
+++ apps/web/src/server/applications/pages/case/documentation/metadata/documentation-metadata.router.js
@@ -0,0 +1,16 @@
+import { Router as createRouter } from 'express';
+import asyncRoute from '../../../../../lib/async-route.js';
+import * as controller from './documentation-metadata.controller.js';
+import * as locals from './documentation-metadata.locals.js';
+import * as validators from './documentation-metadata.validators.js';
+
+const applicationsDocumentationMetadataRouter = createRouter({ mergeParams: true });
+
+applicationsDocumentationMetadataRouter.use(locals.registerDocumentMetaData);
+
+applicationsDocumentationMetadataRouter
+	.route('/:metaDataName')
+	.get(asyncRoute(controller.viewDocumentationMetaData))
+	.post(validators.validatorsDispatcher, asyncRoute(controller.updateDocumentationMetaData));
+
+export default applicationsDocumentationMetadataRouter;
