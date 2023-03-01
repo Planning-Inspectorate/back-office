21769bebe80507bc0468d570c5a338f839896401
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-03-01 10:02:47 +0000

    feat(publish-document-function): allows connection to blob storage via connection string (BOAS-745)

--- apps/publish-document-function/publish-document/config.js
+++ apps/publish-document-function/publish-document/config.js
@@ -4,7 +4,7 @@ import joi from 'joi';
 const schema = joi.object({
 	NODE_ENV: joi.string().valid('development', 'production', 'test'),
 	API_HOST: joi.string(),
-	DOCUMENT_STORAGE_API_HOST: joi.string(),
+	DOCUMENT_STORAGE_CONNECTION_STRING: joi.string(),
 	log: joi.object({
 		levelStdOut: joi.string()
 	})
@@ -15,7 +15,7 @@ const environment = loadEnvironment(process.env.NODE_ENV);
 const { value, error } = schema.validate({
 	NODE_ENV: environment.NODE_ENV,
 	API_HOST: environment.API_HOST,
-	DOCUMENT_STORAGE_API_HOST: environment.DOCUMENT_STORAGE_API_HOST,
+	DOCUMENT_STORAGE_CONNECTION_STRING: environment.DOCUMENT_STORAGE_CONNECTION_STRING,
 	log: {
 		levelStdOut: environment.LOG_LEVEL_STDOUT || 'debug'
 	}
