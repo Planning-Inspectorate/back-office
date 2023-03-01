d2ed6ff43eff2d707769f82018b3266d48831d8a
Lunix <lunix@lunix-swift.Home>
2023-03-01 14:07:27 +0000

    feat(web/applications): metadata form pages (boas-740)

---
+++ apps/web/src/server/applications/pages/case/documentation/metadata/documentation-metadata.controller.js
@@ -0,0 +1,84 @@
+import { url } from '../../../../../lib/nunjucks-filters/url.js';
+import { updateDocumentMetaData } from './documentation-metadata.service.js';
+
+/** @typedef {import('@pins/express').ValidationErrors} ValidationErrors */
+/** @typedef {"name" | "description"} MetaDataNames */
+/** @typedef {{label: string, hint: string, pageTitle: string}} MetaDataLayoutParams */
+/** @typedef {{documentGuid: string, metaDataName: MetaDataNames}} RequestParams */
+/** @typedef {{caseId: number, folderId: number}} ResponseLocals */
+
+/** @type {Record<MetaDataNames, MetaDataLayoutParams>} */
+const layouts = {
+	name: {
+		label: 'What is the file name?',
+		hint: 'There is a limit of 255 characters',
+		pageTitle: 'Enter file name'
+	},
+	description: {
+		label: 'Description of the document',
+		hint: 'There is a limit of 800 characters',
+		pageTitle: 'Enter document description'
+	}
+};
+
+/**
+ * View the page for editing/creating documentation metadata
+ *
+ * @type {import('@pins/express').RenderHandler<{}, {}, {}, {}, RequestParams, ResponseLocals>}
+ */
+export async function viewDocumentationMetaData({ params }, response) {
+	const layoutParameters = getLayoutParameters(params, response.locals);
+
+	response.render(`applications/case-documentation/documentation-edit.njk`, layoutParameters);
+}
+
+/**
+ * Update changes for documentation metadata or return errors
+ *
+ *
+ * @type {import('@pins/express').RenderHandler<{}, {}, {}, {}, RequestParams, ResponseLocals>}
+ */
+export async function updateDocumentationMetaData(request, response) {
+	const { errors: validationErrors, params, body } = request;
+	const { caseId } = response.locals;
+	const { documentGuid } = params;
+
+	const { errors: apiErrors } = await updateDocumentMetaData(caseId, documentGuid, body);
+
+	if (validationErrors || apiErrors) {
+		const layoutParameters = getLayoutParameters(params, response.locals);
+
+		return response.render(`applications/case-documentation/documentation-edit.njk`, {
+			errors: validationErrors || apiErrors,
+			...layoutParameters
+		});
+	}
+	response.redirect('../properties');
+}
+
+/**
+ * Create layout parameters for metadata pages
+ *
+ * @param {RequestParams} requestParameters
+ * @param {ResponseLocals} responseLocals
+ * @returns {{metaDataName: MetaDataNames, layout: MetaDataLayoutParams, backLink: string }}
+ */
+const getLayoutParameters = (requestParameters, responseLocals) => {
+	const { documentGuid, metaDataName } = requestParameters;
+	const { caseId, folderId } = responseLocals;
+
+	const backLink = url('document', {
+		caseId,
+		folderId,
+		documentGuid,
+		step: 'properties'
+	});
+
+	const layout = layouts[metaDataName];
+
+	return {
+		metaDataName,
+		layout,
+		backLink
+	};
+};
---
+++ apps/web/src/server/applications/pages/case/documentation/metadata/documentation-metadata.validators.js
@@ -0,0 +1,43 @@
+import { createValidator } from '@pins/express';
+import { body } from 'express-validator';
+
+/** @typedef{import('express').RequestHandler} RequestHandler */
+
+/**
+ * Dispatch the POST route to the right validator
+ *
+ * @type {RequestHandler}
+ */
+export const validatorsDispatcher = async (request, response, next) => {
+	const { metaDataName } = request.params;
+
+	/** @type {Record<string, RequestHandler>} */
+	const validators = {
+		name: validateDocumentationMetaName,
+		description: validateDocumentationMetaDescription
+	};
+
+	if (validators[metaDataName]) {
+		return validators[metaDataName](request, response, next);
+	}
+
+	return next();
+};
+
+export const validateDocumentationMetaName = createValidator(
+	body('name')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Enter the file name')
+		.isLength({ max: 255 })
+		.withMessage('The name must be 255 characters or fewer')
+);
+
+export const validateDocumentationMetaDescription = createValidator(
+	body('description')
+		.trim()
+		.isLength({ min: 1 })
+		.withMessage('Enter the document description')
+		.isLength({ max: 800 })
+		.withMessage('The description must be 800 characters or fewer')
+);
