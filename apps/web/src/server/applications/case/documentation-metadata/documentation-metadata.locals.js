b531bd1d11d959183881f598619d4a6277013826
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-03-02 15:51:27 +0000

    fix(web/applications): fix backlink (boas-740)

--- apps/web/src/server/applications/case/documentation-metadata/documentation-metadata.locals.js
+++ apps/web/src/server/applications/case/documentation-metadata/documentation-metadata.locals.js
@@ -1,16 +1,30 @@
 import { getDocumentMetaData } from './documentation-metadata.service.js';
 
 /**
+ * Register url parameters.
+ *
+ * @type {import('express').RequestHandler<*, *, *, *, *>}
+ */
+export const registerUrlParameters = async ({ params }, response, next) => {
+	const { caseId, documentGuid, folderId } = params;
+
+	response.locals.folderId = Number.parseInt(folderId, 10);
+	response.locals.caseId = Number.parseInt(caseId, 10);
+	response.locals.documentGuid = documentGuid;
+
+	next();
+};
+
+/**
  * Register the metadata for the given document.
  *
  * @type {import('express').RequestHandler<*, *, *, *, *>}
  */
 export const registerDocumentMetaData = async ({ params }, response, next) => {
-	const { caseId, documentGuid, folderId } = params;
+	const { caseId, documentGuid } = params;
 
 	const documentMetaData = await getDocumentMetaData(caseId, documentGuid);
 
-	response.locals.folderId = Number.parseInt(folderId, 10);
 	response.locals.documentMetaData = documentMetaData;
 
 	next();
