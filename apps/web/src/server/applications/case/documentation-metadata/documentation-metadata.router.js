f13e85f2e3573deca5b0b45d95333b7407f8824b
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-03-02 15:47:14 +0000

    test(web/applications): add tests to metadata section (boas-740)

--- apps/web/src/server/applications/case/documentation-metadata/documentation-metadata.router.js
+++ apps/web/src/server/applications/case/documentation-metadata/documentation-metadata.router.js
@@ -1,11 +1,13 @@
 import { Router as createRouter } from 'express';
 import asyncRoute from '../../../lib/async-route.js';
+import { assertDomainTypeIsNotInspector } from '../../create-new-case/applications-create.guards.js';
 import * as controller from './documentation-metadata.controller.js';
 import * as locals from './documentation-metadata.locals.js';
 import * as validators from './documentation-metadata.validators.js';
 
 const applicationsDocumentationMetadataRouter = createRouter({ mergeParams: true });
 
+applicationsDocumentationMetadataRouter.use(assertDomainTypeIsNotInspector);
 applicationsDocumentationMetadataRouter.use(locals.registerDocumentMetaData);
 
 applicationsDocumentationMetadataRouter
