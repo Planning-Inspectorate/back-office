1ee336c5c9620725f85d1229f7ea5a11dcd7aa49
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-03-02 12:33:27 +0000

    refactor(web/applications): move search results to root folder (boas-740)

--- apps/web/src/server/applications/search-results/application-search.service.js
+++ apps/web/src/server/applications/search-results/application-search.service.js
@@ -1,8 +1,8 @@
-import { post } from '../../../lib/request.js';
+import { post } from '../../lib/request.js';
 
 /** @typedef {import('./applications-search.types.js').ApplicationsSearchResultsBody} ApplicationsSearchResultsBody */
-/** @typedef {import('../../applications.types').Case} Case */
-/** @typedef {import('../../applications.types.js').PaginatedResponse<Case>} PaginatedApplicationsResponse */
+/** @typedef {import('../applications.types').Case} Case */
+/** @typedef {import('../applications.types.js').PaginatedResponse<Case>} PaginatedApplicationsResponse */
 
 /**
  * @param {ApplicationsSearchResultsBody} payload
