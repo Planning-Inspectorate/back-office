a1a6fbef57cd9e9e94d9240687a992dae913ec1f
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-03-02 16:54:38 +0000

    fix(web/applications): fix generic type on metadata locals (boas-778)

--- apps/web/src/server/applications/case/documentation-metadata/documentation-metadata.locals.js
+++ apps/web/src/server/applications/case/documentation-metadata/documentation-metadata.locals.js
@@ -3,13 +3,13 @@ import { getDocumentMetaData } from './documentation-metadata.service.js';
 /**
  * Register url parameters.
  *
- * @type {import('express').RequestHandler<*, *, *, *, *>}
+ * @type {import('express').RequestHandler<{caseId: string, documentGuid: string, folderId: string}>}
  */
 export const registerUrlParameters = async ({ params }, response, next) => {
 	const { caseId, documentGuid, folderId } = params;
 
-	response.locals.folderId = Number.parseInt(folderId, 10);
-	response.locals.caseId = Number.parseInt(caseId, 10);
+	response.locals.folderId = Number.parseInt(`${folderId}`, 10);
+	response.locals.caseId = Number.parseInt(`${caseId}`, 10);
 	response.locals.documentGuid = documentGuid;
 
 	next();
@@ -18,12 +18,12 @@ export const registerUrlParameters = async ({ params }, response, next) => {
 /**
  * Register the metadata for the given document.
  *
- * @type {import('express').RequestHandler<*, *, *, *, *>}
+ * @type {import('express').RequestHandler<{caseId: string, documentGuid: string}>}
  */
 export const registerDocumentMetaData = async ({ params }, response, next) => {
 	const { caseId, documentGuid } = params;
 
-	const documentMetaData = await getDocumentMetaData(caseId, documentGuid);
+	const documentMetaData = await getDocumentMetaData(Number.parseInt(caseId, 10), documentGuid);
 
 	response.locals.documentMetaData = documentMetaData;
 
