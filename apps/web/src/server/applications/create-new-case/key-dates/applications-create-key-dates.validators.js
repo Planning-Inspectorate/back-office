11e2912a8c7df67585389f9f0ec5cfb4b03b63c8
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-03-07 17:32:16 +0000

    refactor(web/applications): generalize the date validtor (boas-752)

--- apps/web/src/server/applications/create-new-case/key-dates/applications-create-key-dates.validators.js
+++ apps/web/src/server/applications/create-new-case/key-dates/applications-create-key-dates.validators.js
@@ -1,60 +1,36 @@
 import { createValidator } from '@pins/express';
-import { body } from 'express-validator';
-import { dateIsValid } from '../../../lib/dates.js';
+import {
+	createValidationDateFuture,
+	createValidationDateValid
+} from '../../common/validators/dates.validators.js';
 
-export const validateApplicationsCreateKeyDates = createValidator(
-	body('keyDates.submissionDateInternal')
-		.custom((value, { req }) => {
-			const { submissionInternalDay } = req.body;
+/**
+ * Validator for key dates
+ *
+ * @type {import('express').RequestHandler}
+}
+ */
+export const validateApplicationsCreateKeyDates = (request, response, next) => {
+	const fieldName = 'keyDates.submissionDateInternal';
+	const extendedFieldName = 'anticipated submission date internal';
 
-			return (
-				submissionInternalDay === '' ||
-				(Number.parseInt(submissionInternalDay, 10) > 0 &&
-					Number.parseInt(submissionInternalDay, 10) < 32)
-			);
-		})
-		.withMessage('Enter a valid day for the anticipated submission date internal')
-		.custom((value, { req }) => {
-			const { submissionInternalMonth } = req.body;
+	const {
+		submissionInternalDay: day,
+		submissionInternalMonth: month,
+		submissionInternalYear: year
+	} = request.body;
 
-			return (
-				submissionInternalMonth === '' ||
-				(Number.parseInt(submissionInternalMonth, 10) > 0 &&
-					Number.parseInt(submissionInternalMonth, 10) < 13)
-			);
-		})
-		.withMessage('Enter a valid month for the anticipated submission date internal')
-		.custom((value, { req }) => {
-			const { submissionInternalYear } = req.body;
+	const checkValidDate = createValidationDateValid(fieldName, extendedFieldName, {
+		day,
+		month,
+		year
+	});
 
-			return submissionInternalYear === '' || `${submissionInternalYear}`.length === 4;
-		})
-		.withMessage('Please check the year of the date of the anticipated submission date internal')
-		.custom((value, { req }) => {
-			const { submissionInternalDay, submissionInternalMonth, submissionInternalYear } = req.body;
+	const checkFutureDate = createValidationDateFuture(fieldName, extendedFieldName, {
+		day,
+		month,
+		year
+	});
 
-			return (
-				(submissionInternalDay === '' &&
-					submissionInternalMonth === '' &&
-					submissionInternalMonth === '') ||
-				dateIsValid(submissionInternalYear, submissionInternalMonth, submissionInternalDay)
-			);
-		})
-		.withMessage('Enter a valid submission date')
-		.custom((value, { req }) => {
-			const { submissionInternalDay, submissionInternalMonth, submissionInternalYear } = req.body;
-			const submissionInternalDate = new Date(
-				submissionInternalYear,
-				Number.parseInt(submissionInternalMonth, 10) - 1,
-				submissionInternalDay
-			);
-
-			return (
-				(submissionInternalDay === '' &&
-					submissionInternalMonth === '' &&
-					submissionInternalYear === '') ||
-				submissionInternalDate > new Date()
-			);
-		})
-		.withMessage('The anticipated submission date internal must be in the future')
-);
+	return createValidator([checkValidDate, checkFutureDate])(request, response, next);
+};
