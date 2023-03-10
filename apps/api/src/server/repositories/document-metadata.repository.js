2fe4f45819b45c986cd9fb5fad54644328afd14a
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-03-10 16:42:47 +0000

    fix(api/applications): change fieldnames for dates (boas-779)

--- apps/api/src/server/repositories/document-metadata.repository.js
+++ apps/api/src/server/repositories/document-metadata.repository.js
@@ -1,24 +1,19 @@
 import { databaseConnector } from '../utils/database-connector.js';
 
 /**
+
  * @param {any} metadata
  * @returns {import('@prisma/client').PrismaPromise<import('@pins/api').Schema.DocumentVersion>}
  */
-export const upsert = (metadata) => {
-	if (metadata.datePublished) {
-		metadata.publishedDate = metadata.datePublished;
-		delete metadata.datePublished;
-	}
 
-	if (metadata.dateCreated) {
-		metadata.receivedDate = metadata.dateCreated;
-		delete metadata.dateCreated;
-	}
-
+export const upsert = ({ documentGuid, version = 1, ...metadata }) => {
 	return databaseConnector.documentVersion.upsert({
 		create: { ...metadata, version, Document: { connect: { guid: documentGuid } } },
+
 		where: { documentGuid_version: { documentGuid, version } },
+
 		update: { ...metadata, version },
+
 		include: {
 			Document: {
 				include: {
@@ -38,9 +33,11 @@ export const upsert = (metadata) => {
 };
 
 /**
+
  * @param {{guid: string, status: import('xstate').StateValue, version: number }} documentStatusUpdate
  * @returns {import('@prisma/client').PrismaPromise<import('@pins/api').Schema.DocumentVersion>}
  */
+
 export const updateDocumentStatus = ({ guid, status, version = 1 }) => {
 	return databaseConnector.documentVersion.update({
 		where: { documentGuid_version: { documentGuid: guid, version } },
@@ -49,15 +46,20 @@ export const updateDocumentStatus = ({ guid, status, version = 1 }) => {
 };
 
 /**
+
  * Get a document metadata by documentGuid
+
  *
+
  * @param {string} documentGuid
  * @param {number} version
  * @returns {import('@prisma/client').PrismaPromise<import('@pins/api').Schema.DocumentVersion |null>}
  */
+
 export const getById = (documentGuid, version = 1) => {
 	return databaseConnector.documentVersion.findUnique({
 		where: { documentGuid_version: { documentGuid, version } },
+
 		include: {
 			Document: {
 				include: {
@@ -75,24 +77,33 @@ export const getById = (documentGuid, version = 1) => {
 		}
 	});
 };
+
 /**
+
  * Get all document metadata
+
  *
+
  * @returns {import('@prisma/client').PrismaPromise<import('@pins/api').Schema.DocumentVersion[] |null>}
  */
+
 export const getAll = () => {
 	return databaseConnector.documentVersion.findMany();
 };
 
 /**
+
  *
+
  * @param {string} documentGuid
  * @param {import('@pins/api').Schema.DocumentVersionUpdateInput} documentDetails
  * @returns {import('@prisma/client').PrismaPromise<import('@pins/api').Schema.DocumentVersion>}
  */
+
 export const update = (documentGuid, { version = 1, ...documentDetails }) => {
 	return databaseConnector.documentVersion.update({
 		where: { documentGuid_version: { documentGuid, version } },
+
 		data: documentDetails
 	});
 };
