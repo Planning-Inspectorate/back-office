04a747011ea3de82fdaeef9f1751f172c2843472
personal <personal@personals-MacBook-Pro.local>
2023-03-15 10:11:20 +0000

    feat(api/applications): reverting back to normal pipeline structure

--- apps/api/src/server/repositories/document-metadata.repository.js
+++ apps/api/src/server/repositories/document-metadata.repository.js
@@ -34,7 +34,7 @@ export const upsert = ({ documentGuid, version = 1, ...metadata }) => {
 
 /**
  
- * @param {{guid: string, status: import('xstate').StateValue, version: number }} documentStatusUpdate
+ * @param {{guid: string, status: string, version?: number }} documentStatusUpdate
  * @returns {import('@prisma/client').PrismaPromise<import('@pins/api').Schema.DocumentVersion>}
  */
 
@@ -42,6 +42,21 @@ export const updateDocumentStatus = ({ guid, status, version = 1 }) => {
 	return databaseConnector.documentVersion.update({
 		where: { documentGuid_version: { documentGuid: guid, version } },
 		data: { publishedStatus: status }
+	});
+};
+
+/**
+ *  Deletes a document from the database based on its `guid`
+ *
+ * @async
+ * @param {string} documentGuid
+ * @returns {import('@prisma/client').PrismaPromise<import('@pins/api').Schema.DocumentVersion>}
+ */
+export const deleteDocument = (documentGuid) => {
+	return databaseConnector.document.delete({
+		where: {
+			guid: documentGuid
+		}
 	});
 };
 
