34add441f97fb2405e6715e3a98ce45a79f60a74
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-03-16 12:47:01 +0000

    fix(document-publish-function): gets document info from azure service bus message (BOAS-745)

--- apps/document-publish-function/publish-document/index.js
+++ apps/document-publish-function/publish-document/index.js
@@ -4,7 +4,9 @@ import { publishDocument } from './publish-document.js';
  * @type {import('@azure/functions').AzureFunction}
  */
 export const index = async (context, myQueueItem) => {
-	await publishDocument(context, myQueueItem);
+	const { documentToCopy, documentDestination } = myQueueItem;
+
+	await publishDocument(context, documentToCopy, documentDestination);
 	context.log('Publishing document');
 	context.log(myQueueItem);
 };
