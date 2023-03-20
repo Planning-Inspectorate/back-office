22816a68b08d66b578bf300bc76c9733b9117d71
David Beale <david.beale@kinandcarta.com>
2023-03-20 14:58:15 +0000

    feat(api/applications): pagination stub

--- apps/api/src/server/repositories/representation.repository.js
+++ apps/api/src/server/repositories/representation.repository.js
@@ -3,8 +3,32 @@ import { databaseConnector } from '../utils/database-connector.js';
 /**
  *
  * @param {number} caseId
- * @returns {Promise<import('@pins/api').Schema.Representation[]>}
+ * @returns {Promise<{ count: number, items: any[]}>}
  */
-export const getByCaseId = (caseId) => {
-	return databaseConnector.representation.findMany({ where: { caseId } });
+export const getByCaseId = async (caseId) => {
+	const where = { caseId };
+
+	const [count, items] = await databaseConnector.$transaction([
+		databaseConnector.representation.count({
+			where
+		}),
+		databaseConnector.representation.findMany({
+			select: {
+				id: true,
+				reference: true,
+				status: true,
+				originalRepresentation: true,
+				redactedRepresentation: true,
+				redacted: true,
+				received: true
+			},
+			where,
+			take: 25
+		})
+	]);
+
+	return {
+		count,
+		items
+	};
 };
