fde4ea621daf4326254d29edeea2799f45b11061
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-03-28 16:48:49 +0100

    test: fixes mocking out azure storage blob package (BOAS-745)

--- apps/document-publish-function/.eslintrc.cjs
+++ apps/document-publish-function/.eslintrc.cjs
@@ -7,6 +7,12 @@ module.exports = {
 				// https://github.com/weiran-zsd/eslint-plugin-node/blob/cb4f099ae928d627abfc635966622cf4266a7b1c/docs/rules/no-unpublished-import.md
 				'n/no-unpublished-import': 'off',
 				'no-undef': 'off',
+				'max-classes-per-file': ['off'],
+			}
+		},
+		{
+			files: ['setup-tests.js'],
+			rules: {
 				'max-classes-per-file': ['off']
 			}
 		},
--- apps/document-publish-function/publish-document/__tests__/publish-document.test.js
+++ apps/document-publish-function/publish-document/__tests__/publish-document.test.js
@@ -1,113 +1,59 @@
 // @ts-nocheck
 import { jest } from '@jest/globals';
-// import nock from 'nock';
-// import { publishDocument } from '../publish-document.js';
+import { publishDocument } from '../publish-document.js';
 
-// class Context {
-// 	constructor() {
-// 		this.log = () => {
-// 			jest.fn();
-// 		};
-// 		this.log.verbose = () => {
-// 			jest.fn();
-// 		};
-// 		this.log.info = () => {
-// 			jest.fn();
-// 		};
-// 		this.log.warn = () => {
-// 			jest.fn();
-// 		};
-// 		this.log.error = () => {
-// 			jest.fn();
-// 		};
-// 	}
-// }
-
-// nock('http://127.0.0.1:10000').persist().get(/.*/).reply(200, [], {'Content-Length': 1, ETag: "0x8CB171DBEAD6A6B"});
-// nock('http://127.0.0.1:10000').persist().patch(/.*/).reply(200);
-// nock('http://127.0.0.1:10000').persist().put(/.*/).reply(200);
-
-class MockBlockBlobClient {
-	constructor(blobPath) {
-		this.blobPath = blobPath;
-	}
-
-	async download() {
-		return Promise.new({ readableStreamBody: new ReadableStream() });
-	}
-
-	async uploadStream(stream) {
-		return Promise.new(stream);
+class Context {
+	constructor() {
+		this.log = () => {
+			jest.fn();
+		};
+		this.log.verbose = () => {
+			jest.fn();
+		};
+		this.log.info = () => {
+			jest.fn();
+		};
+		this.log.warn = () => {
+			jest.fn();
+		};
+		this.log.error = () => {
+			jest.fn();
+		};
 	}
 }
 
-class MockContainerClient {
-	constructor(container) {
-		this.container = container;
-	}
+describe('Publishing document', () => {
+	test(
+		'Newly received document ' +
+			'is copied over to the public blob storage container ' +
+			'applications front office notified and back office notified',
+		async () => {
+			// GIVEN
+			const documentContainer = 'some-container';
+			const documentPath = 'some-path';
+			const documentDestinationContainer = 'some-published-container';
+			const documentDestinationPath = 'some-new-path';
 
-	getBlockBlobClient(blobPath) {
-		return new MockBlockBlobClient(blobPath);
-	}
-}
+			// WHEN
+			await publishDocument(
+				new Context(),
+				{
+					container: documentContainer,
+					path: documentPath
+				},
+				{
+					container: documentDestinationContainer,
+					path: documentDestinationPath
+				}
+			);
 
-class MockBlobServiceClient {
-	constructor(connectionString) {
-		this.connectionString = connectionString;
-	}
+			// THEN
+			// expect(backOfficeApiClient).toHaveBeenCalledOnce();
+		}
+	);
 
-	static fromConnectionString(connectionString) {
-		return new MockBlobServiceClient(connectionString);
-	}
-
-	getContainerClient(container) {
-		return new MockContainerClient(container);
-	}
-}
-
-jest.unstable_mockModule('@azure/storage-blob', () => ({
-	BlobServiceClient: MockBlobServiceClient
-}));
-
-// const blobStorageClient = jest.fn();
-// const backOfficeApiClient = jest.fn();
-
-test('test', () => {
-	expect(true).toEqual(true);
+	test.todo(
+		'A document that has already been copied over to the public blob storage container ' +
+			'should not block notification to applications front office and back office'
+	);
 });
-// describe('Publishing document', () => {
-// 	test(
-// 		'Newly received document ' +
-// 			'is copied over to the public blob storage container ' +
-// 			'applications front office notified and back office notified',
-// 		async () => {
-// 			// GIVEN
-// 			const documentContainer = 'some-container';
-// 			const documentPath = 'some-path';
-// 			const documentDestinationContainer = 'some-published-container';
-// 			const documentDestinationPath = 'some-new-path';
-
-// 			// WHEN
-// 			await publishDocument(
-// 				new Context(),
-// 				{
-// 					container: documentContainer,
-// 					path: documentPath
-// 				},
-// 				{
-// 					container: documentDestinationContainer,
-// 					path: documentDestinationPath
-// 				}
-// 			);
-
-// 			// THEN
-// 			// expect(blobStorageClient).toHaveBeenCalledOnce();
-// 			// expect(backOfficeApiClient).toHaveBeenCalledOnce();
-// 		}
-// 	);
-
-// 	test.todo(
-// 		'A document that has already been copied over to the public blob storage container ' +
-// 			'should not block notification to applications front office and back office'
-// 	);
-// });
