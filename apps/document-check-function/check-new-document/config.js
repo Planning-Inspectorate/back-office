492571120da127d2ef4a4027e1cd41e620dde200
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-04-20 13:38:35 +0100

    fix(document-check-function): uses blob store host as connection (BOAS-834)

--- apps/document-check-function/check-new-document/config.js
+++ apps/document-check-function/check-new-document/config.js
@@ -4,7 +4,7 @@ import joi from 'joi';
 const schema = joi.object({
 	NODE_ENV: joi.string().valid('development', 'production', 'test'),
 	API_HOST: joi.string(),
-	DOCUMENT_STORAGE_API_HOST: joi.string(),
+	AzureWebJobsStorage__blobServiceUri: joi.string(),
 	CLAM_AV_HOST: joi.string(),
 	CLAM_AV_PORT: joi.number(),
 	log: joi.object({
@@ -17,7 +17,7 @@ const environment = loadEnvironment(process.env.NODE_ENV);
 const { value, error } = schema.validate({
 	NODE_ENV: environment.NODE_ENV,
 	API_HOST: environment.API_HOST,
-	DOCUMENT_STORAGE_API_HOST: environment.DOCUMENT_STORAGE_API_HOST,
+	DOCUMENT_STORAGE_API_HOST: environment.AzureWebJobsStorage__blobServiceUri,
 	CLAM_AV_HOST: environment.CLAM_AV_HOST,
 	CLAM_AV_PORT: environment.CLAM_AV_PORT,
 	log: {
