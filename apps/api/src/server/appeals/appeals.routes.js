2ad035d69e2c302b2bc6a8e995e71a9d96b4a2dc
shepda <david@x-designs.co.uk>
2023-04-21 16:26:04 +0100

    feat(api): add /appeals route for returning appeals list data (BOAT-40)

--- apps/api/src/server/appeals/appeals.routes.js
+++ apps/api/src/server/appeals/appeals.routes.js
@@ -1,9 +1,12 @@
 import { Router as createRouter } from 'express';
+import { appealsRoutes } from './appeals/appeals.routes.js';
 import { caseOfficerRoutes } from './case-officer/case-officer.routes.js';
 import { inspectorRoutes } from './inspector/inspector.routes.js';
 import { validationRoutes } from './validation/validation.routes.js';
 
 const router = createRouter();
+
+router.use('/', appealsRoutes);
 
 router.use('/validation', validationRoutes);
 
