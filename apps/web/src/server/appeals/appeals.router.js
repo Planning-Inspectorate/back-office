0514dfe3421a4f4a28822cb4f0cf01f2b94e23d9
james-hill-kc <james.hill@kinandcarta.com>
2023-04-21 16:34:30 +0100

    BOAT-53 and BOAT-54: add nunjucks template for national list dashboard

--- apps/web/src/server/appeals/appeals.router.js
+++ apps/web/src/server/appeals/appeals.router.js
@@ -7,6 +7,17 @@ import validationRouter from './validation/validation.router.js';
 
 const router = createRouter();
 
+// Temporary placeholder for router being developed in BOAT-55
+import * as validationService from './validation/validation.service.js';
+router.route('/appeals-list').get(async (req, response) => {
+	const appeals = await validationService.findAllAppeals();
+
+	response.render('appeals/all-appeals/dashboard.njk', {
+		userRole: 'Case officer',
+		appeals
+	});
+});
+
 router.use(
 	'/validation',
 	assertGroupAccess(config.referenceData.appeals.validationOfficerGroupId),
