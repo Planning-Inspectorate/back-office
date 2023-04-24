e17efcf9edd3d65f94c35401e9bf411da06e46c2
David White <50454208+DavidJWhiteDev@users.noreply.github.com>
2023-04-24 18:58:14 +0100

    feat(api/applications): tests for examination-timetable-type api (BOAS-814)

---
+++ apps/api/src/server/applications/examination-timetable-type/__tests__/get-examination-timetable-type.test.js
@@ -0,0 +1,70 @@
+import { jest } from '@jest/globals';
+import supertest from 'supertest';
+import { app } from '../../../app-test.js';
+import { nodeCache, setCache } from '../../../utils/cache-data.js';
+
+const { databaseConnector } = await import('../../../utils/database-connector.js');
+
+const request = supertest(app);
+
+const examinationTimetableTypes = {
+	id: 1,
+	name: 'Accompanied Site Inspection',
+	displayNameEn: 'Accompanied site inspection',
+	displayNameCy: 'Accompanied site inspection Cy'
+};
+
+describe('Get examination timetable types', () => {
+	beforeEach(() => {
+		nodeCache.flushAll();
+	});
+
+	afterEach(() => {
+		jest.resetAllMocks();
+	});
+
+	test('gets all examination timetable types', async () => {
+		// GIVEN
+		databaseConnector.examinationTimetableType.findMany.mockResolvedValue([
+			examinationTimetableTypes
+		]);
+
+		// WHEN
+		const resp = await request.get('/applications/examination-timetable-type');
+
+		// THEN
+		expect(resp.status).toEqual(200);
+		expect(resp.body).toEqual([
+			{
+				id: examinationTimetableTypes.id,
+				name: examinationTimetableTypes.name,
+				displayNameEn: examinationTimetableTypes.displayNameEn,
+				displayNameCy: examinationTimetableTypes.displayNameCy
+			}
+		]);
+	});
+});
+
+test('tests if cached data for examination-timetable-type call is returned without hitting db', async () => {
+	const cachedExaminationTimetableTypes = {
+		id: 2,
+		name: 'cached test',
+		displayNameEn: 'cached test name en',
+		displayNameCy: 'cached test name cy'
+	};
+
+	setCache('examination-timetable-type', [cachedExaminationTimetableTypes]);
+
+	const resp = await request.get('/applications/examination-timetable-type');
+
+	expect(databaseConnector.examinationTimetableType.findMany).not.toHaveBeenCalled();
+	expect(resp.status).toEqual(200);
+	expect(resp.body).toEqual([
+		{
+			id: cachedExaminationTimetableTypes.id,
+			name: cachedExaminationTimetableTypes.name,
+			displayNameEn: cachedExaminationTimetableTypes.displayNameEn,
+			displayNameCy: cachedExaminationTimetableTypes.displayNameCy
+		}
+	]);
+});
