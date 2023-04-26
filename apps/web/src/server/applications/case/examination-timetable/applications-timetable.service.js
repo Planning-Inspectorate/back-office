a2ab33fef71d9c7273a43d9ea2a2abe4ca261cfe
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-04-26 17:14:47 +0100

    feat(web/applications): replace mock service with real one (boas-487)

--- apps/web/src/server/applications/case/examination-timetable/applications-timetable.service.js
+++ apps/web/src/server/applications/case/examination-timetable/applications-timetable.service.js
@@ -1,4 +1,4 @@
-// import { get } from '../../../lib/request.js';
+import { get } from '../../../lib/request.js';
 
 /**
  * Get the timetable item types
@@ -6,36 +6,5 @@
  * @returns {Promise<{name: string, templateType: string, displayNameEn: string, displayNameCy: string}[]>}
  */
 export const getCaseTimetableItemTypes = async () => {
-	// return get(`applications//documents/${fileGuid}/properties`);
-
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve([
-				{
-					name: 'Accompanied Site Inspection',
-					displayNameEn: 'Accompanied site inspection',
-					displayNameCy: 'Accompanied site inspection',
-					templateType: 'starttime-mandatory'
-				},
-				{
-					name: 'Compulsory Acquisition Hearing',
-					displayNameEn: 'Compulsory acquisition hearing',
-					displayNameCy: 'Compulsory acquisition hearing',
-					templateType: 'deadline'
-				},
-				{
-					name: 'Deadline',
-					displayNameEn: 'Deadline',
-					displayNameCy: 'Deadline',
-					templateType: 'deadline-startdate-mandatory'
-				},
-				{
-					name: 'Deadline For Close Of Examination',
-					displayNameEn: 'Deadline for close of examination',
-					displayNameCy: 'Deadline for close of examination',
-					templateType: 'no-times'
-				}
-			]);
-		}, 300);
-	});
+	return get(`applications/examination-timetable-type`);
 };
