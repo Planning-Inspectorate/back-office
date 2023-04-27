331386089f6fa160c0a0e214015af67f84f33e5a
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-04-27 17:14:18 +0100

    test(document-publish-function): fixes env vars for tests (BOAS-834)

--- apps/document-publish-function/publish-document/config.js
+++ apps/document-publish-function/publish-document/config.js
@@ -7,6 +7,7 @@ const schema = joi.object({
 	DOCUMENT_STORAGE_CONNECTION_STRING: joi.string(),
 	DOCUMENT_STORAGE_HOST: joi.string(),
 	NSIP_DOC_TOPIC_NAME: joi.string(),
+	SERVICE_BUS_HOSTNAME: joi.string(),
 	log: joi.object({
 		levelStdOut: joi.string()
 	})
@@ -19,7 +20,8 @@ const { value, error } = schema.validate({
 	API_HOST: environment.API_HOST,
 	DOCUMENT_STORAGE_CONNECTION_STRING: environment.DOCUMENT_STORAGE_CONNECTION_STRING,
 	DOCUMENT_STORAGE_HOST: environment.DOCUMENT_STORAGE_HOST,
-	NSIP_DOC_EVENT_QUEUE: environment.NSIP_DOC_TOPIC_NAME,
+	NSIP_DOC_TOPIC_NAME: environment.NSIP_DOC_TOPIC_NAME,
+	SERVICE_BUS_HOSTNAME: environment.SERVICE_BUS_HOSTNAME,
 	log: {
 		levelStdOut: environment.LOG_LEVEL_STDOUT || 'debug'
 	}
