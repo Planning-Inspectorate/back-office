913da17c6f7d6a8bfea58feca9b849f455f610ba
Yan Jeyko <yan.jeyko@planninginspectorate.gov.uk>
2023-04-27 16:23:17 +0100

    feat(web): add web router controller and mapper for national list

--- apps/web/src/server/appeals/appeals.router.js
+++ apps/web/src/server/appeals/appeals.router.js
@@ -2,21 +2,17 @@ import config from '@pins/web/environment/config.js';
 import { Router as createRouter } from 'express';
 import { assertGroupAccess } from '../app/auth/auth.guards.js';
 import caseOfficerRouter from '../appeals/case-officer/case-officer.router.js';
+import nationalListRouter from '../appeals/national-list/national-list.router.js';
 import inspectorRouter from './inspector/inspector.router.js';
 import validationRouter from './validation/validation.router.js';
 
 const router = createRouter();
 
-// Temporary placeholder for router being developed in BOAT-55
-import * as validationService from './validation/validation.service.js';
-router.route('/appeals-list').get(async (req, response) => {
-	const appeals = await validationService.findAllAppeals();
-
-	response.render('appeals/all-appeals/dashboard.njk', {
-		userRole: 'Case officer',
-		appeals
-	});
-});
+router.use(
+	'/appeals-list',
+	assertGroupAccess(config.referenceData.appeals.caseOfficerGroupId),
+	nationalListRouter
+);
 
 router.use(
 	'/validation',
