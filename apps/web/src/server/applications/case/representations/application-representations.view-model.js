28cf9b513f614076e71f91202a22498e7d28353e
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2023-04-27 13:55:23 +0100

    feat(web/applications): add representation details page

--- apps/web/src/server/applications/case/representations/application-representations.view-model.js
+++ apps/web/src/server/applications/case/representations/application-representations.view-model.js
@@ -56,7 +56,7 @@ const getRedacted = ({ redacted }) => (redacted ? 'Redacted' : 'Unredacted');
 /**
  *
  * @param {object} args
- * @param {Array.<{reference: string, organisationName?: string, firstName?: string , lastName?: string, received: string, redacted: boolean, status: string}>} args.items
+ * @param {Array.<{reference: string, organisationName?: string, firstName?: string , lastName?: string, received: string, redacted: boolean, status: string, id: string}>} args.items
  * @returns {Array.<{}>}
  */
 export function getRepresentationsViewModel({ items }) {
@@ -65,7 +65,8 @@ export function getRepresentationsViewModel({ items }) {
 		title: getTitle(rep),
 		received: formatDate(rep),
 		redacted: getRedacted(rep),
-		status: getStatus(rep)
+		status: getStatus(rep),
+		id: rep.id
 	}));
 }
 
