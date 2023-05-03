3ae9b1eaf78763d2c271f311a216ab8def2f395c
Christopher Daly <christopher.daly@kinandcarta.com>
2023-05-03 10:55:21 +0100

    fix(api): set base reference number if one doesn't exist

--- apps/api/src/server/repositories/raw_sql_queries/update-application-reference.sql
+++ apps/api/src/server/repositories/raw_sql_queries/update-application-reference.sql
@@ -3,24 +3,24 @@ declare @sub_sector_abbreviation nchar(4), @max_reference int, @reference_number
 declare @minimum_new_reference int = 10001;
 declare @id int = CASE_ID;
 
-select @sub_sector_abbreviation = sub_sector_table.abbreviation 
-FROM [dbo].[Case] as case_table 
-    join [dbo].[ApplicationDetails] as application_details_table 
-    on case_table.id = application_details_table.caseId 
-    join [dbo].[SubSector] as sub_sector_table 
-    on application_details_table.subSectorId = sub_sector_table.id 
-where case_table.id = @id; 
+select @sub_sector_abbreviation = sub_sector_table.abbreviation
+FROM [dbo].[Case] as case_table
+    join [dbo].[ApplicationDetails] as application_details_table
+    on case_table.id = application_details_table.caseId
+    join [dbo].[SubSector] as sub_sector_table
+    on application_details_table.subSectorId = sub_sector_table.id
+where case_table.id = @id;
 
-SELECT @max_reference = max(cast(SUBSTRING(case_table.reference, 5, len(case_table.reference)) as int)) 
-FROM [dbo].[Case] as case_table 
-    join [dbo].[ApplicationDetails] as application_details_table 
-    on case_table.id = application_details_table.caseId 
-    join [dbo].[SubSector] as sub_sector_table 
-    on application_details_table.subSectorId = sub_sector_table.id 
-        and sub_sector_table.abbreviation = @sub_sector_abbreviation; 
+SELECT @max_reference = max(cast(SUBSTRING(case_table.reference, 5, len(case_table.reference)) as int))
+FROM [dbo].[Case] as case_table
+    join [dbo].[ApplicationDetails] as application_details_table
+    on case_table.id = application_details_table.caseId
+    join [dbo].[SubSector] as sub_sector_table
+    on application_details_table.subSectorId = sub_sector_table.id
+        and sub_sector_table.abbreviation = @sub_sector_abbreviation;
 
-if(@max_reference < @minimum_new_reference) select @reference_number = @minimum_new_reference else select @reference_number = @max_reference + 1;
+if(@max_reference < @minimum_new_reference OR @max_reference IS NULL) select @reference_number = @minimum_new_reference else select @reference_number = @max_reference + 1;
 
-update [dbo].[Case] 
-    set reference = @sub_sector_abbreviation + cast(@reference_number as nchar(5)) 
+update [dbo].[Case]
+    set reference = CONCAT(@sub_sector_abbreviation, cast(@reference_number as nchar(5)))
     where id = @id;
