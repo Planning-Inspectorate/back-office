869f42b52dcbd30fd8a2b5f365d1ff1a2bca2782
shepda <david@x-designs.co.uk>
2023-05-04 09:26:13 +0100

    feat(api): add pagination for /appeals route

--- apps/api/src/server/appeals/appeals/__tests__/appeals.test.js
+++ apps/api/src/server/appeals/appeals/__tests__/appeals.test.js
@@ -3,7 +3,6 @@ import { app } from '../../../app-test.js';
 const { databaseConnector } = await import('../../../utils/database-connector.js');
 
 const request = supertest(app);
-
 const appeal = {
 	id: 1,
 	reference: 'APP/Q9999/D/21/1345264',
@@ -45,15 +44,25 @@ const appeal = {
 		outcome: 'Not issued yet'
 	}
 };
+const appealTwo = {
+	...appeal,
+	id: 2
+};
 
 describe('Appeals', () => {
 	describe('/appeals', () => {
-		test('get all appeals', async () => {
+		test('gets appeals when not given pagination params', async () => {
 			// @ts-ignore
-			databaseConnector.appeal.findMany.mockResolvedValue([appeal]);
+			databaseConnector.appeal.findMany.mockResolvedValue([appeal, appealTwo]);
 
 			const response = await request.get('/appeals');
 
+			expect(databaseConnector.appeal.findMany).toBeCalledWith(
+				expect.objectContaining({
+					skip: 0,
+					take: 30
+				})
+			);
 			expect(response.status).toEqual(200);
 			expect(response.body).toEqual([
 				{
@@ -69,13 +78,124 @@ describe('Appeals', () => {
 					appealType: appeal.appealType.type,
 					createdAt: appeal.createdAt.toISOString(),
 					localPlanningDepartment: appeal.localPlanningDepartment
+				},
+				{
+					appealId: appealTwo.id,
+					appealReference: appeal.reference,
+					appealSite: {
+						addressLine1: appeal.address.addressLine1,
+						town: appeal.address.town,
+						county: appeal.address.county,
+						postCode: appeal.address.postcode
+					},
+					appealStatus: appeal.appealStatus[0].status,
+					appealType: appeal.appealType.type,
+					createdAt: appeal.createdAt.toISOString(),
+					localPlanningDepartment: appeal.localPlanningDepartment
 				}
 			]);
+		});
+
+		test('gets appeals when given pagination params', async () => {
+			// @ts-ignore
+			databaseConnector.appeal.findMany.mockResolvedValue([appealTwo]);
+
+			const response = await request.get('/appeals?pageNumber=2&pageSize=1');
+
+			expect(databaseConnector.appeal.findMany).toBeCalledWith(
+				expect.objectContaining({
+					skip: 1,
+					take: 1
+				})
+			);
+			expect(response.status).toEqual(200);
+			expect(response.body).toEqual([
+				{
+					appealId: appealTwo.id,
+					appealReference: appeal.reference,
+					appealSite: {
+						addressLine1: appeal.address.addressLine1,
+						town: appeal.address.town,
+						county: appeal.address.county,
+						postCode: appeal.address.postcode
+					},
+					appealStatus: appeal.appealStatus[0].status,
+					appealType: appeal.appealType.type,
+					createdAt: appeal.createdAt.toISOString(),
+					localPlanningDepartment: appeal.localPlanningDepartment
+				}
+			]);
+		});
+
+		test('returns an error if pageNumber is given and pageSize is not given', async () => {
+			const response = await request.get('/appeals?pageNumber=1');
+
+			expect(response.status).toEqual(400);
+			expect(response.body).toEqual({
+				errors: {
+					pageNumber: 'Both pageNumber and pageSize are required for pagination'
+				}
+			});
+		});
+
+		test('returns an error if pageSize is given and pageNumber is not given', async () => {
+			const response = await request.get('/appeals?pageSize=1');
+
+			expect(response.status).toEqual(400);
+			expect(response.body).toEqual({
+				errors: {
+					pageSize: 'Both pageNumber and pageSize are required for pagination'
+				}
+			});
+		});
+
+		test('returns an error if pageNumber is not numeric', async () => {
+			const response = await request.get('/appeals?pageNumber=one&pageSize=1');
+
+			expect(response.status).toEqual(400);
+			expect(response.body).toEqual({
+				errors: {
+					pageNumber: 'Must be a number'
+				}
+			});
+		});
+
+		test('returns an error if pageNumber is less than 1', async () => {
+			const response = await request.get('/appeals?pageNumber=-1&pageSize=1');
+
+			expect(response.status).toEqual(400);
+			expect(response.body).toEqual({
+				errors: {
+					pageNumber: 'Must be greater than 0'
+				}
+			});
+		});
+
+		test('returns an error if pageSize is not numeric', async () => {
+			const response = await request.get('/appeals?pageNumber=1&pageSize=one');
+
+			expect(response.status).toEqual(400);
+			expect(response.body).toEqual({
+				errors: {
+					pageSize: 'Must be a number'
+				}
+			});
+		});
+
+		test('returns an error if pageSize is less than 1', async () => {
+			const response = await request.get('/appeals?pageNumber=1&pageSize=-1');
+
+			expect(response.status).toEqual(400);
+			expect(response.body).toEqual({
+				errors: {
+					pageSize: 'Must be greater than 0'
+				}
+			});
 		});
 	});
 
 	describe('/appeals/:appealId', () => {
-		test('get a single appeal', async () => {
+		test('gets a single appeal', async () => {
 			// @ts-ignore
 			databaseConnector.appeal.findUnique.mockResolvedValue(appeal);
 
