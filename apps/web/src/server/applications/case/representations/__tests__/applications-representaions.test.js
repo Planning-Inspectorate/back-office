2caa409a50b95ae7e15dfb212967c8559f07341b
Jonathan Goulding <jonathangoulding21@gmail.com>
2023-05-05 13:48:34 +0100

    feat(web/applications): add representations pagination

--- apps/web/src/server/applications/case/representations/__tests__/applications-representaions.test.js
+++ apps/web/src/server/applications/case/representations/__tests__/applications-representaions.test.js
@@ -13,7 +13,13 @@ const nocks = () => {
 	nock('http://test/')
 		.get(`/applications/1/representations`)
 		.query({ searchTerm: 'mock-search-term', sortBy: '', page: 1, pageSize: 25 })
-		.reply(200, { items: [representationsFixture.items[0]] })
+		.reply(200, {
+			page: 1,
+			pageSize: 25,
+			pageCount: 1,
+			itemCount: 3,
+			items: [representationsFixture.items[0]]
+		})
 		.persist();
 	nock('http://test/')
 		.get(`/applications/1/representations`)
--- apps/web/src/server/applications/case/representations/applications-relevant-reps.controller.js
+++ apps/web/src/server/applications/case/representations/applications-relevant-reps.controller.js
@@ -1,20 +1,16 @@
+import * as express from 'express';
+import { buildQueryString } from '../../../../common/build-query-string.js';
 import {
 	getCaseReferenceViewModel,
 	getRepresentationsViewModel
 } from './application-representations.view-model.js';
 import { getCase, getRepresentations } from './applications-relevant-reps.service.js';
+import { getPagination } from './utils/pagination.js';
+import { hasSearchUpdated } from './utils/search/has-search-updated.js';
+import { tableSortLinks } from './utils/table.js';
+
 const view = 'applications/representations/representations.njk';
 
-import querystring from 'node:querystring';
-
-/**
- *
- * @param {any} query
- * @returns {string}
- */
-const buildQueryString = (query) => querystring.stringify(query);
-
-import * as express from 'express';
 /**
  * @param {express.Request} req
  * @param {express.Response} res
@@ -22,30 +18,28 @@ import * as express from 'express';
 export async function relevantRepsApplications({ params, query }, res) {
 	const { caseId } = params;
 
+	hasSearchUpdated(query);
+
 	const { searchTerm, sortBy, pageSize = 25, page = 1 } = query;
-
-	const queryString = buildQueryString({
-		searchTerm,
-		sortBy,
-		pageSize,
-		page
-	});
-
 	const caseReference = await getCase(caseId);
-	const representations = await getRepresentations(caseId, queryString);
-
-	const representationsVieModel = getRepresentationsViewModel(representations);
-	const caseReferenceViewModel = getCaseReferenceViewModel(caseReference);
+	const representations = await getRepresentations(
+		caseId,
+		buildQueryString({ searchTerm, sortBy, pageSize, page })
+	);
 
 	return res.render(view, {
-		representations: representationsVieModel,
-		caseReference: caseReferenceViewModel,
+		representations: getRepresentationsViewModel(representations),
+		caseReference: getCaseReferenceViewModel(caseReference),
 		caseId,
-		queries: {
+		table: {
+			sortLinks: tableSortLinks(query)
+		},
+		pagination: getPagination(query, representations),
+		queryData: {
 			searchTerm,
-			activeSort: sortBy,
-			page,
-			pageSize
+			sortBy,
+			pageSize,
+			page
 		}
 	});
 }
