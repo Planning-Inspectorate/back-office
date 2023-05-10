2374c4a065014b3e5419d1589411f2f0419fb1b2
Ola Bale <Olanrewaju.Bale@planninginspectorate.gov.uk>
2023-05-10 16:44:17 +0100

    ci: new e2e pipeline

--- apps/e2e/install-chromium.sh
+++ apps/e2e/install-chromium.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Add the Google Chrome signing key
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# Install Homebrew if it is not already installed
+which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Add the Google Chrome repository
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+# Add the Google Chrome cask to Homebrew
+brew tap homebrew/cask
+brew update
 
-# Update the package list and install Google Chrome
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+# Install Google Chrome
+brew install --cask google-chrome
