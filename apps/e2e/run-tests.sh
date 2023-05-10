3559ff701a829cb7ebf4d3c57341934f568a5652
Ola Bale <Olanrewaju.Bale@planninginspectorate.gov.uk>
2023-05-10 17:37:40 +0100

    ci: create new yaml file for e2e, add shell script to run tests

--- apps/e2e/run-tests.sh
+++ apps/e2e/run-tests.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# ./env.sh
-# docker build -t my-cypress-tests .
-# container_id=$(docker run -d -v "$(pwd)/failed-tests:/app/cypress/screenshots" --env-file .env my-cypress-tests)
-# docker logs -f $container_id
-
 npx cypress verify
 npm run cy:ci
