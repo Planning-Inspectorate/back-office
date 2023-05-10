3dbc81397fe958088509da1ad913019b4c29775c
Ola Bale <Olanrewaju.Bale@planninginspectorate.gov.uk>
2023-05-10 17:37:40 +0100

    ci: create new yaml file for e2e, add shell script to run tests

--- apps/e2e/install-chromium.sh
+++ apps/e2e/install-chromium.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Install Homebrew if it is not already installed
-which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Add the Google Chrome signing key
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-# Add the Google Chrome cask to Homebrew
-brew tap homebrew/cask
-brew update
+# Add the Google Chrome repository
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
-# Install Google Chrome
-brew install --cask google-chrome
+# Update the package list and install Google Chrome
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
