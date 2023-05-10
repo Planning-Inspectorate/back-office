b4b90e5172ea3b0e00fbea1ff293955e5cb0c964
Ola Bale <Olanrewaju.Bale@planninginspectorate.gov.uk>
2023-05-10 13:57:20 +0100

    ci: new e2e pipeline

--- apps/e2e/run-tests.sh
+++ apps/e2e/run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./env.sh
-docker build -t my-cypress-tests .
+# ./env.sh
+# docker build -t my-cypress-tests .
 docker run -it -v "$(pwd)/failed-tests:/app/cypress/screenshots" my-cypress-tests
