bf3be75ce764c5d94b007d4e52d68de40d6a2330
Ola Bale <Olanrewaju.Bale@planninginspectorate.gov.uk>
2023-05-10 09:12:23 +0100

    ci: new e2e pipeline

--- apps/e2e/cypress/e2e/back-office-applications/createCase.spec.js
+++ apps/e2e/cypress/e2e/back-office-applications/createCase.spec.js
@@ -12,17 +12,15 @@ const applicationsHomePage = new ApplicationsHomePage();
 
 describe('Create A Case', () => {
 	context('As Inspector', () => {
-		beforeEach(() => {
-			cy.login(users.inspector);
-		});
-
 		it('Should not be able to create a case - button is not available', () => {
+			cy.login(users.inspector);
 			cy.visit('/');
 			applicationsHomePage.verifyInspectorIsSignedIn();
 			applicationsHomePage.verifyCreateCaseIsNotAvailable();
 		});
 
 		it('Should not be able to create a case - cannot navigate to URL', () => {
+			cy.login(users.inspector);
 			cy.visit('/');
 			cy.visit('/applications-service/create-new-case', {
 				failOnStatusCode: false
