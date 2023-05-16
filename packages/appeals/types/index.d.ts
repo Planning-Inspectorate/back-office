fabb1ad5528d73fcffaa905d94c05707c5b04446
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-05-16 13:34:34 +0100

    chore(tooling): restore appeals index imports

--- packages/appeals/types/index.d.ts
+++ packages/appeals/types/index.d.ts
@@ -1,1 +1,6 @@
+import * as CaseOfficer from './case-officer';
+import * as Inspector from './inspector';
+import * as Validation from './validation';
+
 export * from './appeal';
+export { Inspector, CaseOfficer, Validation };
