1ab2fd402cab38980e75574db1afe48637a63dae
Shivanshu Patel <shivanshuit914@gmail.com>
2023-05-17 22:25:54 +0100

    test(api): add tests for document version upload

---
+++ apps/api/src/server/applications/application/documents/__tests__/document.service.test.js
@@ -0,0 +1,63 @@
+import { jest } from '@jest/globals';
+const { databaseConnector } = await import('../../../../utils/database-connector.js');
+const { default: got } = await import('got');
+
+import { obtainURLForDocumentVersion } from '../document.service.js';
+
+const document = {
+	documentName: 'test',
+	folderId: 1111,
+	documentSize: 1111,
+	documentType: 'test'
+};
+
+const application = {
+	id: 1,
+	reference: 'case reference'
+};
+
+const caseId = 1234;
+const documentGuid = '1111-2222-3333';
+
+describe('Document service test', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	test('obtainURLForDocumentVersion throws error when case not exist', async () => {
+		databaseConnector.case.findUnique.mockResolvedValue(null);
+		await expect(obtainURLForDocumentVersion(document, caseId, documentGuid)).rejects.toThrow(
+			Error
+		);
+	});
+
+	test('obtainURLForDocumentVersion throws error when document not exist', async () => {
+		databaseConnector.case.findUnique.mockResolvedValue(application);
+		databaseConnector.document.findUnique.mockResolvedValue(null);
+		await expect(obtainURLForDocumentVersion(document, caseId, documentGuid)).rejects.toThrow(
+			Error
+		);
+	});
+
+	test('obtainURLForDocumentVersion throws error when document not exist', async () => {
+		databaseConnector.case.findUnique.mockResolvedValue(application);
+		databaseConnector.document.findUnique.mockResolvedValue(document);
+		got.post.mockReturnValue({
+			json: jest.fn().mockResolvedValue({
+				blobStorageHost: 'blob-store-host',
+				blobStorageContainer: 'blob-store-container',
+				documents: [document]
+			})
+		});
+
+		const ressponse = await obtainURLForDocumentVersion(document, caseId, documentGuid);
+
+		expect(ressponse.blobStorageHost).toEqual('blob-store-host');
+		expect(ressponse.blobStorageContainer).toEqual('blob-store-container');
+		expect(ressponse.documents).toEqual([document]);
+		expect(databaseConnector.document.findUnique).toHaveBeenCalledTimes(1);
+		expect(databaseConnector.documentVersion.upsert).toHaveBeenCalledTimes(1);
+		expect(databaseConnector.documentVersion.update).toHaveBeenCalledTimes(1);
+		expect(databaseConnector.document.update).toHaveBeenCalledTimes(1);
+	});
+});
