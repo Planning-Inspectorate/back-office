5332e2b23c4c3c11f4187758055de04080485e8f
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2023-05-17 13:18:08 +0100

    fix(web/applications): fix filtering by 'invalid' issue [ASB1476]
    -filtering by 'invalid' status was also checking the 'valid' checkbox in the UI

--- apps/web/src/server/applications/case/representations/utils/filter/filter-view-model.js
+++ apps/web/src/server/applications/case/representations/utils/filter/filter-view-model.js
@@ -9,19 +9,35 @@ const buildStatus = (text, value, filters) => ({ text, value, checked: filters?.
 
 /**
  *
+ * @param {string|Array<string>} stringOrArray
+ * @returns {Array<string>}
+ */
+const ensureArray = (stringOrArray) => {
+	if (!Array.isArray(stringOrArray) && typeof stringOrArray === 'string') {
+		return [stringOrArray];
+	}
+	return stringOrArray;
+};
+
+/**
+ *
  * @param {any} filters
  * @returns {{checked: boolean | undefined, text: string, value: string}[]}}
  */
-export const getFilterViewModel = (filters) => [
-	buildStatus('Awaiting review', 'AWAITING_REVIEW', filters),
-	buildStatus('Referred', 'REFERRED', filters),
-	buildStatus('Invalid', 'INVALID', filters),
-	buildStatus('Valid', 'VALID', filters),
-	buildStatus('Published', 'PUBLISHED', filters),
-	buildStatus('Withdrawn', 'WITHDRAWN', filters),
-	buildStatus('Archived', 'ARCHIVED', filters),
-	buildStatus('Under 18', 'UNDER_18', filters)
-];
+export const getFilterViewModel = (filters) => {
+	const filtersArray = ensureArray(filters);
+
+	return [
+		buildStatus('Awaiting review', 'AWAITING_REVIEW', filtersArray),
+		buildStatus('Referred', 'REFERRED', filtersArray),
+		buildStatus('Invalid', 'INVALID', filtersArray),
+		buildStatus('Valid', 'VALID', filtersArray),
+		buildStatus('Published', 'PUBLISHED', filtersArray),
+		buildStatus('Withdrawn', 'WITHDRAWN', filtersArray),
+		buildStatus('Archived', 'ARCHIVED', filtersArray),
+		buildStatus('Under 18', 'UNDER_18', filtersArray)
+	];
+};
 
 /**
  *
@@ -29,10 +45,10 @@ export const getFilterViewModel = (filters) => [
  * @returns {{under18: boolean, status: *[]}}
  */
 export const buildFilterQueryString = (filters) => {
-	const array = Array.isArray(filters) ? filters : [filters];
+	const filtersArray = ensureArray(filters);
 
 	return {
-		status: array.filter((element) => element !== 'UNDER_18'),
-		under18: array.includes('UNDER_18')
+		status: filtersArray.filter((element) => element !== 'UNDER_18'),
+		under18: filtersArray.includes('UNDER_18')
 	};
 };
