7fdade3bb77f7a43180a0e82852a0223563d1ad0
Shivanshu Patel <shivanshuit914@gmail.com>
2023-05-22 15:46:49 +0100

    feat(api): add endpoint to retrieve single examination item

--- apps/api/src/server/repositories/examination-timetable-items.repository.js
+++ apps/api/src/server/repositories/examination-timetable-items.repository.js
@@ -2,6 +2,15 @@ import { databaseConnector } from '../utils/database-connector.js';
 
 /**
  *
+ * @param {number} id
+ * @returns {Promise<import('@pins/api').Schema.ExaminationTimetableItems | null>}
+ */
+export const getById = (id) => {
+	return databaseConnector.examinationTimetableItems.findUnique({ where: { id } });
+};
+
+/**
+ *
  * @param {number} caseId
  * @returns {Promise<import('@pins/api').Schema.ExaminationTimetableItems[] | null>}
  */
