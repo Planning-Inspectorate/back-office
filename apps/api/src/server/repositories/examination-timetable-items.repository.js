d48ce98b80a38a06c665b8a256b73af45c53e949
Shivanshu Patel <shivanshuit914@gmail.com>
2023-05-22 14:43:17 +0100

    feat(api): add an endpoint to get examination timetable items by case id

---
+++ apps/api/src/server/repositories/examination-timetable-items.repository.js
@@ -0,0 +1,10 @@
+import { databaseConnector } from '../utils/database-connector.js';
+
+/**
+ *
+ * @param {number} caseId
+ * @returns {Promise<import('@pins/api').Schema.ExaminationTimetableItems[] | null>}
+ */
+export const getByCaseId = (caseId) => {
+	return databaseConnector.examinationTimetableItems.findMany({ where: { caseId } });
+};
