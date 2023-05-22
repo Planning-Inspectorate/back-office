a644ff117d466d786d5f9b2cda3e38aa7b4b13eb
Shivanshu Patel <shivanshuit914@gmail.com>
2023-05-22 15:46:49 +0100

    feat(api): add endpoint to retrieve single examination item

--- apps/api/src/server/applications/examination-timetable-items/examination-timetable-items.controller.js
+++ apps/api/src/server/applications/examination-timetable-items/examination-timetable-items.controller.js
@@ -11,3 +11,27 @@ export const getExaminationTimetableItems = async (_request, response) => {
 
 	response.send(examinationTimetableItems);
 };
+
+/**
+ * @type {import('express').RequestHandler}
+ * @throws {Error}
+ * @returns {Promise<void>}
+ */
+export const getExaminationTimetableItem = async (_request, response) => {
+	const { id } = _request.params;
+	const examinationTimetableItems = await exminationTimetableItemsRepository.getById(+id);
+
+	response.send(examinationTimetableItems);
+};
+
+/**
+ * @type {import('express').RequestHandler}
+ * @throws {Error}
+ * @returns {Promise<void>}
+ */
+export const createExaminationTimetableItem = async (_request, response) => {
+	const { body } = _request;
+	const examinationTimetableItem = await exminationTimetableItemsRepository.create(body);
+
+	response.send(examinationTimetableItem);
+};
