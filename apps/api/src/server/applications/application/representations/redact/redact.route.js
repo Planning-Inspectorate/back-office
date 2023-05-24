79f0d8ab8c8d80968b0f589fd74d8bda3f2c4e9b
Jonathan Goulding <jonathangoulding21@gmail.com>
2023-05-24 08:37:48 +0100

    feat(api/applications): fix PR issues

--- apps/api/src/server/applications/application/representations/redact/redact.route.js
+++ apps/api/src/server/applications/application/representations/redact/redact.route.js
@@ -9,7 +9,7 @@ router.patch(
 	/*
         #swagger.tags = ['Applications']
         #swagger.path = '/applications/{id}/representations/{repId}/redact'
-        #swagger.description = 'Updates a redacted representation for an application'
+        #swagger.description = 'Redact a representation for an application'
         #swagger.parameters['id'] = {
             in: 'path',
 			description: 'Application ID',
@@ -18,14 +18,13 @@ router.patch(
 		}
 		#swagger.parameters['body'] = {
             in: 'body',
-            description: 'Representation Details',
+            description: 'Representation redact details',
             schema: {
-            actionBy: "Joe Bloggs",
+	          actionBy: "Joe Bloggs",
 						redactedRepresentation: "This is the original Rep",
             notes: "Removed PII from original",
             type: 'REDACTION',
-            invalidReason: 'an invalid reason',
-
+            invalidReason: 'an invalid reason'
 			}
         }
         #swagger.responses[200] = {
