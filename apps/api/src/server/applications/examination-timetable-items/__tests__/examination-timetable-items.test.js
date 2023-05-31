fd06a0fd0c380217839c870327ddca50e730fbc2
Shivanshu Patel <shivanshuit914@gmail.com>
2023-05-31 17:02:18 +0100

    feat(api): improve unit test, use better way of creating examination folder

--- apps/api/src/server/applications/examination-timetable-items/__tests__/examination-timetable-items.test.js
+++ apps/api/src/server/applications/examination-timetable-items/__tests__/examination-timetable-items.test.js
@@ -54,7 +54,7 @@ describe('Test examination timetable items API', () => {
 		});
 	});
 
-	test('creates examination timetable item', async () => {
+	test('creates examination timetable item and examination sub folder', async () => {
 		databaseConnector.case.findUnique.mockResolvedValue({ id: 1 });
 		databaseConnector.folder.findFirst.mockResolvedValue({
 			id: 1,
@@ -77,6 +77,27 @@ describe('Test examination timetable items API', () => {
 		expect(resp.status).toEqual(200);
 		expect(databaseConnector.case.findUnique).toHaveBeenCalledWith({ where: { id: 1 } });
 		expect(databaseConnector.examinationTimetableItems.create).toHaveBeenCalledTimes(1);
+		expect(databaseConnector.folder.create).toHaveBeenCalledTimes(1);
+	});
+
+	test('creates examination timetable item and examination folder with sub folder', async () => {
+		databaseConnector.case.findUnique.mockResolvedValue({ id: 1 });
+		databaseConnector.folder.findFirst.mockResolvedValue(null);
+		databaseConnector.folder.create.mockResolvedValue({
+			id: 2,
+			caseId: 1,
+			displayNameEn: 'Examination',
+			parentFolderId: 1,
+			displayOrder: 100
+		});
+		databaseConnector.examinationTimetableItems.create.mockResolvedValue(examinationTimetableItem);
+		const resp = await request
+			.post('/applications/examination-timetable-items')
+			.send(examinationTimetableItem);
+		expect(resp.status).toEqual(200);
+		expect(databaseConnector.case.findUnique).toHaveBeenCalledWith({ where: { id: 1 } });
+		expect(databaseConnector.examinationTimetableItems.create).toHaveBeenCalledTimes(1);
+		expect(databaseConnector.folder.create).toHaveBeenCalledTimes(2);
 	});
 
 	test('create examination timetable item returns 400 when invalid data is sent', async () => {
