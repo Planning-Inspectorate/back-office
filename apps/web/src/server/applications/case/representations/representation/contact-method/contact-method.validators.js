df9ccaef192fe3d25256da42dab288c6d4851092
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2023-06-01 16:53:50 +0100

    feat(web/applications): add contact method page

--- apps/web/src/server/applications/case/representations/representation/contact-method/contact-method.validators.js
+++ apps/web/src/server/applications/case/representations/representation/contact-method/contact-method.validators.js
@@ -1,8 +1,8 @@
 import { body } from 'express-validator';
 import { createValidator } from '@pins/express';
 
-const validContactMethod = createValidator(
+const validateContactMethod = createValidator(
 	body('contactMethod').exists().withMessage('Select one option')
 );
 
-export const contactMethodValidation = [validContactMethod];
+export const contactMethodValidation = [validateContactMethod];
