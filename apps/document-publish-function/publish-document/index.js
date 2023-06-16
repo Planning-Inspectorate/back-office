b06716e9760d15e7a54ebedc58f6355140ed0d44
Christopher Daly <christopher.daly@kinandcarta.com>
2023-06-16 11:18:47 +0100

    feat(api): spit out entire message for poc

--- apps/document-publish-function/publish-document/index.js
+++ apps/document-publish-function/publish-document/index.js
@@ -2,5 +2,5 @@
  * @type {import('@azure/functions').AzureFunction}
  */
 export const index = async (context, document) => {
-	context.log(`Message ${JSON.stringify(document.body)}`);
+	context.log(`Message ${JSON.stringify(document)}`);
 };
