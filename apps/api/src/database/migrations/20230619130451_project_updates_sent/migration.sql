8b1dbc58958b2ccbbc5e3d253da362cef6c1d378
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-06-19 14:06:59 +0100

    fix(api): add sent field to project updates

---
+++ apps/api/src/database/migrations/20230619130451_project_updates_sent/migration.sql
@@ -0,0 +1,19 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[ProjectUpdate] ADD [sentToSubscribers] BIT NOT NULL CONSTRAINT [ProjectUpdate_sentToSubscribers_df] DEFAULT 0;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
