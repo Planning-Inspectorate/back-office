b05f7198b05e3a756bbd7bd46fa4bc77d16df932
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2023-06-20 14:54:43 +0100

    chore(appeals/api): upgrade to openapi 3.0 (BOAT-268)

--- appeals/api/src/server/config/config.js
+++ appeals/api/src/server/config/config.js
@@ -7,7 +7,7 @@ const environment = loadEnvironment(process.env.NODE_ENV);
 const { value, error } = schema.validate({
 	NODE_ENV: environment.NODE_ENV,
 	PORT: environment.PORT,
-	SWAGGER_JSON_DIR: environment.SWAGGER_JSON_DIR || './src/server/swagger-output.json',
+	SWAGGER_JSON_DIR: environment.SWAGGER_JSON_DIR || './src/server/openapi.json',
 	DATABASE_URL: environment.DATABASE_URL,
 	defaultApiVersion: environment.DEFAULT_API_VERSION || '1',
 	documentStorageApi: {
