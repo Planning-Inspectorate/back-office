b901bd076e3e2541fbac28c4da57d11b809a5d6d
Yan Jeyko <yan.jeyko@planninginspectorate.gov.uk>
2023-06-20 09:46:16 +0100

    feat(web): lpaq review complete

--- apps/web/src/server/appeals/appeal-details/lpa-questionaire-review/lpa-questionnaire-review.router.js
+++ apps/web/src/server/appeals/appeal-details/lpa-questionaire-review/lpa-questionnaire-review.router.js
@@ -1,7 +1,14 @@
 import { Router as createRouter } from 'express';
 import * as lpaQuestionnaireController from './lpa-questionnaire-review.controller.js';
+import * as validators from './lpa-questionnaire-review.validators.js';
+
 const router = createRouter({ mergeParams: true });
 
-router.route('/:lpaQId').get(lpaQuestionnaireController.viewLpaQuestionnaire);
+router
+	.route('/:lpaQId')
+	.get(lpaQuestionnaireController.getLpaQuestionnaire)
+	.post(validators.validateReviewOutcome, lpaQuestionnaireController.postLpaQuestionnaire);
+
+router.route('/:lpaQId/complete').get(lpaQuestionnaireController.getLpaQuestionnaireReviewComplete);
 
 export default router;
