7486dfabb7e6326b2dc4aeaaad62ffcb18a6045d
james-hill-kc <james.hill@kinandcarta.com>
2023-06-21 16:23:46 +0100

    feat(web): appellant case issuing valid decision

--- apps/web/src/server/appeals/appellant-case/appellant-case.router.js
+++ apps/web/src/server/appeals/appellant-case/appellant-case.router.js
@@ -1,8 +1,12 @@
 import { Router as createRouter } from 'express';
 import * as controller from './appellant-case.controller.js';
+import * as validators from './appellant-case.validators.js';
 
 const router = createRouter({ mergeParams: true });
 
-router.route('/').get(controller.getAppellantCase);
+router
+	.route('/')
+	.get(controller.getAppellantCase)
+	.post(validators.validateReviewOutcome, controller.postAppellantCase);
 
 export default router;
