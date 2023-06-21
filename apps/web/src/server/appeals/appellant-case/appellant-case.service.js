1eb9770a239d1afa8bb785bde7637bb46b07c5bb
james-hill-kc <james.hill@kinandcarta.com>
2023-06-21 16:23:46 +0100

    feat(web): appellant case issuing valid decision

--- apps/web/src/server/appeals/appellant-case/appellant-case.service.js
+++ apps/web/src/server/appeals/appellant-case/appellant-case.service.js
@@ -1,4 +1,8 @@
-import { get } from '../../lib/request.js';
+import { get, patch } from '../../lib/request.js';
+
+/**
+ * @typedef {import('./appellant-case.types.js').SingleAppellantCaseResponse} AppellantCaseResponse
+ */
 
 /**
  * @param {number} appealId
@@ -6,4 +10,22 @@ import { get } from '../../lib/request.js';
  */
 export function getAppellantCaseFromAppealId(appealId, appellantCaseId) {
 	return get(`appeals/${appealId}/appellant-cases/${appellantCaseId}`);
+}
+
+/**
+ * @param {number} appealId
+ * @param {number} appellantCaseId
+ * @param {string} validationOutcome
+ * @returns {Promise<AppellantCaseResponse>}
+ */
+export async function setReviewOutcomeForAppellantCase(
+	appealId,
+	appellantCaseId,
+	validationOutcome
+) {
+	return patch(`appeals/${appealId}/appellant-cases/${appellantCaseId}`, {
+		json: {
+			validationOutcome
+		}
+	});
 }
