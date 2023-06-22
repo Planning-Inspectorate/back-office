ad85339f0853f047b7ec5434ac81d00ae884d146
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-06-22 15:35:01 +0100

    chore(appeals/api): share a supertest instance across tests

--- appeals/api/src/server/app-test.js
+++ appeals/api/src/server/app-test.js
@@ -1,6 +1,10 @@
 import { buildApp } from './build-app.js';
+import supertest from 'supertest';
 
 // This instance of the app will only ever be used by Jest fixtures, and has Swagger omitted.
 const app = buildApp();
 
-export { app };
+// init supertest once for all tests to use
+const request = supertest(app);
+
+export { app, request };
