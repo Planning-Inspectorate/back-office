3c14ef27bd2a065f269ce3864e8670843332697d
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2023-06-22 12:29:44 +0100

    chore(appeals): clear up application code from appeals/*

--- appeals/web/src/server/applications/case/representations/representation/contact-method/contact-method.validators.js
+++
@@ -1,8 +0,0 @@
-import { body } from 'express-validator';
-import { createValidator } from '@pins/express';
-
-const validateContactMethod = createValidator(
-	body('contactMethod').exists().withMessage('Select one option')
-);
-
-export const contactMethodValidation = [validateContactMethod];
--- appeals/web/src/server/applications/case/representations/representation/representation-entity/entity.validators.js
+++
@@ -1,8 +0,0 @@
-import { body } from 'express-validator';
-import { createValidator } from '@pins/express';
-
-const validateRepresentationEntity = createValidator(
-	body('representationEntity').exists().withMessage('Select one option')
-);
-
-export const representationEntityValidation = [validateRepresentationEntity];
