96c2d70391188f02f6b7b8f8378e55ddbad6d4d6
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2023-06-29 09:20:12 +0100

    feat(api/appeals): cleared DB objects for Applications / addressed comments

--- appeals/api/src/server/appeals/appeals.routes.js
+++ appeals/api/src/server/appeals/appeals.routes.js
@@ -1,6 +1,5 @@
 import { Router as createRouter } from 'express';
 import { appealsRoutes } from './appeals/appeals.routes.js';
-import { documentsRoutes } from '../documents/documents.routes.js';
 import { appellantCaseIncompleteReasonsRoutes } from './appellant-case-incomplete-reasons/appellant-case-incomplete-reasons.routes.js';
 import { appellantCaseInvalidReasonsRoutes } from './appellant-case-invalid-reasons/appellant-case-invalid-reasons.routes.js';
 import { lpaQuestionnaireIncompleteReasonsRoutes } from './lpa-questionnaire-incomplete-reasons/lpa-questionnaire-incomplete-reasons.routes.js';
@@ -10,7 +9,6 @@ const router = createRouter();
 router.use('/appellant-case-incomplete-reasons', appellantCaseIncompleteReasonsRoutes);
 router.use('/appellant-case-invalid-reasons', appellantCaseInvalidReasonsRoutes);
 router.use('/lpa-questionnaire-incomplete-reasons', lpaQuestionnaireIncompleteReasonsRoutes);
-router.use('/documents', documentsRoutes);
 router.use('/', appealsRoutes);
 
 export { router as appealsRoutes };
