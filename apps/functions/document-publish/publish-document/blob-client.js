bf37b67cc5b0c89f8ca1479184bb93c261d2ff70
Christopher Daly <christopher.daly@kinandcarta.com>
2023-06-30 13:32:31 +0100

    fix(api): fix config import

--- apps/functions/document-publish/publish-document/blob-client.js
+++ apps/functions/document-publish/publish-document/blob-client.js
@@ -1,4 +1,4 @@
 import { BlobStorageClient } from '@pins/blob-storage-client';
-import config from './config';
+import config from './config.js';
 
 export const blobClient = BlobStorageClient.fromUrl(config.BLOB_STORAGE_ACCOUNT_HOST);
