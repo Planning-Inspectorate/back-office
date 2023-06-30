89ebce21267680242e15c2e465cafb116cfe41cc
christopher-semmens <christopher.semmens@kinandcarta.com>
2023-06-30 13:02:55 +0100

    feat(web/applications): remove common validators file

--- apps/web/src/server/applications/case/representations/representation/check-answers/check-answers.validators.js
+++ apps/web/src/server/applications/case/representations/representation/check-answers/check-answers.validators.js
@@ -1,23 +1,26 @@
-import {
-	validateRepresentedAddress,
-	validateRepresentedContactMethod,
-	validateType,
-	validateRepresentedUnder18,
-	validateRepresentationEntity,
-	validateRepresentativeName,
-	validateRepresentativeAddress,
-	validateRepresentativeContactMethod,
-	validateOriginalRepresentation
-} from './utils/common-validators.js';
+import { createValidator } from '@pins/express';
+import { body } from 'express-validator';
 
 export const checkAnswersValidation = [
-	validateRepresentedAddress,
-	validateRepresentedContactMethod,
-	validateType,
-	validateRepresentedUnder18,
-	validateRepresentationEntity,
-	validateRepresentativeName,
-	validateRepresentativeAddress,
-	validateRepresentativeContactMethod,
-	validateOriginalRepresentation
+	createValidator(
+		body('represented.address.postcode').notEmpty().withMessage('Enter address details')
+	),
+	createValidator(
+		body('represented.contactMethod').notEmpty().withMessage('Enter preferred method of contact')
+	),
+	createValidator(body('type').notEmpty().withMessage('Enter type')),
+	createValidator(body('represented.under18').notEmpty().withMessage('Enter Under 18')),
+	createValidator(body('represented.type').notEmpty().withMessage('Enter on behalf of')),
+	createValidator(
+		body('representative.firstName').notEmpty().withMessage('Enter agent contact details')
+	),
+	createValidator(
+		body('representative.address.postcode').notEmpty().withMessage('Enter agent address details')
+	),
+	createValidator(
+		body('representative.contactMethod')
+			.notEmpty()
+			.withMessage('Enter agent preferred method of contact')
+	),
+	createValidator(body('originalRepresentation').notEmpty().withMessage('Enter representation'))
 ];
