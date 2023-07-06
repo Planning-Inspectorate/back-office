5db62077d7164fede35bc6023b1116c810820914
james-hill-kc <james.hill@kinandcarta.com>
2023-07-06 16:10:32 +0100

    chore(web): remove feature code for setting start date

--- appeals/web/src/server/appeals/appeal-details/appeal-details.router.js
+++ appeals/web/src/server/appeals/appeal-details/appeal-details.router.js
@@ -1,6 +1,4 @@
 import { Router as createRouter } from 'express';
-import enterStartDateRouter from '../enter-start-date/enter-start-date.router.js';
-import startDateEnteredRouter from '../start-date-entered/start-date-entered.router.js';
 import lpaQuestionnaireReviewRouter from './lpa-questionaire-review/lpa-questionnaire-review.router.js';
 import appellantCaseRouter from '../appellant-case/appellant-case.router.js';
 import appealDocumentsRouter from '../appeal-documents/appeal-documents.router.js';
@@ -12,8 +10,6 @@ const router = createRouter();
 router.route('/:appealId').get(controller.viewAppealDetails);
 router.use('/:appealId/documents', appealDocumentsRouter);
 router.use('/:appealId/lpa-questionnaire-review', lpaQuestionnaireReviewRouter);
-router.use('/:appealId/enter-start-date', enterStartDateRouter);
-router.use('/:appealId/start-date-entered', startDateEnteredRouter);
 router.use('/:appealId/appellant-case', appellantCaseRouter);
 
 export default router;
