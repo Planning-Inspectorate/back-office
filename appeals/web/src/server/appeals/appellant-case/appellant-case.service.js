f760ba45e9851786736578ca60ba6be05e6ee48c
james-hill-kc <james.hill@kinandcarta.com>
2023-07-10 12:30:12 +0100

    feat(web): appellant case issuing invalid decision

--- appeals/web/src/server/appeals/appellant-case/appellant-case.service.js
+++ appeals/web/src/server/appeals/appellant-case/appellant-case.service.js
@@ -13,19 +13,28 @@ export function getAppellantCaseFromAppealId(appealId, appellantCaseId) {
 }
 
 /**
+ * @typedef {Object} AppellantCaseReviewOutcome
+ * @property {string} validationOutcome
+ * @property {number[]} [invalidReasons]
+ * @property {number[]} [incompleteReasons]
+ * @property {string} [otherNotValidReasons] - free text field for "other" (shared by both invalid and incomplete outcomes)
+ */
+
+/**
  * @param {number} appealId
  * @param {number} appellantCaseId
- * @param {string} validationOutcome
+ * @param {AppellantCaseReviewOutcome} reviewOutcome
  * @returns {Promise<AppellantCaseResponse>}
  */
-export async function setReviewOutcomeForAppellantCase(
-	appealId,
-	appellantCaseId,
-	validationOutcome
-) {
+export async function setReviewOutcomeForAppellantCase(appealId, appellantCaseId, reviewOutcome) {
 	return patch(`appeals/${appealId}/appellant-cases/${appellantCaseId}`, {
-		json: {
-			validationOutcome
-		}
+		json: reviewOutcome
 	});
+}
+
+/**
+ * @returns {Promise<import('.prisma/client').AppellantCaseInvalidReason[]>}
+ */
+export async function getAppellantCaseInvalidReasons() {
+	return get(`appeals/appellant-case-invalid-reasons`);
 }
