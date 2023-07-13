08f9d315d5067bcf24de16e28e3df1bdb6b16564
Richard Mohammed <richard.mohammed@kinandcarta.com>
2023-07-13 15:14:26 +0100

    feat(api): boas-956 - publish exam timetable to front office

--- apps/api/src/message-schemas/events/nsip-exam-timetable.d.ts
+++ apps/api/src/message-schemas/events/nsip-exam-timetable.d.ts
@@ -2,28 +2,29 @@
  * nsip-exam-timetable schema for use in code
  */
 
-export interface NSIPExamTimetableLineItem {
+export interface NSIPExamTimetableItemDescriptionLineItem {
 	eventLineItemId?: number;
 	eventLineItemDescription: string;
 }
 
-export interface NSIPExamTimetable {
+export interface NSIPExamTimetableItem {
 	eventId?: number;
-	type: 'Accompanied Site Inspection' |
-		'Compulsory Acquisition Hearing' |
-		'Deadline' |
-		'Deadline For Close Of Examination' |
-		'Issued By' |
-		'Issue Specific Hearing' |
-		'Open Floor Hearing' |
-		'Other Meeting' |
-		'Preliminary Meeting' |
-		'Procedural Deadline (Pre-Examination)' |
-		'Procedural Decision' |
-		'Publication Of';
+	type:
+		| 'Accompanied Site Inspection'
+		| 'Compulsory Acquisition Hearing'
+		| 'Deadline'
+		| 'Deadline For Close Of Examination'
+		| 'Issued By'
+		| 'Issue Specific Hearing'
+		| 'Open Floor Hearing'
+		| 'Other Meeting'
+		| 'Preliminary Meeting'
+		| 'Procedural Deadline (Pre-Examination)'
+		| 'Procedural Decision'
+		| 'Publication Of';
 	eventTitle: string;
 	description: string;
 	eventDeadlineStartDate?: string;
 	date: string;
-	eventLineItems?: NSIPExamTimetableLineItem[];
+	eventLineItems?: NSIPExamTimetableItemDescriptionLineItem[];
 }
