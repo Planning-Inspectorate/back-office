527575a407ea362558b91d199c38e57a6790b0aa
Richard Mohammed <richard.mohammed@kinandcarta.com>
2023-07-13 15:14:26 +0100

    feat(api): boas-956 - publish exam timetable to front office

--- apps/api/src/server/applications/examination-timetable-items/examination-timetable-items.service.js
+++ apps/api/src/server/applications/examination-timetable-items/examination-timetable-items.service.js
@@ -1,28 +1,86 @@
 import { eventClient } from '#infrastructure/event-client.js';
 import { NSIP_EXAM_TIMETABLE } from '#infrastructure/topics.js';
 import * as examinationTimetableRepository from '../../repositories/examination-timetable.repository.js';
+import * as examinationTimetableItemsRepository from '../../repositories/examination-timetable-items.repository.js';
+// @ts-ignore
 import { EventType } from '@pins/event-client';
 
 /**
- * @typedef {import('../../../message-schemas/events/nsip-exam-timetable.js').NSIPExamTimetable} NSIPExamTimetable
+ * @typedef {import('../../../message-schemas/events/nsip-exam-timetable.js').NSIPExamTimetableItem} NSIPExamTimetableItem
+ * @typedef {import('../../../message-schemas/events/nsip-exam-timetable.js').NSIPExamTimetableItemDescriptionLineItem} NSIPExamTimetableItemDescriptionLineItem
  */
 
 /**
+ * Grabs the description from the examinationTimetableItem and parses it into a description string and
+ * NSIPExamTimetableItemDescriptionLineItem array.
+ *
  * @param {import('@prisma/client').ExaminationTimetableItem} examinationTimetableItem
- * @returns { NSIPExamTimetable }
+ * @returns { { description: string, eventLineItems: NSIPExamTimetableItemDescriptionLineItem[] } }
  */
-function buildPublishPayload(examinationTimetableItem) {
-	return {
-		date: examinationTimetableItem.date,
-		description: examinationTimetableItem.description,
-		eventTitle: examinationTimetableItem.name,
-		type: examinationTimetableItem.examinationTypeId
-
+function extractDescriptionAndLineItems(examinationTimetableItem) {
+	if (!examinationTimetableItem.description) {
+		return {
+			description: '',
+			eventLineItems: []
+		};
 	}
+
+	const parsedDescription = JSON.parse(examinationTimetableItem.description);
+
+	// The event line items are the bullet points that are added to the description. They are different from ExaminationTimetableItems.
+	const eventLineItems = parsedDescription.bulletPoints.map((/** @type {string} */ bulletPoint) => {
+		return {
+			eventLineItemDescription: bulletPoint
+		};
+	});
+
+	return {
+		description: parsedDescription.preText,
+		eventLineItems
+	};
 }
 
 /**
- * Publishes an examination timetable.
+ * Returns the payload containing all the examination timetable items.
+ *
+ * @param {import('@prisma/client').ExaminationTimetable} examinationTimetable
+ * @returns { Promise<NSIPExamTimetableItem[]> }
+ */
+async function buildExamTimetableItemsPayload(examinationTimetable) {
+	const examinationTimetableItems =
+		await examinationTimetableItemsRepository.getByExaminationTimetableId(examinationTimetable.id);
+
+	if (!examinationTimetableItems) {
+		return [];
+	}
+
+	return examinationTimetableItems?.map(buildSingleExaminationTimetableItemPayload);
+}
+
+/**
+ * Returns a single examination timetable item to add to the full payload.
+ *
+ * @param {import('@prisma/client').ExaminationTimetableItem} examinationTimetableItem
+ * @returns NSIPExamTimetableItem
+ */
+function buildSingleExaminationTimetableItemPayload(examinationTimetableItem) {
+	const { description, eventLineItems } = extractDescriptionAndLineItems(examinationTimetableItem);
+
+	return {
+		// @ts-ignore
+		type: examinationTimetableItem.ExaminationTimetableType.name,
+		date: examinationTimetableItem.date.toISOString(),
+		description,
+		eventTitle: examinationTimetableItem.name,
+		eventDeadlineStartDate: examinationTimetableItem.startDate?.toDateString(),
+		eventId: examinationTimetableItem.id,
+		eventLineItems
+	};
+}
+
+/**
+ * Publishes an examination timetable. Does so by updating the examination timetable to be published and
+ * sending the examination timetable items to the event queue to be published on the front office.
  *
  * @param { String } id
  * @returns {Promise<void>}
@@ -39,9 +97,9 @@ export async function publish(id) {
 		}
 	);
 
-	await eventClient.sendEvents(
-		NSIP_EXAM_TIMETABLE,
-		examinationTimetableItems,
-		EventType.Create
+	const examTimetableItemsPayload = await buildExamTimetableItemsPayload(
+		updatedExaminationTimetable
 	);
+
+	await eventClient.sendEvents(NSIP_EXAM_TIMETABLE, examTimetableItemsPayload, EventType.Create);
 }
