2a6a58da8cb7337352009a7dec1aa2d37208f6bd
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-07-24 15:10:37 +0100

    chore(web): remove now unused function in file uploader client code

--- apps/web/src/client/components/file-uploader/_relevant_representations_attachment.js
+++ apps/web/src/client/components/file-uploader/_relevant_representations_attachment.js
@@ -6,7 +6,6 @@
  * Adds the document id to a relevant representations attachment to link a document to a representation
  * @param {{ documents: UploadInfo[] }} uploadInfo
  * @param {HTMLElement} uploadForm
- * @return {Promise<void>}
  */
 export const relevantRepresentationsAttachmentUpload = async (uploadInfo, uploadForm) => {
 	if (window.location.href.includes('relevant-representations')) {
@@ -19,40 +18,6 @@ export const relevantRepresentationsAttachmentUpload = async (uploadInfo, upload
 			: [uploadInfo.documents];
 
 		const url = `/applications-service/case/${caseId}/relevant-representations/${repId}/api/upload`;
-		for (const document of listOfDocuments) {
-			await fetch(url, {
-				method: 'POST',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({ documentId: document.GUID })
-			});
-		}
-	}
-};
-
-/**
- * Adds the document id to a relevant representations attachment to link a document to a representation
- * @param {{ documents: UploadInfo[] }} uploadInfo
- * @param {HTMLElement} uploadForm
- * @return {Promise<void>}
- */
-export const relevantRepresentationsAttachmentUploadVersionFile = async (
-	uploadInfo,
-	uploadForm
-) => {
-	if (window.location.href.includes('relevant-representations')) {
-		const { caseId } = uploadForm.dataset;
-		const urlParams = new URLSearchParams(window.location.search);
-		const repId = urlParams.get('repId');
-
-		const listOfDocuments = Array.isArray(uploadInfo.documents)
-			? uploadInfo.documents
-			: [uploadInfo.documents];
-
-		const url = `/applications-service/case/${caseId}/relevant-representations/${repId}/api/upload`;
-
 		for (const document of listOfDocuments) {
 			await fetch(url, {
 				method: 'POST',
