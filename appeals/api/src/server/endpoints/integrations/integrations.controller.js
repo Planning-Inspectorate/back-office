e8a20beb8fef2a19cc522fdd7056d9373cda42cc
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2023-08-02 16:38:46 +0100

    feat(api/appeals): scaffolding of integration flows and azure functions

--- appeals/api/src/server/endpoints/integrations/integrations.controller.js
+++ appeals/api/src/server/endpoints/integrations/integrations.controller.js
@@ -1,31 +1,27 @@
 import { createAppeal, createDocument } from '#repositories/integrations.repository.js';
 import { mapAppealFromTopic, mapDocumentFromTopic } from './integrations.mapper.js';
 
-/** @typedef {import('@pins/appeals/index.js').BlobInfo} BlobInfo */
-/** @typedef {import('@pins/appeals/index.js').AddDocumentsResponse} AddDocumentsResponse */
-/** @typedef {import('@pins/appeals.api').Schema.Folder} Folder */
 /** @typedef {import('express').RequestHandler} RequestHandler */
+/** @typedef {import('express').Response} Response */
 
 /**
  * @type {RequestHandler}
- * @returns {Promise<any>}
+ * @returns {Promise<Response>}
  */
 export const postAppealSubmission = async (req, res) => {
 	const data = mapAppealFromTopic(req.body);
 	const result = await createAppeal(data);
 
-	// @ts-ignore
 	return res.send(result);
 };
 
 /**
  * @type {RequestHandler}
- * @returns {Promise<any>}
+ * @returns {Promise<Response>}
  */
 export const postDocumentSubmission = async (req, res) => {
 	const data = mapDocumentFromTopic(req.body);
 	const result = await createDocument(data);
 
-	// @ts-ignore
 	return res.send(result);
 };
---
+++ appeals/functions/create-document/index.js
@@ -0,0 +1,3 @@
+//TODO: Azure function
+// subscribes to the '[TBD]' service bus topic (ODW document metadata)
+// posts updates to the /appeals/document-submission API endpoint
