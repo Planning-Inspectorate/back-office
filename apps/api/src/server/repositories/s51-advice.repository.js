2eefe27d85d287b203b8d944030d0674a4ca3d36
David J White <50454208+DavidJWhiteDev@users.noreply.github.com>
2023-08-03 15:23:45 +0100

    feat(api/applications): get all s51 advices api (boas-1038)

--- apps/api/src/server/repositories/s51-advice.repository.js
+++ apps/api/src/server/repositories/s51-advice.repository.js
@@ -18,4 +18,45 @@ export const get = (id) => {
 	return databaseConnector.s51Advice.findUnique({
 		where: { id }
 	});
+}
+
+/**
+ * Returns total number of S51 Advice on a case
+ *
+ * @param {number} caseId
+ * @param {boolean} getAllS51Advice
+ * @returns {import('@prisma/client').PrismaPromise<number>}
+ */
+export const getS51AdviceCountOnCase = (caseId, getAllS51Advice = false) => {
+	/** @type {{caseId: number, isDeleted?:boolean}} */
+	const where = { caseId };
+
+	if (getAllS51Advice) {
+		where.isDeleted = true;
+	}
+
+	return databaseConnector.s51Advice.count({
+		where
+	});
+};
+
+/**
+ * returns and array of all the S51 Advice on a case
+ *
+ * @param {{caseId: number, skipValue: number, pageSize: number }} caseId
+ * @returns {import('@prisma/client').PrismaPromise<import('@pins/applications.api').Schema.S51Advice[]>}
+ */
+export const getManyS51AdviceOnCase = ({ caseId, skipValue, pageSize }) => {
+	return databaseConnector.s51Advice.findMany({
+		skip: skipValue,
+		take: pageSize,
+		orderBy: [
+			{
+				createdAt: 'desc' // TODO: make this the ref id
+			}
+		],
+		where: {
+			caseId
+		}
+	});
 };
---
+++ apps/api/src/server/utils/mapping/map-s51-advice-details.js
@@ -0,0 +1,53 @@
+import { mapDateStringToUnixTimestamp } from './map-date-string-to-unix-timestamp.js';
+
+/** @typedef {import('@pins/applications.api').Schema.S51Advice} S51Advice */
+/** @typedef {import('@pins/applications').S51AdviceDetails} S51AdviceDetails */
+
+/**
+ * returns a formatted S52 Advice ref
+ * eg for case EN01001 and advice number 12, returns EN010001-Advice-00012
+ *
+ * @param { string } caseRef
+ * @param { number } adviceRef
+ */
+const formatS51AdviceReferenceNumber = (caseRef, adviceRef) => {
+	return `${caseRef}-Advice-` + String(adviceRef).padStart(5, '0');
+};
+
+/**
+ * maps a single S51 Advice record to UI usable format
+ *
+ * @param { string } caseRef
+ * @param { S51Advice } s51Advice
+ * @returns { S51AdviceDetails }
+ */
+export const mapS51Advice = (caseRef, s51Advice) => {
+	return {
+		id: s51Advice.id,
+		referenceNumber: formatS51AdviceReferenceNumber(caseRef, 1), // TODO: after DB update use real field data
+		title: s51Advice.title,
+		enquirer: s51Advice.enquirer ?? '',
+		firstName: s51Advice.firstName ?? '',
+		lastName: s51Advice.lastName ?? '',
+		enquiryMethod: s51Advice.enquiryMethod,
+		enquiryDate: mapDateStringToUnixTimestamp(s51Advice.enquiryDate.toString()),
+		enquiryDetails: s51Advice.enquiryDetails,
+		adviser: s51Advice.adviser,
+		adviceDate: mapDateStringToUnixTimestamp(s51Advice.adviceDate.toString()),
+		adviceDetails: s51Advice.adviceDetails,
+		publishedStatus: 'not_checked', // TODO: s51Advice.publishedStatus ?? '',
+		redactedStatus: 'not_redacted', // TODO: s51Advice.redactedStatus ?? '',
+		dateCreated: mapDateStringToUnixTimestamp(s51Advice.createdAt.toString()),
+		dateUpdated: mapDateStringToUnixTimestamp(s51Advice.updatedAt.toString())
+	};
+};
+
+/** maps an array of S51 Advice records into a UI usable format
+ *
+ * @param { string } caseRef
+ * @param { S51Advice[] } s51Advices
+ * @returns { S51AdviceDetails[]}
+ */
+export const mapManyS51Advice = (caseRef, s51Advices) => {
+	return s51Advices.map((s51Advice) => mapS51Advice(caseRef, s51Advice));
+};
