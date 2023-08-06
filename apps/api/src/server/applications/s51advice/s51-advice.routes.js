e96bde8807b9bf0a68e89105bc4bb955fc66e491
Shivanshu Patel <shivanshuit914@gmail.com>
2023-08-06 21:56:46 +0100

    feat(api): add endpoint to get S51 advice by id

--- apps/api/src/server/applications/s51advice/s51-advice.routes.js
+++ apps/api/src/server/applications/s51advice/s51-advice.routes.js
@@ -1,6 +1,6 @@
 import { Router as createRouter } from 'express';
 import { asyncHandler } from '../../middleware/async-handler.js';
-import { createS51Advice } from './s51-advice.controller.js';
+import { createS51Advice, getS51Advice } from './s51-advice.controller.js';
 import { validateCreateS51Advice } from './s51-advice.validators.js';
 
 const router = createRouter();
@@ -24,6 +24,26 @@ router.post(
     */
 	validateCreateS51Advice,
 	asyncHandler(createS51Advice)
+);
+
+router.get(
+	'/id',
+	/*
+        #swagger.tags = ['Applications']
+        #swagger.path = '/applications/s51-advice/{id}'
+        #swagger.description = 'S51 advice ID'
+        #swagger.parameters['id'] = {
+            in: 'path',
+			description: 'S51 advice ID',
+			required: true,
+			type: 'integer'
+        }
+        #swagger.responses[200] = {
+            description: 'List of examination timetable items',
+            schema: { $ref: '#/definitions/S51AdviceResponseBody' }
+        }
+    */
+	asyncHandler(getS51Advice)
 );
 
 export { router as s51AdviceRoutes };
