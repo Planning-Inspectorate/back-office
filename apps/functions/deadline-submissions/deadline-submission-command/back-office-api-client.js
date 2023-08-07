c4cde1ed549edc061b66187b08fe32b1e91799bf
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-08-07 15:27:27 +0100

    feat(functions): set up calls to API to create documents

---
+++ apps/functions/deadline-submissions/deadline-submission-command/back-office-api-client.js
@@ -0,0 +1,68 @@
+import got from 'got';
+import config from './config';
+
+/** @typedef {{ id: number, displayNameEn: string }} FolderJSON */
+
+/**
+ * @param {string} caseReference
+ * @returns {Promise<number>}
+ * */
+export async function getCaseID(caseReference) {
+	const caseDetails = await got
+		.get(`https://${config.API_HOST}/applications?=${caseReference}`)
+		.json();
+	return caseDetails.id;
+}
+
+/**
+ *
+ * @param {{ caseID: number, documentName: string, documentType: string, documentSize: number, folderID: number, userEmail: string }} _
+ * */
+export async function submitDocument({
+	caseID,
+	documentName,
+	documentType,
+	documentSize,
+	folderID,
+	userEmail
+}) {
+	return await got
+		.post(`https://${config.API_HOST}/applications/${caseID}`, {
+			json: {
+				documentName,
+				documentType,
+				documentSize,
+				folderId: folderID,
+				username: userEmail
+			}
+		})
+		.json();
+}
+
+/**
+ *
+ * @param {number} caseID
+ * @param {string} caseName
+ * @param {string} lineItem
+ * @returns {Promise<number>}
+ * */
+export async function getFolderID(caseID, caseName, lineItem) {
+	/** @type {FolderJSON[]} */
+	const folders = await got.get(`https://${config.API_HOST}/applications/${caseID}/folders`).json();
+
+	const folder = folders.find((f) => f.displayNameEn === caseName);
+	if (!folder) {
+		throw new Error(`No folder found with name '${caseName}'`);
+	}
+
+	/** @type {FolderJSON[]} */
+	const subfolders = await got
+		.get(`https://${config.API_HOST}/applications/${caseID}/folders/${folder.id}/sub-folders`)
+		.json();
+	const subfolder = subfolders.find((f) => f.displayNameEn === lineItem);
+	if (!subfolder) {
+		throw new Error(`No folder found with name '${lineItem}'`);
+	}
+
+	return subfolder.id;
+}
