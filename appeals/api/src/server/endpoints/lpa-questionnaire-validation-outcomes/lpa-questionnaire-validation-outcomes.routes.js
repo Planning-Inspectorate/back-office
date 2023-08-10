c4c18fe074962913682b6e111ee71ea4dd58374d
shepda <david@x-designs.co.uk>
2023-08-10 09:25:21 +0100

    feat(appeals/api): add api endpoint to get the lpa questionnaire validation outcomes

---
+++ appeals/api/src/server/endpoints/lpa-questionnaire-validation-outcomes/lpa-questionnaire-validation-outcomes.routes.js
@@ -0,0 +1,22 @@
+import { Router as createRouter } from 'express';
+import { asyncHandler } from '../../middleware/async-handler.js';
+import { getLookupData } from '../../common/controllers/lookup-data.controller.js';
+
+const router = createRouter();
+
+router.get(
+	'/lpa-questionnaire-validation-outcomes',
+	/*
+		#swagger.tags = ['LPA Questionnaire Validation Outcomes']
+		#swagger.path = '/appeals/lpa-questionnaire-validation-outcomes'
+		#swagger.description = 'Gets LPA questionnaire validation outcomes'
+		#swagger.responses[200] = {
+			description: 'LPA questionnaire validation outcomes',
+			schema: { $ref: '#/definitions/AllLPAQuestionnaireValidationOutcomesResponse' },
+		}
+		#swagger.responses[400] = {}
+	 */
+	asyncHandler(getLookupData('lPAQuestionnaireValidationOutcome'))
+);
+
+export { router as lpaQuestionnaireValidationOutcomesRoutes };
