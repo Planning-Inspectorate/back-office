287f49318c2d7954bd01daad5218cd6002cc8e45
Rachel Harvey <rachel.harvey@kinandcarta.com>
2023-08-10 11:46:58 +0100

    refactor(e2e): added new method for national list search

--- apps/e2e/cypress/e2e/back-office-appeals/nationalListSearch.spec.js
+++ apps/e2e/cypress/e2e/back-office-appeals/nationalListSearch.spec.js
@@ -14,29 +14,25 @@ describe('Appeals feature', () => {
 
 	it('Case admin user should be able to use search using appeal id', () => {
 		cy.visit('/appeals-service/appeals-list');
-		page.fillInput('92623353');
-		page.clickButtonByText('Search');
+		page.nationalListSearch('92623353');
 		page.clearSearchResults();
 	});
 
 	it('Case admin user should be able to use search using postcode with spaces', () => {
 		cy.visit('/appeals-service/appeals-list');
-		page.fillInput('BS7 8LQ');
-		page.clickButtonByText('Search');
+		page.nationalListSearch('BS9 7AL');
 		page.clearSearchResults();
 	});
 
 	it('Case admin user should see error validation if not enough characters are entered', () => {
 		cy.visit('/appeals-service/appeals-list');
-		page.fillInput('9');
-		page.clickButtonByText('Search');
+		page.nationalListSearch('9');
 		page.validateErrorMessage('Search query must be between 2 and 8 characters');
 	});
 
 	it('Case admin user should see error validation if too many characters are entered', () => {
 		cy.visit('/appeals-service/appeals-list');
-		page.fillInput('999999999');
-		page.clickButtonByText('Search');
+		page.nationalListSearch('999999999');
 		page.validateErrorMessage('Search query must be between 2 and 8 characters');
 	});
 });
