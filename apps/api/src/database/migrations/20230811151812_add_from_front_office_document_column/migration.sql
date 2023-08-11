273b959136fddfbedc7b393284a1412e235c69e9
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-08-11 16:20:10 +0100

    feat(applications/api): add a fromFrontOffice column to Document table

---
+++ apps/api/src/database/migrations/20230811151812_add_from_front_office_document_column/migration.sql
@@ -0,0 +1,19 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[Document] ADD [fromFrontOffice] BIT NOT NULL CONSTRAINT [Document_fromFrontOffice_df] DEFAULT 0;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
