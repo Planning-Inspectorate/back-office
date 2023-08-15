48ebfc9bed7450c5820747b2c929005a9e9166ce
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-08-15 12:37:59 +0100

    feat(functions): publish a Service Bus message for result of submissions

--- apps/functions/deadline-submissions/deadline-submission-command/config.js
+++ apps/functions/deadline-submissions/deadline-submission-command/config.js
@@ -3,20 +3,24 @@ import joi from 'joi';
 
 const schema = joi.object({
 	NODE_ENV: joi.string().valid('development', 'production', 'test'),
-  apiHost: joi.string(),
+	apiHost: joi.string(),
 	storageUrl: joi.string(),
-  submissionsContainer: joi.string(),
-  uploadsContainer: joi.string(),
+	submissionsContainer: joi.string(),
+	uploadsContainer: joi.string(),
+	serviceBusHost: joi.string(),
+	serviceBusTopic: joi.string()
 });
 
 const environment = loadEnvironment(process.env.NODE_ENV);
 
 const { value, error } = schema.validate({
 	NODE_ENV: environment.NODE_ENV,
-  apiHost: environment.API_HOST,
-  storageUrl: environment.BLOB_STORAGE_URL,
-  submissionsContainer: environment.SUBMISSIONS_BLOB_CONTAINER_NAME,
-  uploadsContainer: environment.UPLOADS_BLOB_CONTAINER_NAME,
+	apiHost: environment.API_HOST,
+	storageUrl: environment.BLOB_STORAGE_URL,
+	submissionsContainer: environment.SUBMISSIONS_BLOB_CONTAINER_NAME,
+	uploadsContainer: environment.UPLOADS_BLOB_CONTAINER_NAME,
+	serviceBusHost: environment.SERVICE_BUS_HOST,
+	serviceBusTopic: environment.SERVICE_BUS_TOPIC
 });
 
 if (error) {
