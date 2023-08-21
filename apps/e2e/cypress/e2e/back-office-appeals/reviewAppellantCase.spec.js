4f563d5bad397dec960260359eea97664ddeea0b
Rachel Harvey <rachel.harvey@kinandcarta.com>
2023-08-21 14:43:15 +0100

    test(e2e): added e2e tests for incomplete appellant case flow

--- apps/e2e/cypress/e2e/back-office-appeals/reviewAppellantCase.spec.js
+++ apps/e2e/cypress/e2e/back-office-appeals/reviewAppellantCase.spec.js
@@ -3,8 +3,12 @@
 
 import { users } from '../../fixtures/users';
 import { AppealsListPage } from '../../page_objects/appealsListPage';
+import { UpdateDueDatePage } from '../../page_objects/updateDueDatePage';
+import { Page } from '../../page_objects/basePage';
+const appealsListPage = new AppealsListPage();
+const updateDueDatePage = new UpdateDueDatePage();
+const basePage = new Page();
 
-const page = new AppealsListPage();
 describe('Appeals feature', () => {
 	beforeEach(() => {
 		cy.login(users.appeals.caseAdmin);
@@ -12,24 +16,74 @@ describe('Appeals feature', () => {
 
 	it('Validate appellant case', () => {
 		cy.visit('/appeals-service/appeals-list');
-		page.clickAppealFromList(2);
-		page.clickAccordionByText('case documentation');
-		page.clickLinkByText('Review');
-		page.basePageElements.radioButton().contains('Valid').click();
-		page.clickButtonByText('Continue');
-		page.clickLinkByText('Go to case details');
+		appealsListPage.clickAppealFromList(2);
+		appealsListPage.clickAccordionByText('case documentation');
+		appealsListPage.clickLinkByText('Review');
+		basePage.basePageElements.radioButton().contains('Valid').click();
+		appealsListPage.clickButtonByText('Continue');
+		appealsListPage.clickLinkByText('Go to case details');
 	});
 
 	it('Invalidate appellant case', () => {
 		cy.visit('/appeals-service/appeals-list');
-		page.clickAppealFromList(2);
-		page.clickAccordionByText('case documentation');
-		page.clickLinkByText('Review');
-		page.basePageElements.radioButton().contains('Invalid').click();
-		page.clickButtonByText('Continue');
-		page.chooseCheckboxByIndex(1);
-		page.clickButtonByText('Continue');
-		page.clickButtonByText('Confirm');
-		page.clickLinkByText('Go to case details');
+		appealsListPage.clickAppealFromList(2);
+		appealsListPage.clickAccordionByText('case documentation');
+		appealsListPage.clickLinkByText('Review');
+		basePage.basePageElements.radioButton().contains('Invalid').click();
+		appealsListPage.clickButtonByText('Continue');
+		appealsListPage.chooseCheckboxByIndex(1);
+		appealsListPage.clickButtonByText('Continue');
+		appealsListPage.clickButtonByText('Confirm');
+		appealsListPage.clickLinkByText('Go to case details');
+	});
+
+	it('incomplete appellant case', () => {
+		cy.visit('/appeals-service/appeals-list');
+		appealsListPage.clickAppealFromList(14);
+		appealsListPage.clickAccordionByText('case documentation');
+		appealsListPage.clickLinkByText('Review');
+		basePage.basePageElements.radioButton().contains('Incomplete').click();
+		appealsListPage.clickButtonByText('Continue');
+		appealsListPage.chooseCheckboxByIndex(1);
+		appealsListPage.clickButtonByText('Continue');
+		updateDueDatePage.enterDateDay('29');
+		updateDueDatePage.enterDateMonth('12');
+		updateDueDatePage.enterDateYear('2024');
+		appealsListPage.clickButtonByText('Save and Continue');
+		appealsListPage.clickButtonByText('Confirm');
+		appealsListPage.clickLinkByText('Go to case details');
+	});
+
+	it('incomplete appellant case reason: other', () => {
+		cy.visit('/appeals-service/appeals-list');
+		appealsListPage.clickAppealFromList(14);
+		appealsListPage.clickAccordionByText('case documentation');
+		appealsListPage.clickLinkByText('Review');
+		basePage.basePageElements.radioButton().contains('Incomplete').click();
+		appealsListPage.clickButtonByText('Continue');
+		appealsListPage.chooseCheckboxByIndex(7);
+		basePage.fillTextArea('Hello here is some extra info, have a nice day 7384_+!£ =');
+		appealsListPage.clickButtonByText('Continue');
+		updateDueDatePage.enterDateDay('29');
+		updateDueDatePage.enterDateMonth('12');
+		updateDueDatePage.enterDateYear('2024');
+		appealsListPage.clickButtonByText('Save and Continue');
+		appealsListPage.clickButtonByText('Confirm');
+		appealsListPage.clickLinkByText('Go to case details');
+	});
+
+	it('incomplete appellant case skip due date', () => {
+		cy.visit('/appeals-service/appeals-list');
+		appealsListPage.clickAppealFromList(14);
+		appealsListPage.clickAccordionByText('case documentation');
+		appealsListPage.clickLinkByText('Review');
+		basePage.basePageElements.radioButton().contains('Incomplete').click();
+		appealsListPage.clickButtonByText('Continue');
+		appealsListPage.chooseCheckboxByIndex(7);
+		basePage.fillTextArea('Hello here is some extra info, have a nice day 7384_+!£ =');
+		appealsListPage.clickButtonByText('Continue');
+		updateDueDatePage.clickSkipButton('Skip');
+		appealsListPage.clickButtonByText('Confirm');
+		appealsListPage.clickLinkByText('Go to case details');
 	});
 });
