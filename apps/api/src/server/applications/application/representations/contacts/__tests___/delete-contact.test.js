e49c893d7d8f615e2358236ddde336578df42775
James Paterson <james.paterson@kinandcarta.com>
2023-08-30 13:00:05 +0100

    feat(api): set unpublishedUpdates when deleting contact from published representation

--- apps/api/src/server/applications/application/representations/contacts/__tests___/delete-contact.test.js
+++ apps/api/src/server/applications/application/representations/contacts/__tests___/delete-contact.test.js
@@ -10,21 +10,53 @@ const existingRepresentations = [
 		status: 'VALID',
 		redacted: true,
 		received: '2023-03-14T14:28:25.704Z'
+	},
+	{
+		id: 2,
+		representationId: 200,
+		reference: 'BC0110001-3',
+		status: 'PUBLISHED',
+		redacted: true,
+		received: '2023-03-14T14:28:25.704Z',
+		unpublishedUpdates: false
 	}
 ];
 
 describe('Delete Application Representation Contact', () => {
 	beforeAll(() => {
-		databaseConnector.representation.findFirst.mockResolvedValue(existingRepresentations[0]);
 		databaseConnector.representation.findMany.mockResolvedValue(existingRepresentations);
 	});
 
 	it('Delete representation contact', async () => {
+		databaseConnector.representation.findFirst.mockResolvedValue(existingRepresentations[0]);
 		databaseConnector.representationContact.delete.mockResolvedValue();
+
+		const response = await request
+			.delete('/applications/1/representations/2/contacts/1')
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json');
+
+		expect(response.status).toEqual(200);
+		expect(response.body).toEqual({
+			contactId: '1'
+		});
+	});
+
+	it('Delete published representation contact', async () => {
+		databaseConnector.representation.findFirst.mockResolvedValue(existingRepresentations[1]);
+		databaseConnector.representationContact.delete.mockResolvedValue();
+
 		const response = await request
 			.delete('/applications/1/representations/1/contacts/1')
 			.set('Content-Type', 'application/json')
 			.set('Accept', 'application/json');
+
+		expect(databaseConnector.representation.update).toHaveBeenCalledWith({
+			where: { id: 2 },
+			data: {
+				unpublishedUpdates: true
+			}
+		});
 
 		expect(response.status).toEqual(200);
 		expect(response.body).toEqual({
