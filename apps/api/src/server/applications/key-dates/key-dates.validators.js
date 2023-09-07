7f674f478a396876f3eabad4564af2ee1e7a971d
Christopher Daly <christopher.daly@kinandcarta.com>
2023-09-07 14:07:35 +0100

    feat(api): refactor as per pr comments

--- apps/api/src/server/applications/key-dates/key-dates.validators.js
+++ apps/api/src/server/applications/key-dates/key-dates.validators.js
@@ -2,26 +2,26 @@ import { composeMiddleware } from '@pins/express';
 import { body } from 'express-validator';
 import { validationErrorHandler } from '../../middleware/error-handler.js';
 import {
-	acceptanceDateKeys,
-	decisionDateKeys,
-	examinationDateKeys,
-	postDecisionDateKeys,
-	preApplicationDateKeys,
-	preExaminationDateKeys,
-	recommendationDateKeys,
-	withdrawalDateKeys
+	acceptanceDateNames,
+	decisionDateNames,
+	examinationDateNames,
+	postDecisionDateNames,
+	preApplicationDateNames,
+	preExaminationDateNames,
+	recommendationDateNames,
+	withdrawalDateNames
 } from './key-dates.utils.js';
 import { mapUnixTimestampToDate } from '#utils/mapping/map-unix-timestamp-to-date.js';
 
 /**
  *
  * @param {string} keyPath The object path like 'preApplication', 'acceptance', etc.
- * @param {Array<string>} dateKeys Array of date keys
+ * @param {Array<string>} dateNames Array of date names
  * @returns {Array<import('express-validator').ValidationChain>}
  */
-const validationMiddleware = (keyPath, dateKeys) => {
-	return dateKeys.map((dateKey) => {
-		return body(`${keyPath}.${dateKey}`)
+const validationMiddleware = (keyPath, dateNames) => {
+	return dateNames.map((dateName) => {
+		return body(`${keyPath}.${dateName}`)
 			.customSanitizer(mapUnixTimestampToDate)
 			.optional({ nullable: true, checkFalsy: true });
 	});
@@ -31,15 +31,15 @@ export const validateUpdateKeyDates = composeMiddleware(
 	// 'submissionAtPublished' is a string not a date, so we don't do the typical validation
 	...validationMiddleware(
 		'preApplication',
-		preApplicationDateKeys.filter((key) => key != 'submissionAtPublished')
+		preApplicationDateNames.filter((key) => key != 'submissionAtPublished')
 	),
 	body('preApplication.submissionAtPublished').optional({ nullable: true }),
-	...validationMiddleware('acceptance', acceptanceDateKeys),
-	...validationMiddleware('preExamination', preExaminationDateKeys),
-	...validationMiddleware('examination', examinationDateKeys),
-	...validationMiddleware('recommendation', recommendationDateKeys),
-	...validationMiddleware('decision', decisionDateKeys),
-	...validationMiddleware('postDecision', postDecisionDateKeys),
-	...validationMiddleware('withdrawal', withdrawalDateKeys),
+	...validationMiddleware('acceptance', acceptanceDateNames),
+	...validationMiddleware('preExamination', preExaminationDateNames),
+	...validationMiddleware('examination', examinationDateNames),
+	...validationMiddleware('recommendation', recommendationDateNames),
+	...validationMiddleware('decision', decisionDateNames),
+	...validationMiddleware('postDecision', postDecisionDateNames),
+	...validationMiddleware('withdrawal', withdrawalDateNames),
 	validationErrorHandler
 );
