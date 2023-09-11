289e04ebbc95ff8aefaa3b92987889cf08a47257
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-09-11 15:05:49 +0100

    chore(web): improve implementation of statusName filter

--- apps/web/src/server/lib/nunjucks-filters/status-name.js
+++ apps/web/src/server/lib/nunjucks-filters/status-name.js
@@ -5,43 +5,28 @@
  * @returns {string}
  */
 export const statusName = (key) => {
-	let documentStatusName = '';
-
 	switch (key) {
 		case 'awaiting_upload':
-			documentStatusName = 'Awaiting upload';
-			break;
+			return 'Awaiting upload';
 		case 'awaiting_virus_check':
-			documentStatusName = 'Awaiting virus check';
-			break;
+			return 'Awaiting virus check';
 		case 'failed_virus_check':
-			documentStatusName = 'Failed virus check';
-			break;
+			return 'Failed virus check';
 		case 'not_checked':
-			documentStatusName = 'Unchecked';
-			break;
+			return 'Unchecked';
 		case 'checked':
-			documentStatusName = 'Checked';
-			break;
+			return 'Checked';
 		case 'ready_to_publish':
-			documentStatusName = 'Ready to publish';
-			break;
+			return 'Ready to publish';
 		case 'publishing':
-			documentStatusName = 'Publishing';
-			break;
+			return 'Publishing';
 		case 'published':
-			documentStatusName = 'Published';
-			break;
+			return 'Published';
 		case 'do_not_publish':
-			documentStatusName = 'Do not publish';
-			break;
+			return 'Do not publish';
 		case 'unpublished':
-			documentStatusName = 'Unpublished';
-			break;
+			return 'Unpublished';
 		default:
-			documentStatusName = '';
-			break;
+			return '';
 	}
-
-	return documentStatusName;
 };
