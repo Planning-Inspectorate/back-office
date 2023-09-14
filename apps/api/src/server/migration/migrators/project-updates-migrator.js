1f17c770cb3fbb5b847596613f8c89bff6a003f0
Christopher Daly <christopher.daly@kinandcarta.com>
2023-09-14 13:18:48 +0100

    feat(api): implement poc for updates migration

---
+++ apps/api/src/server/migration/migrators/project-updates-migrator.js
@@ -0,0 +1,45 @@
+import { QueryTypes, Sequelize } from 'sequelize';
+import config from '#config/config.js';
+
+const { username, password, database, host, port, dialect } = config.wordpressDatabase;
+
+console.info(`Connecting to server ${host}`);
+
+const sequelize = new Sequelize(database, username, password, {
+	host,
+	port,
+	dialect
+});
+
+/**
+ * Handle an HTTP trigger/request to run the migration
+ *
+ * @param {string[]} caseReferences
+ */
+export const migrateProjectUpdates = async (caseReferences) => {
+	console.info(`Migrating ${caseReferences.length} CASES`);
+
+	for (const caseReference of caseReferences) {
+		const subscribers = await sequelize.query(
+			'SELECT * FROM ipclive.wp_ipc_subscribers s inner join ipclive.wp_ipc_subscriptions sc on s.user_id = sc.user_id where verified  = 1 and s.case_reference  = ?;',
+			{
+				replacements: [caseReference],
+				type: QueryTypes.SELECT
+			}
+		);
+
+		console.log('subscribers', JSON.stringify(subscribers));
+
+		const banners = await sequelize.query(
+			"SELECT * FROM ipclive.wp_posts p inner join ipclive.wp_term_relationships r on r.object_id = p.id  inner join ipclive.wp_terms t on r.term_taxonomy_id = t.term_id   where p.post_type = 'ipc_project_update' and left(t.name,8) = ? and p.post_status = 'publish' order by post_date  desc;",
+			{
+				replacements: [caseReference],
+				type: QueryTypes.SELECT
+			}
+		);
+
+		console.log('banners', JSON.stringify(banners));
+	}
+
+	throw Error('Not implemented');
+};
