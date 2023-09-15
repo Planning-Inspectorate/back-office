d5fd759538d8e5e0acc03133298f911f834daad1
Christopher Daly <christopher.daly@kinandcarta.com>
2023-09-15 13:08:25 +0100

    feat(api): implement queries for project updates

--- apps/functions/applications-migration/project-updates-migration/src/project-updates-migration.js
+++ apps/functions/applications-migration/project-updates-migration/src/project-updates-migration.js
@@ -12,44 +12,138 @@ const sequelize = new Sequelize(database, username, password, {
 	// @ts-ignore
 	dialect
 });
-export class ProjectUpdatesMigration {
-	/**
-	 * Handle an HTTP trigger/request to run the migration
-	 *
-	 * @param {import('@azure/functions').Context} context
-	 * @param {import('@azure/functions').HttpRequest} req
-	 */
-	async handleRequest(context, { body: { caseReferences } }) {
-		context.log.info('handleRequest', caseReferences);
 
-		try {
-			context.log.info(`Migrating ${caseReferences.length} CASES`);
+/**
+ * Handle an HTTP trigger/request to run the migration
+ *
+ * @param {import('@azure/functions').Logger} log
+ * @param {string[]} caseReferences
+ */
+export const migrateProjectUpdates = async (log, caseReferences) => {
+	log.info('handleRequest', caseReferences);
 
-			for (const caseReference of caseReferences) {
-				const subscribers = await sequelize.query(
-					'SELECT * FROM ipclive.wp_ipc_subscribers s inner join ipclive.wp_ipc_subscriptions sc on s.user_id = sc.user_id where verified  = 1 and s.case_reference  = ?;',
-					{
-						replacements: [caseReference],
-						type: QueryTypes.SELECT
-					}
-				);
+	try {
+		log.info(`Migrating ${caseReferences.length} CASES`);
 
-				context.log.info('subscribers', JSON.stringify(subscribers));
+		for (const caseReference of caseReferences) {
+			const updates = await getProjectUpdates(log, caseReference);
 
-				const banners = await sequelize.query(
-					"SELECT * FROM ipclive.wp_posts p inner join ipclive.wp_term_relationships r on r.object_id = p.id  inner join ipclive.wp_terms t on r.term_taxonomy_id = t.term_id   where p.post_type = 'ipc_project_update' and left(t.name,8) = ? and p.post_status = 'publish' order by post_date  desc;",
-					{
-						replacements: [caseReference],
-						type: QueryTypes.SELECT
-					}
-				);
+			// TODO: Post them to the migration endpoint
+			log.info('Project Updates', JSON.stringify(updates));
 
-				context.log.info('banners', JSON.stringify(banners));
-			}
+			const subscriptions = await getProjectSubscriptions(log, caseReference);
 
-			throw Error('Not implemented');
-		} catch (e) {
-			context.log.error(e);
+			// TODO: Post them to the migration endpoint
+			log.info('subscriptions', JSON.stringify(subscriptions));
 		}
+
+		throw Error('Not implemented');
+	} catch (e) {
+		log.error(e);
 	}
-}
+};
+
+/**
+ * @param {import('@azure/functions').Logger} log
+ * @param {string} caseReference
+ */
+const getProjectUpdates = async (log, caseReference) => {
+	// Get all of the updates (They contain three additional properties; caseName, caseDescription and caseStage (int))
+	const updates = await sequelize.query(getUpdatesQuery, {
+		replacements: [caseReference],
+		type: QueryTypes.SELECT
+	});
+
+	updates.forEach((update) => {
+		// @ts-ignore
+		const caseStage = projectStages[update.caseStage];
+
+		if (!caseStage) {
+			// @ts-ignore
+			throw Error(`caseStage ${update.caseStage} invalid for update ${update.id}`);
+		}
+
+		// @ts-ignore
+		update.caseStage = caseStage;
+		// @ts-ignore
+		update.updateStatus = 'published';
+	});
+
+	return updates;
+};
+
+/**
+ * @param {import('@azure/functions').Logger} log
+ * @param {string} caseReference
+ */
+const getProjectSubscriptions = async (log, caseReference) => {
+	const subscribers = await sequelize.query(getSubscriptionsQuery, {
+		replacements: [caseReference],
+		type: QueryTypes.SELECT
+	});
+
+	subscribers.forEach((subscription) => {
+		// @ts-ignore
+		const subscriptionType = subscriptionTypes[subscription.subscriptionType];
+
+		if (!subscriptionType) {
+			// @ts-ignore
+			throw Error(
+				// @ts-ignore
+				`subscriptionType ${subscription.subscriptionType} invalid for subscription ${subscription.subscriptionId}`
+			);
+		}
+
+		// @ts-ignore
+		subscription.subscriptionType = subscriptionType;
+	});
+
+	return subscribers;
+};
+
+const projectStages = {
+	1: 'Pre-application',
+	2: 'Acceptance (review of the application)',
+	3: 'Pre-examination',
+	4: 'Examination',
+	5: 'Recommendation',
+	6: 'Decision',
+	7: 'Decided',
+	8: 'Withdrawn'
+};
+
+const subscriptionTypes = {
+	application_decided: 'applicationDecided',
+	application_submitted: 'applicationSubmitted',
+	registration_opens: 'registrationOpen',
+	all_updates: 'allUpdates'
+};
+
+const getUpdatesQuery = `SELECT p.id,
+       pr.casereference AS caseReference,
+       p.post_date      AS updateDate,
+       p.post_title     AS updateName,
+       p.post_content   AS updateContentEnglish,
+       -- Additional columns we need to migrate to create cases
+       pr.projectname   AS caseName,
+       pr.summary       AS caseDescription,
+       pr.stage         AS caseStage
+FROM   ipclive.wp_posts p
+       INNER JOIN ipclive.wp_term_relationships r ON r.object_id = p.id
+       INNER JOIN ipclive.wp_terms t ON r.term_taxonomy_id = t.term_id
+       INNER JOIN ipclive.wp_ipc_projects pr ON LEFT(t.NAME, 8) = pr.casereference
+WHERE  p.post_type = 'ipc_project_update'
+       AND p.post_status = 'publish'
+       AND pr.casereference = ?
+ORDER  BY post_date DESC;`;
+
+const getSubscriptionsQuery = `SELECT sc.subscription_id   AS subscriptionId,
+s.case_reference     AS caseReference,
+s.useremail          AS emailAddress,
+sc.subscription_type AS subscriptionType,
+sc.subscription_date AS startDate
+FROM   ipclive.wp_ipc_subscribers s
+INNER JOIN ipclive.wp_ipc_subscriptions sc
+		ON s.user_id = sc.user_id
+WHERE  verified = 1
+AND s.case_reference = ?;`;
