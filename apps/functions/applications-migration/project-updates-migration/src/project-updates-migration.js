05f06ba82017311409d5756dcd83ddeca9155d2a
Christopher Daly <christopher.daly@kinandcarta.com>
2023-09-15 10:39:04 +0100

    fix(api): use context.log instead of console.log

--- apps/functions/applications-migration/project-updates-migration/src/project-updates-migration.js
+++ apps/functions/applications-migration/project-updates-migration/src/project-updates-migration.js
@@ -19,11 +19,11 @@ export class ProjectUpdatesMigration {
 	 * @param {import('@azure/functions').Context} context
 	 * @param {import('@azure/functions').HttpRequest} req
 	 */
-	async handleRequest(context, { body: caseReferences }) {
+	async handleRequest(context, { body: { caseReferences } }) {
 		context.log.info('handleRequest', caseReferences);
 
 		try {
-			console.info(`Migrating ${caseReferences.length} CASES`);
+			context.log.info(`Migrating ${caseReferences.length} CASES`);
 
 			for (const caseReference of caseReferences) {
 				const subscribers = await sequelize.query(
@@ -34,7 +34,7 @@ export class ProjectUpdatesMigration {
 					}
 				);
 
-				console.log('subscribers', JSON.stringify(subscribers));
+				context.log.info('subscribers', JSON.stringify(subscribers));
 
 				const banners = await sequelize.query(
 					"SELECT * FROM ipclive.wp_posts p inner join ipclive.wp_term_relationships r on r.object_id = p.id  inner join ipclive.wp_terms t on r.term_taxonomy_id = t.term_id   where p.post_type = 'ipc_project_update' and left(t.name,8) = ? and p.post_status = 'publish' order by post_date  desc;",
@@ -44,7 +44,7 @@ export class ProjectUpdatesMigration {
 					}
 				);
 
-				console.log('banners', JSON.stringify(banners));
+				context.log.info('banners', JSON.stringify(banners));
 			}
 
 			throw Error('Not implemented');
