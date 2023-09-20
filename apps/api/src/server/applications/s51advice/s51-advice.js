093338ba679f04721331bdcf2fcd4e9c38141928
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-09-20 10:31:13 +0100

    feat(api/applications): build and publish S51 advice publish events

---
+++ apps/api/src/server/applications/s51advice/s51-advice.js
@@ -0,0 +1,45 @@
+import { pick } from 'lodash-es';
+
+/**
+ * NSIP S51 Advice
+ *
+ * @typedef {Object} NsipS51AdvicePayload
+ * @property {number} adviceId
+ * @property {number} caseId
+ * @property {string} title
+ * @property {string} from
+ * @property {string} agent
+ * @property {'phone' | 'email' | 'meeting' | 'post'} method
+ * @property {string} enquiryDate
+ * @property {string} enquiryDetails
+ * @property {string} adviceGivenBy
+ * @property {string} adviceDate
+ * @property {string} adviceDetails
+ * @property {'checked' | 'unchecked' | 'readytopublish' | 'published' | 'donotpublish'} status
+ * @property {'unredacted' | 'redacted'} redactionStatus
+ * @property {string[]} attachmentIds
+ * */
+
+/**
+ * @param {import('apps/api/src/database/schema.js').S51Advice} s51Advice
+ * @returns {NsipS51AdvicePayload}
+ * */
+export const buildNsipS51AdvicePayload = (s51Advice) => ({
+	...pick(s51Advice, [
+		'caseId',
+		'title',
+		'enquiryDate',
+		'enquiryDetails',
+		'adviceDate',
+		'adviceDetails'
+	]),
+	adviceId: s51Advice.id,
+	from: s51Advice.enquirer,
+	agent: `${s51Advice.firstName} ${s51Advice.lastName}`,
+	method: s51Advice.enquiryMethod,
+	adviceGivenBy: s51Advice.adviser,
+	status: s51Advice.publishedStatus,
+	redactionStatus: s51Advice.redactedStatus,
+	// @ts-ignore
+	attachmentIds: s51Advice.S51AdviceDocument.map((doc) => doc.id)
+});
