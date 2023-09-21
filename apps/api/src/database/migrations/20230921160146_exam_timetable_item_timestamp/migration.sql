db9c99b178a8cfcc2a1d94881ea524a53ac65730
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-09-21 17:01:56 +0100

    feat(api/applications): record createdAt for exam timetable item

---
+++ apps/api/src/database/migrations/20230921160146_exam_timetable_item_timestamp/migration.sql
@@ -0,0 +1,19 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[ExaminationTimetableItem] ADD [createdAt] DATETIME2 NOT NULL CONSTRAINT [ExaminationTimetableItem_createdAt_df] DEFAULT CURRENT_TIMESTAMP;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
