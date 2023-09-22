3bd7819d8428a06b50f2442a7142ac3f812523bd
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-09-22 13:07:12 +0100

    fix(api): ensure required fields are checked before start

--- appeals/api/src/server/config/schema.js
+++ appeals/api/src/server/config/schema.js
@@ -1,68 +1,75 @@
 import joi from 'joi';
 
-export default joi.object({
-	NODE_ENV: joi.string().valid('development', 'production', 'test'),
-	PORT: joi.number(),
-	SWAGGER_JSON_DIR: joi.string(),
-	DATABASE_URL: joi.string(),
-	DISABLE_INTEGRATIONS: joi.boolean(),
-	defaultApiVersion: joi.string(),
-	serviceBusOptions: joi.object({
-		hostname: joi.string().optional()
-	}),
-	msal: joi.object({
-		clientId: joi.string().optional(),
-		clientSecret: joi.string().optional(),
-		tenantId: joi.string().optional()
-	}),
-	log: joi.object({
-		levelFile: joi.string(),
-		levelStdOut: joi.string()
-	}),
-	cwd: joi.string(),
-	featureFlags: joi.object().pattern(/featureFlagBoas\d+[A-Za-z]+/, joi.boolean()),
-	serviceBusEnabled: joi.boolean().optional(),
-	timetable: joi.object({
-		FPA: joi.object({
-			lpaQuestionnaireDueDate: joi.object({
-				daysFromStartDate: joi.number()
+export default joi
+	.object({
+		NODE_ENV: joi.string().valid('development', 'production', 'test'),
+		PORT: joi.number(),
+		SWAGGER_JSON_DIR: joi.string(),
+		DATABASE_URL: joi.string(),
+		DISABLE_INTEGRATIONS: joi.boolean(),
+		defaultApiVersion: joi.string(),
+		serviceBusOptions: joi.object({
+			hostname: joi.string().optional()
+		}),
+		msal: joi.object({
+			clientId: joi.string().optional(),
+			clientSecret: joi.string().optional(),
+			tenantId: joi.string().optional()
+		}),
+		log: joi.object({
+			levelFile: joi.string(),
+			levelStdOut: joi.string()
+		}),
+		cwd: joi.string(),
+		featureFlags: joi.object().pattern(/featureFlagBoas\d+[A-Za-z]+/, joi.boolean()),
+		serviceBusEnabled: joi.boolean().optional(),
+		timetable: joi.object({
+			FPA: joi.object({
+				lpaQuestionnaireDueDate: joi.object({
+					daysFromStartDate: joi.number()
+				}),
+				statementReviewDate: joi.object({
+					daysFromStartDate: joi.number()
+				}),
+				finalCommentReviewDate: joi.object({
+					daysFromStartDate: joi.number()
+				})
 			}),
-			statementReviewDate: joi.object({
-				daysFromStartDate: joi.number()
+			HAS: joi.object({
+				lpaQuestionnaireDueDate: joi.object({
+					daysFromStartDate: joi.number()
+				})
+			})
+		}),
+		govNotify: joi
+			.object({
+				api: joi.object({
+					key: joi.string() // optional if not NODE_ENV=production
+				}),
+				template: joi.object({
+					validAppellantCase: joi.object({
+						id: joi.string()
+					})
+				}),
+				testMailbox: joi.string()
+			})
+			.when('NODE_ENV', {
+				not: 'production',
+				then: joi.object({ api: joi.object({ key: joi.optional() }) })
 			}),
-			finalCommentReviewDate: joi.object({
-				daysFromStartDate: joi.number()
-			})
+		bankHolidayFeed: joi.object({
+			hostname: joi.string()
 		}),
-		HAS: joi.object({
-			lpaQuestionnaireDueDate: joi.object({
-				daysFromStartDate: joi.number()
+		appealAllocationLevels: joi.array().items(
+			joi.object({
+				level: joi.string(),
+				band: joi.number()
 			})
+		),
+		appealFolderPaths: joi.array().items(joi.string()),
+		appealStages: joi.object({
+			appellantCase: joi.string(),
+			lpaQuestionnaire: joi.string()
 		})
-	}),
-	govNotify: joi.object({
-		api: joi.object({
-			key: joi.string()
-		}),
-		template: joi.object({
-			validAppellantCase: joi.object({
-				id: joi.string()
-			})
-		}),
-		testMailbox: joi.string()
-	}),
-	bankHolidayFeed: joi.object({
-		hostname: joi.string()
-	}),
-	appealAllocationLevels: joi.array().items(
-		joi.object({
-			level: joi.string(),
-			band: joi.number()
-		})
-	),
-	appealFolderPaths: joi.array().items(joi.string()),
-	appealStages: joi.object({
-		appellantCase: joi.string(),
-		lpaQuestionnaire: joi.string()
 	})
-});
+	.options({ presence: 'required' }); // required by default;
