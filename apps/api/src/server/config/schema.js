5cdcc52df0812cf761e7285b3cc11d71a705c0a9
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-09-22 13:07:12 +0100

    fix(api): ensure required fields are checked before start

--- apps/api/src/server/config/schema.js
+++ apps/api/src/server/config/schema.js
@@ -1,30 +1,33 @@
 import joi from 'joi';
 
-export default joi.object({
-	NODE_ENV: joi.string().valid('development', 'production', 'test'),
-	PORT: joi.number(),
-	SWAGGER_JSON_DIR: joi.string(),
-	DATABASE_URL: joi.string(),
-	blobStorageUrl: joi.string(),
-	virusScanningDisabled: joi.boolean().optional(),
-	defaultApiVersion: joi.string(),
-	documentStorageApi: joi.object({
-		host: joi.string(),
-		port: joi.number()
-	}),
-	serviceBusOptions: joi.object({
-		hostname: joi.string().optional()
-	}),
-	msal: joi.object({
-		clientId: joi.string().optional(),
-		clientSecret: joi.string().optional(),
-		tenantId: joi.string().optional()
-	}),
-	log: joi.object({
-		levelFile: joi.string(),
-		levelStdOut: joi.string()
-	}),
-	cwd: joi.string(),
-	featureFlags: joi.object().pattern(/featureFlagBoas\d+[A-Za-z]+/, joi.boolean()),
-	serviceBusEnabled: joi.boolean().optional()
-});
+export default joi
+	.object({
+		NODE_ENV: joi.string().valid('development', 'production', 'test'),
+		PORT: joi.number(),
+		SWAGGER_JSON_DIR: joi.string(),
+		DATABASE_URL: joi.string(),
+		blobStorageUrl: joi.string().when('NODE_ENV', { is: 'development', then: joi.optional() }),
+		virusScanningDisabled: joi.boolean().optional(),
+		defaultApiVersion: joi.string(),
+		documentStorageApi: joi
+			.object({
+				host: joi.string()
+			})
+			.when('NODE_ENV', { not: 'production', then: joi.object({ host: joi.optional() }) }),
+		serviceBusOptions: joi.object({
+			hostname: joi.string().optional()
+		}),
+		msal: joi.object({
+			clientId: joi.string().optional(),
+			clientSecret: joi.string().optional(),
+			tenantId: joi.string().optional()
+		}),
+		log: joi.object({
+			levelFile: joi.string(),
+			levelStdOut: joi.string()
+		}),
+		cwd: joi.string(),
+		featureFlags: joi.object().pattern(/featureFlagBoas\d+[A-Za-z]+/, joi.boolean()),
+		serviceBusEnabled: joi.boolean().optional()
+	})
+	.options({ presence: 'required' }); // required by default
