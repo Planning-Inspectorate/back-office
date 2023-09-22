2df31c02e7ad9169e284e58108f60b261639d0e8
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-09-22 09:41:32 +0100

    feat(tooling): only load .env or .env.test config

--- packages/platform/utils/dotenv.js
+++ packages/platform/utils/dotenv.js
@@ -2,28 +2,18 @@ import dotenv from 'dotenv';
 import path from 'node:path';
 
 /**
- * Evaluates all .env files, and returns a merged configuration of locally
- * defined variables.
+ * Load .env into process.env
  *
  * @param {string} [environment='development']
  * @returns {Record<string, string | undefined>}
  */
 export function loadEnvironment(environment = 'development') {
-	/** @type {Record<string, string | undefined>} */
-	const config = {};
-	const sourceFiles = ['.env', `.env.${environment}`];
+	const isTest = environment === 'test';
 
-	// Ensure tests can run in a deterministic fashion regardless of changes made
-	// to local environment (such as during development)
-	if (environment !== 'test') {
-		sourceFiles.push('.env.local', `.env.${environment}.local`);
-	}
+	// either load .env.test for tests, or just .env regardless of environment
+	const sourceFile = isTest ? '.env.test' : '.env';
+	// load into process.env (process.env takes precendence)
+	dotenv.config({ path: path.resolve(sourceFile) });
 
-	for (const pathToFile of sourceFiles) {
-		Object.assign(config, dotenv.config({ path: path.resolve(pathToFile) }).parsed);
-	}
-
-	return Object.fromEntries(
-		Object.entries({ ...config, ...process.env }).filter(([, value]) => Boolean(value))
-	);
+	return process.env;
 }
