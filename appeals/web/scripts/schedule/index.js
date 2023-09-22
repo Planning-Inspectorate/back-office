0e38377175b81a314f952c67793b4d416ec2c102
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-09-22 15:48:18 +0100

    chore(web): remove unused schedule/task code

--- appeals/web/scripts/schedule/index.js
+++
@@ -1,7 +0,0 @@
-import schedule from 'node-schedule';
-import clearCache from '../tasks/clear-cache.js';
-
-// Clear the cache at 5am each day
-const clearCacheJob = schedule.scheduleJob('* 5 * * *', clearCache);
-
-clearCacheJob.invoke();
