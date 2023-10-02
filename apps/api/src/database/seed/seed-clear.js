97400c256c8eb6bf95989e6a076d7ea3e5218ca4
David J White <50454208+DavidJWhiteDev@users.noreply.github.com>
2023-10-02 13:10:50 +0100

    fix(api/applications): add missing tables to seed script (boas-1216)

--- apps/api/src/database/seed/seed-clear.js
+++ apps/api/src/database/seed/seed-clear.js
@@ -1,6 +1,8 @@
 import { truncateTable } from '../prisma.truncate.js';
 
 /**
+ * Seeding function to clear down a database, deletes all cases, documents, records, reference tables, etc
+ *
  * @param {import('@prisma/client').PrismaClient} databaseConnector
  */
 export async function deleteAllRecords(databaseConnector) {
@@ -13,12 +15,16 @@ export async function deleteAllRecords(databaseConnector) {
 	const deleteGridReference = databaseConnector.gridReference.deleteMany();
 	const deleteDocuments = databaseConnector.document.deleteMany();
 	const deleteDocumentsVersions = databaseConnector.documentVersion.deleteMany();
+	const deleteDocumentActivityLog = databaseConnector.documentActivityLog.deleteMany();
+	const deletes51Advice = databaseConnector.s51Advice.deleteMany();
+	const deletes51AdviceDocument = databaseConnector.s51AdviceDocument.deleteMany();
 	const deleteFolders = databaseConnector.folder.deleteMany();
 	const deleteRepresentationContact = databaseConnector.representationContact.deleteMany();
 	const deleteRepresentationAttachment = databaseConnector.representationAttachment.deleteMany();
 	const deleteRepresentation = databaseConnector.representation.deleteMany();
 	const deleteRepresentationAction = databaseConnector.representationAction.deleteMany();
 	const deleteProjectUpdates = databaseConnector.projectUpdate.deleteMany();
+	const deleteExaminationTimetable = databaseConnector.examinationTimetable.deleteMany();
 
 	// and reference data tables
 	const deleteSubSector = databaseConnector.subSector.deleteMany();
@@ -27,21 +33,26 @@ export async function deleteAllRecords(databaseConnector) {
 	const deleteZoomLevel = databaseConnector.zoomLevel.deleteMany();
 	const deleteExaminationTimetableType = databaseConnector.examinationTimetableItem.deleteMany();
 
-	// Truncate calls on data tables
+	// start deleting ...
 	await deleteRepresentationAttachment;
 	await deleteRepresentationAction;
 	await deleteRepresentationContact;
 	await deleteRepresentation;
 
-	// delete document versions, documents, and THEN the folders.  Has to be in this order for integrity constraints
+	// delete S51 advice documents, S51 Advice, document Activity logs, document versions, documents, and THEN the folders.
+	// Has to be in this order for integrity constraints
+	await deletes51AdviceDocument;
+	await deletes51Advice;
 	// TODO: Currently an issue with cyclic references, hence this hack to clear the latestVersionId
 	await databaseConnector.$queryRawUnsafe(`UPDATE Document SET latestVersionId = NULL;`);
+	await deleteDocumentActivityLog;
 	await deleteDocumentsVersions;
 	await deleteDocuments;
 
-	// truncate tables
+	// truncate / delete tables
 	await truncateTable('RegionsOnApplicationDetails');
 	await truncateTable('ExaminationTimetableItem');
+	await deleteExaminationTimetable;
 
 	await deleteLowestFolders(databaseConnector);
 	await deleteLowestFolders(databaseConnector);
