06c03029eb305d8db4fb05a19830b57f5035a8e2
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-10-03 08:37:22 +0100

    chore(applications): remove unused document-storage-api

--- apps/document-storage/src/server/app/middleware/async-handler.js
+++
@@ -1,8 +0,0 @@
-/**
- * @template T
- * @param {import('express').RequestHandler<T>} requestHandler request handler
- * @returns {import('express').RequestHandler<T>} request handler
- */
-export const asyncHandler = (requestHandler) => (request, response, next) =>
-	Promise.resolve(requestHandler(request, response, next))
-		.catch(next);
