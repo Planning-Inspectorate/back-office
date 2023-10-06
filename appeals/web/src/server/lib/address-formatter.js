7199fe0658563fffa4f652d2d8e7bc4752d772fe
james-hill-kc <james.hill@kinandcarta.com>
2023-10-06 11:26:06 +0100

    fix(web): site details to display neighbour addresses

--- appeals/web/src/server/lib/address-formatter.js
+++ appeals/web/src/server/lib/address-formatter.js
@@ -3,14 +3,14 @@ import { join, map, pick } from 'lodash-es';
 /**
  * converts a multi part address to a single string
  *
- * @param {{ addressLine1:string, addressLine2:string, town:string, county:string, postCode:string}} address
+ * @param {import('@pins/appeals').Address} address
  * @returns {string}
  */
 export const addressToString = (address) => {
 	return join(
 		map(pick(address, ['addressLine1', 'addressLine2', 'town', 'county', 'postCode']), (value) => {
 			return value?.trim();
-		}).filter((value) => value.length),
+		}).filter((value) => value?.length),
 		', '
 	);
 };
