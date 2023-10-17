817213b93c7f4dd8ba1857efd896acb8b0e8dbf5
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-10-17 11:22:58 +0100

    fix(web): revert to in memory session storage until infra is fixed

--- apps/web/src/server/app/config/session.js
+++ apps/web/src/server/app/config/session.js
@@ -1,65 +1,66 @@
 import config from '@pins/applications.web/environment/config.js';
 import session from 'express-session';
-import RedisStore from 'connect-redis';
-import { createClient } from 'redis';
-import { parseRedisConnectionString } from '@pins/platform';
-import logger from '../../lib/logger.js';
+// import RedisStore from 'connect-redis';
+// import { createClient } from 'redis';
+// import { parseRedisConnectionString } from '@pins/platform';
+// import logger from '../../lib/logger.js';
 
-/**
- * @param {string} redisConnectionString
- * @returns {import('express-session').Store}
- */
-function createRedisStore(redisConnectionString) {
-	if (!redisConnectionString) {
-		throw new Error('expected redis connection string');
-	}
-	const details = parseRedisConnectionString(redisConnectionString);
+// /**
+//  * @param {string} redisConnectionString
+//  * @returns {import('express-session').Store}
+//  */
+// function createRedisStore(redisConnectionString) {
+// 	if (!redisConnectionString) {
+// 		throw new Error('expected redis connection string');
+// 	}
+// 	const details = parseRedisConnectionString(redisConnectionString);
 
-	const client = createClient({
-		socket: {
-			host: details.host,
-			port: details.port,
-			tls: details.ssl
-		},
-		password: details.password
-	});
+// 	const client = createClient({
+// 		socket: {
+// 			host: details.host,
+// 			port: details.port,
+// 			tls: details.ssl
+// 		},
+// 		password: details.password
+// 	});
 
-	/** @param {Error|any} err */
-	const onError = (err) =>
-		logger.error(`Could not establish a connection with redis server: ${err}`);
-	client.on('connect', () => logger.info('Initiating connection to redis server...'));
-	client.on('ready', () => logger.info('Connected to redis server successfully...'));
-	client.on('end', () => logger.info('Disconnected from redis server...'));
-	client.on('error', onError);
-	client.on('reconnecting', () => logger.info('Reconnecting to redis server...'));
+// 	/** @param {Error|any} err */
+// 	const onError = (err) =>
+// 		logger.error(`Could not establish a connection with redis server: ${err}`);
+// 	client.on('connect', () => logger.info('Initiating connection to redis server...'));
+// 	client.on('ready', () => logger.info('Connected to redis server successfully...'));
+// 	client.on('end', () => logger.info('Disconnected from redis server...'));
+// 	client.on('error', onError);
+// 	client.on('reconnecting', () => logger.info('Reconnecting to redis server...'));
 
-	// kick off the connection - no await here, in the background
-	client.connect().catch(onError);
+// 	// kick off the connection - no await here, in the background
+// 	client.connect().catch(onError);
 
-	// dev note: this may 'error' in vscode, but tscheck is all OK
-	return new RedisStore({ client });
-}
+// 	// dev note: this may 'error' in vscode, but tscheck is all OK
+// 	return new RedisStore({ client });
+// }
 
-/**
- * @param {string} [redisConnectionString]
- * @returns {import('express-session').Store}
- */
-function configureStore(redisConnectionString) {
-	if (redisConnectionString && typeof redisConnectionString === 'string') {
-		logger.info('Configuring Redis for session storage');
-		return createRedisStore(redisConnectionString);
-	}
-	// default to memory store if redis is not configured
-	// config.session.redis is required by config schema when NODE_ENV is production
-	// check here to be sure
-	if (config.isProduction) {
-		throw new Error('REDIS_CONNECTION_STRING is required in production');
-	}
-	logger.info('Configuring memory store for session storage');
-	return new session.MemoryStore();
-}
+// /**
+//  * @param {string} [redisConnectionString]
+//  * @returns {import('express-session').Store}
+//  */
+// function configureStore(redisConnectionString) {
+// 	if (redisConnectionString && typeof redisConnectionString === 'string') {
+// 		logger.info('Configuring Redis for session storage');
+// 		return createRedisStore(redisConnectionString);
+// 	}
+// 	// default to memory store if redis is not configured
+// 	// config.session.redis is required by config schema when NODE_ENV is production
+// 	// check here to be sure
+// 	if (config.isProduction) {
+// 		throw new Error('REDIS_CONNECTION_STRING is required in production');
+// 	}
+// 	logger.info('Configuring memory store for session storage');
+// 	return new session.MemoryStore();
+// }
 
-const store = configureStore(config.session.redis);
+// TODO: for now we want to use memory store, until Redis setup is fixed
+const store = new session.MemoryStore(); // configureStore(config.session.redis);
 
 export default session({
 	secret: config.session?.secret,
