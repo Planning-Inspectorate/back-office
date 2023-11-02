73fc94d5ba9734114bc5702b4c2eb1db057e4004
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-11-02 16:49:59 +0000

    fix(web/applications): make fixture project team unique (boas-975)

--- apps/web/testing/applications/factory/project-team.js
+++ apps/web/testing/applications/factory/project-team.js
@@ -1,15 +1,22 @@
 /** @typedef {import("apps/web/src/server/applications/applications.types.js").ProjectTeamMember} ProjectTeamMember */
 
-import { faker } from '@faker-js/faker';
 import { fake } from '@pins/platform';
+import { createRandomDescription, createUniqueRandomNumberFromSeed } from './util.js';
 
 /**
  * @param {Partial<ProjectTeamMember>} [options={}]
  * @returns {ProjectTeamMember}
  */
 export function createProjectTeamMember({ id = fake.createUniqueId() } = {}) {
-	const firstName = faker.name.firstName();
-	const lastName = faker.name.lastName();
+	const firstName = `${createRandomDescription({
+		wordsNumber: 1,
+		startOffset: createUniqueRandomNumberFromSeed(3, 12, id)
+	})}`;
+
+	const lastName = `${createRandomDescription({
+		wordsNumber: 1,
+		startOffset: createUniqueRandomNumberFromSeed(4, 12, id)
+	})}`;
 
 	const name = `${firstName} ${lastName}`;
 
