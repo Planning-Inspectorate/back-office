d4714ff436ac569e29c3432f35ccc8e5d22a4342
David J White <50454208+DavidJWhiteDev@users.noreply.github.com>
2023-11-14 15:27:00 +0000

    fix(functions): fix azure doc unpublishing 2 (boas-1321)

--- apps/functions/document-publish/unpublish-document/config.js
+++ apps/functions/document-publish/unpublish-document/config.js
@@ -5,6 +5,7 @@ const schema = joi.object({
 	NODE_ENV: joi.string().valid('development', 'production', 'test'),
 	API_HOST: joi.string(),
 	BLOB_STORAGE_ACCOUNT_HOST: joi.string(),
+	BLOB_SOURCE_CONTAINER: joi.string(),
 	BLOB_PUBLISH_CONTAINER: joi.string(),
 	log: joi.object({
 		levelStdOut: joi.string()
@@ -17,6 +18,7 @@ const { value, error } = schema.validate({
 	NODE_ENV: environment.NODE_ENV,
 	API_HOST: environment.API_HOST,
 	BLOB_STORAGE_ACCOUNT_HOST: environment.BLOB_STORAGE_ACCOUNT_HOST,
+	BLOB_SOURCE_CONTAINER: environment.BLOB_SOURCE_CONTAINER,
 	BLOB_PUBLISH_CONTAINER: environment.BLOB_PUBLISH_CONTAINER,
 	log: {
 		levelStdOut: environment.LOG_LEVEL_STDOUT || 'debug'
