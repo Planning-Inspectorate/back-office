75ac2357a2838f90d1a19d8082e037195feea60a
Alex Moulin <alex.moulin@planninginspectorate.gov.uk>
2023-11-15 16:35:48 +0000

    fix(appeals/api): add logging to auditTrail to identify source of upload doc bug

--- appeals/api/src/server/endpoints/audit-trails/audit-trails.service.js
+++ appeals/api/src/server/endpoints/audit-trails/audit-trails.service.js
@@ -1,5 +1,6 @@
 import auditTrailRepository from '#repositories/audit-trail.repository.js';
 import userRepository from '#repositories/user.repository.js';
+import logger from '#utils/logger.js';
 
 /** @typedef {import('@pins/appeals.api').Appeals.CreateAuditTrail} CreateAuditTrail */
 /** @typedef {import('@pins/appeals.api').Schema.AuditTrail} AuditTrail */
@@ -9,19 +10,24 @@ import userRepository from '#repositories/user.repository.js';
  * @returns {Promise<AuditTrail | undefined>}
  */
 const createAuditTrail = async ({ appealId, azureAdUserId, details }) => {
-	if (azureAdUserId && details) {
-		const { id: userId } = await userRepository.findOrCreateUser(azureAdUserId);
+	try {
+		if (azureAdUserId && details) {
+			const { id: userId } = await userRepository.findOrCreateUser(azureAdUserId);
 
-		if (userId) {
-			const auditTrail = await auditTrailRepository.createAuditTrail({
-				appealId,
-				details,
-				loggedAt: new Date(),
-				userId
-			});
+			if (userId) {
+				const auditTrail = await auditTrailRepository.createAuditTrail({
+					appealId,
+					details,
+					loggedAt: new Date(),
+					userId
+				});
 
-			return auditTrail;
+				return auditTrail;
+			}
 		}
+	} catch (error) {
+		logger.error(error);
+		throw new Error('Failed to create audit trail');
 	}
 };
 
