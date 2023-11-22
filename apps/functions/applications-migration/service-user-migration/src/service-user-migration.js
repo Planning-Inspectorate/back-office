333d5e7ccb05dd1a2a11011d80232e38bd92d6dd
Christopher Daly <christopher.daly@kinandcarta.com>
2023-11-22 15:01:49 +0000

    feat(api): poc for querying service users directly from synapse

---
+++ apps/functions/applications-migration/service-user-migration/src/service-user-migration.js
@@ -0,0 +1,16 @@
+import { SynapseDB } from '../../common/synapse-db.js';
+
+/**
+ * Handle an HTTP trigger/request to run the migration
+ *
+ * @param {import('@azure/functions').Logger} log
+ */
+export const migrateServiceUsers = async (log) => {
+	log.info('Migrating All Service Users');
+
+	const serviceUsers = await SynapseDB.query(
+		'SELECT TOP (100) * FROM [odw_curated_db].[dbo].[service_user]'
+	);
+
+	log.info(serviceUsers);
+};
