b70c5673f843a619b89993a5229535a23a4dd1e6
Lsoma <Laxman.Soma@planninginspectorate.gov.uk>
2023-11-22 15:51:26 +0000

    test(e2e): added team member related scenarios

---
+++ apps/e2e/cypress/e2e/back-office-applications/addTeam.spec.js
@@ -0,0 +1,85 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { users } from '../../fixtures/users';
+import { ApplicationsHomePage } from '../../page_objects/applicationsHomePage';
+import { CreateCasePage } from '../../page_objects/createCasePage';
+import { SearchResultsPage } from '../../page_objects/searchResultsPage';
+import { ProjectTeamPage } from '../../page_objects/projectTeamPage';
+import { projectInformation } from '../../support/utils/createProjectInformation';
+
+const createCasePage = new CreateCasePage();
+const projectTeamPage = new ProjectTeamPage();
+const applicationsHomePage = new ApplicationsHomePage();
+const searchResultsPage = new SearchResultsPage();
+const { applications: applicationUsers } = users;
+
+describe('Create case ', () => {
+	let projectInfo;
+
+	before(() => {
+		projectInfo = projectInformation();
+		cy.login(applicationUsers.caseAdmin);
+		createCasePage.createCase(projectInfo);
+	});
+
+	it('Add team member and verify the role is added to project team', () => {
+		cy.login(applicationUsers.caseAdmin);
+		cy.visit('/');
+		const caseRef = Cypress.env('currentCreatedCase');
+		applicationsHomePage.searchFor(caseRef);
+		searchResultsPage.clickTopSearchResult();
+		searchResultsPage.clickLinkByText('Project team');
+		searchResultsPage.clickButtonByText('Add team member');
+		projectTeamPage.addTeamMeber('caseofficeradmin.test@planninginspectorate.gov.uk');
+		projectTeamPage.verifyCaseManagerRoleAdded();
+	});
+
+	it('Verify team member is added', () => {
+		cy.login(applicationUsers.caseAdmin);
+		cy.visit('/');
+		const caseRef = Cypress.env('currentCreatedCase');
+		applicationsHomePage.searchFor(caseRef);
+		searchResultsPage.clickTopSearchResult();
+		searchResultsPage.clickLinkByText('Project team');
+		searchResultsPage.clickButtonByText('Add team member');
+		projectTeamPage.searchTeamMemberByEmail('caseofficeradmin.test@planninginspectorate.gov.uk');
+        projectTeamPage.verifyTeamMemberIsAdded();
+	});
+
+	it('Enter valid input and verify multiple results count', () => {
+		cy.login(applicationUsers.caseAdmin);
+		cy.visit('/');
+		const caseRef = Cypress.env('currentCreatedCase');
+		applicationsHomePage.searchFor(caseRef);
+		searchResultsPage.clickTopSearchResult();
+		searchResultsPage.clickLinkByText('Project team');
+		searchResultsPage.clickButtonByText('Add team member');
+		projectTeamPage.searchTeamMemberByEmail('test');
+		projectTeamPage.validateMultipleSearchCount();
+	});
+
+	it('Enter invalid input and verify the error and count', () => {
+		cy.login(applicationUsers.caseAdmin);
+		cy.visit('/');
+		const caseRef = Cypress.env('currentCreatedCase');
+		applicationsHomePage.searchFor(caseRef);
+		searchResultsPage.clickTopSearchResult();
+		searchResultsPage.clickLinkByText('Project team');
+		searchResultsPage.clickButtonByText('Add team member');
+		projectTeamPage.searchTeamMemberByEmail('abc');
+		projectTeamPage.verifyInvalidSearchResultsMessageAndCount();
+	});
+
+	it('Verify the error message without entering the search criteria', () => {
+		cy.login(applicationUsers.caseAdmin);
+		cy.visit('/');
+		const caseRef = Cypress.env('currentCreatedCase');
+		applicationsHomePage.searchFor(caseRef);
+		searchResultsPage.clickTopSearchResult();
+		searchResultsPage.clickLinkByText('Project team');
+		searchResultsPage.clickButtonByText('Add team member');
+		projectTeamPage.validateErrorMessageWithoutEnteringAnything();
+	});
+
+});
