b0f2f1faed14475d448c114c35e74a85d34c11a0
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-11-24 12:09:26 +0000

    feat(api/applications): initialise App Insights in back office API

--- apps/api/src/server/app.js
+++ apps/api/src/server/app.js
@@ -1,8 +1,21 @@
+import appInsights from 'applicationinsights';
 import fs from 'node:fs';
 import path from 'node:path';
 import swaggerUi from 'swagger-ui-express';
+import logger from '#utils/logger.js';
 import { buildApp } from './build-app.js';
 import config from './config/config.js';
+
+try {
+	appInsights
+		.setup(config.APPLICATIONINSIGHTS_CONNECTION_STRING)
+		.setAutoDependencyCorrelation(true)
+		.setAutoCollectConsole(true)
+		.setSendLiveMetrics(true)
+		.start();
+} catch (err) {
+	logger.warn({ err }, 'Application insights failed to start: ');
+}
 
 const app = buildApp((expressApp) => {
 	const swaggerAuto = JSON.parse(fs.readFileSync(path.resolve(config.SWAGGER_JSON_DIR)));
