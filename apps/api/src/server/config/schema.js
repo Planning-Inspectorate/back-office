dcc996b3b4759a93ec893a8ce96b3972d13402ee
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-11-24 11:56:38 +0000

    chore(api/applications): add App Insights connection string to config

--- apps/api/src/server/config/schema.js
+++ apps/api/src/server/config/schema.js
@@ -4,6 +4,7 @@ export default joi
 	.object({
 		NODE_ENV: joi.string().valid('development', 'production', 'test'),
 		PORT: joi.number(),
+		APPLICATIONINSIGHTS_CONNECTION_STRING: joi.string(),
 		SWAGGER_JSON_DIR: joi.string(),
 		DATABASE_URL: joi.string(),
 		blobStorageUrl: joi.string().when('NODE_ENV', { not: 'production', then: joi.optional() }),
