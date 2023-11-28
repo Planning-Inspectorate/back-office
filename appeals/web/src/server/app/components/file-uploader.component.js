36a4ac9801183d9c766b91cc5fad5a2db19b621b
james-hill-kc <james.hill@kinandcarta.com>
2023-11-28 17:45:24 +0000

    fix(appeals): handle duplicate document upload error

--- appeals/web/src/server/app/components/file-uploader.component.js
+++ appeals/web/src/server/app/components/file-uploader.component.js
@@ -1,5 +1,6 @@
 import getActiveDirectoryAccessToken from '#lib/active-directory-token.js';
 import config from '@pins/appeals.web/environment/config.js';
+import logger from '#lib/logger.js';
 
 /** @typedef {import('../auth/auth-session.service').SessionWithAuth} SessionWithAuth */
 /** @typedef {import('@azure/core-auth').AccessToken} AccessToken */
@@ -11,10 +12,20 @@ import config from '@pins/appeals.web/environment/config.js';
  * @param {import('got').Got} apiClient
  * @param {string} caseId
  * @param {AddDocumentsRequest} payload
- * @returns {Promise<AddDocumentsResponse>}
+ * @returns {Promise<AddDocumentsResponse|undefined>}
  */
 export const createNewDocument = async (apiClient, caseId, payload) => {
-	return apiClient.post(`appeals/${caseId}/documents`, { json: payload }).json();
+	// try {
+	// 	return await apiClient.post(`appeals/${caseId}/documents`, { json: payload }).json();
+	// } catch (error) {
+	// 	logger.error(
+	// 		error,
+	// 		error instanceof Error
+	// 			? error.message
+	// 			: 'An error occurred while attempting to patch the documents API endpoint'
+	// 	);
+	// }
+	return await apiClient.post(`appeals/${caseId}/documents`, { json: payload }).json();
 };
 
 /**
@@ -33,31 +44,40 @@ export const createNewDocumentVersion = async (apiClient, caseId, documentId, pa
  *
  * @param {{apiClient: import('got').Got, params: {caseId: string}, session: SessionWithAuth, body: AddDocumentsRequest}} request
  * @param {*} response
- * @returns {Promise<{}>}
+ * @returns {Promise<{}|undefined>}
  */
 export async function postDocumentsUpload({ apiClient, params, body, session }, response) {
-	const { caseId } = params;
-	const uploadInfo = await createNewDocument(apiClient, caseId, body);
-	const { documents } = uploadInfo;
+	try {
+		const { caseId } = params;
+		const uploadInfo = await createNewDocument(apiClient, caseId, body);
 
-	let accessToken = undefined;
-	if (config.useBlobEmulator !== true) {
-		accessToken = await getActiveDirectoryAccessToken(session);
-	}
+		if (uploadInfo) {
+			const { documents } = uploadInfo;
 
-	const documentsWithRowId = documents.map((document) => {
-		if (document) {
-			const fileToUpload = body.documents.find(
-				(file) => file.documentName === document.documentName
-			);
-			const documentWithRowId = { ...document };
-			documentWithRowId.fileRowId = fileToUpload?.fileRowId || '';
+			let accessToken = undefined;
+			if (config.useBlobEmulator !== true) {
+				accessToken = await getActiveDirectoryAccessToken(session);
+			}
 
-			return documentWithRowId;
+			const documentsWithRowId = documents.map((document) => {
+				if (document) {
+					const fileToUpload = body.documents.find(
+						(file) => file.documentName === document.documentName
+					);
+					const documentWithRowId = { ...document };
+					documentWithRowId.fileRowId = fileToUpload?.fileRowId || '';
+
+					return documentWithRowId;
+				}
+			});
+
+			return response.send({ ...uploadInfo, documents: documentsWithRowId, accessToken });
 		}
-	});
+	} catch (error) {
+		logger.error(error);
 
-	return response.send({ ...uploadInfo, documents: documentsWithRowId, accessToken });
+		return response.send({ error });
+	}
 }
 
 /**
