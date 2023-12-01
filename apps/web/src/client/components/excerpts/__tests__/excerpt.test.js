94b595cfcb08c55c467da82e5de543c7bd14ce0d
Ludovico <ludovico.tamigio@kinandcarta.com>
2023-12-01 12:23:52 +0000

    fix(web/applications): handle js link showing in noscript situation (boas-1244)

--- apps/web/src/client/components/excerpts/__tests__/excerpt.test.js
+++ apps/web/src/client/components/excerpts/__tests__/excerpt.test.js
@@ -9,33 +9,39 @@ import initExcerpt from '../excerpt.js';
 
 const DOM = `
 <div class="excerpt">
-    <div class="excerpt-summary" id="summary">
-        The applicant has agreed that all application documents can be published as soon as practicable to help everyone become familiar with the detail of what is being proposed in this application. The...
+	<div class="excerpt-summary" id="summary">
+		The applicant has agreed that all application documents can be published as soon as practicable to help everyone become familiar with the detail of what is being proposed in this application. The...
+		<noscript>
+			<details
+				class="govuk-details govuk-!-margin-bottom-1 govuk-!-margin-top-4"
+				data-module="govuk-details"
+			>
+				<summary class="govuk-details__summary">
+					<span class="govuk-details__summary-text">Show full update</span>
+				</summary>
+				<div class="govuk-details__text">
+					The applicant has agreed that all application documents can be published as soon as practicable to help everyone become familiar with the detail of what is being proposed in this application. The Planning Inspectorate will therefore make the application documents available as soon as practicable. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+				</div>
+			</details>
+		</noscript>
+		<div class="display--js-enabled-only">
+			<p class="govuk-body govuk-!-margin-bottom-1 govuk-!-margin-top-4">
+				<a href="#" id='show' class="govuk-link govuk-link--no-visited-state excerpt-link">
+					Show full update
+				</a>
+			</p>
+		</div>
+	</div>
 
-        <noscript>
-            <details class="govuk-details govuk-!-margin-bottom-1 govuk-!-margin-top-4" data-module="govuk-details">
-            <summary class="govuk-details__summary">
-                <span class="govuk-details__summary-text">
-                Show full update
-                </span>
-            </summary>
-            <div class="govuk-details__text">
-                The applicant has agreed that all application documents can be published as soon as practicable to help everyone become familiar with the detail of what is being proposed in this application. The Planning Inspectorate will therefore make the application documents available as soon as practicable. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-            </div>
-            </details>
-        </noscript>
-        <p class="govuk-body govuk-!-margin-bottom-1 govuk-!-margin-top-4">
-            <a id="show" href="#" class="govuk-link govuk-link--no-visited-state excerpt-link">Show full update</a>
-        </p>
-    </div>
-
-    <div class="excerpt-full display--none" id="full">
-        The applicant has agreed that all application documents can be published as soon as practicable to help everyone become familiar with the detail of what is being proposed in this application. The Planning Inspectorate will therefore make the application documents available as soon as practicable. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-
-        <p class="govuk-body govuk-!-margin-bottom-1 govuk-!-margin-top-4">
-            <a id="hide" href="#" class="govuk-link govuk-link--no-visited-state excerpt-link">Hide full update</a>
-        </p>
-    </div>
+	<div class="excerpt-full display--none" id="full">
+		The applicant has agreed that all application documents can be published as soon as practicable to help everyone become familiar with the detail of what is being proposed in this application. The Planning Inspectorate will therefore make the application documents available as soon as practicable. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+		<p class="govuk-body govuk-!-margin-bottom-1 govuk-!-margin-top-4">
+			<a href="#" id='hide' class="govuk-link govuk-link--no-visited-state excerpt-link">
+				Hide full update
+			</a>
+		</p>
+		<div></div>
+	</div>
 </div>
 `;
 
