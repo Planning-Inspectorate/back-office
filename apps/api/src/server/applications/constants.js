bc14cd75136a52a197f01e2fb90254cd7dc7fa2a
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-12-08 15:05:17 +0000

    feat(api/applications): record documentType when inserting

--- apps/api/src/server/applications/constants.js
+++ apps/api/src/server/applications/constants.js
@@ -2,3 +2,8 @@ export const DEFAULT_PAGE_NUMBER = 1;
 export const DEFAULT_PAGE_SIZE = 25;
 
 export const SYSTEM_USER_NAME = 'System';
+
+export const DOCUMENT_TYPES = {
+	Document: 'document',
+	S51Attachment: 's51-attachment'
+};
