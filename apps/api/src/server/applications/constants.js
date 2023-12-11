4980fec1c4cae25771bc5df2ce35689cfd3a7f95
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-12-11 11:20:57 +0000

    feat(api/applications): add relevant representation type constants

--- apps/api/src/server/applications/constants.js
+++ apps/api/src/server/applications/constants.js
@@ -5,5 +5,6 @@ export const SYSTEM_USER_NAME = 'System';
 
 export const DOCUMENT_TYPES = {
 	Document: 'document',
-	S51Attachment: 's51-attachment'
+	S51Attachment: 's51-attachment',
+	RelevantRepresentation: 'relevant-representation'
 };
