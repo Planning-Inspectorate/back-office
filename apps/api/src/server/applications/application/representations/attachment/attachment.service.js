5dadffe64426dfa59fc737ab7fa3c10badf09490
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-12-11 11:21:22 +0000

    feat(api/applications): update document to relevant rep attachment
    We upload documents and then link them as RR attachments as two separate
    steps, so with this change the type of a document will initially be
    'document', then when it is linked as an attachment it will change to
    'relevant-representation'.

--- apps/api/src/server/applications/application/representations/attachment/attachment.service.js
+++ apps/api/src/server/applications/application/representations/attachment/attachment.service.js
@@ -1,9 +1,28 @@
 import * as representationsRepository from '../../../../repositories/representation.repository.js';
+import * as documentRepository from '../../../../repositories/document.repository.js';
+import { DOCUMENT_TYPES } from '../../../constants.js';
 
+/**
+ * @param {number} repId
+ * @param {string} documentId
+ * */
 export const addAttachmentRepresentation = async (repId, documentId) => {
-	return representationsRepository.addApplicationRepresentationAttachment(repId, documentId);
+	const result = await representationsRepository.addApplicationRepresentationAttachment(
+		repId,
+		documentId
+	);
+
+	await documentRepository.update(documentId, {
+		documentType: DOCUMENT_TYPES.RelevantRepresentation
+	});
+
+	return result;
 };
 
+/**
+ * @param {number} repId
+ * @param {number} attachmentId
+ * */
 export const deleteAttachmentRepresentation = async (repId, attachmentId) => {
 	return representationsRepository.deleteApplicationRepresentationAttachment(repId, attachmentId);
 };
