7a4b1175e7b251286caad81e12f1b4705cfcefa7
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2023-12-12 17:37:39 +0000

    feat(appeals/api): additional mapping for broadcast

--- appeals/api/src/server/endpoints/integrations/integrations.service.js
+++ appeals/api/src/server/endpoints/integrations/integrations.service.js
@@ -4,22 +4,70 @@ import { eventClient } from '#infrastructure/event-client.js';
 import { EventType } from '@pins/event-client';
 import { mapAppeal } from './integrations.mappers/index.js';
 import { schemas, validateFromSchema } from './integrations.validators.js';
+import { randomUUID } from 'node:crypto';
 import {
 	createAppeal,
 	createOrUpdateLpaQuestionnaire,
 	createDocument,
 	loadAppeal
 } from '#repositories/integrations.repository.js';
+import { databaseConnector } from '#utils/database-connector.js';
 
-export const importAppellantCase = async (/** @type {any} */ data, /** @type {any} */ documents) =>
-	await createAppeal(data, documents);
+export const importAppellantCase = async (
+	/** @type {any} */ data,
+	/** @type {any} */ documents
+) => {
+	const existingDocs = await databaseConnector.document.findMany({
+		where: {
+			guid: {
+				in: documents.map((/** @type {{ documentGuid: string; }} */ d) => d.documentGuid)
+			}
+		}
+	});
+
+	if (existingDocs?.length >= 0) {
+		const docIDs = existingDocs.map((d) => d.guid);
+		for (const document of documents) {
+			if (docIDs.indexOf(document.documentGuid) === 0) {
+				document.documentGuid = randomUUID();
+			}
+		}
+	}
+
+	const result = await createAppeal(data, documents);
+	return result;
+};
 
 export const importLPAQuestionnaire = async (
 	/** @type {any} */ caseReference,
 	/** @type {any} */ nearbyCaseReferences,
 	/** @type {any} */ data,
 	/** @type {any} */ documents
-) => await createOrUpdateLpaQuestionnaire(caseReference, nearbyCaseReferences, data, documents);
+) => {
+	const existingDocs = await databaseConnector.document.findMany({
+		where: {
+			guid: {
+				in: documents.map((/** @type {{ documentGuid: string; }} */ d) => d.documentGuid)
+			}
+		}
+	});
+
+	if (existingDocs?.length >= 0) {
+		const docIDs = existingDocs.map((d) => d.guid);
+		for (const document of documents) {
+			if (docIDs.indexOf(document.documentGuid) === 0) {
+				document.documentGuid = randomUUID();
+			}
+		}
+	}
+	const result = await createOrUpdateLpaQuestionnaire(
+		caseReference,
+		nearbyCaseReferences,
+		data,
+		documents
+	);
+	return result;
+};
 
 export const importDocument = async (/** @type {any} */ document) => await createDocument(document);
 
