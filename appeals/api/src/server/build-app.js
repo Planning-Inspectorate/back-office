024287bed4c10d90ccba9f6f8489e1fa4fcfe8e0
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2023-12-14 10:39:19 +0000

    feat(appeals/api): healthcheck on / and /health

--- appeals/api/src/server/build-app.js
+++ appeals/api/src/server/build-app.js
@@ -7,11 +7,12 @@ import { appealsRoutes } from './endpoints/appeals.routes.js';
 import { defaultErrorHandler, stateMachineErrorHandler } from './middleware/error-handler.js';
 import versionRoutes from './middleware/version-routes.js';
 import BackOfficeAppError from './utils/app-error.js';
+import { databaseConnector } from '#utils/database-connector.js';
 
 // The purpose of this is to allow the jest environment to create an instance of the app without loading Swagger
 // We have to use a HOF (i.e. we can't just conditionally register swagger UI) because Jest doesn't care about our conditionals and loads all of the modules based on the top-level imports
-// @ts-ignore
 const buildApp = (
+	// @ts-ignore
 	/** @type {(app: import("express-serve-static-core").Express) => void} */ addSwaggerUi = null
 ) => {
 	const app = express();
@@ -25,6 +26,17 @@ const buildApp = (
 	app.use(compression());
 	app.use(morgan('combined'));
 	app.use(helmet());
+
+	app.get(['/', '/health'], async (req, res) => {
+		try {
+			await databaseConnector.$queryRaw`SELECT 1;`;
+		} catch {
+			res.status(500).send('Database connection failed during health check');
+			return;
+		}
+
+		res.status(200).send('OK');
+	});
 
 	app.use(
 		'/appeals',
