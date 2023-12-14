bd3f7fdf03bfc9920641ca4d7fe680d5264b7ac8
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-12-14 10:21:15 +0000

    chore(api/applications): remove pretty print transport for pino
    Also removed the pino/file transport as strangely the service fails to
    startup if the pino/file transport is used alone (not in conjuction with
    pino-pretty).

--- apps/api/src/server/utils/logger.js
+++ apps/api/src/server/utils/logger.js
@@ -1,33 +1,10 @@
 import AppInsights from 'applicationinsights';
-import path from 'node:path';
 import pino from 'pino';
 import pinoHttp from 'pino-http';
-import config from '../config/config.js';
 
 export const pinoLogger = pino({
 	timestamp: pino.stdTimeFunctions.isoTime,
-	level: 'trace',
-	transport: {
-		targets: [
-			{
-				target: 'pino/file',
-				level: config.log.levelFile,
-				options: {
-					destination: path.join(config.cwd, './server.log')
-				}
-			},
-			{
-				target: 'pino-pretty',
-				level: config.log.levelStdOut,
-				options: {
-					destination: 1,
-					ignore: 'pid,hostname',
-					colorize: true,
-					translateTime: 'HH:MM:ss.l'
-				}
-			}
-		]
-	}
+	level: 'trace'
 });
 
 const decorator = {
