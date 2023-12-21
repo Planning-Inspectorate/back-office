c4a396be63954997da1d824f8dc4638c234a8295
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-12-21 13:07:36 +0000

    feat(functions): validate curated odw entities

--- apps/functions/applications-migration/nsip-project-migration/src/nsip-project-migration.js
+++ apps/functions/applications-migration/nsip-project-migration/src/nsip-project-migration.js
@@ -1,3 +1,4 @@
+import { loadSchemaValidator, summariseErrors } from '../../common/validation.js';
 import { SynapseDB } from '../../common/synapse-db.js';
 
 /**
@@ -6,11 +7,25 @@ import { SynapseDB } from '../../common/synapse-db.js';
  * @param {import('@azure/functions').Logger} log
  */
 export const migrationNsipProjects = async (log) => {
-	log.info('Migrating 100 NSIP Projects');
+	log.info('loading nsip-project validator');
+	const validator = await loadSchemaValidator('nsip-project');
+
+	log.info('reading 100 NSIP Projects');
 
 	const nsipProjects = await SynapseDB.query(
 		'SELECT TOP (100) * FROM [odw_curated_db].[dbo].[nsip_data]'
 	);
 
 	log.info(nsipProjects);
+	let validCount = 0;
+	for (let i = 0; i < nsipProjects.length; i++) {
+		const nsipProject = nsipProjects[i];
+		const isValid = validator(nsipProject);
+		if (isValid) {
+			validCount++;
+			continue;
+		}
+		log.info(`invalid message (index ${i}): ${summariseErrors(validator.errors)}`);
+	}
+	log.info(`${nsipProjects.length} messages, ${validCount} valid`);
 };
