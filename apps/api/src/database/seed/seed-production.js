62a76ae8bfd7fb3e2b2c044199326d34b8c49144
David J White <50454208+DavidJWhiteDev@users.noreply.github.com>
2024-01-03 10:58:26 +0000

    fix(api/applications): fix seeding typing issues (boas-1393)

--- apps/api/src/database/seed/seed-production.js
+++ apps/api/src/database/seed/seed-production.js
@@ -1,4 +1,4 @@
-import { databaseConnector } from '../../server/utils/database-connector.js';
+import { databaseConnector } from '#utils/database-connector.js';
 import { seedStaticData } from './data-static.js';
 
 /**
