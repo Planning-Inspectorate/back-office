0023c9ffc61992b44daf19c94394f9a75f662268
Christopher Daly <christopher.daly@kinandcarta.com>
2024-01-05 15:20:17 +0000

    feat(api): migration for nsip project

---
+++ apps/api/src/database/migrations/20240105141100_seed_case_tables/migration.sql
@@ -0,0 +1,20 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- Add the new seeds for the identity columns to 100 million
+DBCC CHECKIDENT ('dbo.Case', RESEED, 100000000);
+DBCC CHECKIDENT ('dbo.ApplicationDetails', RESEED, 100000000);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
---
+++ apps/functions/applications-migration/README.md
@@ -0,0 +1,3 @@
+# Developing against Synapse
+Most of the steps followed are detailed here:
+https://pins-ds.atlassian.net/browse/BOAS-1343
