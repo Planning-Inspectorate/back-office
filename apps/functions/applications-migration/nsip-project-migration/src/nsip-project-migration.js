4fb689a5825fa4d053db607b67287b2704aa8a70
Christopher Daly <christopher.daly@kinandcarta.com>
2024-01-05 15:20:17 +0000

    feat(api): migration for nsip project

--- apps/functions/applications-migration/nsip-project-migration/src/nsip-project-migration.js
+++ apps/functions/applications-migration/nsip-project-migration/src/nsip-project-migration.js
@@ -1,31 +1,58 @@
-import { loadSchemaValidator, summariseErrors } from '../../common/validation.js';
 import { SynapseDB } from '../../common/synapse-db.js';
+import { QueryTypes } from 'sequelize';
+import { makePostRequest } from 'project-updates-migration/src/back-office-api-client.js';
 
+/**
 /**
  * Handle an HTTP trigger/request to run the migration
  *
  * @param {import('@azure/functions').Logger} log
+ * @param {string[]} caseReferences
  */
-export const migrationNsipProjects = async (log) => {
-	log.info('loading nsip-project validator');
-	const validator = await loadSchemaValidator('nsip-project');
+export const migrateProjectUpdates = async (log, caseReferences) => {
+	log.info(`Migrating ${caseReferences.length} Cases`);
 
-	log.info('reading 100 NSIP Projects');
+	for (const caseReference of caseReferences) {
+		try {
+			log.info(`Migrating NSIP Project for case ${caseReference}`);
 
-	const nsipProjects = await SynapseDB.query(
-		'SELECT TOP (100) * FROM [odw_curated_db].[dbo].[nsip_data]'
+			const projects = await getNsipProjects(log, caseReference);
+
+			if (projects.length > 0) {
+				log.info(`Migrating ${projects.length} NSIP Projects for case ${caseReference}`);
+
+				await makePostRequest(log, '/migration/nsip-project', projects);
+
+				log.info('Successfully migrated NSIP Project');
+			} else {
+				log.warn(`No NSIP Project found for case ${caseReference}`);
+			}
+		} catch (e) {
+			log.error(`Failed to migrate NSIP Project for case ${caseReference}`, e);
+			throw e;
+		}
+	}
+};
+
+/**
+ * @param {import('@azure/functions').Logger} log
+ * @param {string} caseReference
+ */
+const getNsipProjects = async (log, caseReference) => {
+	const projects = await SynapseDB.query(
+		'SELECT * FROM [odw_curated_db].[dbo].[nsip_data] WHERE caseReference = ?;',
+		{
+			replacements: [caseReference],
+			type: QueryTypes.SELECT
+		}
 	);
 
-	log.info(nsipProjects);
-	let validCount = 0;
-	for (let i = 0; i < nsipProjects.length; i++) {
-		const nsipProject = nsipProjects[i];
-		const isValid = validator(nsipProject);
-		if (isValid) {
-			validCount++;
-			continue;
-		}
-		log.info(`invalid message (index ${i}): ${summariseErrors(validator.errors)}`);
-	}
-	log.info(`${nsipProjects.length} messages, ${validCount} valid`);
+	log.info(`Retrieved projects ${JSON.stringify(projects)}`);
+
+	projects.forEach((project) => {
+		// @ts-ignore
+		projects.regions = [project.region];
+	});
+
+	return projects;
 };
