973ed7a65cba3409691618a56f1a93a954c9cef3
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2024-01-05 14:45:49 +0000

    feat(appeals/api): enabling late docs flag

---
+++ appeals/api/src/database/migrations/20240105140802_version_late_entries/migration.sql
@@ -0,0 +1,29 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[Appeal] ADD [resubmitTypeId] INT;
+
+-- AlterTable
+ALTER TABLE [dbo].[AppealTimetable] ADD [resubmitAppealTypeDate] DATETIME2;
+
+-- AlterTable
+ALTER TABLE [dbo].[AppealType] ADD [code] NVARCHAR(1000),
+[enabled] BIT;
+
+-- AlterTable
+ALTER TABLE [dbo].[DocumentVersion] ADD [isLateEntry] BIT;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
