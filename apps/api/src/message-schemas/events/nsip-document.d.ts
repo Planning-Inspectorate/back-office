b3a0099f8d88e1f3baf681b147332be8fd75eb24
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-01-11 11:27:07 +0000

    feat(api/applications): build correct NSIPDocument payload structure

--- apps/api/src/message-schemas/events/nsip-document.d.ts
+++ apps/api/src/message-schemas/events/nsip-document.d.ts
@@ -1,3 +1,36 @@
+export type VirusCheckStatus = 'not_scanned' | 'scanned' | 'affected' | null;
+export type RedactedStatus = 'not_redacted' | 'redacted' | null;
+export type PublishedStatus =
+	| 'not_checked'
+	| 'checked'
+	| 'ready_to_publish'
+	| 'do_not_publish'
+	| 'publishing'
+	| 'published'
+	| 'archived'
+	| null;
+export type SecurityClassification = 'public' | 'official' | 'secret' | 'top-secret' | null;
+export type SourceSystem =
+	| 'back-office-appeals'
+	| 'back-office-applications'
+	| 'horizon'
+	| 'ni_file'
+	| 'sharepoint'
+	| null;
+export type Origin = 'pins' | 'citizen' | 'lpa' | 'ogd' | null;
+export type Stage =
+	| 'draft'
+	| 'pre-application'
+	| 'acceptance'
+	| 'pre-examination'
+	| 'examination'
+	| 'recommendation'
+	| 'decision'
+	| 'post_decision'
+	| 'withdrawn'
+	| 'developers_application'
+	| null;
+
 export interface NSIPDocument {
 	documentId: string;
 	caseRef?: string;
@@ -11,47 +44,22 @@ export interface NSIPDocument {
 	mime: string | null;
 	documentURI?: string;
 	publishedDocumentURI?: string;
-	virusCheckStatus: 'not_scanned' | 'scanned' | 'affected' | null;
+	virusCheckStatus: VirusCheckStatus;
 	fileMD5: string | null;
 	dateCreated: string | null;
 	lastModified?: string;
-	redactedStatus: 'not_redacted' | 'redacted' | null;
-	publishedStatus:
-		| 'not_checked'
-		| 'checked'
-		| 'ready_to_publish'
-		| 'do_not_publish'
-		| 'publishing'
-		| 'published'
-		| 'archived'
-		| null;
+	redactedStatus: RedactedStatus;
+	publishedStatus: PublishedStatus;
 	datePublished?: string;
 	documentType: string | null;
-	securityClassification: 'public' | 'official' | 'secret' | 'top-secret' | null;
-	sourceSystem:
-		| 'back-office-appeals'
-		| 'back-office-applications'
-		| 'horizon'
-		| 'ni_file'
-		| 'sharepoint'
-		| null;
-	origin: 'pins' | 'citizen' | 'lpa' | 'ogd' | null;
+	securityClassification: SecurityClassification;
+	sourceSystem: SourceSystem;
+	origin: Origin;
 	owner: string | null;
 	author: string | null;
 	representative: string | null;
 	description: string | null;
-	stage:
-		| 'draft'
-		| 'pre-application'
-		| 'acceptance'
-		| 'pre-examination'
-		| 'examination'
-		| 'recommendation'
-		| 'decision'
-		| 'post_decision'
-		| 'withdrawn'
-		| 'developers_application'
-		| null;
+	stage: Stage;
 	filter1: string | null;
 	filter2: string | null;
 	horizonFolderId: string | null;
