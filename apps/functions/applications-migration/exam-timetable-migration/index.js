65563c354cacc34faea49daeb79a0dc2f8784dc6
Christopher Daly <christopher.daly@kinandcarta.com>
2024-01-16 17:01:34 +0000

    fix(api): bad copy/paste

--- apps/functions/applications-migration/exam-timetable-migration/index.js
+++ apps/functions/applications-migration/exam-timetable-migration/index.js
@@ -1,11 +1,11 @@
-import { migrationNsipProjects } from './src/nsip-project-migration.js';
+import { migrateExamTimetables } from './src/exam-timetable-migration.js';
 
 /**
  * @param {import('@azure/functions').Context} context
  * @param {import('@azure/functions').HttpRequest} req
  */
 export default async function (context, { body: { caseReferences } }) {
-	console.info('Migrating NSIP Projects for', JSON.stringify(caseReferences));
+	console.info('Migrating Exam Timetable for', JSON.stringify(caseReferences));
 
-	await migrationNsipProjects(context.log, caseReferences);
+	await migrateExamTimetables(context.log, caseReferences);
 }
