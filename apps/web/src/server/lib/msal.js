e8d3a1432d5905e20b8cc87f2c6e1460959f0876
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-01-17 13:24:59 +0000

    feat(web/applications): persist MSAL cache in redis

--- apps/web/src/server/lib/msal.js
+++ apps/web/src/server/lib/msal.js
@@ -1,6 +1,29 @@
 import msal, { LogLevel } from '@azure/msal-node';
 import config from '@pins/applications.web/environment/config.js';
 import pino from './logger.js';
+import * as redis from './redis.js';
+
+/**
+ * @param {*} cacheContext
+ * */
+const beforeCacheAccess = async (cacheContext) => {
+	const cacheData = await redis.get('msal');
+	cacheContext.tokenCache.deserialize(cacheData);
+};
+
+/**
+ * @param {*} cacheContext
+ * */
+const afterCacheAccess = async (cacheContext) => {
+	if (cacheContext.cacheHasChanged) {
+		await redis.set('msal', cacheContext.tokenCache.serialize());
+	}
+};
+
+const cachePlugin = {
+	beforeCacheAccess,
+	afterCacheAccess
+};
 
 export const msalClient = new msal.ConfidentialClientApplication({
 	auth: {
@@ -35,7 +58,8 @@ export const msalClient = new msal.ConfidentialClientApplication({
 			piiLoggingEnabled: false,
 			logLevel: msal.LogLevel.Warning
 		}
-	}
+	},
+	cache: { cachePlugin }
 });
 
 /**
