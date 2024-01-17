b2a76ff23a6307e098bd8fa253d5ec58f99e7b10
James Paterson <james.paterson@kinandcarta.com>
2024-01-17 12:56:07 +0000

    fix(api/applications): subscription service user migration

---
+++ apps/api/src/server/migration/migrators/service-user-migrator.js
@@ -0,0 +1,110 @@
+import { MigratedEntityIdCeiling } from '../migrator.consts.js';
+import { buildUpsertForEntity } from './sql-tools.js';
+import { databaseConnector } from '#utils/database-connector.js';
+
+/**
+ * @typedef import('pins-data-model').Schemas.ServiceUser ServiceUser
+ */
+
+/**
+ * @param {ServiceUser[]} serviceUsers
+ */
+export const migrateServiceUsers = async (serviceUsers) => {
+	console.info(`Migrating ${serviceUsers.length} Service Users`);
+
+	for (const serviceUser of serviceUsers) {
+		const serviceUserEntity = mapModelToServiceUserEntity(serviceUser);
+		const addressEntity = mapModelToAddressEntity(serviceUser);
+
+		if (serviceUserEntity.id >= MigratedEntityIdCeiling) {
+			throw Error(`Unable to migrate entity id=${serviceUserEntity.id} - identity above threshold`);
+		}
+
+		const { statement: serviceUserStatement, parameters: serviceUserParameters } =
+			buildUpsertForEntity('ServiceUser', serviceUserEntity, 'id');
+
+		console.info(`Upserting ServiceUser with ID ${serviceUserEntity.id}`);
+
+		await databaseConnector.$transaction([
+			databaseConnector.$executeRawUnsafe(serviceUserStatement, ...serviceUserParameters)
+		]);
+
+		const transactions = [];
+		transactions.push(
+			databaseConnector.serviceUser.update({
+				where: {
+					id: serviceUserEntity.id
+				},
+				data: {
+					address: {
+						upsert: {
+							create: addressEntity,
+							update: addressEntity
+						}
+					}
+				}
+			})
+		);
+
+		if (serviceUser.serviceUserType === 'Applicant') {
+			transactions.push(
+				databaseConnector.case.updateMany({
+					where: {
+						reference: serviceUser.caseReference
+					},
+					data: {
+						applicantId: serviceUserEntity.id
+					}
+				})
+			);
+		}
+		await databaseConnector.$transaction(transactions);
+	}
+};
+
+/**
+ * @param {ServiceUser} serviceUser
+ */
+const mapModelToServiceUserEntity = ({
+	id,
+	firstName,
+	lastName,
+	organisation,
+	role,
+	telephoneNumber,
+	emailAddress,
+	webAddress
+}) => {
+	const parsedId = Number(id);
+	if (!parsedId) throw `Invalid ServiceUser ID: ${id}`;
+
+	return {
+		id: parsedId,
+		organisationName: organisation,
+		firstName,
+		lastName,
+		email: emailAddress,
+		website: webAddress,
+		phoneNumber: telephoneNumber,
+		jobTitle: role
+	};
+};
+
+/**
+ * @param {ServiceUser} serviceUser
+ */
+const mapModelToAddressEntity = ({
+	addressLine1,
+	addressLine2,
+	addressTown,
+	addressCounty,
+	postcode,
+	addressCountry
+}) => ({
+	addressLine1,
+	addressLine2,
+	town: addressTown,
+	county: addressCounty,
+	postcode,
+	country: addressCountry
+});
--- apps/functions/applications-migration/service-user-migration/index.js
+++ apps/functions/applications-migration/service-user-migration/index.js
@@ -2,7 +2,10 @@ import { migrateServiceUsers } from './src/service-user-migration.js';
 
 /**
  * @param {import('@azure/functions').Context} context
+ * @param  {object} message
  */
-export default async function (context) {
-	await migrateServiceUsers(context.log);
+export default async function (context, { body: { caseReferences } }) {
+	console.info('Migrating Service Users for', JSON.stringify(caseReferences));
+
+	await migrateServiceUsers(context.log, caseReferences);
 }
