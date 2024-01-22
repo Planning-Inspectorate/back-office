a9cdb6f73e94d29e0e509fef9983b122f791a987
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-01-22 10:47:54 +0000

    refactor(packages/redis): use classes for MSAL plugin configuration
    This is the way the documentation suggests to initialise an MSAL plugin.
    Also, the MSAL library expects the Redis client to always return a
    string, never null as is default in the redis npm package. The custom
    MSALCacheClient class gets around this by returning an empty string if
    the true result is null.

--- packages/redis/src/msal-plugin.js
+++ packages/redis/src/msal-plugin.js
@@ -1,31 +1,67 @@
-/**
- * @param {ReturnType<import('redis').createClient>} client
- * @param {import('./types').Logger} logger
- * @returns {import('@azure/msal-node').ICachePlugin}
- * */
-export const createMsalPlugin = (client, logger) => ({
+export class MSALPlugin {
+	/**
+	 * @param {import('@azure/msal-node').ICacheClient} client
+	 * @param {import('./types').Logger} logger
+	 * */
+	constructor(client, logger) {
+		this.client = client;
+		this.logger = logger;
+	}
+
+	/**
+	 * @param {import('@azure/msal-node').TokenCacheContext} cacheContext
+	 * */
 	async beforeCacheAccess(cacheContext) {
-		if (!client) {
-			logger.error('Tried to access uninitialised Redis cache.');
+		if (!this.client) {
+			this.logger.error('Tried to access uninitialised Redis cache.');
 			return;
 		}
 
-		const cacheData = await client.get('msal');
+		const cacheData = await this.client.get('msal');
 		if (!cacheData) {
 			return;
 		}
 
 		cacheContext.tokenCache.deserialize(cacheData);
-	},
+	}
 
+	/**
+	 * @param {import('@azure/msal-node').TokenCacheContext} cacheContext
+	 * */
 	async afterCacheAccess(cacheContext) {
-		if (!client) {
-			logger.error('Tried to access uninitialised Redis cache.');
+		if (!this.client) {
+			this.logger.error('Tried to access uninitialised Redis cache.');
 			return;
 		}
 
 		if (cacheContext.cacheHasChanged) {
-			await client.set('msal', cacheContext.tokenCache.serialize());
+			await this.client.set('msal', cacheContext.tokenCache.serialize());
 		}
 	}
-});
+}
+
+export class MSALCacheClient {
+	/**
+	 * @param {ReturnType<import('redis').createClient>} redisClient
+	 * */
+	constructor(redisClient) {
+		this.redisClient = redisClient;
+	}
+
+	/**
+	 * @param {string} key
+	 * @returns {Promise<string>}
+	 * */
+	async get(key) {
+		return (await this.redisClient.get(key)) ?? '';
+	}
+
+	/**
+	 * @param {string} key
+	 * @param {string} value
+	 * @returns {Promise<string>}
+	 * */
+	async set(key, value) {
+		return (await this.redisClient.set(key, value)) ?? '';
+	}
+}
