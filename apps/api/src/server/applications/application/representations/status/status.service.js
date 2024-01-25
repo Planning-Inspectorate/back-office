0e7583e67892aadeaf76e11513559d264924ad96
David J White <50454208+DavidJWhiteDev@users.noreply.github.com>
2024-01-25 14:12:02 +0000

    chore(api/applications): event broadcasts representations status refactor (boas-1430)

--- apps/api/src/server/applications/application/representations/status/status.service.js
+++ apps/api/src/server/applications/application/representations/status/status.service.js
@@ -1,5 +1,4 @@
-import * as representationsRepository from '../../../../repositories/representation.repository.js';
-import { publishRepresentationStatusUpdate } from '../publish/publish.service.js';
+import * as representationsRepository from '#repositories/representation.repository.js';
 
 export const updateStatusRepresentation = async (repId, action) => {
 	const representation = await representationsRepository.getFirstById(repId);
@@ -7,8 +6,6 @@ export const updateStatusRepresentation = async (repId, action) => {
 	const previousStatus = representation.status;
 	const newStatus = action.status;
 	const unpublished = previousStatus === 'PUBLISHED' && newStatus !== 'PUBLISHED';
-
-	if (unpublished) await publishRepresentationStatusUpdate(representation, newStatus);
 
 	return representationsRepository.updateApplicationRepresentationStatusById(
 		representation,
