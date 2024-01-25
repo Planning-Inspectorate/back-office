557778c7988bf104c60fc77df2405c65e7379ca9
David J White <50454208+DavidJWhiteDev@users.noreply.github.com>
2024-01-25 14:12:02 +0000

    chore(api/applications): event broadcasts representations status refactor (boas-1430)

--- apps/api/src/server/applications/application/representations/status/__tests__/patch-status.test.js
+++ apps/api/src/server/applications/application/representations/status/__tests__/patch-status.test.js
@@ -1,17 +1,77 @@
 import { jest } from '@jest/globals';
-import { request } from '../../../../../app-test.js';
+import { request } from '#app-test';
 import { eventClient } from '#infrastructure/event-client.js';
 
-const { databaseConnector } = await import('../../../../../utils/database-connector.js');
+const { databaseConnector } = await import('#utils/database-connector.js');
 
 const existingRepresentations = [
 	{
 		id: 1,
-		caseId: 1,
+		caseId: 200,
 		reference: 'BC0110001-2',
 		status: 'VALID',
 		redacted: true,
-		received: '2023-03-14T14:28:25.704Z'
+		received: '2023-03-14T14:28:25.704Z',
+		originalRepresentation: 'the original representation',
+		redactedRepresentation: 'redacted version',
+		case: { id: 1, reference: 'BC0110001' },
+		representationActions: [
+			{
+				actionBy: '',
+				actionDate: '2022-03-31T23:00:00.000Z',
+				invalidReason: '',
+				notes: '',
+				previousRedactStatus: false,
+				previousStatus: 'AWAITING_REVIEW',
+				redactStatus: true,
+				referredTo: '',
+				status: 'VALID',
+				type: 'STATUS'
+			}
+		],
+		represented: {
+			id: 10381,
+			representationId: 6579,
+			firstName: 'Mrs',
+			lastName: 'Sue',
+			jobTitle: null,
+			under18: false,
+			organisationName: null,
+			email: 'sue@example.com',
+			phoneNumber: '01234 567890',
+			contactMethod: null,
+			address: {
+				id: 17059,
+				addressLine1: '123 Some Street',
+				addressLine2: 'Somewhere Else',
+				postcode: 'B1 9BB',
+				county: 'A County',
+				town: 'Some Town',
+				country: 'England'
+			}
+		},
+		representative: {
+			id: 10382,
+			representationId: 6579,
+			firstName: 'James',
+			lastName: 'Bond',
+			jobTitle: null,
+			under18: false,
+			organisationName: null,
+			email: 'test-agent@example.com',
+			phoneNumber: '01234 567890',
+			contactMethod: null,
+			address: {
+				id: 17060,
+				addressLine1: '1 Long Road',
+				addressLine2: 'Smallville',
+				postcode: 'P7 9LN',
+				county: 'A County',
+				town: 'Some Town',
+				country: 'England'
+			}
+		},
+		attachments: []
 	},
 	{
 		id: 2,
@@ -23,12 +83,25 @@ const existingRepresentations = [
 	}
 ];
 
-const expectedUnpublishPayload = [
-	{
-		representationId: 2,
-		status: 'VALID'
-	}
-];
+const expectedRepresentationUpdatePayload = {
+	representationId: 1,
+	referenceId: 'BC0110001-2',
+	examinationLibraryRef: '',
+	caseRef: 'BC0110001',
+	caseId: 200,
+	status: 'VALID',
+	redacted: true,
+	originalRepresentation: 'the original representation',
+	representationType: undefined,
+	representedId: '10381',
+	representativeId: '10382',
+	representationFrom: 'AGENT',
+	registerFor: undefined,
+	dateReceived: '2023-03-14T14:28:25.704Z',
+	attachmentIds: [],
+	redactedRepresentation: 'redacted version'
+};
+
 const mockDate = new Date('2023-01-02');
 
 describe('Patch Application Representation Status', () => {
@@ -40,6 +113,7 @@ describe('Patch Application Representation Status', () => {
 		databaseConnector.representationAction.create.mockResolvedValue();
 		jest.useFakeTimers().setSystemTime(mockDate);
 	});
+	afterEach(() => jest.clearAllMocks());
 
 	it('Patch representation status', async () => {
 		const response = await request
@@ -70,7 +144,14 @@ describe('Patch Application Representation Status', () => {
 				type: 'STATUS'
 			}
 		});
-		expect(eventClient.sendEvents).not.toHaveBeenCalled();
+
+		// check event broadcast
+		expect(eventClient.sendEvents).toHaveBeenCalledWith(
+			'nsip-representation',
+			expectedRepresentationUpdatePayload,
+			'Update'
+		);
+
 		expect(response.status).toEqual(200);
 		expect(response.body).toEqual({
 			repId: 1
@@ -180,7 +261,7 @@ describe('Patch Application Representation Status', () => {
 		});
 		expect(eventClient.sendEvents).toHaveBeenCalledWith(
 			'nsip-representation',
-			expectedUnpublishPayload,
+			expectedRepresentationUpdatePayload,
 			'Update'
 		);
 		expect(response.status).toEqual(200);
