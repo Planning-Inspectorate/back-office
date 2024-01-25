aaabeeb1f3aa523dce35d9d18ddbedb1203d1535
Gavin Lilley <Gavin.Lilley@planninginspectorate.gov.uk>
2024-01-25 15:52:07 +0000

    fix(appeals): viewing personal list - shows page not found error

--- appeals/api/src/server/utils/appeal-sorter.js
+++ appeals/api/src/server/utils/appeal-sorter.js
@@ -6,6 +6,10 @@
  * @returns {AppealListResponse[]}
  */
 export const sortAppeals = (appeals) => {
-	// @ts-ignore
-	return appeals.sort((a, b) => a.dueDate.getTime() - b.dueDate.getTime());
+	return appeals.sort((a, b) => {
+		if (!a.dueDate) return 1;
+		if (!b.dueDate) return -1;
+
+		return new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime();
+	});
 };
