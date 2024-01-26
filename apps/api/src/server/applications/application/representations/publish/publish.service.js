f939563c6a8577a20e717fd76284ac245adbdd72
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-01-26 12:04:46 +0000

    feat(api/applications): block events for training representations

--- apps/api/src/server/applications/application/representations/publish/publish.service.js
+++ apps/api/src/server/applications/application/representations/publish/publish.service.js
@@ -1,5 +1,6 @@
 import * as representationsRepository from '#repositories/representation.repository.js';
 import { eventClient } from '#infrastructure/event-client.js';
+import logger from '#utils/logger.js';
 import { NSIP_REPRESENTATION, SERVICE_USER } from '#infrastructure/topics.js';
 import { EventType } from '@pins/event-client';
 import {
@@ -7,7 +8,13 @@ import {
 	buildRepresentationServiceUserPayload
 } from '../representations.service.js';
 import { setRepresentationsAsPublished } from '#repositories/representation.repository.js';
+import { verifyNotTraining } from '../../application.validators.js';
 
+/**
+ * @param {number} caseId
+ * @param {number[]} representationIds
+ * @param {string} actionBy
+ * */
 export const publishCaseRepresentations = async (caseId, representationIds, actionBy) => {
 	const representations = await representationsRepository.getPublishableRepresentationsById(
 		caseId,
@@ -18,14 +25,20 @@ export const publishCaseRepresentations = async (caseId, representationIds, acti
 		const nsipRepresentationsPayload = representations.map(buildNsipRepresentationPayload);
 		const serviceUsersPayload = representations.flatMap(buildRepresentationServiceUserPayload);
 
-		await eventClient.sendEvents(
-			NSIP_REPRESENTATION,
-			nsipRepresentationsPayload,
-			EventType.Publish
-		);
-		await eventClient.sendEvents(SERVICE_USER, serviceUsersPayload, EventType.Publish, {
-			entityType: 'RepresentationContact'
-		});
+		try {
+			await verifyNotTraining(caseId);
+
+			await eventClient.sendEvents(
+				NSIP_REPRESENTATION,
+				nsipRepresentationsPayload,
+				EventType.Publish
+			);
+			await eventClient.sendEvents(SERVICE_USER, serviceUsersPayload, EventType.Publish, {
+				entityType: 'RepresentationContact'
+			});
+		} catch (/** @type {*} */ err) {
+			logger.info(`Blocked sending event for representations: ${representationIds}`, err.message);
+		}
 
 		await setRepresentationsAsPublished(representations, actionBy);
 	}
