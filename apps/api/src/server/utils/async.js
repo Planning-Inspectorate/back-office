bd61af14dc89589fba3e6a3e4f23c2cbc548d90d
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-01-29 12:51:08 +0000

    refactor(api/applications): make filterAsync function run in parallel

--- apps/api/src/server/utils/async.js
+++ apps/api/src/server/utils/async.js
@@ -1,5 +1,11 @@
 /**
+ * @template T
+ * @typedef {{ state: true, value: T } | { state: false, value: null }} Maybe
+ * */
+
+/**
  * Filter items using an asynchronous predicate function.
+ * Use of a Maybe<T> type is needed instead of using null, as null could be included in the type of T (e.g. T might be `string | null`), so filtering out nulls could lead to unexpected behaviour.
  *
  * @template T
  * @param {(t: T) => Promise<boolean>} pred
@@ -7,13 +13,13 @@
  * @returns {Promise<T[]>}
  * */
 export const filterAsync = async (pred, items) => {
-	let filtered = [];
+	/** @type {Promise<Maybe<T>>[]} */
+	const promises = items.map(async (item) =>
+		(await pred(item))
+			? /** @type {Maybe<T>} */ ({ state: true, value: item })
+			: /** @type {Maybe<T>} */ ({ state: false, value: null })
+	);
 
-	for (const item of items) {
-		if (await pred(item)) {
-			filtered.push(item);
-		}
-	}
-
-	return filtered;
+	const results = await Promise.all(promises);
+	return results.flatMap((item) => (item.state === true ? [item.value] : []));
 };
