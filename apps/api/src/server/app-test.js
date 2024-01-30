2a3b4f72e49f91578b77591e1dc27d5784e694da
maby0 <mabonapgwyn@hotmail.co.uk>
2024-01-30 15:27:35 +0000

    feat(api): ensure unit tests use test api key

--- apps/api/src/server/app-test.js
+++ apps/api/src/server/app-test.js
@@ -1,10 +1,26 @@
 import { buildApp } from './build-app.js';
 import supertest from 'supertest';
+import { getKeyVaultSecretsClient } from '@pins/key-vault-secrets-client';
 
 // This instance of the app will only ever be used by Jest fixtures, and has Swagger omitted.
 const app = buildApp();
+const testApiKeyObject = await getKeyVaultSecretsClient(false).getSecret('test-api-key');
+const testApiKey = JSON.parse(testApiKeyObject.value || '[]')[0].key;
+
+const makeRequest = (app) => ({
+	get: (url) => supertest(app).get(url).set('x-service-name', 'test').set('x-api-key', testApiKey),
+	post: (url) =>
+		supertest(app).post(url).set('x-service-name', 'test').set('x-api-key', testApiKey),
+	put: (url) => supertest(app).put(url).set('x-service-name', 'test').set('x-api-key', testApiKey),
+	patch: (url) =>
+		supertest(app).patch(url).set('x-service-name', 'test').set('x-api-key', testApiKey),
+	head: (url) =>
+		supertest(app).head(url).set('x-service-name', 'test').set('x-api-key', testApiKey),
+	delete: (url) =>
+		supertest(app).delete(url).set('x-service-name', 'test').set('x-api-key', testApiKey)
+});
 
 // init supertest once for all tests to use
-const request = supertest(app);
+const request = makeRequest(app);
 
 export { app, request };
