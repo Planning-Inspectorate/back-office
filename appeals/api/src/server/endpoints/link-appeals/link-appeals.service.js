aa2d1897d5858172230b3bd2d80ef302932e04d9
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2024-02-01 09:19:19 +0000

    fix(appeals/api): logic to add linked appeals

--- appeals/api/src/server/endpoints/link-appeals/link-appeals.service.js
+++ appeals/api/src/server/endpoints/link-appeals/link-appeals.service.js
@@ -1,11 +1,36 @@
 /** @typedef {import('@pins/appeals.api').Appeals.RepositoryGetByIdResultItem} RepositoryGetByIdResultItem */
 
 /**
- * Checks if an appeal is linked to other appeals.
+ * Checks if an appeal is linked to other appeals as a parent.
  * @param {RepositoryGetByIdResultItem} appeal The appeal to check for linked appeals.
- * @returns
+ * @returns {boolean}
  */
-export const isAppealLinked = (appeal) => {
+const isAppealLead = (appeal) => {
 	const linkedAppeals = appeal.linkedAppeals || [];
-	return linkedAppeals.length > 0;
+	const linkedAppealsAsLead = linkedAppeals.filter((link) => link.parentRef === appeal.reference);
+	return linkedAppealsAsLead.length > 0;
+};
+
+/**
+ * Checks if an appeal is linked to other appeals as a child.
+ * @param {RepositoryGetByIdResultItem} appeal The appeal to check for linked appeals.
+ * @returns {boolean}
+ */
+const isAppealChild = (appeal) => {
+	const linkedAppeals = appeal.linkedAppeals || [];
+	const linkedAppealsAsChild = linkedAppeals.filter((link) => link.childRef === appeal.reference);
+	return linkedAppealsAsChild.length > 0;
+};
+
+/**
+ * Checks if an appeal can be linked, with a specific relationship type (parent/child).
+ * @param {RepositoryGetByIdResultItem} appeal The appeal to check for linked appeals.
+ * @param {'lead'|'child'} relationship The relationship to check for.
+ * @returns {boolean}
+ */
+export const canLinkAppeals = (appeal, relationship) => {
+	const isLead = isAppealLead(appeal);
+	const isChild = isAppealChild(appeal);
+
+	return relationship === 'lead' ? !isChild : !isChild && !isLead;
 };
