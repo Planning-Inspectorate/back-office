1fbad32d1483788f05215c03b70f845400876a66
Ludovico <ludovico.tamigio@kinandcarta.com>
2024-02-06 15:30:56 +0000

    fix(web/appeals): revert retry extend on functions api (boas-1472)

--- apps/functions/applications-background-jobs/common/backend-api-request.js
+++ apps/functions/applications-background-jobs/common/backend-api-request.js
@@ -1,13 +1,8 @@
 import { addAuthHeadersForBackend } from '@pins/add-auth-headers-for-backend';
-import { createHttpRetryParams } from '@pins/platform';
 import got from 'got';
-import config from '@pins/appeals.web/environment/config.js';
-
-const retryParams = createHttpRetryParams(config.retry);
 
 const serviceName = 'function';
 export const requestWithApiKey = got.extend({
-	retry: retryParams,
 	hooks: {
 		beforeRequest: [
 			async (options) =>
