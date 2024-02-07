3dcfac32871ddc0f1bc411b33e73a4a014401e8c
James Paterson <james.paterson@kinandcarta.com>
2024-02-07 15:52:55 +0000

    feat(functions): publish s51 and service user migration events

--- apps/api/src/server/infrastructure/event-client.js
+++ apps/api/src/server/infrastructure/event-client.js
@@ -2,6 +2,7 @@ import { getEventClient } from '@pins/event-client';
 import config from '../config/config.js';
 import logger from '../utils/logger.js';
 
+/** @type {ServiceBusEventClient | LocalEventClient} */
 export const eventClient = getEventClient(
 	config.serviceBusEnabled,
 	logger,
