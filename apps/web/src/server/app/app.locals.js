f0f00a37c2388b007da29a649c415a3a927437ed
Ludovico <ludovico.tamigio@kinandcarta.com>
2024-02-26 14:41:14 +0000

    fix(web/applications): better validation for download url params (boas-1566)

--- apps/web/src/server/app/app.locals.js
+++ apps/web/src/server/app/app.locals.js
@@ -6,14 +6,14 @@ import pino from '../lib/logger.js';
  * @type {import('express').RequestHandler<*, *, *, *, {fileGuid: string, caseId: string, version: string}>}
  */
 export const registerDownloadParams = ({ params }, response, next) => {
-	const { guid: fileGuid, caseId, version } = params;
+	const { guid, caseId, version } = params;
 
-	if (!/^[A-Za-z0-9-]+$/.test(fileGuid) || Number(caseId) < 0 || Number(version) < 0) {
-		pino.error(`[WEB] Bad params for file download ${fileGuid}`);
+	if (!/^[A-Za-z0-9-]+$/.test(guid) || isNaN(Number(caseId)) || isNaN(Number(version))) {
+		pino.error(`[WEB] Bad params for file download ${guid}`);
 		return response.render(`app/500.njk`);
 	}
 
-	response.locals.fileGuid = fileGuid || '';
+	response.locals.fileGuid = String(guid) || '';
 	response.locals.caseId = String(params.caseId) || '';
 	response.locals.version = String(params.version) || '';
 
