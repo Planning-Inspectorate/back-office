3dbe78e37efea1d551d1de91bcf927d7a3a349b7
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2024-02-27 15:30:37 +0000

    fix(appeals/web): async route handlers

--- appeals/web/src/server/app/app.router.js
+++ appeals/web/src/server/app/app.router.js
@@ -1,9 +1,10 @@
 import config from '#environment/config.js';
 import { assertGroupAccess } from './auth/auth.guards.js';
 import { Router as createRouter } from 'express';
-import { installAuthMock } from '#testing/app/mocks/auth.js';
+import asyncRoute from '#lib/async-route.js';
+import authRouter from './auth/auth.router.js';
 import appealsRouter from '../appeals/appeals.router.js';
-import asyncRoute from '../lib/async-route.js';
+import { installAuthMock } from '#testing/app/mocks/auth.js';
 import { handleHeathCheck, viewHomepage, viewUnauthenticatedError } from './app.controller.js';
 import { handleSignout } from './auth/auth.controller.js';
 import { assertIsAuthenticated } from './auth/auth.guards.js';
@@ -16,7 +17,6 @@ import {
 	postUploadDocumentVersion
 } from './components/file-uploader.component.js';
 import { addApiClientToRequest } from '../lib/middleware/add-apiclient-to-request.js';
-import authRouter from './auth/auth.router.js';
 
 const router = createRouter();
 
@@ -56,14 +56,14 @@ const groupIds = [
 router.use(assertGroupAccess(...groupIds));
 
 router.route('/').get(viewHomepage);
-router.route('/auth/signout').get(asyncRoute(handleSignout));
+router.route('/auth/signout').get(handleSignout);
 
 router
 	.route('/documents/:caseId/upload')
 	.post(
 		assertGroupAccess(allowedGroups.caseOfficerGroupId),
 		addApiClientToRequest,
-		postDocumentsUpload
+		asyncRoute(postDocumentsUpload)
 	);
 
 router
@@ -71,7 +71,7 @@ router
 	.post(
 		assertGroupAccess(allowedGroups.caseOfficerGroupId),
 		addApiClientToRequest,
-		postUploadDocumentVersion
+		asyncRoute(postUploadDocumentVersion)
 	);
 
 router
