07f04b21a62ef9f744dbf2eea28afb5c546e391b
Lsoma <Laxman.Soma@planninginspectorate.gov.uk>
2024-03-11 15:54:43 +0000

    test(e2e): added smoke tests to one spec file

--- apps/e2e/cypress/e2e/back-office-applications/smokeTests.spec.js
+++ apps/e2e/cypress/e2e/back-office-applications/smokeTests.spec.js
@@ -7,12 +7,30 @@ import { SearchResultsPage } from '../../page_objects/searchResultsPage';
 import { projectInformation } from '../../support/utils/createProjectInformation';
 import { validateProjectOverview } from '../../support/utils/utils';
 import { FileUploadPage } from '../../page_objects/uploadFiles';
+import { DocumentPropertiesPage } from '../../page_objects/documentPropertiesPage.js';
+import { ExaminationTimetablePage } from '../../page_objects/examinationTimetablePage';
+import { FolderDocumentsPage } from '../../page_objects/folderDocumentsPage';
+import { timetableItem } from '../../support/utils/createTimetableItemInfo';
 
 const createCasePage = new CreateCasePage();
 const applicationsHomePage = new ApplicationsHomePage();
 const searchResultsPage = new SearchResultsPage();
 const fileUploadPage = new FileUploadPage();
+const documentPropertiesPage = new DocumentPropertiesPage();
+const examTimetablePage = new ExaminationTimetablePage();
+const folderPage = new FolderDocumentsPage();
+const folderDocumentsPage = new FolderDocumentsPage();
 const { applications: applicationsUsers } = users;
+
+const texts = {
+	examTimetableLinkText: 'Examination timetable',
+	createTimetableButtonText: 'Create new timetable item',
+	successMessageText: 'Timetable item successfully added'
+};
+const itemOptions = [
+	'Accompanied Site Inspection',
+	'Compulsory Acquisition Hearing',
+];
 
 describe('Smoke tests', () => {
 	let projectInfo;
@@ -28,7 +46,7 @@ describe('Smoke tests', () => {
 		cy.deleteDownloads();
 	});
 
-	it('As a user able to upload file to the case', () => {
+	it('As a user able to upload file to the case, publish and unpublish the document', () => {
 		const fileName = 'sample-doc.pdf';
 		cy.visit('/');
 		const caseRef = Cypress.env('currentCreatedCase');
@@ -46,6 +64,46 @@ describe('Smoke tests', () => {
     // Verify the document is downloaded from properties page
 		fileUploadPage.clickLinkByText('View/Edit properties');
 		fileUploadPage.clickDownloadFile();
+
+	// Verify the document version
+	    fileUploadPage.uploadFile('test.pdf', true);
+		fileUploadPage.clickButtonByText('Upload');
+		documentPropertiesPage.validateVersionCount(2);
+
+	// Verify exam timetable is created
+		documentPropertiesPage.goBackToPrjdocumentationPage();
+		documentPropertiesPage.goBackToOverviewPage();
+		examTimetablePage.clickLinkByText(texts.examTimetableLinkText);
+		examTimetablePage.clickButtonByText(texts.createTimetableButtonText);
+		const itemType = itemOptions[0];
+		const options = timetableItem();
+		examTimetablePage.selectTimetableItem(itemType);
+		examTimetablePage.clickButtonByText('Continue');
+		examTimetablePage.fillItemDetails(options);
+		examTimetablePage.clickButtonByText('Continue');
+		examTimetablePage.checkAnswer('Item type', itemType);
+		examTimetablePage.checkAnswer('Item name', options.itemName);
+		examTimetablePage.checkAnswer('Start time', options.startTimeFormatted);
+		examTimetablePage.checkAnswer('End time', options.endTimeFormatted);
+		examTimetablePage.checkAnswer('Timetable item description', options.description);
+		examTimetablePage.clickButtonByText('Save item');
+		examTimetablePage.validateSuccessPanelTitle(texts.successMessageText);
+		examTimetablePage.clickLinkByText('Go back to examination timetable');
+
+	// Publish and Unpublish document
+        searchResultsPage.clickLinkByText('Project documentation');
+		searchResultsPage.clickLinkByText('Project management');
+		fileUploadPage.clickLinkByText('View/Edit properties');
+		documentPropertiesPage.updateAllProperties('Redacted');
+		folderPage.markAllReadyToPublish();
+		folderPage.clickLinkByText('View publishing queue');
+		folderPage.validatePublishingQueueCase(projectInfo, caseRef);
+		folderPage.publishAllDocumentsInList();
+		folderPage.validateSuccessfulPublish(projectInfo, caseRef, 1);
+        folderPage.navigateToProjectFolder();
+		searchResultsPage.clickLinkByText('View/Edit properties');
+		folderDocumentsPage.unpublishDocument();
+
 	});
 
 });
