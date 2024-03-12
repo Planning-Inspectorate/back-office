b51fcd3b6478a4733fd4f96a7a5f7f0797ba8e28
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-03-12 16:53:15 +0000

    chore(functions/applications-migration): refactor to have a function for a single case

--- apps/functions/applications-migration/nsip-representation-migration/src/nsip-representation-migration.js
+++ apps/functions/applications-migration/nsip-representation-migration/src/nsip-representation-migration.js
@@ -26,46 +26,52 @@ const query = `SELECT ${representationProperties.join(', ')}
 			   WHERE caseRef = ?`;
 
 /**
- * Handle an HTTP trigger/request to run the migration
+ * Migrate nsip-respresentations
  *
  * @param {import('@azure/functions').Logger} logger
  * @param {string[]} caseReferences
  */
 export const migrateRepresentations = async (logger, caseReferences) => {
 	for (const caseReference of caseReferences) {
-		try {
-			logger.info(`reading Representations with caseReference ${caseReference}`);
-
-			const [representationRows, count] = await SynapseDB.query(query, {
-				replacements: [caseReference]
-			});
-
-			const representationEntities = representationRows.map((row) => {
-				removeValues(row, [null, '']);
-
-				return {
-					...row,
-					originalRepresentation: row.originalRepresentation || ''
-					// attachmentIds: row.attachmentIds?.split(',')
-				};
-			});
-
-			logger.info(
-				`found ${count} Representations: ${JSON.stringify(
-					representationEntities.map((u) => u.representationId)
-				)}`
-			);
-
-			if (representationEntities.length > 0) {
-				await makePostRequest(logger, '/migration/nsip-representation', representationEntities);
-			}
-		} catch (e) {
-			logger.error(
-				`Failed to migrate Representations for case ${caseReference}`,
-				e?.response?.body,
-				e
-			);
-			throw e;
-		}
+		await migrationRepresentationsForCase(logger, caseReference);
 	}
 };
+
+/**
+ * Migrate nsip-respresentations for a case
+ *
+ * @param {import('@azure/functions').Logger} log
+ * @param {string} caseReference
+ */
+export async function migrationRepresentationsForCase(log, caseReference) {
+	try {
+		log.info(`reading Representations with caseReference ${caseReference}`);
+
+		const [representationRows, count] = await SynapseDB.query(query, {
+			replacements: [caseReference]
+		});
+
+		const representationEntities = representationRows.map((row) => {
+			removeValues(row, [null, '']);
+
+			return {
+				...row,
+				originalRepresentation: row.originalRepresentation || ''
+				// attachmentIds: row.attachmentIds?.split(',')
+			};
+		});
+
+		log.info(
+			`found ${count} Representations: ${JSON.stringify(
+				representationEntities.map((u) => u.representationId)
+			)}`
+		);
+
+		if (representationEntities.length > 0) {
+			await makePostRequest(log, '/migration/nsip-representation', representationEntities);
+		}
+	} catch (e) {
+		log.error(`Failed to migrate Representations for case ${caseReference}`, e?.response?.body, e);
+		throw e;
+	}
+}
