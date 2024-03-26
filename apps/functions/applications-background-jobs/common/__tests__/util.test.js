d51880e52814df1fdea1aa535f175b07257b7e50
maby0 <mabonapgwyn@hotmail.co.uk>
2024-03-26 15:43:12 +0000

    feat(functions): refactor test suite to add reusability and reduce floating strings

--- apps/functions/applications-background-jobs/common/__tests__/util.test.js
+++ apps/functions/applications-background-jobs/common/__tests__/util.test.js
@@ -1,19 +1,17 @@
-import { ensureTrailingSlash } from "../util.js";
+import { ensureTrailingSlash } from '../util.js';
 
 describe('util', () => {
-    describe('ensureTrailingSlash', () => {
-        const tests = [
-            {input: '', expected: '/'},
-            {input: 'my-string', expected: 'my-string/'},
-            {input: 'with-slash/', expected: 'with-slash/'},
-            {input: undefined, expected: undefined}
-        ];
+	describe('ensureTrailingSlash', () => {
+		const tests = [
+			{ input: '', expected: '/' },
+			{ input: 'my-string', expected: 'my-string/' },
+			{ input: 'with-slash/', expected: 'with-slash/' },
+			{ input: undefined, expected: undefined }
+		];
 
-        for (const {input, expected} of tests) {
-            it(`should handle ${input}`, () => {
-                const got = ensureTrailingSlash(input);
-                expect(got).toEqual(expected);
-            });
-        }
-    });
+		it.each(tests)(`should handle $input`, ({ input, expected }) => {
+			const got = ensureTrailingSlash(input);
+			expect(got).toEqual(expected);
+		});
+	});
 });
