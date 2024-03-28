1c5b65ba8621be4bbc1e451fc3d0d5daf459bff0
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-03-28 10:06:31 +0000

    chore(deps): remove appeals packages from project

--- packages/appeals/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as CaseOfficer from './case-officer';
-import * as Inspector from './inspector';
-import * as Validation from './validation';
-
-export * from './appeal';
-export * from './document';
-export { Inspector, CaseOfficer, Validation };
