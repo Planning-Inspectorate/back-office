2e27bc32e4e8d2fcf8be97084c31202814a83fc1
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-04-11 14:32:58 +0100

    feat(applications/api): add check to not publish general section 51 project

--- apps/api/src/server/applications/application/application.controller.js
+++ apps/api/src/server/applications/application/application.controller.js
@@ -8,6 +8,7 @@ import { setCaseUnpublishedChangesIfTrue } from '#utils/published-case-fields-ch
 import { broadcastNsipProjectEvent } from '#infrastructure/event-broadcasters.js';
 import { mapCreateApplicationRequestToRepository } from './application.mapper.js';
 import { getCaseDetails, getCaseByRef, startApplication } from './application.service.js';
+import { generalSection51CaseReference } from '../../../../../web/src/server/applications/case/general-s51/applications-general-s51.config.js';
 
 /**
  * Express request handler for creating application
@@ -105,6 +106,15 @@ export const queryApplications = async ({ params }, response) => {
  */
 export const publishCase = async ({ params: { id } }, response) => {
 	logger.info(`attempting to publish a case with id ${id}`);
+
+	const caseDetails = await caseRepository.getById(id, {});
+
+	if (caseDetails && caseDetails.reference === generalSection51CaseReference) {
+		throw new BackOfficeAppError(
+			`case reference: ${caseDetails.reference} is not authorised for publishing`,
+			403
+		);
+	}
 
 	const publishedCase = await caseRepository.publishCase({
 		caseId: id
