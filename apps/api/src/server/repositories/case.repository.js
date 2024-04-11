02966a27535f9d57a86683806f9f698b916dde63
christopher-semmens <christopher.semmens@kinandcarta.com>
2024-04-11 14:32:58 +0100

    feat(applications/api): add check to not publish general section 51 project

--- apps/api/src/server/repositories/case.repository.js
+++ apps/api/src/server/repositories/case.repository.js
@@ -4,6 +4,7 @@ import path from 'node:path';
 import url from 'node:url';
 import { databaseConnector } from '../utils/database-connector.js';
 import { separateStatusesToSaveAndInvalidate } from './separate-statuses-to-save-and-invalidate.js';
+import { generalSection51CaseReference } from '../../../../web/src/server/applications/case/general-s51/applications-general-s51.config.js';
 
 const DEFAULT_CASE_CREATE_STATUS = 'draft';
 
@@ -103,7 +104,7 @@ export const getBySearchCriteria = (query, skipValue, pageSize) => {
 		],
 		where: {
 			NOT: {
-				reference: 'GS5110001'
+				reference: generalSection51CaseReference
 			},
 			OR: [
 				{
