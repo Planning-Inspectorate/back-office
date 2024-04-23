d767f088d4bb6d8b20d7f424a53c81877f54e735
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-04-23 14:11:51 +0100

    feat(web/applications): define a FeatureFlagClient at the top level

--- apps/web/src/common/feature-flags.js
+++ apps/web/src/common/feature-flags.js
@@ -1,24 +1,5 @@
+import { FeatureFlagClient } from '@pins/feature-flags';
 import config from '@pins/applications.web/environment/config.js';
 import pino from '@pins/applications.web/src/server/lib/logger.js';
 
-/**
- *
- * @param {string | null} featureFlagName?
- * @returns {boolean}
- */
-
-export const isFeatureActive = (featureFlagName = null) => {
-	pino.info(`[WEB] flag name: ${featureFlagName}`);
-
-	if (
-		!featureFlagName ||
-		!Object.prototype.hasOwnProperty.call(config.featureFlags, featureFlagName)
-	) {
-		pino.info(`[WEB] a flag name must be supplied: ${featureFlagName} does not exist`);
-		return false;
-	}
-
-	pino.info(`[WEB] is flag ${featureFlagName} enabled: ${config.featureFlags[featureFlagName]}`);
-
-	return config.featureFlags[featureFlagName];
-};
+export const featureFlagClient = new FeatureFlagClient(pino, config.featureFlagConnectionString);
