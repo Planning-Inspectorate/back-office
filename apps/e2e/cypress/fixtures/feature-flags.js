289e4c4ff5a42fff5a73095c07f04320d64363fc
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-04-26 11:21:17 +0100

    feat(e2e): create Cypress commands for switching tests based on flags

--- apps/e2e/cypress/fixtures/feature-flags.js
+++ apps/e2e/cypress/fixtures/feature-flags.js
@@ -1,19 +1,10 @@
 import { FeatureFlagClient } from '@pins/feature-flags';
 
-const featureFlagClient = new FeatureFlagClient(
-	{ debug: console.log, error: console.error },
-	Cypress.env('FEATURE_FLAG_CONNECTION_STRING')
-);
+export async function loadFlags() {
+	const featureFlagClient = new FeatureFlagClient(
+		{ debug: console.log, error: console.error },
+		Cypress.env('FEATURE_FLAG_CONNECTION_STRING')
+	);
 
-/** @type{Record<string, boolean>} */
-let featureFlags = {};
-
-featureFlagClient.listFlags().then((flags) => {
-	featureFlags = flags;
-});
-
-/**
- * @param {string} id
- * @returns {boolean}
- * */
-export const getFeatureFlag = (id) => featureFlags[id] ?? false;
+	return await featureFlagClient.listFlags();
+}
