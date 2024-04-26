0c6660037dd695601c32854d399f9a8d934f1fe8
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-04-26 10:02:33 +0100

    feat(e2e): fetch all feature flags before running tests

--- apps/e2e/cypress/fixtures/feature-flag-client.js
+++
@@ -1,6 +0,0 @@
-import { FeatureFlagClient } from '@pins/feature-flags';
-
-const featureFlagClient = new FeatureFlagClient(
-	{ debug: console.log, error: console.error },
-	Cypress.env('FEATURE_FLAG_CONNECTION_STRING')
-);
