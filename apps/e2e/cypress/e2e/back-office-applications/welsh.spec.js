3536a4a1e0485675e7b1d5b410b14f3957e1b34a
lsoma <Laxman.Soma@planninginspectorate.gov.uk>
2024-05-17 08:59:47 +0100

    test(e2e): added welsh test

---
+++ apps/e2e/cypress/e2e/back-office-applications/welsh.spec.js
@@ -0,0 +1,53 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { users } from '../../fixtures/users';
+import { ApplicationsHomePage } from '../../page_objects/applicationsHomePage';
+import { CreateCasePage } from '../../page_objects/createCasePage';
+import { SearchResultsPage } from '../../page_objects/searchResultsPage';
+import {
+	updateProjectInformation,
+	validateProjectInformation,
+	validateProjectOverview,
+	validatePreviewAndPublishInfo,
+	validateWelshProjectInformation
+} from '../../support/utils/utils';
+import { projectInformation } from '../../support/utils/createProjectInformation';
+import { CasePage } from '../../page_objects/casePage';
+
+const applicationsHomePage = new ApplicationsHomePage();
+const casePage = new CasePage();
+const createCasePage = new CreateCasePage();
+const searchResultsPage = new SearchResultsPage();
+const { applications: applicationsUsers } = users;
+
+describe('Enable and update Project Information with Welsh fields', () => {
+	context('As a user', () => {
+		let projectInfo = projectInformation();
+		let projectInfoNew = projectInformation();
+
+		before(() => {
+			cy.login(applicationsUsers.caseAdmin);
+			createCasePage.createCaseWithWelshAsRegion(projectInfo, true);
+		});
+
+		it('As a user able to update the case information with welsh fields', () => {
+			cy.login(applicationsUsers.caseAdmin);
+			cy.visit('/');
+			const caseRef = Cypress.env('currentCreatedCase');
+			applicationsHomePage.searchFor(caseRef);
+			searchResultsPage.clickTopSearchResult();
+			validateProjectOverview(projectInfo, true);
+			casePage.clickLinkByText('Update project information');
+			casePage.showAllSections();
+			validateWelshProjectInformation(projectInfo, true);
+			updateProjectInformation(projectInfoNew);
+			validateProjectInformation(projectInfoNew, false, true);
+			casePage.clickLinkByText('Overview');
+			casePage.clickButtonByText('Preview and publish project');
+			validatePreviewAndPublishInfo(projectInfoNew);
+			casePage.clickButtonByText('Accept and publish project');
+			casePage.validatePublishBannerMessage('Project page successfully published');
+		});
+	});
+});
--- apps/e2e/cypress/page_objects/createCaseSections/checkYourAnswers.js
+++ apps/e2e/cypress/page_objects/createCaseSections/checkYourAnswers.js
@@ -44,6 +44,34 @@ export class CheckYourAnswersSection extends SectionBase {
 		}
 	}
 
+	checkAllAnswersForWelsh(projectInfo, mandatoryOnly = false) {
+		// P R O J E C T  I N F O R M A T I O N
+		this.checkAnswer('Project name', projectInfo.projectName);
+		this.checkAnswer('Project description', projectInfo.projectDescription);
+		this.checkAnswer('Sector', projectInfo.sector);
+		this.checkAnswer('Subsector', projectInfo.subsector);
+		this.checkAnswer('Project location', projectInfo.projectLocation);
+		this.checkAnswer('Grid reference Easting', projectInfo.gridRefEasting);
+		this.checkAnswer('Grid reference Northing', projectInfo.gridRefNorthing);
+		if (Cypress.env('featureFlags')['applic-55-welsh-translation']) {
+			this.checkAnswer('Region(s)', 'Wales');
+		}
+		this.checkAnswer('Map zoom level', mandatoryOnly ? 'None' : projectInfo.zoomLevel);
+
+		if (!mandatoryOnly) {
+			// A P P L I C A N T  I N F O R M A T I O N
+			this.checkAnswer('Organisation name', projectInfo.orgName);
+			this.checkAnswer('Contact name', projectInfo.applicantFullName);
+			this.checkAnswer('Address', projectInfo.applicantFullAddress1);
+			this.checkAnswer('Website', projectInfo.applicantWebsite);
+			this.checkAnswer('Email address', projectInfo.applicantEmail);
+			this.checkAnswer('Telephone number', projectInfo.applicantPhoneNumber);
+			// K E Y  D A T E S
+			this.checkAnswer('Anticipated submission date internal', projectInfo.internalDateFull);
+			this.checkAnswer('Anticipated submission date published', projectInfo.publishedDate);
+		}
+	}
+
 	validatePage() {
 		this.validateSectionHeader('Check your answers before creating a new project');
 	}
