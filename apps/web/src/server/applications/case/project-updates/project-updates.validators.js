9fcbca83de307386b512a407910fd5bde283fdac
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2024-06-28 16:12:42 +0100

    feat(applications/web): create welsh project update (APPLICS-30)
    feat(applications/web): create welsh project update (APPLICS-30)

--- apps/web/src/server/applications/case/project-updates/project-updates.validators.js
+++ apps/web/src/server/applications/case/project-updates/project-updates.validators.js
@@ -2,12 +2,37 @@ import { createValidator } from '@pins/express';
 import { body } from 'express-validator';
 import sanitizeHtml from 'sanitize-html';
 
+/** @typedef {import('express').RequestHandler} RequestHandler */
+
 export const validateProjectUpdatesContent = createValidator(
 	body('backOfficeProjectUpdateContent')
 		.trim()
+		.custom((value) => htmlToText(decodeURI(value)).length !== 0)
+		.withMessage('Enter details about the update')
 		.custom((value) => htmlToText(decodeURI(value)).length > 12)
-		.withMessage('The project update needs to be at least 12 characters long')
+		.withMessage('Details about the update must be 12 characters or more')
 );
+
+/**
+ * Only validates backOfficeProjectUpdateContentWelsh if the case is Welsh
+ *
+ * @type{RequestHandler}
+ */
+export const validateProjectUpdatesContentWelsh = (req, res, next) => {
+	const { caseIsWelsh = false } = res.locals || {};
+	if (caseIsWelsh) {
+		// @ts-ignore
+		return createValidator(
+			body('backOfficeProjectUpdateContentWelsh')
+				.trim()
+				.custom((value) => htmlToText(decodeURI(value)).length !== 0)
+				.withMessage('Enter details about the update in Welsh')
+				.custom((value) => htmlToText(decodeURI(value)).length > 12)
+				.withMessage('Details about the update in Welsh must be 12 characters or more')
+		)(req, res, next);
+	}
+	next();
+};
 
 /**
  * Remove HTML tags from a string
