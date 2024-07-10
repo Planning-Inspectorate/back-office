72c3e36f285c751fcf6b31a62df7adccc7d6d866
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2024-07-10 09:04:22 +0100

    feat(applications/web): project update links contain corrupt ampersands (APPLICS-604)

--- apps/web/src/server/applications/case/project-updates/__tests__/project-updates.mapper.test.js
+++ apps/web/src/server/applications/case/project-updates/__tests__/project-updates.mapper.test.js
@@ -39,4 +39,38 @@ describe('project-updates.mapper', () => {
 			emailSubscribers: false
 		});
 	});
+
+	it('decodes encoded text correctly', () => {
+		expect(
+			bodyToUpdateRequest({
+				backOfficeProjectUpdateContent:
+					'%3Cp%3EHere%20is%20a%20%3Ca%20href=%22https://test.com/cheese%20and%20ham%201.pdf%22%3Elink%3C/a%3E%20to%20decode%3C/p%3E',
+				backOfficeProjectUpdateContentWelsh:
+					'%3Cp%3EHere%20is%20a%20Welsh%20%3Ca%20href=%22http://test.com/cheese%20and%20ham%20in%20Welsh%201.pdf%22%3Elink%3C/a%3E%20to%20decode%3C/p%3E',
+				emailSubscribers: 'false'
+			})
+		).toEqual({
+			htmlContent:
+				'<p>Here is a <a href="https://test.com/cheese and ham 1.pdf">link</a> to decode</p>',
+			htmlContentWelsh:
+				'<p>Here is a Welsh <a href="http://test.com/cheese and ham in Welsh 1.pdf">link</a> to decode</p>',
+			emailSubscribers: false
+		});
+	});
+
+	it('cleans up ampersands corrupted by the rich text editor', () => {
+		expect(
+			bodyToUpdateRequest({
+				backOfficeProjectUpdateContent:
+					'<a href="https://test.com/cheese &amp;amp; ham 1.pdf">link</a>',
+				backOfficeProjectUpdateContentWelsh:
+					'<a href="http://test.com/cheese &amp;amp; ham in Welsh 1.pdf">link</a>',
+				emailSubscribers: 'false'
+			})
+		).toEqual({
+			htmlContent: '<a href="https://test.com/cheese &amp; ham 1.pdf">link</a>',
+			htmlContentWelsh: '<a href="http://test.com/cheese &amp; ham in Welsh 1.pdf">link</a>',
+			emailSubscribers: false
+		});
+	});
 });
