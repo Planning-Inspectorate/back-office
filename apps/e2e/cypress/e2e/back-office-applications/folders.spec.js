2e35022c1156d65a2b589f593d0264aa9d7d617e
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-07-19 15:30:41 +0100

    feat(e2e): add a test case for creating a top-level folder

---
+++ apps/e2e/cypress/e2e/back-office-applications/folders.spec.js
@@ -0,0 +1,49 @@
+// @ts-nocheck
+/// <reference types="cypress"/>
+
+import { users } from '../../fixtures/users';
+import { ApplicationsHomePage } from '../../page_objects/applicationsHomePage';
+import { SearchResultsPage } from '../../page_objects/searchResultsPage';
+import { FileUploadPage } from '../../page_objects/uploadFiles';
+import { Page } from '../../page_objects/basePage';
+import { projectInformation } from '../../support/utils/createProjectInformation';
+import { CreateCasePage } from '../../page_objects/createCasePage';
+
+const { applications: applicationsUsers } = users;
+const applicationsHomePage = new ApplicationsHomePage();
+const createCasePage = new CreateCasePage();
+const searchResultsPage = new SearchResultsPage();
+const fileUploadPage = new FileUploadPage();
+const page = new Page();
+
+describe('Folders', () => {
+	let projectInfo;
+
+	before(() => {
+		projectInfo = projectInformation();
+		cy.login(applicationsUsers.caseAdmin);
+		createCasePage.createCase(projectInfo);
+	});
+
+	beforeEach(() => {
+		cy.visit('/');
+		const caseRef = Cypress.env('currentCreatedCase');
+		applicationsHomePage.searchFor(caseRef);
+		searchResultsPage.clickTopSearchResult();
+		searchResultsPage.clickLinkByText('Project documentation');
+		searchResultsPage.clickLinkByText('Project management');
+
+		fileUploadPage.showSubfolders();
+	});
+
+	it('Should be able to create a subfolder of one of the hardcoded folders', () => {
+		fileUploadPage.createFolder();
+
+		const folderName = 'Test folder 1';
+
+		page.fillInput(folderName);
+		page.clickButtonByText('Save and return');
+
+		fileUploadPage.verifyTableContains(folderName);
+	});
+});
