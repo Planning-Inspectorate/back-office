abea903c30e7d227ee35ba99eafba34c9f1b93c9
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-08-15 11:21:58 +0100

    chore(packages/feature-flags): add a material changes feature flag

--- packages/feature-flags/src/static-feature-flags.js
+++ packages/feature-flags/src/static-feature-flags.js
@@ -1,5 +1,6 @@
 export default {
 	'boas-1-test-feature': true,
 	'applic-55-welsh-translation': true,
-	'applic-625-custom-folders': true
+	'applic-625-custom-folders': true,
+	'applics-156-material-changes': true
 };
