14a9650aa48a8368945a02c1b985efffeb6886a6
BenSurgisonPINS <ben.surgison@planninginspectorate.gov.uk>
2024-09-11 14:54:23 +0100

    fix(e2e): fix e2e test failure in documents updates

--- apps/e2e/cypress/page_objects/basePage.js
+++ apps/e2e/cypress/page_objects/basePage.js
@@ -140,6 +140,14 @@ export class Page {
 		});
 	}
 
+	checkPartialAnswer(question, answer) {
+		this.basePageElements.answerCell(question).then(($elem) => {
+			cy.wrap($elem)
+				.invoke('text')
+				.then((text) => expect(text.trim()).to.contains(answer));
+		});
+	}
+
 	clickChangeLink(question) {
 		this.basePageElements.changeLink(question).click();
 	}
