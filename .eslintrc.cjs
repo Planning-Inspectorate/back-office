5cf1ef44381f654b9604b91098c37a87611a4237
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-09-18 09:28:19 +0100

    chore(tooling): remove references to appeals from ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,9 +30,7 @@ module.exports = {
 		{
 			files: [
 				'apps/web/src/client/**',
-				'apps/web/testing/app/mocks/client-side.js',
-				'appeals/web/src/client/**',
-				'appeals/web/testing/app/mocks/client-side.js'
+				'apps/web/testing/app/mocks/client-side.js'
 			],
 			env: {
 				browser: true
