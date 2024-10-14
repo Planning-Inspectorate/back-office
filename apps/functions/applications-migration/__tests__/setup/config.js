c7b6e884b186088f4109daa03b7f021d76e6459d
maby0 <mabonapgwyn@hotmail.co.uk>
2024-10-14 14:38:28 +0100

    wip

--- apps/functions/applications-migration/__tests__/setup/config.js
+++ apps/functions/applications-migration/__tests__/setup/config.js
@@ -1,2 +1,5 @@
+export const TEST_DB_PORT = 1434;
 export const TEST_API_PORT = 3001;
 export const TEST_CONTAINER_NAME = 'test-pins-sql-server';
+export const TEST_DB_PASSWORD = 'test-db-password';
+export const TEST_DATABASE_URL = `sqlserver://127.0.0.1:${TEST_DB_PORT};database=pins_apps_development;user=sa;password=${TEST_DB_PASSWORD};trustServerCertificate=true`;
