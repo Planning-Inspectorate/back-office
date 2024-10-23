48b2d7299b37c00cc03e1c2f96e60a6a91a375a8
maby0 <mabonapgwyn@hotmail.co.uk>
2024-10-23 12:47:17 +0100

    feat(functions): project data setup complete

--- apps/functions/applications-migration/__tests__/setup/config.js
+++
@@ -1,5 +0,0 @@
-export const TEST_DB_PORT = 1433;
-export const TEST_API_PORT = 3001;
-export const TEST_CONTAINER_NAME = 'test-pins-sql-server';
-export const TEST_DB_PASSWORD = 'test-db-PASSWORD-123';
-export const TEST_DATABASE_URL = `sqlserver://127.0.0.1:${TEST_DB_PORT};database=pins_apps_development;user=sa;password=${TEST_DB_PASSWORD};trustServerCertificate=true`;
