3fd14701ad69e2aa158b6276642009a8c8c4752b
David J White <50454208+DavidJWhiteDev@users.noreply.github.com>
2024-12-09 12:18:23 +0000

    fix(api): doc case stages publish fail (applics-1169)

--- apps/api/src/server/utils/mapping/map-case-status.js
+++ apps/api/src/server/utils/mapping/map-case-status.js
@@ -1,4 +1,8 @@
-import { mapCaseStatusString } from './map-case-status-string.js';
+import { folderDocumentCaseStageMappings } from '#api-constants';
+import {
+	mapCaseStatusString,
+	projectCaseStageToDocumentCaseStageSchemaMap
+} from './map-case-status-string.js';
 
 /**
  *
@@ -23,4 +27,42 @@ export const mapCaseStatus = (caseStatuses) => {
 	}
 
 	return mapCaseStatusString(validStatus.status);
+};
+
+/**
+ * convert CBOS DB Document Version stage value into the event schema value.
+ * Note that these are different values to the Case stage
+ *
+ * @param {string |null} stage
+ * @returns
+ */
+export const mapDocumentCaseStageToSchema = (stage) => {
+	if (!stage) {
+		return null;
+	}
+
+	const { DEVELOPERS_APPLICATION, POST_DECISION } = folderDocumentCaseStageMappings;
+
+	// mostly the schema value is just the lower equivalent, with some exceptions:
+	switch (stage) {
+		case DEVELOPERS_APPLICATION:
+			return 'developers_application';
+		case POST_DECISION:
+			// note that post decision has inconsistent format in the schema, it uses underscore instead of hyphen
+			// our DB 'Post-decision' maps to 'post_decision'
+			return 'post_decision';
+		default:
+			return stage.toLowerCase();
+	}
+};
+
+/**
+ * Maps the project case stage (CBOS and Schema - both the same) to the document case stage schema value
+ *
+ * @param {string} caseStage
+ * @returns {string |null}
+ */
+export const mapProjectCaseStageToDocumentCaseStageSchema = (caseStage) => {
+	// @ts-ignore
+	return projectCaseStageToDocumentCaseStageSchemaMap[caseStage];
 };
