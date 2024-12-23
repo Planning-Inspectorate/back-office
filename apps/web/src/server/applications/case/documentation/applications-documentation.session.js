b4d1d305dc49e4cda163155e83d91da40042af1f
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2024-12-23 16:05:55 +0000

    feat(web): move documents folder explorer functionality

--- apps/web/src/server/applications/case/documentation/applications-documentation.session.js
+++ apps/web/src/server/applications/case/documentation/applications-documentation.session.js
@@ -1,5 +1,15 @@
 /**
  * @typedef {import('express-session').Session & { folderPage?: string }} SessionWithFolderPage
+ * @typedef {import('express-session').Session & {
+ *	moveDocuments?: {
+ *		documentationFilesToMove?: object[],
+ *		parentFolderId?: number|undefined|null,
+ * 		folderList?: object[],
+ * 		isFolderRoot?: boolean,
+ * 		rootFolderList?: object[]
+ * 		breadcrumbs?: {href: string, text: string}[]
+ *	}
+ * }} SessionWithMoveDocuments
  */
 
 /**
@@ -8,13 +18,13 @@
  * @param {SessionWithFolderPage} session
  * @returns {string | null}
  */
-export const getSessionFolderPage = (session) => session.folderPage || null;
+const getSessionFolderPage = (session) => session.folderPage || null;
 
 /**
  * @param {SessionWithFolderPage} session
  * @returns {void}
  */
-export const destroySessionFolderPage = (session) => {
+const destroySessionFolderPage = (session) => {
 	delete session.folderPage;
 };
 
@@ -25,6 +35,136 @@ export const destroySessionFolderPage = (session) => {
  * @param {string} folderPage
  * @returns {void}
  */
-export const setSessionFolderPage = (session, folderPage) => {
+const setSessionFolderPage = (session, folderPage) => {
 	session.folderPage = folderPage;
+};
+
+/**
+ * Start move documents session
+ *
+ * @param {SessionWithMoveDocuments} session
+ */
+const startMoveDocumentsSession = (session) => {
+	session.moveDocuments = session.moveDocuments ?? {};
+};
+
+/**
+ * Save in the session the document metadata for files selected to be moved between folders
+ *
+ * @param {SessionWithMoveDocuments} session
+ * @param {object[]} documentationFiles
+ * @returns {void}
+ */
+const setSessionMoveDocumentsFilesToMove = (session, documentationFiles) => {
+	session.moveDocuments && (session.moveDocuments.documentationFilesToMove = documentationFiles);
+};
+
+/**
+ * Get the documents data for the files selected to be moved between folders
+ *
+ * @param {SessionWithMoveDocuments} session
+ * @returns {object[]}
+ */
+
+const getSessionMoveDocumentsFilesToMove = (session) => {
+	return session.moveDocuments?.documentationFilesToMove
+		? session.moveDocuments?.documentationFilesToMove
+		: [];
+};
+
+/**
+ * Save in the session the list of folders to move the documents to
+ * @param {SessionWithMoveDocuments} session
+ * @param {import('../../applications.types.js').DocumentationCategory[]} folderList
+ **/
+const setSessionMoveDocumentsFolderList = (session, folderList) => {
+	session.moveDocuments && (session.moveDocuments.folderList = folderList);
+};
+
+/**
+ * @param {SessionWithMoveDocuments} session
+ * @returns {*}
+ */
+const getSessionMoveDocumentsFolderList = (session) => {
+	return session.moveDocuments?.folderList;
+};
+
+/**
+ * @param {SessionWithMoveDocuments} session
+ * @param {object[]} rootFolderList
+ * @returns
+ */
+const setSessionMoveDocumentsRootFolderList = (session, rootFolderList) => {
+	session.moveDocuments && (session.moveDocuments.rootFolderList = rootFolderList);
+};
+
+/**
+ * @param {SessionWithMoveDocuments} session
+ * @returns
+ */
+const getSessionMoveDocumentsRootFolderList = (session) => {
+	return session.moveDocuments?.rootFolderList;
+};
+
+/**
+ * Save in the session whether the selected folder is the root folder
+ * @param {SessionWithMoveDocuments} session
+ * @param {boolean} isFolderRoot
+ */
+
+const setSessionMoveDocumentsIsFolderRoot = (session, isFolderRoot) => {
+	session.moveDocuments && (session.moveDocuments.isFolderRoot = isFolderRoot);
+};
+
+/**
+ * @param {SessionWithMoveDocuments} session
+ * @returns {boolean|undefined}
+ */
+const getSessionMoveDocumentsIsFolderRoot = (session) => {
+	return session.moveDocuments?.isFolderRoot;
+};
+
+/**
+ * Save in the session the breadcrumbs for the move documents page
+ * @param {SessionWithMoveDocuments} session
+ * @param {{href: string, text: string}[]} breadcrumbItems
+ */
+
+const setSessionMoveDocumentsBreadcrumbs = (session, breadcrumbItems) => {
+	session.moveDocuments && (session.moveDocuments.breadcrumbs = breadcrumbItems);
+};
+
+/**
+ * @param {SessionWithMoveDocuments} session
+ * @returns {{href: string, text: string}[]|undefined}
+ */
+const getSessionMoveDocumentsBreadcrumbs = (session) => {
+	return session.moveDocuments?.breadcrumbs;
+};
+/**
+ * Delete the documents to move data from the session.
+ *
+ * @param {*} session
+ * @returns {void}
+ */
+const deleteMoveDocumentsSession = (session) => {
+	delete session.moveDocuments;
+};
+
+export default {
+	getSessionFolderPage,
+	setSessionFolderPage,
+	destroySessionFolderPage,
+	startMoveDocumentsSession,
+	setSessionMoveDocumentsFilesToMove,
+	getSessionMoveDocumentsFilesToMove,
+	setSessionMoveDocumentsFolderList,
+	getSessionMoveDocumentsFolderList,
+	setSessionMoveDocumentsRootFolderList,
+	getSessionMoveDocumentsRootFolderList,
+	setSessionMoveDocumentsIsFolderRoot,
+	getSessionMoveDocumentsIsFolderRoot,
+	setSessionMoveDocumentsBreadcrumbs,
+	getSessionMoveDocumentsBreadcrumbs,
+	deleteMoveDocumentsSession
 };
---
+++ apps/web/src/server/applications/case/documentation/utils/move-documents/utils.js
@@ -0,0 +1,146 @@
+import { url } from '../../../../../lib/nunjucks-filters/url.js';
+import logger from '../../../../../lib/logger.js';
+import { getCaseFolders } from '../../applications-documentation.service.js';
+import documentationSessionHandlers from '../../applications-documentation.session.js';
+
+const excludedFolders = ['relevant representations', 's51 advice'];
+
+/**
+ * @param {number} caseId
+ * @param {number} parentFolderId
+ * @returns {Promise<import('../../applications-documentation.service.js').DocumentationCategory[]>}
+ */
+const getFolderList = async (caseId, parentFolderId) => {
+	try {
+		let folderList = parentFolderId
+			? await getCaseFolders(caseId, parentFolderId)
+			: await getCaseFolders(caseId);
+
+		return formatFolderList(folderList);
+	} catch (error) {
+		logger.info(`Failed to fetch folder list: ${error}`);
+		return [];
+	}
+};
+
+/**
+ * @param {import('../../applications-documentation.service.js').DocumentationCategory[]} folderList
+ * @returns {{text: string, value: number|undefined}[]}
+
+ */
+const getFolderViewData = (folderList) =>
+	folderList.map((folder) => {
+		return { text: folder.displayNameEn, value: folder.id };
+	});
+
+/**
+ *
+ * @param {import('../../applications-documentation.service.js').DocumentationCategory[]} folderList
+ */
+const isFolderRoot = (folderList) =>
+	folderList.every((folder) => folder.parentFolderId === null) && folderList.length > 0;
+
+/**
+ *
+ * @param {import('../../applications-documentation.service.js').DocumentationCategory[]} folderList
+ * @returns {import('../../applications-documentation.service.js').DocumentationCategory[]}
+ */
+const formatFolderList = (folderList) => {
+	return folderList
+		.sort((a, b) => (a.displayOrder ?? 0) - (b.displayOrder ?? 0))
+		.filter((folder) => !excludedFolders.includes(folder.displayNameEn.toLowerCase()));
+};
+
+/**
+ * @param {import('../../applications-documentation.service.js').DocumentationCategory[]} folderList
+ * @param {number} openFolderId
+ * @returns {string|undefined}
+ */
+const getFolderNameById = (folderList, openFolderId) =>
+	folderList.find((folder) => folder.id === openFolderId)?.displayNameEn;
+
+/**
+ * @param {*} session
+ * @param {number} parentFolderId
+ * @param {string|undefined} parentFolderName
+ * @returns {{href: string, text: string}[]}
+
+ */
+
+const buildMoveDocumentsBreadcrumbItems = (session, parentFolderId, parentFolderName) => {
+	const currentBreadcrumbs =
+		documentationSessionHandlers.getSessionMoveDocumentsBreadcrumbs(session);
+	if (!currentBreadcrumbs) return [];
+	if (!parentFolderId) return [];
+
+	const breadcrumbItems = [...currentBreadcrumbs];
+
+	//add root folder to crumbs, if it's not there already
+	const rootFolderIndex = breadcrumbItems.findIndex(
+		(item) => item.text === 'Project documentation'
+	);
+	if (rootFolderIndex === -1) {
+		breadcrumbItems.unshift({
+			href: `./folder-explorer`,
+			text: 'Project documentation'
+		});
+	}
+
+	if (parentFolderId && parentFolderName) {
+		//if the current parentId is in breadcrumbs, user went back to previous folder so we trim to that point
+		const parentFolderIndex = breadcrumbItems.findIndex((item) => item.text === parentFolderName);
+		if (parentFolderIndex > -1) {
+			breadcrumbItems.splice(parentFolderIndex + 1);
+		} else {
+			//otherwise we add the current parent to the crumbs
+			breadcrumbItems.push({
+				href: encodeURI(
+					`./folder-explorer?parentFolderId=${parentFolderId}&parentFolderName=${parentFolderName}`
+				),
+				text: parentFolderName
+			});
+		}
+	}
+
+	return breadcrumbItems;
+};
+
+/**
+ * @param {{href: string, text: string}[] | null} breadcrumbItems
+ * @param {number} caseId
+ * @param {number} folderId
+ * @param {string} folderName
+ * @returns {string}
+ */
+const getBackLinkUrlFromBreadcrumbs = (breadcrumbItems, caseId, folderId, folderName) => {
+	const baseURL = url('document-category', {
+		caseId: caseId,
+		documentationCategory: {
+			id: folderId,
+			displayNameEn: folderName
+		}
+	});
+
+	let backLink = '';
+
+	if (!breadcrumbItems || breadcrumbItems.length === 0) {
+		return (backLink = `${baseURL}move-documents`);
+	}
+
+	if (breadcrumbItems.length > 1) {
+		const folderPage = breadcrumbItems[breadcrumbItems.length - 2].href.slice(1);
+		backLink = `${baseURL}move-documents${folderPage}`;
+	}
+
+	return backLink;
+};
+
+export default {
+	getFolderList,
+	isFolderRoot,
+	buildMoveDocumentsBreadcrumbItems,
+	getBackLinkUrlFromBreadcrumbs,
+	getFolderNameById,
+	getFolderViewData,
+	formatFolderList
+};
