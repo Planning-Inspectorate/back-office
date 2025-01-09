dd5101f0f59fec50a2af32158b4e0b1e3c913612
emil-placheta <emil.placheta@planninginspectorate.gov.uk>
2025-01-09 15:59:23 +0000

    feat(web): remove logs

--- apps/web/src/server/applications/case/documentation/applications-documentation.controller.js
+++ apps/web/src/server/applications/case/documentation/applications-documentation.controller.js
@@ -865,18 +865,13 @@ export async function postDocumentationFolderExplorer(request, response) {
 	const breadcrumbItems = documentationSessionHandlers.getSessionMoveDocumentsBreadcrumbs(session);
 
 	if (body.action === 'moveDocuments') {
-		console.log('yoyo1');
 		const destinationFolder =
 			documentationSessionHandlers.getSessionMoveDocumentsParentFolder(session);
 		const payload = utils.getMoveDocumentsPayload(session);
-		console.log('yoyo2');
 		const { errors: updateErrors } = await updateDocumentsFolderId(caseId, payload);
-		console.log('yoyo3');
 		if (updateErrors) {
-			console.log('yoyo4 errors:', updateErrors);
 			validationErrors = updateErrors;
 		} else {
-			console.log('yoyo4');
 			setSessionBanner(
 				session,
 				`Selected documents moved to the ${destinationFolder?.displayNameEn} folder`
