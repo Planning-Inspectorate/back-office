f3299c00b58b59c11bcdb66ab4de2bb0343e3104
Kartikay Jainwal <kartikayjainwal@gmail.com>
2025-02-11 15:14:02 +0000

    fix(functions): move loose s51 attachments to archive in migration

--- apps/api/src/server/migration/migration.routes.js
+++ apps/api/src/server/migration/migration.routes.js
@@ -3,6 +3,7 @@ import { asyncHandler } from '@pins/express';
 import { postMigrateFolders, postMigrateModel } from './migration.controller.js';
 import { validateMigration } from './validate-migration.controller.js';
 import { getArchiveFolderInformation } from './archive-folder-info.controller.js';
+import { migrationCleanup } from './migrators/migration-cleanup.controller.js';
 
 const router = createRouter();
 
@@ -36,6 +37,36 @@ router.post(
         }
 	 */
 	asyncHandler(postMigrateFolders)
+);
+
+router.post(
+	'/cleanup',
+	/*
+				#swagger.tags = ['Migration']
+				#swagger.path =  'migration/cleanup'
+				#swagger.description = 'Cleanup migration data'
+				#swagger.parameters['query'] = {
+						caseReferences: 'TR020002,TR020003'
+				}
+				#swagger.parameters['x-service-name'] = {
+			in: 'header',
+			type: 'string',
+			description: 'Service name header',
+			default: 'swagger'
+		}
+		#swagger.parameters['x-api-key'] = {
+			in: 'header',
+			type: 'string',
+			description: 'API key header',
+			default: '123'
+		}
+				#swagger.responses[200] = {
+						description: '',
+            schema: { }
+				}
+			*/
+
+	asyncHandler(migrationCleanup)
 );
 
 router.post(
@@ -110,7 +141,7 @@ router.get(
 	/*
 				#swagger.tags = ['Migration']
 				#swagger.path =  'migration/archive-folder-info'
-				#swagger.description = 'Validate migration'
+				#swagger.description = 'Get archive folder information'
 				#swagger.parameters['query'] = {
 						caseReferences: 'TR020002,TR020003'
 				}
