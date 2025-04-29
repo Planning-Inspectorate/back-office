1b51e7498ca5b2f28cf3ceb91c884e4b609c97d9
David J White <50454208+DavidJWhiteDev@users.noreply.github.com>
2025-04-29 12:10:29 +0100

    fix(functions): migrated cases exam timetable folder cleanup (applics-1482)

--- apps/functions/applications-migration/common/migration-cleanup.js
+++ apps/functions/applications-migration/common/migration-cleanup.js
@@ -1,9 +1,26 @@
 import { makePostRequestStreamResponse } from './back-office-api-client.js';
 
-export const startMigrationCleanup = async (log, caseReference, skipHtmlTransform) => {
+/**
+ *
+ * @param {*} log
+ * @param {string} caseReference
+ * @param {boolean} skipLooseS51Attachments
+ * @param {boolean} skipHtmlTransform
+ * @param {boolean} skipFixExamFolders
+ * @returns
+ */
+export const startMigrationCleanup = async (
+	log,
+	caseReference,
+	skipLooseS51Attachments,
+	skipHtmlTransform,
+	skipFixExamFolders
+) => {
 	log.info(`Making API request to start migration for case: ${caseReference}`);
 	return makePostRequestStreamResponse(log, '/migration/cleanup', {
 		caseReference,
-		skipHtmlTransform
+		skipLooseS51Attachments,
+		skipHtmlTransform,
+		skipFixExamFolders
 	});
 };
