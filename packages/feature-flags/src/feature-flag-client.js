894ad864a09f03f717bf651b07eed4afbdd2ae82
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-06-16 09:15:58 +0100

    feat(packages/feature-flags): support feature-flags enabled per-case

--- packages/feature-flags/src/feature-flag-client.js
+++ packages/feature-flags/src/feature-flag-client.js
@@ -1,5 +1,5 @@
 import { AppConfigurationClient, isFeatureFlag, parseFeatureFlag } from '@azure/app-configuration';
-import staticFlags from './static-feature-flags.js';
+import staticFlags, { flagsByReference } from './static-feature-flags.js';
 
 /**
  * @typedef {Function} LoggerFn
@@ -36,12 +36,42 @@ export class FeatureFlagClient {
 
 		/** @type {Record<string, boolean>} */
 		this.featureFlags = {};
+		/**
+		 * An array of case reference for each feature flag entry.
+		 * If a case reference is not present, the feature flag is not enabled for that case.
+		 * @type {Record<string, string[]>}
+		 */
+		this.featureFlagsByCaseReference = {};
 
 		this._logger = logger;
 		this._useStaticFlags = useStaticFlags;
 	}
 
-	/** @type {(flagName: string) => boolean} */
+	/**
+	 * Checks if a feature flag is active for a specific case reference.
+	 * If the flag is globally active, it returns true.
+	 *
+	 * @param {string} flagName
+	 * @param {string} caseReference
+	 * @returns {boolean}
+	 */
+	isFeatureActiveForCase(flagName, caseReference) {
+		// If the flag is globally active, return true
+		if (this.isFeatureActive(flagName)) {
+			return true;
+		}
+		if (!(flagName in this.featureFlagsByCaseReference)) {
+			return false;
+		}
+
+		return this.featureFlagsByCaseReference[flagName]?.includes(caseReference);
+	}
+
+	/**
+	 * Checks if a feature flag is active globally.
+	 * @param {string} flagName
+	 * @returns {boolean}
+	 */
 	isFeatureActive(flagName) {
 		return this.featureFlags[flagName] ?? false;
 	}
@@ -51,6 +81,7 @@ export class FeatureFlagClient {
 		if (this._useStaticFlags) {
 			this._logger.debug('returning static feature flags (STATIC_FEATURE_FLAGS_ENABLED=true)');
 			this.featureFlags = staticFlags;
+			this.featureFlagsByCaseReference = flagsByReference;
 			return;
 		}
 
@@ -59,13 +90,16 @@ export class FeatureFlagClient {
 				'Cannot load flags because no Azure App Config client exists. Returning an empty object.'
 			);
 			this.featureFlags = {};
+			this.featureFlagsByCaseReference = {};
 			return;
 		}
 
 		const aacResult = await this.client.listConfigurationSettings();
 
 		/** @type {Record<string, boolean>} */
-		let flags = {};
+		const flags = {};
+		/** @type {Record<string, string[]>} */
+		const flagsByCaseReference = {};
 
 		for await (const setting of aacResult) {
 			if (!isFeatureFlag(setting)) {
@@ -77,10 +111,23 @@ export class FeatureFlagClient {
 				continue;
 			}
 
-			const { id, enabled } = flag.value;
+			const { id, enabled, conditions } = flag.value;
 
 			if (process.env.FEATURE_FLAGS_SETTING === 'ALL_ON') {
 				flags[id] = true;
+				continue;
+			}
+
+			if (!enabled) {
+				flags[id] = false;
+				continue;
+			}
+
+			if (Array.isArray(conditions?.clientFilters) && conditions.clientFilters.length > 0) {
+				const filter = conditions.clientFilters[0];
+				if (isAudienceFilter(filter)) {
+					flagsByCaseReference[id] = filter.parameters.Audience.Users || [];
+				}
 				continue;
 			}
 
@@ -88,5 +135,35 @@ export class FeatureFlagClient {
 		}
 
 		this.featureFlags = flags;
+		this.featureFlagsByCaseReference = flagsByCaseReference;
 	}
+}
+
+/**
+ * @param {{ name: string; parameters?: Record<string, unknown>;}} filter
+ * @returns {filter is { name: string; parameters: {Audience: {Users: string[]}}}}
+ */
+function isAudienceFilter(filter) {
+	if (!filter || typeof filter !== 'object') {
+		return false;
+	}
+	if (!filter.parameters) {
+		return false;
+	}
+	const parameters = filter.parameters;
+	if (!hasAudienceValue(parameters)) {
+		return false;
+	}
+	return Array.isArray(parameters.Audience.Users);
+}
+
+/**
+ * @param {Record<string, unknown>} parameters
+ * @returns {parameters is {Audience: {Users: string[]}}}
+ */
+function hasAudienceValue(parameters) {
+	if (!parameters || typeof parameters !== 'object') {
+		return false;
+	}
+	return 'Audience' in parameters;
 }
