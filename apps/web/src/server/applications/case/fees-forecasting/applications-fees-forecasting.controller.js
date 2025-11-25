bd85929733842c5998ae4b33c001dc0b7955fe87
Tudor-PINS <tudor.monkley.da@planninginspectorate.gov.uk>
2025-11-25 13:34:00 +0000

    feat(web): added first summary list to fees and forecasting (applics-1680)

--- apps/web/src/server/applications/case/fees-forecasting/applications-fees-forecasting.controller.js
+++ apps/web/src/server/applications/case/fees-forecasting/applications-fees-forecasting.controller.js
@@ -1,10 +1,18 @@
+import { getFeesForecastingViewModel } from './applications-fees-forecasting-index.view-model.js';
+
 /**
  * View the index of all the data on the fees and forecasting page
  *
  * @type {import('@pins/express').RenderHandler<{}, {}, {}, {}, {}>}
  */
 export async function getFeesForecastingIndex(request, response) {
-	return response.render(`applications/case-fees-forecasting/fees-forecasting-index.njk`, {
-		selectedPageType: 'fees-forecasting'
-	});
+	console.log(response.locals);
+	console.log(response.locals.case.additionalDetails.numberBand2Inspectors);
+
+	const viewModel = await getFeesForecastingViewModel({ caseData: response.locals.case });
+
+	return response.render(
+		`applications/case-fees-forecasting/fees-forecasting-index.njk`,
+		viewModel
+	);
 }
