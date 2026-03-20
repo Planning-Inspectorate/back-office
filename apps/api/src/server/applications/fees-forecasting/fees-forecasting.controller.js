69428d009a86960283ee87b5d5fb8e200448d9e8
Camilla Priest-Stephens <camilla.priest-stephens@planninginspectorate.gov.uk>
2026-03-20 09:27:13 +0000

    feat(api): trigger project broadcast in fees forecasting controller (idas-279)

--- apps/api/src/server/applications/fees-forecasting/fees-forecasting.controller.js
+++ apps/api/src/server/applications/fees-forecasting/fees-forecasting.controller.js
@@ -1,23 +1,50 @@
 import { update } from '#repositories/key-dates.repository.js';
+import * as caseRepository from '#repositories/case.repository.js';
 import BackOfficeAppError from '#utils/app-error.js';
+import { EventType } from '@pins/event-client';
+import { broadcastNsipProjectEvent } from '#infrastructure/event-broadcasters.js';
 
 /**
  * Updates the existing record for a given case
  *
- * @type {import('express').RequestHandler<{id: number}, any, any, any>}
+ * @type {import('express').RequestHandler<{id: string}, any, any, any>}
  */
-export const updateFeesForecasting = async ({ body, params }, response) => {
+export const updateFeesForecasting = async ({ body, params }, res) => {
 	const { id } = params;
-	const convertedId = Number(id);
+	const caseId = Number(id);
+
+	let updatedFeesForecastingData;
 
 	try {
-		const updatedFeesForecastingData = await update(convertedId, body);
-		return response.status(200).send(updatedFeesForecastingData);
+		updatedFeesForecastingData = await update(caseId, body);
 	} catch (error) {
 		if (error?.code === 'P2025') {
-			throw new BackOfficeAppError(`Case ${convertedId} not found`, 404);
-		} else {
-			throw new BackOfficeAppError(`Error updating case ${convertedId}`, 500);
+			throw new BackOfficeAppError(`Case ${caseId} not found`, 404);
 		}
 	}
+
+	if (!updatedFeesForecastingData) {
+		throw new BackOfficeAppError(`Error updating case ${caseId}`, 500);
+	}
+
+	const project = await caseRepository.getById(caseId, {
+		subSector: true,
+		sector: true,
+		applicationDetails: true,
+		zoomLevel: true,
+		regions: true,
+		caseStatus: true,
+		projectTeam: true,
+		gridReference: true,
+		invoice: true,
+		meeting: true
+	});
+
+	if (!project) {
+		throw new BackOfficeAppError(`Case ${caseId} not found`, 404);
+	}
+
+	await broadcastNsipProjectEvent(project, EventType.Update);
+
+	return res.send(updatedFeesForecastingData);
 };
