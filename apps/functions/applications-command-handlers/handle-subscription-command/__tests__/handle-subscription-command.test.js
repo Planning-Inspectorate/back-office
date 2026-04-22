a4b48261c7b31a0c5e1b9517fd2931e1ba088265
Tudor-PINS <tudor.monkley.da@planninginspectorate.gov.uk>
2026-04-22 15:37:40 +0100

    fix(functions): fixed typos in subscriber azure logging tests

--- apps/functions/applications-command-handlers/handle-subscription-command/__tests__/handle-subscription-command.test.js
+++ apps/functions/applications-command-handlers/handle-subscription-command/__tests__/handle-subscription-command.test.js
@@ -18,7 +18,7 @@ describe('handle-subscription-command', () => {
 				context: {},
 				msg: {},
 				log: {
-					warn: 'Ingoring invalid message, no type'
+					warn: 'Ignoring invalid message, no type'
 				}
 			},
 			{
@@ -32,7 +32,7 @@ describe('handle-subscription-command', () => {
 				},
 				msg: {},
 				log: {
-					warn: `Ingoring invalid message, unsupported type 'random-str'`
+					warn: `Ignoring invalid message, unsupported type 'random-str'`
 				}
 			},
 			{
@@ -107,7 +107,7 @@ describe('handle-subscription-command', () => {
 					nsipSubscription: {}
 				},
 				log: {
-					warn: `Ingoring invalid message, invalid caseReference`
+					warn: `Ignoring invalid message, invalid caseReference`
 				}
 			},
 			{
@@ -125,7 +125,7 @@ describe('handle-subscription-command', () => {
 					}
 				},
 				log: {
-					warn: `Ingoring invalid message, invalid emailAddress`
+					warn: `Ignoring invalid message, invalid emailAddress`
 				}
 			},
 			{
