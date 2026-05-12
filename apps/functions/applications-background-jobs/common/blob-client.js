7c83268d5d10848d234a0d46a50cd7d1cb398547
ypk <Prasanna.Yalala.fa@planninginspectorate.gov.uk>
2026-05-12 23:54:42 +0100

    feat(functions): added shapefile processing (idas-221)
    - refactored blog storage client to pick correct blobServiceUrl

--- apps/functions/applications-background-jobs/common/blob-client.js
+++ apps/functions/applications-background-jobs/common/blob-client.js
@@ -1,4 +1,11 @@
 import { BlobStorageClient } from '@pins/blob-storage-client';
 import config from './config.js';
 
-export const blobClient = BlobStorageClient.fromUrl(config.BLOB_STORAGE_ACCOUNT_CUSTOM_DOMAIN);
+const blobServiceUrl =
+	config.BLOB_STORAGE_ACCOUNT_HOST || config.BLOB_STORAGE_ACCOUNT_CUSTOM_DOMAIN;
+
+if (!blobServiceUrl) {
+	throw new Error('No blob storage endpoint configured');
+}
+
+export const blobClient = BlobStorageClient.fromUrl(blobServiceUrl);
