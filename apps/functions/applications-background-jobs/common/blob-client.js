e3efb2ccf53f9f459efb0cce78dc8103199f6f65
ypk <Prasanna.Yalala.fa@planninginspectorate.gov.uk>
2026-05-14 16:55:37 +0100

    feat(functions): added shapefile processing (idas-221)
    - reverted blob-client.js file

--- apps/functions/applications-background-jobs/common/blob-client.js
+++ apps/functions/applications-background-jobs/common/blob-client.js
@@ -1,11 +1,4 @@
 import { BlobStorageClient } from '@pins/blob-storage-client';
 import config from './config.js';
 
-const blobServiceUrl =
-	config.BLOB_STORAGE_ACCOUNT_HOST || config.BLOB_STORAGE_ACCOUNT_CUSTOM_DOMAIN;
-
-if (!blobServiceUrl) {
-	throw new Error('No blob storage endpoint configured');
-}
-
-export const blobClient = BlobStorageClient.fromUrl(blobServiceUrl);
+export const blobClient = BlobStorageClient.fromUrl(config.BLOB_STORAGE_ACCOUNT_CUSTOM_DOMAIN);
