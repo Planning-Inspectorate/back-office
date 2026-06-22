25aed06580b5967bce39f6ffb9f4284775cf0739
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-06-22 15:44:02 +0100

    chore(deps): replace dotenv with process.loadEnvFile
    only replaces direct dependency on dotenv

--- apps/api-testing/config/config.cjs
+++ apps/api-testing/config/config.cjs
@@ -1,5 +1,6 @@
 /* eslint-disable unicorn/prefer-module */
-require('dotenv').config();
+// prettier-ignore
+try { require('node:process').loadEnvFile(); } catch {/* ignore errors*/}
 
 module.exports = {
 	baseUrl: process.env.BASE_URL || 'http://127.0.0.1:3000',
