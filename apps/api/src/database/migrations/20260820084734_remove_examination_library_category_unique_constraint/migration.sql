98880e3c6dc59fb3c9fdba2a9a62b6f62ce30592
ypk <yprasannakumar@gmail.com>
2026-08-21 16:46:26 +0100

    refactor(api): el static categories on new projects (idas-709)
    - enabled prisma 'partialIndexes' feature in schema
    - restored strict one to one relation beetween timetableitem and librarycategory
    - added partial unique index to timetableitem to enforce constraint
    - updated migration script to create the filtered unique index

--- apps/api/src/database/migrations/20260820084734_remove_examination_library_category_unique_constraint/migration.sql
+++ apps/api/src/database/migrations/20260820084734_remove_examination_library_category_unique_constraint/migration.sql
@@ -5,6 +5,9 @@ BEGIN TRAN;
 -- DropIndex
 ALTER TABLE [dbo].[ExaminationLibraryCategory] DROP CONSTRAINT [ExaminationLibraryCategory_examinationTimetableItemId_key];
 
+-- CreateIndex
+CREATE UNIQUE NONCLUSTERED INDEX [ExaminationLibraryCategory_examinationTimetableItemId_key] ON [dbo].[ExaminationLibraryCategory]([examinationTimetableItemId]) WHERE [examinationTimetableItemId] IS NOT NULL;
+
 COMMIT TRAN;
 
 END TRY
